46e7d5980fee197c2c845aa0c5674cfcbe3602cb
bflood <bflood@gitlab.com>
2018-01-30 15:53:33 -0800

    Add Sales Accepted Date to sfdc opp dbt build.

--- elt/dbt/models/base/opportunity.sql
+++ elt/dbt/models/base/opportunity.sql
@@ -7,7 +7,7 @@ SELECT id AS sfdc_id,
        sql_source__c,
        sales_segmentation_o__c,
        sales_qualified_date__c,
+       sales_accepted_date__c,
        name
-
 FROM sfdc.opportunity
 WHERE isdeleted=FALSE
