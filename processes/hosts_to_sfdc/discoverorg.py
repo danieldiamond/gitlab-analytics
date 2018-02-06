9c1a4dfa9e4eb8a7b60b9d6ee37748b0dc619edc
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-06 17:29:12 -0600

    refactor discoverorg

--- processes/hosts_to_sfdc/discoverorg.py
+++ processes/hosts_to_sfdc/discoverorg.py
@@ -14,10 +14,11 @@ url_base = 'https://papi.discoverydb.com/papi/'
 
 def login():
     """Log into the DiscoverOrg API and return an auth token."""
-    data = {}
-    data['username'] = dorg_user
-    data['password'] = dorg_pass
-    data['partnerKey'] = dorg_key
+    data = dict(
+        username=dorg_user,
+        password=dorg_pass,
+        partnerKey=dorg_key
+    )
     json_data = json.dumps(data)
     url = url_base + 'login'
     r = requests.post(url, json_data)
@@ -25,16 +26,21 @@ def login():
     return token
 
 
-def lookup_by_domain(token, domain):
+def lookup_by_domain(domain):
     """For a given domain, return the DiscoverOrg data."""
     url = url_base + 'v1/search/companies'
-    header = {}
-    header['X-AUTH-TOKEN'] = token
-    header['Content-Type'] = 'application/json'
-    search_request = {}
-    company_criteria = {}
-    company_criteria['queryString'] = domain
-    company_criteria['queryStringApplication'] = ['EMAIL_DOMAIN']
-    search_request['companyCriteria'] = company_criteria
+    token = login()
+    header = {
+        "X-AUTH-TOKEN": token,
+        "Content-Type": 'application/json'
+    }
+
+    search_request = dict(
+        companyCriteria=dict(
+            queryString=domain,
+            queryStringApplication=['EMAIL_DOMAIN']
+        )
+    )
+
     r = requests.post(url, headers=header, data=json.dumps(search_request))
     return json.loads(r.content)
