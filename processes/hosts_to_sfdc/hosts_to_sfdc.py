ea7d82efef42798971c3980a289dc60f76d2cbb9
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-06 14:10:51 -0600

    refactor clearbit query and caching

--- processes/hosts_to_sfdc/hosts_to_sfdc.py
+++ processes/hosts_to_sfdc/hosts_to_sfdc.py
@@ -17,12 +17,13 @@ from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
 import socket
 import tldextract
+from toolz.dicttoolz import dissoc
 from timeout import timeout
-import pprint
 import os
 
 clearbit.key = os.environ.get('CLEARBIT_API_KEY')
 host = os.environ.get('PROCESS_DB_PROD_ADDRESS')
+# host = "localhost"
 username = os.environ.get('PROCESS_DB_PROD_USERNAME')
 password = os.environ.get('PROCESS_DB_PROD_PASSWORD')
 database = os.environ.get('PROCESS_DB_PROD_DBNAME')
@@ -44,7 +45,7 @@ ip_to_url = Table('ip_to_url',
                   autoload_with=engine)
 
 
-def check_cache(domain):
+def in_cache(domain):
     """Return True if domain found in cache, False if not found.
 
     Check the local clearbit cache for a domain to see if the domain has
@@ -70,55 +71,40 @@ def check_cache(domain):
     return value
 
 
-def update_cache(varlist):
-    """If we have retrived new data from the clearbit API, we update the cache.
+def update_cache(dictlist):
+    """If we have retrieved new data from the clearbit API, we update the cache.
 
     Does an upsert to the table using the domain as the unique key.
     Does not return a value.
     """
-    # print("Updating cache for " + varlist[0])
+    # print("Updating cache for " + dictlist.get("parsed_domain", ""))
+
     stmt = postgresql.insert(clearbit_cache, bind=engine).values(
-        domain=str(varlist[0]),
-        company_name=str(varlist[1]),
-        company_legalname=str(varlist[2]),
-        company_domain=str(varlist[3]),
-        company_site=str(varlist[4]),
-        company_industrygroup=str(varlist[5]),
-        company_industry=str(varlist[6]),
-        company_naics=str(varlist[7]),
-        company_desc=str(varlist[8]),
-        company_loc=str(varlist[9]),
-        company_ein=str(varlist[10]),
-        company_emp=str(varlist[11]),
-        company_emp_range=str(varlist[12]),
-        company_rev=str(varlist[13]),
-        company_estrev=str(varlist[14]),
-        company_type=str(varlist[15]),
-        company_phone=str(varlist[16]),
-        company_tech=str(varlist[17]),
-        company_index=str(varlist[18]),
-        last_update=str(varlist[19]))
+        domain=dictlist.get("parsed_domain", ""),
+        company_name=dictlist.get("company_name", ""),
+        company_legalname=dictlist.get("company_legalname", ""),
+        company_domain=dictlist.get("company_domain", ""),
+        company_site=dictlist.get("company_site", ""),
+        company_industrygroup=dictlist.get("company_industrygroup", ""),
+        company_industry=dictlist.get("company_industry", ""),
+        company_naics=dictlist.get("company_naics", ""),
+        company_desc=dictlist.get("company_desc", ""),
+        company_loc=dictlist.get("company_loc", ""),
+        company_ein=dictlist.get("company_ein", ""),
+        company_emp=dictlist.get("company_emp", ""),
+        company_emp_range=dictlist.get("company_emp_range", ""),
+        company_rev=dictlist.get("company_rev", ""),
+        company_estrev=dictlist.get("company_estrev", ""),
+        company_type=dictlist.get("company_type", ""),
+        company_phone=dictlist.get("company_phone", ""),
+        company_tech=dictlist.get("company_tech", ""),
+        company_index=dictlist.get("company_index", ""),
+        last_update=dictlist.get("last_update", "")
+    )
     on_update_stmt = stmt.on_conflict_do_update(
         index_elements=['domain'],
-        set_=dict(company_name=str(varlist[1]),
-                  company_legalname=str(varlist[2]),
-                  company_domain=str(varlist[3]),
-                  company_site=str(varlist[4]),
-                  company_industrygroup=str(varlist[5]),
-                  company_industry=str(varlist[6]),
-                  company_naics=str(varlist[7]),
-                  company_desc=str(varlist[8]),
-                  company_loc=str(varlist[9]),
-                  company_ein=str(varlist[10]),
-                  company_emp=str(varlist[11]),
-                  company_emp_range=str(varlist[12]),
-                  company_rev=str(varlist[13]),
-                  company_estrev=str(varlist[14]),
-                  company_type=str(varlist[15]),
-                  company_phone=str(varlist[16]),
-                  company_tech=str(varlist[17]),
-                  company_index=str(varlist[18]),
-                  last_update=str(varlist[19])))
+        set_=dissoc(dictlist, "parsed_domain")
+    )
     conn = engine.connect()
     conn.execute(on_update_stmt)
     conn.close()
@@ -167,7 +153,8 @@ def check_clearbit(domain):
     try:
         company = clearbit.Company.find(domain=domain, stream=True)
     except:
-        return None
+        company = None
+
     if company is None or not company['name']:
         try:
             update_cache_not_found(domain)
@@ -237,94 +224,47 @@ def process_domain(domain):
     """
     domain = ''.join(domain).encode('utf-8')
     parsed_domain = url_parse(domain)
-    if check_cache(parsed_domain):
+    if in_cache(parsed_domain) is True:
         pass
     else:
         company = check_clearbit(parsed_domain)
         if not isinstance(company, type(None)):
-            if company['name'] is not None:
-                company_name = company['name'].encode("utf-8")
-            else:
-                company_name = ''
-            if company['legalName'] is not None:
-                company_legalname = \
-                    company['legalName'].encode("utf-8")
-            else:
-                company_legalname = ''
-            if company['domain'] is not None:
-                company_domain = company['domain'].encode("utf-8")
-            else:
-                company_domain = ''
-            if company['category']['sector'] is not None:
-                company_site = \
-                    company['category']['sector'].encode("utf-8")
-            else:
-                company_site = ''
-            if company['category']['industryGroup'] is not None:
-                company_industrygroup = \
-                    company['category']['industryGroup'].encode("utf-8")
-            else:
-                company_industrygroup = ''
-            if company['category']['industry'] is not None:
-                company_industry = \
-                    company['category']['industry'].encode("utf-8")
-            else:
-                company_industry = ''
-            if company['category']['naicsCode'] is not None:
-                company_naics = \
-                    company['category']['naicsCode'].encode("utf-8")
-            else:
-                company_naics = ''
-            if company['description'] is not None:
-                company_desc = \
-                    company['description'].encode("utf-8")
-            else:
-                company_desc = ''
-            if company['location'] is not None:
-                company_loc = company['location'].encode("utf-8")
-            else:
-                company_loc = ''
-            if company['identifiers']['usEIN'] is not None:
-                company_ein = \
-                    company['identifiers']['usEIN'].encode("utf-8")
-            else:
-                company_ein = ''
-            if company['metrics']['employees'] is not None:
-                company_emp = company['metrics']['employees']
-            else:
-                company_emp = ''
-            if company['metrics']['employeesRange'] is not None:
-                company_emp_range = \
-                    company['metrics']['employeesRange'].encode("utf-8")
-            else:
-                company_emp_range = ''
-            company_rev = company['metrics']['annualRevenue']
-            company_estrev = \
-                company['metrics']['estimatedAnnualRevenue']
-            if company['type'].encode("utf-8") is not None:
-                company_type = company['type'].encode("utf-8")
-            else:
-                company_type = ''
-            if company['phone'] is not None:
-                company_phone = company['phone'].encode("utf-8")
-            else:
-                company_phone = ''
-            company_tech = company['tech']
-            if company['indexedAt'] is not None:
-                company_index = company['indexedAt'].encode("utf-8")
-            else:
-                company_index = ''
+            company_dict = dict(company)
+            category = company_dict.get("category", {})
+            metrics = company_dict.get("metrics", {})
 
-            varlist = [parsed_domain, company_name,
-                       company_legalname, company_domain, company_site,
-                       company_industrygroup, company_industry,
-                       company_naics, company_desc, company_loc,
-                       company_ein, company_emp, company_emp_range,
-                       company_rev, company_estrev, company_type,
-                       company_phone, company_tech, company_index,
-                       datetime.datetime.now()]
+            dictlist = dict(parsed_domain=parsed_domain,
+                           company_name=company_dict.get('name', ''),
+                           company_legalname=company_dict.get('legalName', ''),
+                           company_domain=company_dict.get('domain', ''),
+                           company_site=category.get('sector', ''),
+                           company_industrygroup=category.get('industryGroup', ''),
+                           company_industry=category.get('industry', ''),
+                           company_naics=category.get('naicsCode', ''),
+                           company_desc=company_dict.get('description', ''),
+                           company_loc=company_dict.get('location', ''),
+                           company_ein=company_dict.get('identifiers', {}).get('usEIN', ''),
+                           company_emp=metrics.get('employees', ''),
+                           company_emp_range=metrics.get('employeesRange', ''),
+                           company_rev=metrics.get('annualRevenue', ''),
+                           company_estrev=metrics.get('estimatedAnnualRevenue', ''),
+                           company_type=company_dict.get('type', ''),
+                           company_phone=company_dict.get('phone', ''),
+                           company_tech=company_dict.get('tech', ''),
+                           company_index=company_dict.get('indexedAt', ''),
+                           last_update=datetime.datetime.now())
 
-            update_cache(varlist)
+            #TODO Feel like there shouldn't be this much error catching for strings
+            for key in dictlist:
+                value = dictlist[key]
+                if value is None:
+                    dictlist[key] = ""
+                elif key == "last_update" or isinstance(value, list):
+                    dictlist[key] = str(value)
+                else:
+                    dictlist[key] = str(value.encode("utf-8"))
+
+            update_cache(dictlist)
 
         else:
             pass
@@ -409,16 +349,16 @@ def ask_whois(ip):
     else:
         try:
             if r['network']['name'] is not None:
-                org = r['network']['name'].encode("utf-8")
+                org = r['network']['name']
         except TypeError:
-            continue
+            pass
             # print("Whois has no name. Updating the organization desc.")
         try:
             if r['network']['remarks'][0]['description'] is not None:
-                desc =\
-                    r['network']['remarks'][0]['description'].encode("utf-8")
+                desc = \
+                    r['network']['remarks'][0]['description']
         except TypeError:
-            continue
+            pass
         #     print("Whois has no description. Updating the organization name.")
         # print("Whois " + ip + "? ARIN says it's " + org +
         #       ". Updating cache..")
@@ -434,7 +374,7 @@ def process_ips():
     ips = get_ips()
     for ip in ips:
         ip = ''.join(ip)
-        if check_cache(ip):
+        if in_cache(ip):
             continue
         else:
             try:
