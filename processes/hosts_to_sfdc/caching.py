3fa5e951c67343798c04be2612263f23835433a9
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-13 16:09:04 -0600

    add WHOIS caching

--- processes/hosts_to_sfdc/caching.py
+++ processes/hosts_to_sfdc/caching.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import datetime
-from ipwhois import IPWhois
 from sqlalchemy import Table
 from sqlalchemy.dialects import postgresql
 import psycopg2
@@ -14,28 +13,71 @@ cleaned_urls = Table('cleaned_urls',
                     autoload=True,
                     autoload_with=engine)
 
-# def update_cache_whois(ip, company_name, company_address):
-#     """Update the cache with whois data for a domain.
-#
-#     If the only data we could get for a domain was from whois,
-#     update the cache with that.
-#     """
-#     # print("Updating cache with whois data for " + ip)
-#     stmt = postgresql.insert(clearbit_cache, bind=engine).values(
-#         domain=str(ip),
-#         company_name=company_name,
-#         company_loc=company_address,
-#         last_update=datetime.datetime.now())
-#     on_update_stmt = stmt.on_conflict_do_update(
-#         index_elements=['domain'],
-#         set_=dict(company_name=company_name,
-#                   company_loc=company_address,
-#                   last_update=datetime.datetime.now()))
-#     conn = engine.connect()
-#     conn.execute(on_update_stmt)
-#     conn.close()
-#     # print("Cache Updated.")
 
+def update_cache_whois(ip, company_name, company_address):
+    """Update the cache with whois data for a domain.
+
+    If the only data we could get for a domain was from whois,
+    update the cache with that.
+    """
+    # print("Updating cache with whois data for " + ip)
+    stmt = postgresql.insert(clearbit_cache, bind=engine).values(
+        domain=str(ip),
+        company_name=company_name,
+        company_loc=company_address,
+        last_update=datetime.datetime.now())
+    on_update_stmt = stmt.on_conflict_do_update(
+        index_elements=['domain'],
+        set_=dict(company_name=company_name,
+                  company_loc=company_address,
+                  last_update=datetime.datetime.now()))
+    conn = engine.connect()
+    conn.execute(on_update_stmt)
+    conn.close()
+    # print("Cache Updated.")
+
+
+def in_cache(domain, table):
+    """Return True if domain found in cache, False if not found.
+
+    Check the specified cache for a domain to see if the domain has
+    been cached in the last 30 days.
+
+    :param table: String of table name
+    """
+    # print("Checking cache for " + domain)
+    mydb = psycopg2.connect(host=host, user=username,
+                            password=password, dbname=database)
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM " + table + " WHERE domain='" +
+                   domain + "' AND last_update >  NOW() - INTERVAL '30 days';")
+    domain_in_cache = False if cursor.rowcount == 0 else True
+
+    mydb.commit()
+    cursor.close()
+    mydb.close()
+
+    return domain_in_cache
+
+
+def update_whois_cache(dictlist, table):
+
+    print "Updating WHOIS Cache", dictlist
+    stmt = postgresql.insert(table, bind=engine).values(
+        domain=dictlist.get("domain",""),
+        name=dictlist.get("name", None),
+        description=dictlist.get("description", None),
+        asn_description=dictlist.get("asn_description", None),
+        country_code=dictlist.get("country_code", None),
+        last_update=dictlist.get("last_update", None)
+    )
+    on_update_stmt = stmt.on_conflict_do_update(
+        index_elements=["domain"],
+        set_=dissoc(dictlist, "domain")
+    )
+    conn = engine.connect()
+    conn.execute(on_update_stmt)
+    conn.close()
 
 def update_cache(dictlist, table):
     """If we have retrieved new data from the relevant API, we update the cache.
@@ -102,7 +144,7 @@ def update_cache_not_found(domain, table):
 
 def write_clean_domain(raw_domain, tldextract, clean_domain, table=cleaned_urls):
 
-    print "Writing " + raw_domain + " as " + clean_domain + " to cache."
+    # print "Writing " + raw_domain + " as " + clean_domain + " to cache."
 
     subdomain = tldextract.subdomain
     primary_domain = tldextract.domain
