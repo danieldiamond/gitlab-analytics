036db1ba92b43eec40fce57db37abe781701cfeb
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-16 14:19:00 -0600

    add dbt model for generating libre hosts

---
+++ elt/dbt/models/pings/clean/libre_hosts.sql
@@ -0,0 +1,21 @@
+with usage_pings as (
+  SELECT * FROM {{ ref('usage_data_clean') }}
+),
+
+vers_pings as (
+  SELECT * FROM {{ ref('version_checks_clean') }}
+)
+
+SELECT
+  ud.clean_url,
+  max(ud.gitlab_version)             AS usage_data_gl_version,
+  max(ud.host_id)                    AS usage_data_host_id,
+  max(cast(vp.request_data AS TEXT)) AS ping_data,
+  max(vp.ping_count)                 AS version_ping_count,
+  max(cast(ud.stats AS TEXT))        AS usage_stats,
+  sum(ud.active_user_count)          AS active_user_count,
+  sum(ud.usage_pings)                AS total_usage_pings
+FROM vers_pings AS vp
+  JOIN usage_pings AS ud ON ud.clean_url = vp.clean_url
+WHERE vp.updated_at >= (now() - '60 days' :: INTERVAL)
+GROUP BY ud.clean_url
