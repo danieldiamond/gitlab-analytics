1c7f8a4bff22ba999022a6777b5b48b49ee24a6f
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-26 08:56:12 -0600

    add dbt and python timechecks

---
+++ elt/dbt/dbt_timecheck.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get's current UTC time
+H=$(date -u +%H)
+STARTTIME=7
+ENDTIME=10
+
+# Checks if between 7 and 10 AM UTC (1-4 AM CST)
+# 10#$H returns the hour in base 10
+if (( STARTTIME <= 10#$H && 10#$H < ENDTIME )); then
+    dbt -d run --profiles-dir profile --target prod --models pings
+else
+    echo dbt not run: Only run between the hours of $STARTTIME AM UTC and $ENDTIME AM UTC
+fi
