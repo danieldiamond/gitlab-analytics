8faed134f883193eefe86e89eed5be2d5965f905
Taylor A. Murphy <tmurphy@gitlab.com>
2018-02-26 13:46:55 -0600

    completely remove localhost

--- processes/hosts_to_sfdc/dw_setup.py
+++ processes/hosts_to_sfdc/dw_setup.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 host = os.environ.get('PROCESS_DB_PROD_ADDRESS')
-# host = "localhost"
 username = os.environ.get('PROCESS_DB_PROD_USERNAME')
 password = os.environ.get('PROCESS_DB_PROD_PASSWORD')
 database = os.environ.get('PROCESS_DB_PROD_DBNAME')
