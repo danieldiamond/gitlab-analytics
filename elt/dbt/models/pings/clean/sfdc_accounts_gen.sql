99356071d984c196388eb784e270f9853541ee6e
Taylor A. Murphy <tmurphy@gitlab.com>
2018-03-14 17:18:52 -0500

    rewrite b/c it wasn't properly accounting for matches

--- elt/dbt/models/pings/clean/sfdc_accounts_gen.sql
+++ elt/dbt/models/pings/clean/sfdc_accounts_gen.sql
@@ -1,3 +1,5 @@
+-- This generates the records that need to be uploaded to SFDC as new accounts
+
 with libre_agg as (
   SELECT * FROM {{ ref('libre_agg_hosts') }}
 ),
@@ -10,49 +12,55 @@ libre AS (
 host_data AS (
     SELECT *
     FROM libre
-    JOIN libre_agg ON libre.clean_domain = libre_agg.the_clean_url
-  )
+      JOIN libre_agg ON libre.clean_domain = libre_agg.the_clean_url
+),
 
+name_match AS (
+/*
+This takes host files (non IP), matches to SFDC on name, and gets those
+host files (uniquely identified by the clean_domain) where there is no name match in salesforce.
+ */
+  SELECT
+    max(company_name)            AS name,
+    clean_domain                 AS website
+  FROM host_data AS lah
+    LEFT OUTER JOIN
+    (
+      SELECT
+        id,
+        name
+      FROM sfdc.account) AS sf
+      ON lah.company_name = sf.name
+  WHERE sf.name IS NULL
+        AND clean_domain !~ '\d+\.\d+.\d+\.\d+'
+        AND lah.company_name NOT IN ('Microsoft', 'Amazon.com')
+  GROUP BY clean_domain
+)
+
+/*
+Takes the remaining host records, attempts to match with SFDC based on website,
+and returns those where there is no match.
+ */
 SELECT
-  max(company_name)            AS name,
-  clean_domain                 AS website,
+  max(name)                    AS name,
+  website                      AS website,
   'Prospect - CE User' :: TEXT AS type,
   '00561000000mpHTAAY' :: TEXT AS OwnerId,
   'True' :: BOOLEAN            AS using_ce__c
-FROM host_data AS lah
-  LEFT OUTER JOIN
+FROM name_match AS lah
+LEFT OUTER JOIN
   (
     SELECT
-      id,
-      website                                              AS original,
-      regexp_replace(website, '^(http(s)?\://)?www\.', '') AS fixed
-    FROM sfdc.account
+      sfdc.id,
+      sfdc.website                                              AS original,
+      regexp_replace(sfdc.website, '^(http(s)?\://)?www\.', '') AS fixed
+    FROM sfdc.account AS sfdc
     WHERE website IS NOT NULL
   ) AS sf
-    ON lah.clean_domain = sf.fixed
+    ON lah.website = sf.fixed
 WHERE sf.fixed IS NULL
-      AND clean_domain !~ '\d+\.\d+.\d+\.\d+'
-      AND lah.company_name NOT IN ('Microsoft', 'Amazon.com')
-GROUP BY clean_domain
+      AND website !~ '\d+\.\d+.\d+\.\d+'
+      AND lah.name NOT IN ('Microsoft', 'Amazon.com')
+GROUP BY website
 
-UNION
-
-SELECT
-  max(company_name)            AS name,
-  clean_domain                 AS website,
-  'Prospect - CE User' :: TEXT AS type,
-  '00561000000mpHTAAY' :: TEXT AS OwnerId,
-  'True' :: BOOLEAN            AS using_ce__c
-FROM host_data AS lah
-  LEFT OUTER JOIN
-  (
-    SELECT
-      id,
-      name
-    FROM sfdc.account) AS sf
-    ON lah.company_name = sf.name
-WHERE sf.name IS NULL
-      AND clean_domain !~ '\d+\.\d+.\d+\.\d+'
-      AND lah.company_name NOT IN ('Microsoft', 'Amazon.com')
-GROUP BY clean_domain
 ORDER BY name
