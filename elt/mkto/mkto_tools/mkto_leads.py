6328d79a60e2354290185dad815c5ae15d541b8f
Taylor A. Murphy <tmurphy@gitlab.com>
2018-03-26 09:50:24 -0500

    prevent sql injection

--- elt/mkto/mkto_tools/mkto_leads.py
+++ elt/mkto/mkto_tools/mkto_leads.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 import os
+import datetime
 import psycopg2
+import psycopg2.sql
 import requests
+from sqlalchemy.dialects import postgresql
+from toolz.dicttoolz import dissoc
 
 from mkto_token import get_token, mk_endpoint
 
@@ -66,12 +70,19 @@ def write_to_db_from_csv(username, password, host, database, port, item):
             cursor = mydb.cursor()
             # cursor.execute('TRUNCATE TABLE sandbox.mkto_leads')
             print("Truncating table")
-            copy = "COPY mkto.{} ({}) FROM STDIN WITH DELIMITER AS ',' NULL AS 'null' CSV HEADER;".format(item, header)
+            table_name = "sandbox.mkto_{}".format(item)
+            copy_query=psycopg2.sql.SQL("COPY {0} ({1}) FROM STDIN WITH DELIMITER AS ',' NULL AS 'null' CSV").format(
+                psycopg2.sql.Identifier(table_name),
+                psycopg2.sql.SQL(', ').join(
+                    psycopg2.sql.Identifier(n) for n in header.split(',')
+                )
+            )
+            print(copy_query.as_string(cursor))
             print("Copying file")
-            cursor.copy_expert(sql=copy, file=file)
+            cursor.copy_expert(sql=copy_query, file=file)
             mydb.commit()
             cursor.close()
             mydb.close()
-            os.remove(item + '.csv')
+            # os.remove(item + '.csv')
         except psycopg2.Error as err:
             print(err)
