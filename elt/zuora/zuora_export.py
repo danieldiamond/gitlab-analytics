be3d8da041e45d99402d59dca58e0c3ac6020d20
Micaël Bergeron <mbergeron@gitlab.com>
2018-05-24 15:26:18 -0400

    make log level debug

--- elt/zuora/zuora_export.py
+++ elt/zuora/zuora_export.py
@@ -369,7 +369,7 @@ def main():
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
-                        level=logging.INFO)
+                        level=logging.DEBUG)
 
     setup_db()
 
