2e43e87e49918b2b17ec0bf14709c532d7c30eac
emilie <emilie.burke@gmail.com>
2018-06-19 16:25:30 -0400

    add account and acv models

---
+++ elt/dbt/models/sfdc/base/sfdc_account.sql
@@ -0,0 +1,26 @@
+WITH source AS (
+
+	SELECT *
+	FROM sfdc.account
+
+), renamed AS(
+
+	SELECT 
+		id as account_id, 
+		
+		-- keys
+		
+
+		-- info
+		sales_segmentation__c as segment
+
+		-- metadata
+		
+
+
+	FROM source
+
+)
+
+SELECT *
+FROM renamed
