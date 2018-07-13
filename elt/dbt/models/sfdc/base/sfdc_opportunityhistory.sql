d5ed1ad9905f0ef13d71b6639fa38e2e189d460c
Taylor A. Murphy <tmurphy@gitlab.com>
2018-07-13 11:16:56 -0500

    fix case when

--- elt/dbt/models/sfdc/base/sfdc_opportunityhistory.sql
+++ elt/dbt/models/sfdc/base/sfdc_opportunityhistory.sql
@@ -11,8 +11,8 @@ WITH base AS (
 SELECT
   *,
   CASE
-    WHEN time_in_stage IS NULL
+    WHEN time_in_stage IS NULL OR extract(EPOCH FROM time_in_stage) / (3600 * 24) = 0
         THEN 0.0001
     ELSE
-        coalesce(extract(EPOCH FROM time_in_stage) / (3600 * 24), 0) END AS days_in_stage
+        coalesce(extract(EPOCH FROM time_in_stage) / (3600 * 24), 0.0001) END AS days_in_stage
 FROM base
