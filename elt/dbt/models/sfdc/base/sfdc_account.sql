622593aa4198865e46ceeaf83665c0f46d8ca5b5
emilie <emilie.burke@gmail.com>
2018-07-23 14:10:18 -0400

    add more values to accounts

--- elt/dbt/models/sfdc/base/sfdc_account.sql
+++ elt/dbt/models/sfdc/base/sfdc_account.sql
@@ -10,6 +10,13 @@ WITH source AS (
 		name as account_name,
 		-- keys
 		account_id_18__c as account_id_18,
+		masterrecordid as master_record_id,
+		ownerid as owner_id,
+		parentid as parent_id,
+		primary_contact_id__c as primary_contact_id,
+		recordtypeid as record_type_id,
+		ultimate_parent_id__c as utimate_parent_id,
+		partner_vat_tax_id__c as partner_vat_tax_id,
 
 		-- key people GL side
 		entity__c as gitlab_entity,
@@ -24,16 +31,21 @@ WITH source AS (
 		business_development_rep__c as business_development_rep,
 		business_development_rep_account_team__c as business_development_rep_team,
 		dedicated_service_engineer__c as dedicated_service_engineer,
+		sdr__c as sales_development_rep,
+		sdr_account_team__c as sales_development_rep_team,
+		solutions_architect__c as solutions_architect,
 
 		--key people outside
 		bill_to_email__c as bill_to_email,
 
 
 		-- info
+		sfdc.id15to18(substring(ultimate_parent_account__c,11, 15)) as ultimate_parent_account,
+		type as account_type,
 		industry,
 		product_category__c as product_category,
 		customer_since__c::date as customer_since_date,
-		sales_segmentation__c as segment,
+		sales_segmentation__c as account_segment, -- I would this be called account_segment or sales_segment, but I think this is a breaking change
 
 		--present state info
 		health__c as health_score,
@@ -53,6 +65,7 @@ WITH source AS (
 		concurrent_ee_subscriptions__c as count_concurrent_ee_subscriptions,
 		ce_instances__c as count_ce_instances,
 		active_ce_users__c as count_active_ce_users,
+		using_ce__c as count_using_ce,
 
 
 		-- metadata
@@ -61,8 +74,10 @@ WITH source AS (
 		isdeleted as is_deleted,
 		lastmodifiedbyid as last_modified_by_id,
 		lastmodifieddate as last_modified_date,
+		lastactivitydate AS last_activity_date,
 		lastreferenceddate as last_referenced_date,
-		lastvieweddate as last_viewed_date
+		lastvieweddate as last_viewed_date,
+		systemmodstamp
 		
 
 
@@ -74,7 +89,7 @@ WITH source AS (
 SELECT *
 FROM renamed
 
---List of not-included columns
+--List of excluded columns
 		--billing info
 		--billingstreet as billing_street,
 		--billingcity as billing_city,
@@ -86,6 +101,16 @@ FROM renamed
 		--billinglongitude as billing_longitude,
 		--billinglatitude as billing_latitude,
 
+		--shippingstreet as shipping_street,
+		--shippingcity as shipping_city,
+		--shippingstate as shipping_state,
+		--shippingstatecode as shipping_state_code,
+		--shippingcountry as shipping_country,
+		--shippingcountrycode as shipping_country_code,
+		--shippingpostalcode as shipping_postal_code,
+		--shippinglongitude as shipping_longitude,
+		--shippinglatitude as shipping_latitude,
+		--shippingcountrycode__c as shipping_country_code,
 
 -- all blanks
 
@@ -119,6 +144,11 @@ FROM renamed
 --zuora__upsellopportunity__c
 --zuora_issue__c
 
+
+
+
+
+
 ----- other columns
 --absd_campaign__c
 --account_initial_start_date__c
@@ -130,24 +160,16 @@ FROM renamed
 --cmrr_all_child_accounts__c
 --cmrr_this_account__c
 --cmrr_total__c
---
 --company_technologies__c --notes about existing tech
---
 --data_quality_description__c
 --data_quality_score__c
 --days_outstanding__c
---
 --description -- text field
 --domains__c -- inconsistent structure, some have multiple separated by commas
---
 --dscorgpkg__fortune_rank__c
 --dscorgpkg__it_employees__c
---
 --gitlab_team__c --all blanks
 --historical_max_users__c --not really used
---
---
---
 --initial_start_date__c
 --invoice_owner__c
 --iseebasiccustomer__c
@@ -165,12 +187,9 @@ FROM renamed
 --ispremiumsupportcustomer__c
 --it_tedd__c
 --large_account__c
---lastactivitydate
---
 --license_user_count__c
 --license_utilization__c
 --manual_support_upgrade__c
---masterrecordid
 --most_recent_expired_subscription_date__c
 --next_invoice_due_date__c
 --next_renewal_date__c
@@ -183,15 +202,12 @@ FROM renamed
 --oldest_outstanding_invoice_date__c
 --opt_out_file_locking__c
 --opt_out_geo__c
---ownerid
---parentid
 --partner_account_iban_number__c
 --partner_bank_address__c
 --partner_bank_name__c
 --partner_beneficiary_name__c
 --partner_name_and_type__c
 --partner_routing__c
---partner_vat_tax_id__c
 --paymenttermnumeric__c
 --phone
 --photourl
@@ -205,10 +221,8 @@ FROM renamed
 --primary_contact__c
 --primary_contact_email__c
 --primary_contact_first_name__c
---primary_contact_id__c
 --primary_contact_last_name__c
 --products_purchased__c
---recordtypeid
 --reference_type__c
 --referenceable_customer__c
 --referenceable_customer_notes__c
@@ -217,20 +231,6 @@ FROM renamed
 --reseller_discount__c
 --reseller_type__c
 --revenue__c
---sdr__c
---sdr_account_team__c
---shippingstreet
---shippingcity
---shippingstate
---shippingstatecode
---shippingcountry
---shippingcountrycode
---shippingpostalcode
---shippinglongitude
---shippinglatitude
---shippingcountrycode__c
---shippinggeocodeaccuracy
---solutions_architect__c
 --special_terms__c
 --sub_industry__c
 --sub_region__c
@@ -239,7 +239,6 @@ FROM renamed
 --support_level__c
 --support_level_new__c
 --support_level_numeric__c
---systemmodstamp
 --technology_stack__c
 --tedd_employees__c
 --temp_duplicated_account__c
@@ -254,12 +253,8 @@ FROM renamed
 --total_invoiced_outstanding__c
 --total_invoiced_paid__c
 --trigger_workflow__c
---type
 --type_amount_close_date__c
 --ultimate_parent__c
---ultimate_parent_account__c
---ultimate_parent_id__c
---using_ce__c
 --website
 
 
