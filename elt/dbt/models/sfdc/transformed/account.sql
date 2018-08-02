d77ad9ad2872c0f62a04a1bc1c0afa7c114d1975
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2018-08-02 22:32:55 +0000

    Update to relative schema ref

--- elt/dbt/models/sfdc/transformed/account.sql
+++ elt/dbt/models/sfdc/transformed/account.sql
@@ -14,5 +14,5 @@ SELECT a.id AS sfdc_account_id,
        a.health__c as health_score,
        a.health_score_reasons__c as health_score_reasons
 FROM sfdc.account a
-LEFT OUTER JOIN sfdc.account p ON sfdc.id15to18(substring(a.ultimate_parent_account__c,11, 15))=p.id
+LEFT OUTER JOIN sfdc.account p ON {{this.schema}}.id15to18(substring(a.ultimate_parent_account__c,11, 15))=p.id
 WHERE a.isdeleted=FALSE
