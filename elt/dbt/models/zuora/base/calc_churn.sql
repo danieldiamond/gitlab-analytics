b5ee203da9dbdcb41d2edc202e58a29ba9edcc87
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2018-08-02 22:44:21 +0000

    Update elt/dbt/models/zuora/base/calc_churn.sql

--- elt/dbt/models/zuora/base/calc_churn.sql
+++ elt/dbt/models/zuora/base/calc_churn.sql
@@ -1,13 +1,13 @@
-/*{{
+{{
   config({
     "materialized":"table",
     "post-hook": [
-       "SELECT zuora.calc_churn()",
-       "SELECT zuora.calc_subacct_churn()"
+       "SELECT 1 --SELECT zuora.calc_churn()",
+       "SELECT 1 --SELECT zuora.calc_subacct_churn()"
     ]
   })
 }}
 
 SELECT rolname
 FROM pg_roles
-LIMIT 1*/
+LIMIT 1
