5bd31fe1319a6bb69b92d4b8f085d2c38e8ec9d6
Taylor A. Murphy <tmurphy@gitlab.com>
2018-08-07 16:30:45 -0500

    add zuora account join [ci skip]

--- elt/dbt/macros/retention_zuora_sub_churn_history_calc.sql
+++ elt/dbt/macros/retention_zuora_sub_churn_history_calc.sql
@@ -9,6 +9,19 @@ WITH acct_churn AS (
         SELECT *
         FROM {{ ref('retention_zuora_sub_trueup') }}
 
+    ),
+
+    zuora_accts AS (
+        SELECT *
+        FROM {{ ref('zuora_account') }}
+    ),
+
+    acct_churn_linked AS (
+        SELECT
+            s.*,
+            a.crm_id
+        FROM acct_churn s
+        JOIN zuora_accts a ON s.account_id = a.account_id
     )
 
 {% for the_month in range(0, n + 1) %}
@@ -20,6 +33,8 @@ WITH acct_churn AS (
             ELSE date_part('month', current_date - '{{ the_month }} month'::INTERVAL) :: TEXT
                 END                            AS period,
           s.subscription_id,
+          s.account_id,
+          s.crm_id,
           s.year_ago_start_date,
           s.year_ago_end_date,
           s.year_ago_mrr,
@@ -33,7 +48,7 @@ WITH acct_churn AS (
           COALESCE(t.current_trueup, 0 :: NUMERIC)                                                                                AS current_trueup,
           round(COALESCE(t.current_trueup, 0 :: NUMERIC) + s.current_arr, 2)                                                      AS current_total,
           COALESCE(t.current_trueup, 0 :: NUMERIC) + s.current_arr - (COALESCE(t.year_ago_trueup, 0 :: NUMERIC) + s.year_ago_arr) AS change
-        FROM acct_churn s
+        FROM acct_churn_linked s
           LEFT JOIN trueups t ON t.subscription_id :: TEXT = s.subscription_id:: TEXT
 
     {% if  the_month != n %}
