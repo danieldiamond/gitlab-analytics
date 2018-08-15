fd835b6d3cc17819936bd31699d0aafdbe2d9cdc
Taylor A. Murphy <tmurphy@gitlab.com>
2018-08-15 13:04:18 -0500

    clean up base models and add scheduled renewals

--- elt/dbt/models/zuora/base/zuora_rate_plan.sql
+++ elt/dbt/models/zuora/base/zuora_rate_plan.sql
@@ -6,21 +6,22 @@ WITH source AS (
 
 ), renamed AS(
 
-	SELECT id as rate_plan_id,
-			name as rate_plan_name,
+	SELECT
+	    id                  as rate_plan_id,
+        name                as rate_plan_name,
 		--keys	
-		subscriptionid as subscription_id,
-		productid as product_id,
-		productrateplanid as product_rate_plan_id,
+		subscriptionid      as subscription_id,
+		productid           as product_id,
+		productrateplanid   as product_rate_plan_id,
 		-- info
-		amendmentid as amendement_id,
-		amendmenttype as amendement_type,
+		amendmentid         as amendement_id,
+		amendmenttype       as amendement_type,
 
 		--metadata
-		updatedbyid as updated_by_id,
-		updateddate as updated_date,
-		createdbyid as created_by_id,
-		createddate as created_date
+		updatedbyid         as updated_by_id,
+		updateddate         as updated_date,
+		createdbyid         as created_by_id,
+		createddate         as created_date
 
 	FROM source
 	
