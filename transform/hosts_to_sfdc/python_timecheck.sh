880e95562fbfc3721e04dda3540e44d45392503b
Taylor A. Murphy <tmurphy@gitlab.com>
2018-08-24 20:37:44 +0200

    move parts to transform

--- transform/hosts_to_sfdc/python_timecheck.sh
+++ transform/hosts_to_sfdc/python_timecheck.sh
@@ -8,8 +8,8 @@ ENDTIME=10
 # Checks if between 7 and 10 AM UTC (1-4 AM CST)
 # 10#$H returns the hour in base 10
 if (( STARTTIME <= 10#$H && 10#$H < ENDTIME )); then
-    echo python3 processes/domain_processor.py
-    python3 processes/domain_processor.py
+    echo python3 transform/domain_processor.py
+    python3 transform/domain_processor.py
 else
     echo Domain Processor not run: Only run between the hours of $STARTTIME AM UTC and $ENDTIME AM UTC
 fi
