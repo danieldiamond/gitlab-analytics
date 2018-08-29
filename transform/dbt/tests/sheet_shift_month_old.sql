0adf4f87de57d6637755c37aaf7ded2124107e5f
emilie <emilie.burke@gmail.com>
2018-08-29 14:45:53 +0200

    duh

--- transform/dbt/tests/sheet_shift_month_old.sql
+++ transform/dbt/tests/sheet_shift_month_old.sql
@@ -6,7 +6,7 @@ with base as (
   UNION ALL
   SELECT max(to_timestamp(updated_at)::date), 'metrics' FROM historical.metrics
   UNION ALL
-  SELECT max(to_timestamp(updated_at)::date), 'regional_quotas' FROM historical.regional_quotas
+  SELECT max(to_timestamp(updated_at)::date), 'regional_quotas' FROM historical.transposed
 
 
 ),  maxdate as (
