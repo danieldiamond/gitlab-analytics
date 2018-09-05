6a4d70c55aa31d677b0f23707128e92ce1d5e584
emilie <emilie.burke@gmail.com>
2018-09-05 18:48:22 -0400

    tighten formatting

--- transform/dbt/models/zendesk/base/zendesk_users.sql
+++ transform/dbt/models/zendesk/base/zendesk_users.sql
@@ -7,43 +7,39 @@ WITH source AS (
 
     renamed AS(
 
-	SELECT
-		id as user_id, 
-		email,
-		name,
-
-		-- keys
-		custom_role_id,
-		external_id,
-		organization_id,
-		default_group_id,
-
-		-- logistical info
-		alias,
-		moderator as is_moderator,
-		chat_only as is_chat_only,
-		role_type,
-		role,
-		details,
-		locale,
-		notes,
-		only_private_comments as has_only_private_comments,
-		restricted_agent as is_restricted_agent,
-		shared as is_shared,
-		shared_agent as is_shared_agent,
-		tags, 
-		ticket_restriction,
-		time_zone,		
-		verified as is_verified,
-		user_fields ->> 'agent_ooo' as is_out_of_office,
-
-    	-- metadata
-    	active as is_active,
-    	created_at,
-    	last_login_at,
-    	suspended as is_suspended,
-    	updated_at,
-    	url as user_url
+	SELECT id                          as user_id,
+				 email,
+				 name,
+			-- keys
+				 custom_role_id,
+				 external_id,
+				 organization_id,
+				 default_group_id,
+			-- logistical info
+				 alias,
+				 moderator                   as is_moderator,
+				 chat_only                   as is_chat_only,
+				 role_type,
+				 role,
+				 details,
+				 locale,
+				 notes,
+				 only_private_comments       as has_only_private_comments,
+				 restricted_agent            as is_restricted_agent,
+				 shared                      as is_shared,
+				 shared_agent                as is_shared_agent,
+				 tags,
+				 ticket_restriction,
+				 time_zone,
+				 verified                    as is_verified,
+				 user_fields ->> 'agent_ooo' as is_out_of_office,
+			-- metadata
+				 active                      as is_active,
+				 created_at,
+				 last_login_at,
+				 suspended                   as is_suspended,
+				 updated_at,
+				 url                         as user_url
 
     FROM source
 
