8b1bbc86a41aa444ed227343c639b96baa410453
emilie <emilie.burke@gmail.com>
2018-09-05 14:11:37 -0400

    add base models & tests

---
+++ transform/dbt/models/zendesk/base/zendesk_groups.sql
@@ -0,0 +1,26 @@
+WITH source AS (
+
+	SELECT *
+	FROM zendesk.groups
+
+),
+
+    renamed AS(
+
+	SELECT
+		id as group_id, 
+		name as group_name,
+
+
+    	-- metadata
+    	url AS group_url,   
+		created_at,
+		updated_at,
+		deleted as is_deleted
+
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
---
+++ transform/dbt/models/zendesk/base/zendesk_organization_memberships.sql
@@ -0,0 +1,32 @@
+WITH source AS (
+
+	SELECT *
+	FROM zendesk.organization_memberships
+
+),
+
+    renamed AS(
+
+	SELECT
+		id as organization_membership_id,
+
+		-- keys
+		user_id,
+		organization_id,
+
+
+		-- logistical info
+		"default" as is_default,
+
+
+    	-- metadata
+    	url as organization_membership_url,
+    	created_at,
+    	updated_at
+
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
