c3f83e36f153fa26ae749bb0db8823c5a034cbd5
emilie <emilie.burke@gmail.com>
2018-09-05 14:43:44 -0400

    add ebr & record type

---
+++ transform/dbt/models/sfdc/base/sfdc_executive_business_review.sql
@@ -0,0 +1,37 @@
+WITH source AS (
+
+	SELECT *
+	FROM sfdc.executive_business_review__c
+
+), renamed AS(
+
+	SELECT 
+		id as ebr_id,
+		name as ebr_name,
+		ebr_date__c::date as ebr_date,
+
+		--keys
+		ebr_account__c as account_id,
+		ownerid as owner_id,
+
+		--info
+		ebr_quarter__c as ebr_quarter,
+		ebr_number__c as ebr_number,
+		ebr_outcome__c as ebr_outcome,
+		ebr_summary__c as ebr_summary,
+		ebr_status__c as ebr_status,
+		ebr_notes__c as ebr_notes,
+		first_date_success_updated__c as first_date_success_updated,
+		ebr_action_items_takeaways__c as ebr_action_items_takeaways,
+		ebr_success__c as ebr_success,
+
+		--metadata 
+		lastmodifiedbyid as last_modified_by_id,
+		createdbyid as created_by_id
+
+	FROM source
+
+)
+
+SELECT *
+FROM renamed
---
+++ transform/dbt/models/sfdc/base/sfdc_record_type.sql
@@ -0,0 +1,32 @@
+WITH source AS (
+
+	SELECT *
+	FROM sfdc.recordtype
+
+), renamed AS(
+
+	SELECT 
+		id as record_type_id,
+		developername as record_type_name,
+
+		--keys
+		businessprocessid as business_process_id,
+
+		--info
+		name as record_type_label,
+		description,
+		sobjecttype as modifying_object_type,
+		namespaceprefix as prefix,
+
+		--metadata 
+		lastmodifiedbyid as last_modified_by_id,
+		createdbyid as created_by_id,
+		createddate as created_date,
+		isactive as is_active
+
+	FROM source
+
+)
+
+SELECT *
+FROM renamed
