61997a161ea344aa8f8328699e575c16ec1ad302
Thomas La Piana <tlapiana@gitlab.com>
2018-10-01 22:29:31 +0000

    Resolve "Enable zero-copy clone on snowflake for MR's"

---
+++ orchestration/manage_snowflake.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import logging
+from os import environ as env
+import sys
+from typing import Dict
+
+from fire import Fire
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+
+
+# Set logging defaults
+logging.basicConfig(stream=sys.stdout, level=20)
+
+class SnowflakeManager(object):
+
+    def __init__(self, config_vars: Dict):
+        self.engine = create_engine(
+                        URL(user=config_vars['SF_USER'],
+                            password=config_vars['SF_PASSWORD'],
+                            account=config_vars['SF_ACCOUNT'],
+                            role='SYSADMIN',
+                            warehouse='LOADING'))
+        self.database = config_vars['SF_DATABASE']
+
+    def manage_clones(self, force: bool=False) -> None:
+        """
+        Manage zero copy clones in Snowflake.
+        """
+
+        # Queries for database cloning
+        create_query = """create or replace database "{0}" clone analytics;""".format(self.database)
+        grant_query = """grant ownership on database "{0}" to transformer;""".format(self.database)
+        queries = [create_query, grant_query]
+
+        # if force is false, check if the database exists
+        if force:
+            logging.info('Forcing a create or replace...')
+            db_exists = False
+        else:
+            try:
+                logging.info('Checking if DB exists...')
+                query = 'use database "{}";'.format(self.database)
+                connection = self.engine.connect()
+                connection.execute(query)
+                logging.info('DB exists...')
+                db_exists = True
+            except:
+                logging.info('DB does not exist...')
+                db_exists = False
+            finally:
+                connection.close()
+                self.engine.dispose()
+
+        # If the DB doesn't exist or --force is true, create or replace the db
+        if not db_exists:
+            logging.info('Creating or replacing DB')
+            try:
+                for query in queries:
+                    logging.info('Executing Query: {}'.format(query))
+                    connection = self.engine.connect()
+                    [result] = connection.execute(query).fetchone()
+                    logging.info('Query Result: {}'.format(result))
+            finally:
+                connection.close()
+                self.engine.dispose()
+
+    def delete_clone(self):
+        """
+        Delete a clone.
+        """
+        query = 'drop database "{}";'.format(self.database)
+
+        try:
+            logging.info('Executing Query: {}'.format(query))
+            connection = self.engine.connect()
+            [result] = connection.execute(query).fetchone()
+            logging.info('Query Result: {}'.format(result))
+        finally:
+            connection.close()
+            self.engine.dispose()
+
+
+if __name__ == "__main__":
+    snowflake_manager = SnowflakeManager(env.copy())
+    Fire(snowflake_manager)
+
