12c761a797e548dbb7d7919e7b62894a4a7cd94c
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2018-10-01 22:00:46 +0000

    Revert "Merge branch '311-update-pings-incremental-models-to-specify-config-in-the-model-2' into 'master'"
    This reverts merge request !433

--- transform/cloudsql-dbt/models/pings/base/pings_usage_data_unpacked.sql
+++ transform/cloudsql-dbt/models/pings/base/pings_usage_data_unpacked.sql
@@ -1,11 +1,3 @@
-{{
-  config(
-    materialized='incremental',
-    sql_where='created_at > (SELECT max(created_at) FROM {{ this }})',
-    unique_key='id'
-  )
-}}
-
 WITH usage_data AS (
     SELECT * FROM {{ ref('pings_usage_data') }}
 )
@@ -88,3 +80,6 @@ SELECT
   (stats -> 'uploads') :: TEXT :: NUMERIC                          AS uploads,
   (stats -> 'web_hooks') :: TEXT :: NUMERIC                        AS web_hooks
 FROM usage_data
+{% if adapter.already_exists(this.schema, this.table) and not flags.FULL_REFRESH %}
+    WHERE created_at > (SELECT max(created_at) FROM {{ this }})
+{% endif %}
