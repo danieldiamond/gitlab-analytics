0ee0a2eafc0c3110f9db5c79fffdb16c924bba12
emilie <emilie.burke@gmail.com>
2018-10-03 11:39:19 -0400

    update base models to be incremental

--- transform/snowflake-dbt/models/snowplow/base/unnested_errors.sql
+++ transform/snowflake-dbt/models/snowplow/base/unnested_errors.sql
@@ -1,3 +1,11 @@
+{{
+  config(
+    materialized='incremental',
+    sql_where='TRUE',
+    unique_key='event_id'
+  )
+}}
+
 SELECT
   JSONTEXT['arrivalTimestamp']::string AS arrivalTimestamp,
   JSONTEXT['attemptEndingTimestamp']::string AS attemptEndingTimestamp,
@@ -5,5 +13,9 @@ SELECT
   JSONTEXT['errorCode']::string AS errorCode,
   JSONTEXT['errorMessage']::string AS errorMessage,
   JSONTEXT['lambdaArn']::string AS lambdaArn,
-  JSONTEXT['rawData'::string] AS rawData
+  JSONTEXT['rawData'::string] AS rawData,
+  uploaded_at
 FROM RAW.SNOWPLOW.EVENTS
+{% if adapter.already_exists(this.schema, this.table) and not flags.FULL_REFRESH %}
+AND uploaded_at > (SELECT max(uploaded_at) FROM {{ this }})
+{% endif %}
