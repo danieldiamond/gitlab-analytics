a0da2273a85981d9656d8f6d805e9610d6f5a78b
emilie <emilie.burke@gmail.com>
2018-10-08 13:07:34 -0400

    add data test

--- transform/cloudsql-dbt/tests/sheet_shift_month_old.sql
+++ transform/cloudsql-dbt/tests/sheet_shift_month_old.sql
@@ -7,8 +7,33 @@ with base as (
   SELECT max(to_timestamp(updated_at)::date), 'metrics' FROM historical.metrics
   UNION ALL
   SELECT max(to_timestamp(updated_at)::date), 'regional_quotas' FROM historical.transposed
-
-
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'sales_weekly_forecast' FROM historical.sales_weekly_forecast
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'ccodashboard_actuals' FROM historical.ccodashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'ccodashboard_goals' FROM historical.ccodashboard_goals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'crodashboard_actuals' FROM historical.crodashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'crodashboard_goals' FROM historical.crodashboard_goals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'cfodashboard_actuals' FROM historical.cfodashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'cfodashboard_goals' FROM historical.cfodashboard_goals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'vpedashboard_actuals' FROM historical.vpedashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'vpedashboard_goals' FROM historical.vpedashboard_goals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'cmodashboard_actuals' FROM historical.cmodashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'cmodashboard_goals' FROM historical.cmodashboard_goals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'alliancesdashboard_actuals' FROM historical.alliancesdashboard_actuals
+  UNION ALL
+  SELECT max(to_timestamp(updated_at)::date), 'alliancesdashboard_goals' FROM historical.alliancesdashboard_goals
+  
 ),  maxdate as (
 
     select 	*,
