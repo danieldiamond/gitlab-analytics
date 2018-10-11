99692ea990b7788ae469fc0413c68bfd3db58b44
emilie <emilie.burke@gmail.com>
2018-10-11 10:53:31 -0400

    handle nulls

--- transform/cloudsql-dbt/models/historical/base/historical_alliancesdashboard_actuals.sql
+++ transform/cloudsql-dbt/models/historical/base/historical_alliancesdashboard_actuals.sql
@@ -3,10 +3,10 @@ WITH source AS (
 	SELECT 
 		  md5(month_of::varchar) as pk,
 		  month_of::date,
-		  active_gitlab_installations,
-		  active_users_aws,
-		  active_users_gcp,
-		  active_users_azure
+		  nullif(active_gitlab_installations, '')::float as active_gitlab_installations,
+		  nullif(active_users_aws, '')::float as active_users_aws,
+		  nullif(active_users_gcp, '')::float as active_users_gcp,
+		  nullif(active_users_azure, '')::float as active_users_azure 
 	FROM historical.alliancesdashboard_actuals
 )
 
--- transform/cloudsql-dbt/models/historical/base/historical_alliancesdashboard_goals.sql
+++ transform/cloudsql-dbt/models/historical/base/historical_alliancesdashboard_goals.sql
@@ -3,11 +3,11 @@ WITH source AS (
 	SELECT 
 		  md5(month_of::varchar) as pk,
 		  month_of::date,
-		  active_gitlab_installations,
-		  active_users_aws,
-		  active_users_gcp,
-		  active_users_azure,
-		  active_users_unknown
+		  nullif(active_gitlab_installations, '')::float as active_gitlab_installations, 
+		  nullif(active_users_aws, '')::float as active_users_aws, 
+		  nullif(active_users_gcp, '')::float as active_users_gcp, 
+		  nullif(active_users_azure, '')::float as active_users_azure, 
+		  nullif(active_users_unknown, '')::float as active_users_unknown
 	FROM historical.alliancesdashboard_goals
 )
 
