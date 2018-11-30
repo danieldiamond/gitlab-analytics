40d79cdeeebc00355b1222860ae0dfc2f33a63f8
emilie <emilie.burke@gmail.com>
2018-11-30 16:04:21 -0500

    remove months_since calculation

--- transform/cloudsql-dbt/models/zuora/xf/zuora_mrr_totals.sql
+++ transform/cloudsql-dbt/models/zuora/xf/zuora_mrr_totals.sql
@@ -33,9 +33,7 @@ WITH base_mrr AS (
       trueup_month AS mrr_month,
       cohort_month AS zuora_subscription_cohort_month,
       cohort_quarter AS zuora_subscription_cohort_quarter,
-      {{ month_diff('cohort_month ', 'trueup_month') }} as months_since_zuora_subscription_cohort_start,
-      charge_name AS mrr_type,
-      null as quantity,
+      --{{ month_diff('cohort_month ', 'trueup_month') }} as months_since_zuora_subscription_cohort_start,
       mrr
     FROM trueup_mrr
 
@@ -50,9 +48,7 @@ WITH base_mrr AS (
       mrr_month,
       cohort_month AS zuora_subscription_cohort_month,
       cohort_quarter AS zuora_subscription_cohort_quarter,
-      {{ month_diff('cohort_month ', 'mrr_month') }} as months_since_zuora_subscription_cohort_start,
-      rate_plan_charge_name AS mrr_type,
-      quantity,
+      --{{ month_diff('cohort_month ', 'mrr_month') }} as months_since_zuora_subscription_cohort_start,
       mrr
     FROM base_mrr
 
@@ -65,9 +61,7 @@ WITH base_mrr AS (
             mrr_month,
             min(zuora_subscription_cohort_month) as zuora_subscription_cohort_month,
             min(zuora_subscription_cohort_quarter) as zuora_subscription_cohort_quarter,
-            max(months_since_zuora_subscription_cohort_start) as months_since_zuora_subscription_cohort_start,
-            --mrr_type,
-            --quantity,
+            --max(months_since_zuora_subscription_cohort_start) as months_since_zuora_subscription_cohort_start,
             sum(mrr) as mrr 
     FROM mrr_combined
     GROUP BY 4, 5
@@ -75,11 +69,14 @@ WITH base_mrr AS (
 ), subs_with_retention_values as (
 
     SELECT
+      last_year.account_number,
+      last_year.subscription_name_slugify,
+      last_year.subscription_name,
       last_year.subscription_slug_for_counting,
       (last_year.mrr_month + INTERVAL '12 month')::date as mrr_month,
       last_year.zuora_subscription_cohort_month,
       last_year.zuora_subscription_cohort_quarter,
-      (last_year.months_since_zuora_subscription_cohort_start + 12) as months_since_zuora_subscription_cohort_start,
+      --(last_year.months_since_zuora_subscription_cohort_start + 12) as months_since_zuora_subscription_cohort_start,
       current.mrr as mrr,
       last_year.mrr::float as mrr_12mo_ago
     FROM uniqueified as last_year --12 months ago
@@ -90,15 +87,18 @@ WITH base_mrr AS (
 ), first_12months as (
 
     SELECT
+      account_number,
+      subscription_name_slugify,
+      subscription_name,
       subscription_slug_for_counting,
       mrr_month,
       zuora_subscription_cohort_month,
       zuora_subscription_cohort_quarter,
-      months_since_zuora_subscription_cohort_start,
+      --months_since_zuora_subscription_cohort_start,
       mrr,
       null::float as mrr_12mo_ago
     FROM uniqueified
-    WHERE months_since_zuora_subscription_cohort_start < 12
+    WHERE mrr_month::date - zuora_subscription_cohort_month::date <= 365
 
 
 ), unioned as (
