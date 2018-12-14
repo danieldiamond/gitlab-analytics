5cf772dbe0fd0189554e21a8d646d4002359ec0c
Taylor A. Murphy <tmurphy@gitlab.com>
2018-12-14 14:14:38 -0600

    Add sfdc 15 to 18 UDF

---
+++ transform/snowflake-dbt/macros/udfs/sfdc_id_15_to_18.sql
@@ -0,0 +1,34 @@
+{% macro sfdc_id_15_to_18() %}
+CREATE OR REPLACE FUNCTION sfdc_id_15_5o_18(input_id string)
+  RETURNS string
+  LANGUAGE JAVASCRIPT
+  AS '
+let suffix = "";
+
+if (input_id.length != 15) {
+    return input_id;
+}
+
+for (let index = 0; index < 3; index++) {
+    let flags = 0;
+
+    for (let inner_index = 0; inner_index < 5; inner_index++) {
+        let chr = input_id.substr(index * 5 + inner_index,1)
+
+        let ascii_code = chr.charCodeAt(0)
+
+        if ((ascii_code >= "A".charCodeAt(0)) && (ascii_code <= "Z".charCodeAt(0))) {
+            flags = flags + (1 << inner_index)
+        }
+
+    }
+    suffix = suffix.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ012345".substr(flags,1))
+
+}
+
+let final_id = input_id.concat(suffix)
+
+return final_id
+
+';
+{% endmacro %}
