dc5d7a486fd1e0bb785f1bc50f15097819d6a801
Cathy <crichards@gitlab.com>
2019-04-08 12:59:54 -0400

    Stashing

--- transform/snowflake-dbt/macros/rename_market_segment.sql
+++ transform/snowflake-dbt/macros/rename_market_segment.sql
@@ -1,12 +1,14 @@
 {%- macro rename_market_segment(column_name) -%}
 
-  CASE WHEN (contains({{ column_name }}, 'smb')) THEN 'SMB'
-  	   WHEN (contains({{ column_name }}, 'mid-market' OR 'Mid-market')) THEN 'Mid-Market'
-       WHEN (contains({{ column_name }}, 'strategic' OR 'large' OR 'unknown'))  THEN initcap({{ column_name }})
-       WHEN (contains({{ column_name }}, 'Strategic')) THEN 'Strategic'
+  CASE WHEN (contains({{ column_name }}, 'Strategic')) THEN 'Strategic'
        WHEN (contains({{ column_name }}, 'Large')) THEN 'Large'
        WHEN (contains({{ column_name }}, 'Unknown')) THEN 'Unknown'
+       WHEN (contains({{ column_name }}, 'smb')) THEN 'SMB'
+       WHEN {{ column_name }} in ('large', 'strategic', 'unknown') THEN initcap({{ column_name }})
+       WHEN {{ column_name }} in ('mid-market', 'Mid-market') THEN 'Mid-Market'
        ELSE 'Unknown'
    END
 
 {%- endmacro -%}
+
+
--- transform/snowflake-dbt/macros/sfdc_rename_segment.sql
+++ transform/snowflake-dbt/macros/sfdc_rename_segment.sql
@@ -1,11 +1,13 @@
 {%- macro sfdc_rename_segment(column_1, column_2) -%}
   COALESCE(initcap(COALESCE(column_1, column_2)), 'Unknown')       AS coalesced_segment
 
-  CASE WHEN coalesced_segment, 'smb')) THEN 'SMB'
-  	   WHEN coalesced_segment, 'mid-market' OR 'Mid-market')) THEN 'Mid-Market'
-       WHEN coalesced_segment, 'strategic' OR 'large' OR 'unknown'))  THEN initcap({{ column_name }})
-       WHEN coalesced_segment, 'Strategic')) THEN 'Strategic'
-       WHEN coalesced_segment, 'Large')) THEN 'Large'
+  CASE WHEN (contains(coalesced_segment, 'Strategic')) THEN 'Strategic'
+       WHEN (contains(coalesced_segment, 'Large')) THEN 'Large'
+       WHEN (contains(coalesced_segment, 'Unknown')) THEN 'Unknown'
+       WHEN (contains(coalesced_segment, 'smb')) THEN 'SMB'
+       WHEN coalesced_segment in ('large', 'strategic', 'unknown') THEN initcap(coalesced_segment)
+       WHEN coalesced_segment in ('mid-market', 'Mid-market') THEN 'Mid-Market'
+
    END 
 
 {%- endmacro -%}
