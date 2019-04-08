5e90427175d243ee2e1a2029c983b601f07a0020
Cathy <crichards@gitlab.com>
2019-04-08 14:09:26 -0400

    Revisions to the macro

--- transform/snowflake-dbt/macros/sfdc_rename_segment.sql
+++ transform/snowflake-dbt/macros/sfdc_rename_segment.sql
@@ -1,15 +1,6 @@
 {%- macro sfdc_rename_segment(column_1, column_2) -%}
-  COALESCE(initcap(COALESCE(column_1, column_2)), 'Unknown')       AS coalesced_segment
-
-  CASE WHEN (contains(coalesced_segment, 'Strategic')) THEN 'Strategic'
-       WHEN (contains(coalesced_segment, 'Large')) THEN 'Large'
-       WHEN (contains(coalesced_segment, 'Unknown')) THEN 'Unknown'
-       WHEN (contains(coalesced_segment, 'smb')) THEN 'SMB'
-       WHEN coalesced_segment in ('large', 'strategic', 'unknown') THEN initcap(coalesced_segment)
-       WHEN coalesced_segment in ('mid-market', 'Mid-market') THEN 'Mid-Market'
-
+  CASE WHEN COALESCE(column_1, column_2) in ('smb', 'Smb') THEN 'SMB'
+       WHEN COALESCE(column_1, column_2) in ('large', 'strategic', 'unknown') THEN initcap(COALESCE(column_1, column_2))
+       WHEN COALESCE(column_1, column_2) in ('mid-market', 'Mid-market') THEN 'Mid-Market'
    END 
-
 {%- endmacro -%}
-
-
