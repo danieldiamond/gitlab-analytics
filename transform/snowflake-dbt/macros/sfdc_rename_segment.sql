c40e757940e3779e2fb6ce6854164b907accc4f8
Cathy <crichards@gitlab.com>
2019-04-08 23:09:52 -0400

    Corrected sfdc macro

--- transform/snowflake-dbt/macros/sfdc_rename_segment.sql
+++ transform/snowflake-dbt/macros/sfdc_rename_segment.sql
@@ -1,12 +1,16 @@
 {%- macro sfdc_rename_segment(column_1, column_2) -%}
 
- CASE WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}), 'Strategic')) THEN 'Strategic'
-      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}), 'Large')) THEN 'Large'
-      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}), 'Unknown')) THEN 'Unknown'
-      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}), 'Mid-Market')) THEN 'Mid-Market'
-      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}), 'Smb')) THEN 'SMB'
-      ELSE 'Unknown' 
+ CASE WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Smb')) THEN 'SMB'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'SMB')) THEN 'SMB'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Strategic')) THEN 'Strategic'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'strategic')) THEN 'Strategic'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Large')) THEN 'Large'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Unknown')) THEN 'Unknown'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Mid-Market')) THEN 'Mid-Market'
+      WHEN (contains(COALESCE({{ column_1 }}, {{ column_2 }}, 'Unknown'), 'Mid-market')) THEN 'Mid-Market'
+
   END
+
 {%- endmacro -%}
 
 
