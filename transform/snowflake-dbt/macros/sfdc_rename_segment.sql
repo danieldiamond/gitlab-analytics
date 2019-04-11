a3386b620f6853ab5c28aa4d490daed96d837866
Cathy <crichards@gitlab.com>
2019-04-11 00:46:51 -0400

    Capitalize AS

--- transform/snowflake-dbt/macros/sfdc_rename_segment.sql
+++ transform/snowflake-dbt/macros/sfdc_rename_segment.sql
@@ -1,6 +1,6 @@
 {%- macro sfdc_rename_segment(column_1, column_2) -%}
 
-coalesce({{ column_1 }}, {{ column_2 }}) as {{ column_1 }}_new_col, 
+coalesce({{ column_1 }}, {{ column_2 }}) AS {{ column_1 }}_new_col, 
 CASE  WHEN lower({{ column_1 }}_new_col) = 'smb' THEN 'SMB' 
       WHEN lower({{ column_1 }}_new_col) = 'mid-market' THEN 'Mid-Market' 
       WHEN {{ column_1 }}_new_col is not null THEN initcap({{ column_1 }}_new_col) 
