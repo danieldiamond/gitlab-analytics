d1ca87af29965b4c9aa2475ae150c98109351186
Alex Mrvaljevich <amrvaljevich@gitlab.com>
2019-05-15 20:12:10 +0000

    Resolve "Add plan values to sheetload"

---
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_planned_values.sql
@@ -0,0 +1,27 @@
+{{ config({
+    "schema": "analytics",
+    "post-hook": "grant select on {{this}} to role reporter"
+    })
+}}
+
+WITH source AS (
+
+	SELECT *
+	FROM {{ var("database") }}.sheetload.planned_values
+
+), renamed AS (
+
+
+	SELECT
+       unique_key::INT as primary_key,
+       plan_month::DATE as plan_month,
+	     planned_new_pipe::INT as planned_new_pipe,
+	     planned_total_iacv::INT as planned_total_iacv,
+	     planned_tcv_minus_gross_opex::INT as planned_tcv_minus_gross_opex
+
+	FROM source
+
+)
+
+SELECT * FROM renamed
+
