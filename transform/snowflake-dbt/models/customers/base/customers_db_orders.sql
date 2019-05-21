6996fdf41bfe8a72dcf6ae31340e937cde843d9e
Eli Kastelein <ekastelein@gitlab.com>
2019-05-21 16:12:59 +0000

    Resolve "Make base models for customers DB"

---
+++ transform/snowflake-dbt/models/customers/base/customers_db_orders.sql
@@ -0,0 +1,27 @@
+WITH renamed AS (
+  SELECT DISTINCT
+    id                                  AS order_id,
+    customer_id,
+    product_rate_plan_id,
+    subscription_id,
+    subscription_name,
+    start_date                          AS order_start_date,
+    end_date                            AS order_end_date,
+    quantity                            AS order_quantity,
+    created_at                          AS order_created_at,
+    updated_at                          AS order_updated_at,
+    DENSE_RANK() OVER (
+        PARTITION BY order_id
+        ORDER BY order_updated_at DESC) AS row_number
+    --gl_namespace_id,
+    --gl_namespace_name,
+    --amendment_type,
+    --trial,
+    --last_extra_ci_minutes_sync_at,
+ FROM {{ source('customers', 'customers_db_orders') }}
+)
+
+SELECT
+  *
+FROM renamed
+WHERE row_number = 1
