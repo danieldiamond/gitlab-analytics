707d20dc0f66f43eac585d112942e1c33e16ba0b
Robert Kohnke <rkohnke@gitlab.com>
2019-05-28 18:05:47 +0000

    Add Sales_Segmentation__c  field to the Lead model

--- transform/snowflake-dbt/models/sfdc/base/sfdc_lead.sql
+++ transform/snowflake-dbt/models/sfdc/base/sfdc_lead.sql
@@ -34,7 +34,7 @@ WITH source AS (
         leadsource                  AS lead_source,
         lead_from__c                AS lead_from,
         lead_source_type__c         AS lead_source_type,
-        lead_conversion_approval_status__c 
+        lead_conversion_approval_status__c
                                     AS lead_conversiona_approval_status,
 
         street                      AS street,
@@ -48,26 +48,27 @@ WITH source AS (
         -- info
         requested_contact__c        AS requested_contact,
         company                     AS company,
-        buying_process_for_procuring_gitlab__c 
+        buying_process_for_procuring_gitlab__c
                                     AS buying_process,
         industry                    AS industry,
         region__c                   AS region,
         largeaccount__c             AS is_large_account,
         outreach_stage__c           AS outreach_stage,
         interested_in_gitlab_ee__c  AS is_interested_gitlab_ee,
-        interested_in_hosted_solution__c 
+        interested_in_hosted_solution__c
                                     AS is_interested_in_hosted,
 
         matched_account_top_list__c AS matched_account_top_list,
         mql_date__c                 AS marketo_qualified_lead_date,
         mql_datetime__c             AS marketo_qualified_lead_datetime,
+        {{ sales_segment_cleaning('sales_segmentation__c') }} AS sales_segmentation,
 
         --gitlab internal
 
         bdr_lu__c                   AS business_development_look_up,
-        business_development_rep_contact__c 
+        business_development_rep_contact__c
                                     AS business_development_representative_contact,
-        business_development_representative__c 
+        business_development_representative__c
                                     AS business_development_representative,
         competition__c              AS competition,
 
@@ -88,4 +89,3 @@ WITH source AS (
 
 SELECT *
 FROM renamed
-
