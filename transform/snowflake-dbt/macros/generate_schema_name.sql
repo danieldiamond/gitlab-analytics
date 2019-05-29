3c8651d27c0c9cac885d2d21cd4d25cb739ed90c
Emilie Schario <eburke@gitlab.com>
2019-05-29 00:15:03 +0000

    Resolve "Update custom schema macro to support sensitive schemas"

--- transform/snowflake-dbt/macros/generate_schema_name.sql
+++ transform/snowflake-dbt/macros/generate_schema_name.sql
@@ -1,10 +1,10 @@
 {% macro generate_schema_name(custom_schema_name=none) -%}
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%} 
+    {%- if custom_schema_name is none -%}
         {{ default_schema }}_staging
     {%- elif target.name != 'prod' and custom_schema_name is not none -%}
     	{{ default_schema }}_{{ custom_schema_name | trim }}
-    {%- else -%} 
+    {%- else -%}
         {{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
