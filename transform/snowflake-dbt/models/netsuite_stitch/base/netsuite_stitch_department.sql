69f09e3dd499a74e1a0be9c17a47ff6bb0425071
Taylor A. Murphy <tmurphy@gitlab.com>
2019-05-31 15:32:10 -0500

    add subsidiary

--- transform/snowflake-dbt/models/netsuite_stitch/base/netsuite_stitch_department.sql
+++ transform/snowflake-dbt/models/netsuite_stitch/base/netsuite_stitch_department.sql
@@ -6,14 +6,14 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-        internalid              AS department_id,
-        includechildren         AS include_children,
-        isinactive              AS is_inactive,
+        internalid                      AS department_id,
+        includechildren                 AS include_children,
+        isinactive                      AS is_inactive,
         parse_json(
             subsidiarylist['recordRef'][0])
-                ['internalId']  AS subsidiary_list,
-        parent['internalId']    AS parent_id,
-        parent['name']          AS parent_name,
+                ['internalId']::NUMBER  AS subsidiary_list,
+        parent['internalId']::NUMBER    AS parent_id,
+        parent['name']::STRING          AS parent_name,
         name
     FROM source
 
