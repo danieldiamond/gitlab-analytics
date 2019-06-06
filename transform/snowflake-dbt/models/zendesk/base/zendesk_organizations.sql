ab7f09028c726be4fe34fa6bb8537859f458e3ec
Cathy Richards <crichards@gitlab.com>
2019-06-06 19:33:42 +0000

    Resolve "Null ARR values in zendesk_tickets_xf"

--- transform/snowflake-dbt/models/zendesk/base/zendesk_organizations.sql
+++ transform/snowflake-dbt/models/zendesk/base/zendesk_organizations.sql
@@ -1,29 +1,29 @@
 with source as (
-    
-    SELECT * 
+
+    SELECT *
     FROM {{ source('zendesk', 'organizations') }}
 ),
 
 renamed as (
-    
+
     SELECT
-        
+
         --ids
         id                                                                                  AS organization_id,
-        
+
         --fields
         name,
-        organization_fields:aar                                                             AS arr,
+        organization_fields:aar :: integer                                            AS arr,
         organization_fields:market_segment                                                  AS organization_market_segment,
         REPLACE(organization_fields:salesforce_id, '"', '')                                 AS sfdc_id,
-        
+
         --dates
         created_at,
         updated_at
-        
+
     FROM source
-    
+
 )
 
-SELECT * 
+SELECT *
 FROM renamed
