e83dc766f3b4a7d7c0886b149e9d1b9f2edecffb
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2019-06-20 20:19:55 +0000

    Resolve "Get employee number from Bamboo"

--- extract/bamboohr/src/execute.py
+++ extract/bamboohr/src/execute.py
@@ -62,13 +62,6 @@ if __name__ == "__main__":
     config_dict = env.copy()
     snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
-    tabular_data = dict(
-        compensation="compensation",
-        jobinfo="jobInfo",
-        employmentstatus="employmentStatus",
-        custombonus="customBonus",
-    )
-
     # Company Directory
     logging.info("Getting latest employee directory.")
 
@@ -82,9 +75,30 @@ if __name__ == "__main__":
     )
 
     # Tabular Data
+    tabular_data = dict(
+        compensation="compensation",
+        jobinfo="jobInfo",
+        employmentstatus="employmentStatus",
+        custombonus="customBonus",
+    )
+
     for key, value in tabular_data.items():
         logging.info(f"Querying for {value} tabular data...")
         data = bamboo.get_tabular_data(value)
+
+        with open(f"{key}.json", "w") as outfile:
+            json.dump(data, outfile)
+
+        upload_to_snowflake_stage(
+            f"{key}.json", "raw.bamboohr.bamboohr_load", f"raw.bamboohr.{key}"
+        )
+
+    # Custom Reports
+    report_mapping = dict(id_employee_number_mapping="416")
+
+    for key, value in report_mapping.items():
+        logging.info(f"Querying for report number {value} into table {key}...")
+        data = bamboo.get_report(value)
 
         with open(f"{key}.json", "w") as outfile:
             json.dump(data, outfile)
