33eb7740805715416a9f1ef18c0954c769c0f94d
Emilie Schario <eburke@gitlab.com>
2019-06-21 21:51:35 +0000

    Resolve "Invalid Identifier in sheetload_employee_location_factor"

--- transform/snowflake-dbt/models/sheetload/base/sheetload_employee_location_factor.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_employee_location_factor.sql
@@ -1,5 +1,5 @@
 {{ config(
-    { "schema": "sensitive", 
+    { "schema": "sensitive",
       "materialized": "table" }
 ) }}
 
@@ -12,12 +12,11 @@ with source as (
 
     SELECT
          nullif("Employee_ID",'') as bamboo_employee_id,
-         nullif("Location_Factor",'')::float as location_factor,
-         nullif(nullif("Metrics_Adjusted_Location_Factor", ''), 'ok')::float as metrics_adjusted_location_factor
+         nullif("Location_Factor",'')::float as location_factor
     FROM source
     WHERE lower(bamboo_employee_id) NOT LIKE '%not in comp calc%'
 )
 
 SELECT bamboo_employee_id::bigint as bamboo_employee_id,
-       coalesce(metrics_adjusted_location_factor, location_factor) as location_factor
+       location_factor
 FROM renamed
