6083776d6b5353c75c577377b334b614186d7acd
Thomas La Piana <tlapiana@gitlab.com>
2019-06-21 22:31:59 +0000

    Resolve "Adopt a way to version control schema changes"

---
+++ transform/migrations/deploy/placeholder.sql
@@ -0,0 +1,5 @@
+-- Deploy gitlab-data:placeholder to snowflake
+
+USE WAREHOUSE &warehouse;
+
+-- XXX Add DDLs here.
---
+++ transform/migrations/verify/placeholder.sql
@@ -0,0 +1,5 @@
+-- Verify gitlab-data:placeholder on snowflake
+
+USE WAREHOUSE &warehouse;
+
+-- XXX Add verifications here.
