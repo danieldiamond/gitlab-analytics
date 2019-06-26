d3de572445be05e58cda9382c6430201daa8b62d
Eli Kastelein <ekastelein@gitlab.com>
2019-06-26 17:41:38 +0000

    Apply suggestion to transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_events.sql

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_events.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+
+  SELECT *,
+    ROW_NUMBER() OVER (PARTITION BY id ORDER BY UPDATED_AT DESC) as rank_in_key
+  FROM {{ source('gitlab_dotcom', 'audit_events') }}
+
+), renamed AS (
+
+    SELECT
+      id :: integer            AS audit_event_id,
+      author_id :: integer     AS author_id,
+      type :: varchar          AS audit_event_type,
+      entity_id :: integer     AS entity_id,
+      entity_type ::varchar    AS entity_type,
+      details :: varchar       AS audit_event_details,
+      created_at ::timestamp   AS audit_event_created_at,
+      updated_at ::timestamp   AS audit_event_updated_at
+
+    FROM source
+    WHERE rank_in_key = 1
+
+)
+
+SELECT * FROM renamed
