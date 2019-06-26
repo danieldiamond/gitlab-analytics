89e8743602bc8b6fb72e12d41c7a4d2d6c6af303
Robert Kohnke <rkohnke@gitlab.com>
2019-06-26 22:00:48 +0000

    Resolve "Add Bizible Person Data to Snowflake/Periscope"

---
+++ transform/snowflake-dbt/models/sfdc/base/sfdc_bizible_person.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    materialized = "table"
+  )
+}}
+
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('salesforce', 'bizible_person') }}
+
+), renamed AS (
+
+    SELECT
+      id                              AS person_id,
+      bizible2__lead__c               AS bizible_lead_id,
+      bizible2__contact__c            AS bizible_contact_id
+      
+    FROM source
+	WHERE isdeleted = FALSE
+
+)
+
+SELECT *
+FROM renamed
