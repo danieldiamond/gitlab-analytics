aaa2a9236b59960e836606f33963dda3f8176351
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2019-07-01 21:43:12 +0000

    Resolve "Delete Old netsuite extract DAGs"

--- extract/netsuite/shared_modules/elt/test_db.py
+++
@@ -1,6 +0,0 @@
-from elt.db import DB
-
-
-def test_connect():
-    db_conn = DB.default.open()
-    assert db_conn
--- extract/netsuite_legacy/src/soap_api/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from netsuite_legacy.src.soap_api.utils.utils import (
-    fetch_attribute,
-    merge_transform_results,
-)
