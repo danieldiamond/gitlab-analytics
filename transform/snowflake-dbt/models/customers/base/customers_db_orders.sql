4c85993f71c96215b38a0b7710a9a8476a596d54
Mathieu Peychet <mpeychet@gitlab.com>
2019-07-11 11:23:29 +0000

    Resolve "Add `subscription_name_slugify` to `customers_db_orders` table"

--- transform/snowflake-dbt/models/customers/base/customers_db_orders.sql
+++ transform/snowflake-dbt/models/customers/base/customers_db_orders.sql
@@ -1,15 +1,16 @@
 WITH renamed AS (
   SELECT DISTINCT
-    id                                  AS order_id,
+    id                                     AS order_id,
     customer_id,
     product_rate_plan_id,
     subscription_id,
     subscription_name,
-    start_date                          AS order_start_date,
-    end_date                            AS order_end_date,
-    quantity                            AS order_quantity,
-    created_at                          AS order_created_at,
-    updated_at                          AS order_updated_at,
+    {{zuora_slugify("subscription_name")}} AS subscription_name_slugify,
+    start_date                             AS order_start_date,
+    end_date                               AS order_end_date,
+    quantity                               AS order_quantity,
+    created_at                             AS order_created_at,
+    updated_at                             AS order_updated_at,
     DENSE_RANK() OVER (
         PARTITION BY order_id
         ORDER BY order_updated_at DESC) AS row_number
