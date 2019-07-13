f5c88a7f8932d391e91612468a3a3db5009d9c14
Emilie Schario <eburke@gitlab.com>
2019-07-13 14:47:47 +0000

    Resolve "Fix Zendesk Models"

--- transform/snowflake-dbt/models/zendesk/base/zendesk_ticket_metrics.sql
+++ transform/snowflake-dbt/models/zendesk/base/zendesk_ticket_metrics.sql
@@ -10,39 +10,42 @@ renamed as (
     SELECT
 
         --ids
-        id,
+        id                                                  AS ticket_metrics_id,
         ticket_id,
 
         --fields
-        agent_wait_time_in_minutes:business                                                 AS agent_wait_time_in_minutes_business_hours,
-        agent_wait_time_in_minutes:calendar                                                 AS agent_wait_time_in_minutes_calendar_hours,
-        first_resolution_time_in_minutes:business                                           AS first_resolution_time_in_minutes_during_business_hours,
-        first_resolution_time_in_minutes:calendar                                           AS first_resolution_time_in_minutes_during_calendar_hours,
-        full_resolution_time_in_minutes:business                                            AS full_resolution_time_in_minutes_during_business_hours,
-        full_resolution_time_in_minutes:calendar                                            AS full_resolution_time_in_minutes_during_calendar_hours,
-        on_hold_time_in_minutes:business                                                    AS on_hold_time_in_minutes_during_business_hours,
-        on_hold_time_in_minutes:calendar                                                    AS on_hold_time_in_minutes_during_calendar_hours,
+        agent_wait_time_in_minutes['business']::float       AS agent_wait_time_in_minutes_business_hours,
+        agent_wait_time_in_minutes['calendar']::float       AS agent_wait_time_in_minutes_calendar_hours,
+        first_resolution_time_in_minutes['business']::float AS first_resolution_time_in_minutes_during_business_hours,
+        first_resolution_time_in_minutes['calendar']::float AS first_resolution_time_in_minutes_during_calendar_hours,
+        full_resolution_time_in_minutes['business']::float  AS full_resolution_time_in_minutes_during_business_hours,
+        full_resolution_time_in_minutes['calendar']::float  AS full_resolution_time_in_minutes_during_calendar_hours,
+        on_hold_time_in_minutes['business']::float          AS on_hold_time_in_minutes_during_business_hours,
+        on_hold_time_in_minutes['calendar']::float          AS on_hold_time_in_minutes_during_calendar_hours,
         reopens,
-        replies                                                                             AS total_replies,
-        reply_time_in_minutes:business                                                      AS reply_time_in_minutes_during_business_hours,
-        reply_time_in_minutes:calendar                                                      AS reply_time_in_minutes_during_calendar_hours,
-        requester_wait_time_in_minutes:business                                             AS requester_wait_time_in_minutes_during_business_hours,
-        requester_wait_time_in_minutes:calendar                                             AS requester_wait_time_in_minutes_during_calendar_hours,
-        assignee_stations                                                                   AS assignee_station_number,
-        group_stations                                                                      AS group_station_number,
-        
+        replies                                             AS total_replies,
+        reply_time_in_minutes['business']::float            AS reply_time_in_minutes_during_business_hours,
+        reply_time_in_minutes['calendar']::float            AS reply_time_in_minutes_during_calendar_hours,
+        requester_wait_time_in_minutes['business']::float   AS requester_wait_time_in_minutes_during_business_hours,
+        requester_wait_time_in_minutes['calendar']::float   AS requester_wait_time_in_minutes_during_calendar_hours,
+        assignee_stations                                   AS assignee_station_number,
+        group_stations                                      AS group_station_number,
+
+        COALESCE(reply_time_in_minutes_during_calendar_hours,
+                first_resolution_time_in_minutes_during_calendar_hours,
+                full_resolution_time_in_minutes_during_calendar_hours) AS first_reply_time,
         --dates
-        created_at                                                                          AS creation_date,
-        assigned_at                                                                         AS latest_assigned_at,
+        created_at,
+        assigned_at,
         initially_assigned_at,
         latest_comment_added_at,
-        solved_at                                                                           AS solved_date,
+        solved_at,
         updated_at,
         assignee_updated_at
 
-FROM source
+    FROM source
 
 )
 
-SELECT * 
+SELECT *
 FROM renamed
