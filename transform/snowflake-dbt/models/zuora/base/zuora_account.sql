a5207a870f0ad32d253357bc0c1657671bbe6f61
Eli Kastelein <ekastelein@gitlab.com>
2019-07-26 19:09:40 +0000

    Resolve "Fix zuora_account_has_crm_id failure on 2019-07-26"

--- transform/snowflake-dbt/models/zuora/base/zuora_account.sql
+++ transform/snowflake-dbt/models/zuora/base/zuora_account.sql
@@ -88,6 +88,7 @@ FROM renamed
 -- temporary filter while finance corrects issue
 WHERE account_id NOT IN  (
   '2c92a0086bc9001b016bcea432160f79',
-	'2c92a0076bff1825016c01f2123b273f'
+  '2c92a0076bff1825016c01f2123b273f',
+  '2c92a0fc6c297e05016c299fb450667d' -- https://gitlab.com/gitlab-data/analytics/issues/2063
 
 )
