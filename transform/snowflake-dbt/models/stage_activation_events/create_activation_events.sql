fd9e5e2c06d8932e7de6dbd5d1d010d290f96424
mpeychet <mpeychet@gitlab.com>
2019-08-20 19:30:40 +0200

    test added

--- transform/snowflake-dbt/models/stage_activation_events/create_activation_events.sql
+++ transform/snowflake-dbt/models/stage_activation_events/create_activation_events.sql
@@ -5,7 +5,7 @@ WITH snowplow_create_activation_events AS (
     user_custom_id      AS gitlab_user_id,
     event_date,
     event_type,
-    sk_id,
+    page_view_id        AS sk_id,
     'snowplow_frontend' AS source_type
   
   FROM {{ ref('snowplow_create_activation_events')}}
