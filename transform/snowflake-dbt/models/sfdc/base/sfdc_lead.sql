e2efa7444f1ddf6006b4cc6cd15d77d3309e4608
Robert Kohnke <rkohnke@gitlab.com>
2019-08-26 21:40:20 +0000

    Updated the pathfactory field formatting in  sfdc_lead.sql

--- transform/snowflake-dbt/models/sfdc/base/sfdc_lead.sql
+++ transform/snowflake-dbt/models/sfdc/base/sfdc_lead.sql
@@ -84,12 +84,17 @@ WITH source AS (
 
 
         --path factory info
-        pathfactory_experience_name__c AS pathfactory_experience_name
-        pathfactory_engagement_score__c AS pathfactory_engagement_score
-        pathfactory_content_count__c   AS pathfactory_content_count
-        pathfactory_content_list__c   AS pathfactory_content_list
-        pathfactory_content_journey__c AS pathfactory_content_journey
-        pathfactory_topic_list__c    AS pathfactory_topic_list
+        pathfactory_experience_name__c   
+                                    AS pathfactory_experience_name
+        pathfactory_engagement_score__c   
+                                    AS pathfactory_engagement_score
+        pathfactory_content_count__c   
+                                    AS pathfactory_content_count
+        pathfactory_content_list__c   
+                                    AS pathfactory_content_list
+        pathfactory_content_journey__c   
+                                    AS pathfactory_content_journey
+        pathfactory_topic_list__c   AS pathfactory_topic_list
 
         --gitlab internal
 
