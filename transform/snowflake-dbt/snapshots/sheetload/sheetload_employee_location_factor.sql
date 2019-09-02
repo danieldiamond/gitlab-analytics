21e3136d57977dd0b1fbeb2cce50806ce74fb08d
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2019-09-02 19:44:34 +0000

    Resolve "Fix snapshots again"

--- transform/snowflake-dbt/snapshots/sheetload/sheetload_employee_location_factor.sql
+++ transform/snowflake-dbt/snapshots/sheetload/sheetload_employee_location_factor.sql
@@ -2,10 +2,8 @@
 
     {{
         config(
-          target_database=env_var("SNOWFLAKE_LOAD_DATABASE"),
+          target_database='RAW',
           target_schema='snapshots',
-          materialized='table', 
-          transient=false,
           unique_key='"Employee_ID"',
           strategy='timestamp',
           updated_at='_UPDATED_AT',
