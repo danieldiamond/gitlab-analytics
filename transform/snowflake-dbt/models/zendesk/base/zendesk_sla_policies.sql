015db813e6535d8c2509a164eb65e9dfd72e0254
emilie <emilie.burke@gmail.com>
2019-09-03 15:49:29 -0400

    add test

---
+++ transform/snowflake-dbt/models/zendesk/base/zendesk_sla_policies.sql
@@ -0,0 +1,34 @@
+{{config({
+    "schema": "staging"
+  })
+}}
+
+with source as (
+
+    SELECT *
+    FROM {{ source('zendesk', 'sla_policies') }}
+),
+
+renamed as (
+
+    SELECT
+
+        --ids
+        id                                                  AS organization_id,
+        organization_fields['salesforce_id']::varchar       AS sfdc_account_id,
+
+        --fields
+        name                                                AS organization_name,
+        organization_fields['aar']::integer                 AS arr,
+        organization_fields['market_segment']::varchar      AS organization_market_segment,
+
+        --dates
+        created_at,
+        updated_at
+
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
