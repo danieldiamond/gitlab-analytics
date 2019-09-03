bf7811a46b19efe128979a00f350f788d8916a39
mpeychet <mpeychet@gitlab.com>
2019-09-03 16:01:42 +0200

    change few things

--- transform/snowflake-dbt/models/snowplow/xf/snowplow_create_activation_events.sql
+++ transform/snowflake-dbt/models/snowplow/xf/snowplow_create_activation_events.sql
@@ -22,8 +22,8 @@ WITH snowplow_page_views AS (
     page_view_start,
     page_url_path,
     page_view_id
-  FROM analytics.snowplow_page_views
-  WHERE page_view_start >= '2019-07-01'
+  FROM {{ ref('snowplow_page_views')}}
+  WHERE TRUE
   {% if is_incremental() %}
     AND page_view_start >= (SELECT MAX(event_date) FROM {{this}})
   {% endif %}
