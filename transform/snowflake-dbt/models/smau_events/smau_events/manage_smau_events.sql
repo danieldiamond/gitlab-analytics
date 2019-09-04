892b1af7f024dfe293a046af27fbc09073978cd3
Eli Kastelein <ekastelein@gitlab.com>
2019-09-04 16:15:47 -0700

    Add unified union

---
+++ transform/snowflake-dbt/models/smau_events/smau_events/manage_smau_events.sql
@@ -0,0 +1,49 @@
+WITH manage_snowplow_smau_events AS (
+  
+  SELECT
+    user_snowplow_domain_id,
+    user_custom_id      AS gitlab_user_id,
+    event_date,
+    event_type,
+    page_view_id        AS sk_id,
+    'snowplow_frontend' AS source_type
+  
+  FROM {{ ref('manage_snowplow_smau_events')}}
+  
+)
+
+, manage_gitlab_dotcom_smau_events AS (
+  
+  SELECT
+    NULL             AS user_snowplow_domain_id,
+    user_id          AS gitlab_user_id,
+    event_date,
+    event_type,
+    sk_id,
+    'gitlab_backend' AS source_type    
+
+  FROM {{ ref('manage_gitlab_dotcom_smau_events')}}
+  
+)
+
+, unioned AS (
+  
+  (
+    
+    SELECT *
+    FROM manage_snowplow_smau_events
+    
+  )
+  
+  UNION 
+  
+  (
+    
+    SELECT *
+    FROM manage_gitlab_dotcom_smau_events
+    
+  )
+)
+
+SELECT * 
+FROM unioned
