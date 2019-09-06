39b500b06cc297e6a78c0113606213cad4085243
mpeychet <mpeychet@gitlab.com>
2019-09-06 10:34:24 +0200

    rrenaming the different files to adjust to new model structure

--- transform/snowflake-dbt/models/smau_events/smau_events/plan_smau_events.sql
+++ transform/snowflake-dbt/models/smau_events/smau_events/plan_smau_events.sql
@@ -1,4 +1,4 @@
-WITH snowplow_create_activation_events AS (
+WITH plan_snowplow_smau_events AS (
   
   SELECT
     user_snowplow_domain_id,
@@ -8,9 +8,9 @@ WITH snowplow_create_activation_events AS (
     page_view_id        AS sk_id,
     'snowplow_frontend' AS source_type
   
-  FROM {{ ref('snowplow_plan_activation_events')}}
+  FROM {{ ref('plan_snowplow_smau_events')}}
   
 )
 
 SELECT * 
-FROM snowplow_create_activation_events
+FROM plan_snowplow_smau_events
