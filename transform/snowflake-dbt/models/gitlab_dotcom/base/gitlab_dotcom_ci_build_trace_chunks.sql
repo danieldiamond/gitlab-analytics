9b3d87ccc2ad7303f3900836396c653aabcb4e30
mpeychet <mpeychet@gitlab.com>
2019-09-06 09:34:49 +0200

    some base models added

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_ci_build_trace_chunks.sql
@@ -0,0 +1,25 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+	SELECT *
+  FROM {{ source('gitlab_dotcom', 'ci_build_trace_chunks') }}
+
+), renamed AS (
+
+    SELECT
+      build_id::INTEGER AS ci_build_id,
+      chunk_index       AS ci_build_trace_chunk_index,
+      data_store        AS ci_build_trace_chunk_data_store,
+      raw_data          AS ci_build_trace_chunk_raw_data
+
+    FROM source
+
+)
+
+
+SELECT *
+FROM renamed
---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_ci_group_variables.sql
@@ -0,0 +1,31 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+	SELECT *,
+				ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) as rank_in_key
+  FROM {{ source('gitlab_dotcom', 'ci_group_variables') }}
+
+), renamed AS (
+
+  SELECT 
+    id::INTEGER             AS ci_group_variable_id, 
+    key                     AS ci_group_variable_key, 
+    value                   AS ci_group_variable_value, 
+    group_id::INTEGER       AS ci_group_variable_group_id, 
+    created_at::TIMESTAMP   AS ci_group_variable_created_at, 
+    updated_at::TIMESTAMP   AS ci_group_variable_updated_at, 
+    masked                  AS ci_group_variable_masked, 
+    variable_type           AS ci_group_variable_variable_type 
+  FROM ci_group_variables 
+  FROM source
+  WHERE rank_in_key = 1
+
+)
+
+
+SELECT *
+FROM renamed
