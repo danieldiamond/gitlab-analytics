35513567499d83ad639018e142511ae9c5be5873
iweeks <iweeks@gitlab.com>
2019-09-09 11:05:08 -0400

    zuora refund model 2

--- transform/snowflake-dbt/models/zuora/xf/zuora_base_refund.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_base_refund.sql
@@ -14,7 +14,10 @@ SELECT zuora_account.sfdc_entity               AS entity,
        zuora_account.account_number,
        zuora_account.currency,
        date_trunc('month',refund_date)::DATE   AS refund_month,
-       amount                                  AS refund_amount
+       amount                                  AS refund_amount,
+			 comment,
+			 reason_code,
+			 zuora_refund_base.status
 FROM zuora_refund_base
 LEFT JOIN zuora_account
   ON zuora_refund_base.account_id = zuora_account.account_id
