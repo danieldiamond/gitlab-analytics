978215467015bf61f12bc7dc53c784e8501afcd6
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2019-09-17 10:22:27 +0000

    Alignment

--- transform/snowflake-dbt/models/zuora/xf/zuora_refund_xf.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_refund_xf.sql
@@ -15,10 +15,10 @@ SELECT zuora_account.sfdc_entity,
        zuora_account.currency,
        date_trunc('month',refund_date)::DATE   AS refund_month,
        amount                                  AS refund_amount,
-			 comment,
-			 reason_code,
-			 is_deleted,
-			 zuora_refund_base.refund_status
+	   comment,
+	   reason_code,
+	   is_deleted,
+	   zuora_refund_base.refund_status
 FROM zuora_refund_base
 LEFT JOIN zuora_account
   ON zuora_refund_base.account_id = zuora_account.account_id
