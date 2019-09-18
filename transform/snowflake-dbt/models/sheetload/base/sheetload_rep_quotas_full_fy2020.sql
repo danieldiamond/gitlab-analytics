2582ac0d523680495c989ee605ba0a1f963ca336
Kathleen Tam <ktam@gitlab.com>
2019-09-18 16:09:53 +0000

    Update sheetload_rep_quotas_full_fy2020.sql to have space for breathing and AS lined up as much as possible

--- transform/snowflake-dbt/models/sheetload/base/sheetload_rep_quotas_full_fy2020.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_rep_quotas_full_fy2020.sql
@@ -1,15 +1,20 @@
 WITH source AS (
+
     SELECT * 
     FROM {{ source('sheetload','rep_quotas_full_fy2020') }}
-), final AS (
+    
+),    final AS (
+
     SELECT 
       sales_rep,
       type, 
       team,
       calendar_month::DATE AS calendar_month,
-      fiscal_quarter::INT(1) AS fiscal_quarter,
-      fiscal_year::INT(4) AS fiscal_year,
+      fiscal_quarter::INT  AS fiscal_quarter,
+      fiscal_year::INT     AS fiscal_year,
       ZEROIFNULL(NULLIF("FULL_QUOTA",'')::DECIMAL(16,5)) AS full_quota
+      
 ) 
+
 SELECT * 
 FROM final
