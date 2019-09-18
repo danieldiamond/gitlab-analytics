8bd11082daa78b22020217c467facdaa5bcdb11e
Taylor A Murphy, PhD <tmurphy@gitlab.com>
2019-09-18 18:25:29 +0000

    Dropped a FROM

--- transform/snowflake-dbt/models/sheetload/base/sheetload_rep_quotas_full_fy2020.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_rep_quotas_full_fy2020.sql
@@ -3,16 +3,17 @@ WITH source AS (
     SELECT * 
     FROM {{ source('sheetload','rep_quotas_full_fy2020') }}
     
-),    final AS (
+), final AS (
 
     SELECT 
       sales_rep,
       type, 
       team,
-      calendar_month::DATE AS calendar_month,
-      fiscal_quarter::INT  AS fiscal_quarter,
-      fiscal_year::INT     AS fiscal_year,
-      ZEROIFNULL(NULLIF("FULL_QUOTA",'')::DECIMAL(16,5)) AS full_quota
+      calendar_month::DATE                                  AS calendar_month,
+      fiscal_quarter::INT                                   AS fiscal_quarter,
+      fiscal_year::INT                                      AS fiscal_year,
+      ZEROIFNULL(NULLIF("FULL_QUOTA",'')::DECIMAL(16,5))    AS full_quota
+    FROM source
       
 ) 
 
