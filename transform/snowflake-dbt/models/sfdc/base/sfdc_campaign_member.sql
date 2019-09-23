87c7d5f9fcd0b3c762d236d2c4e11c7c00b9ca50
Robert Kohnke <rkohnke@gitlab.com>
2019-09-23 21:55:09 +0000

    cast statements as data types

--- transform/snowflake-dbt/models/sfdc/base/sfdc_campaign_member.sql
+++ transform/snowflake-dbt/models/sfdc/base/sfdc_campaign_member.sql
@@ -12,20 +12,20 @@ WITH source AS (
 ), renamed AS(
 
     SELECT
-      id                      AS campaign_member_id,
+      id                               AS campaign_member_id,
 
         --keys
-      campaignid              AS campaign_id,
-      leadorcontactid         AS lead_or_contact_id,
+      campaignid                       AS campaign_id,
+      leadorcontactid                  AS lead_or_contact_id,
 
         --info
-      type                    AS campaign_member_type,
-      hasresponded            AS campaign_member_has_responded,
-      firstrespondeddate      AS campaign_member_response_date,
-      mql_after_campaign__c   AS mql_after_campaign,
+      type                             AS campaign_member_type,
+      hasresponded::boolean            AS campaign_member_has_responded,
+      firstrespondeddate::date         AS campaign_member_response_date,
+      mql_after_campaign__c::boolean   AS is_mql_after_campaign,
 
         --metadata
-      createddate             AS campaign_member_created_date,
+      createddate::date                AS campaign_member_created_date,
       systemmodstamp
 
     FROM source
