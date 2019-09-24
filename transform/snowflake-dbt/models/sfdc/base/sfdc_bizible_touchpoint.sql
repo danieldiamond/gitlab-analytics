2acf766cfa488841cc0387723ad4479ceefb7f6a
Robert Kohnke <rkohnke@gitlab.com>
2019-09-24 20:20:10 +0000

    Updating and organizing the Bizible DBT models with new fields and structured groupings.

--- transform/snowflake-dbt/models/sfdc/base/sfdc_bizible_touchpoint.sql
+++ transform/snowflake-dbt/models/sfdc/base/sfdc_bizible_touchpoint.sql
@@ -9,23 +9,33 @@ WITH source AS (
     SELECT
       id                                      AS bizible_touchpoint_id,
       bizible2__bizible_person__c             AS bizible_person_id,
-      bizible2__marketing_channel__c          AS bizible_marketing_channel,
-      bizible2__marketing_channel_path__c     AS bizible_marketing_channel_path,
+
+      -- sfdc object lookups
+      bizible2__sf_campaign__c                AS campaign_id,
+      bizible2__opportunity__c                AS opportunity_id,
+      bizible2__contact__c                    AS bizible_contact,
+      bizible2__account__c                    AS bizible_account,      
+      
+      -- attribution counts
       bizible2__count_first_touch__c          AS bizible_count_first_touch,
       bizible2__count_lead_creation_touch__c  AS bizible_count_lead_creation_touch,
-      bizible2__touchpoint_source__c          AS bizible_touchpoint_source,
+      bizible2__count_custom_model__c         AS bizible_attribution_percent_full_path,
+      bizible2__count_u_shaped__c             AS bizible_count_u_shaped,
+
+      -- touchpoint info
       bizible2__touchpoint_date__c            AS bizible_touchpoint_date,
-      bizible2__touchpoint_type__c            AS bizible_touchpoint_type,
-      bizible2__landing_page__c               AS bizible_landing_page,
+      bizible2__touchpoint_position__c        AS bizible_touchpoint_position,
+      bizible2__touchpoint_source__c          AS bizible_touchpoint_source,
+      bizible2__touchpoint_type__c            AS bizible_touchpoint_type,      
       bizible2__ad_campaign_name__c           AS bizible_ad_campaign_name,
-      bizible2__medium__c                     AS bizible_medium,
-      bizible2__contact__c                    AS bizible_contact,
-      bizible2__account__c                    AS bizible_account,
-      bizible2__sf_campaign__c                AS bizible_salesforce_campaign,
       bizible2__ad_group_name__c              AS bizible_ad_group_name,
       bizible2__form_url__c                   AS bizible_form_url,
-      bizible2__count_u_shaped__c             AS bizible_count_u_shaped,
-      bizible2__touchpoint_position__c        AS bizible_touchpoint_position
+      bizible2__landing_page__c               AS bizible_landing_page,
+      bizible2__marketing_channel__c          AS bizible_marketing_channel,
+      bizible2__marketing_channel_path__c     AS bizible_marketing_channel_path,
+      bizible2__medium__c                     AS bizible_medium, 
+      bizible2__referrer_page__c              AS bizible_referrer_page,   
+      bizible2__sf_campaign__c                AS bizible_salesforce_campaign  
 
 
     FROM source
