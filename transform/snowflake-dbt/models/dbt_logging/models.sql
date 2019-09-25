2407da50dbe32d13b60a93857a3be541b51ece75
Taylor A. Murphy <tmurphy@gitlab.com>
2019-09-25 16:58:19 -0500

    remove dbt_logging and alphabetize

--- transform/snowflake-dbt/models/dbt_logging/models.sql
+++
@@ -1,7 +0,0 @@
-SELECT [deployment_started_at:aggregation] AS period, 
-		model, 
-		count(1) AS deployment_count
-FROM  {{ ref('stg_dbt_model_deployments') }}
-WHERE [deployment_started_at=daterange]
-GROUP BY 1, 2
-ORDER BY 1 DESC, 2
