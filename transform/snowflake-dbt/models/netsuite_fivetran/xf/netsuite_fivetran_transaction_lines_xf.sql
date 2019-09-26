7dce62c4bcd0208f65410a3f010403202754860e
iweeks <iweeks@gitlab.com>
2019-09-26 15:39:43 -0400

    netsuite model 2

--- transform/snowflake-dbt/models/netsuite_fivetran/xf/netsuite_fivetran_transaction_lines_xf.sql
+++ transform/snowflake-dbt/models/netsuite_fivetran/xf/netsuite_fivetran_transaction_lines_xf.sql
@@ -7,35 +7,35 @@
 
 WITH base AS  (
 
-  SELECT * FROM {{ref('netsuite_fivetran_transaction_lines')}}
+  SELECT * FROM {{ref('netsuite_transaction_lines')}}
 
 ), netsuite_fivetran_accounts AS (
 
-  SELECT * FROM {{ref('netsuite_fivetran_accounts')}}
+  SELECT * FROM {{ref('netsuite_accounts')}}
 
 ), netsuite_fivetran_subsidiaries AS (
 
-  SELECT * FROM {{ref('netsuite_fivetran_subsidiaries')}}
+  SELECT * FROM {{ref('netsuite_subsidiaries')}}
 
 )
 
-SELECT {{ dbt_utils.star(from=ref('netsuite_fivetran_transaction_lines'),
+SELECT {{ dbt_utils.star(from=ref('netsuite_transaction_lines'),
             except=['MEMO'], relation_alias='base') }},
-        netsuite_fivetran_accounts.account_name,
-        netsuite_fivetran_accounts.account_full_name,
-        netsuite_fivetran_accounts.account_full_description,
-        netsuite_fivetran_accounts.account_number,
-        netsuite_fivetran_accounts.account_type,
+        netsuite_accounts.account_name,
+        netsuite_accounts.account_full_name,
+        netsuite_accounts.account_full_description,
+        netsuite_accounts.account_number,
+        netsuite_accounts.account_type,
         CASE
-            WHEN lower(netsuite_fivetran_accounts.account_name) LIKE '%contract%'
-              THEN substring(md5(netsuite_fivetran_subsidiaries.subsidiary_name), 16)
-            ELSE netsuite_fivetran_subsidiaries.subsidiary_name END AS subsidiary_name,
+            WHEN lower(netsuite_accounts.account_name) LIKE '%contract%'
+              THEN substring(md5(netsuite_subsidiaries.subsidiary_name), 16)
+            ELSE netsuite_subsidiaries.subsidiary_name END AS subsidiary_name,
         CASE
-            WHEN lower(netsuite_fivetran_accounts.account_name) LIKE '%contract%'
+            WHEN lower(netsuite_accounts.account_name) LIKE '%contract%'
               THEN substring(md5(base.memo),16)
             ELSE base.memo END                                      AS memo
 FROM base
-LEFT JOIN netsuite_fivetran_accounts
-  ON base.account_id = netsuite_fivetran_accounts.account_id
-LEFT JOIN netsuite_fivetran_subsidiaries
-  ON base.subsidiary_id = netsuite_fivetran_subsidiaries.subsidiary_id
+LEFT JOIN netsuite_accounts
+  ON base.account_id = netsuite_accounts.account_id
+LEFT JOIN netsuite_subsidiaries
+  ON base.subsidiary_id = netsuite_subsidiaries.subsidiary_id
