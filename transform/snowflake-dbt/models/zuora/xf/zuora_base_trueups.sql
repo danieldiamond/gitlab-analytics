0219a2561598bcbcf5c2ef76e645dce2bb9fabdc
mpeychet <mpeychet@gitlab.com>
2019-09-26 17:22:38 +0200

    add final at the end

--- transform/snowflake-dbt/models/zuora/xf/zuora_base_trueups.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_base_trueups.sql
@@ -6,22 +6,28 @@ WITH invoice_details AS (
 
 )
 
+, final AS (
+  
+  SELECT
+    country,
+    account_number,
+    subscription_name,
+    subscription_name_slugify,
+    oldest_subscription_in_cohort,
+    lineage,
+    cohort_month,
+    cohort_quarter,
+    service_month                 AS trueup_month,
+    charge_name,
+    service_start_date,
+    charge_amount,
+    charge_amount/12              AS mrr,
+    unit_of_measure,
+    unit_price
 
-SELECT
-  country,
-  account_number,
-  subscription_name,
-  subscription_name_slugify,
-  oldest_subscription_in_cohort,
-  lineage,
-  cohort_month,
-  cohort_quarter,
-  service_month                 AS trueup_month,
-  charge_name,
-  service_start_date,
-  charge_amount,
-  charge_amount/12              AS mrr,
-  unit_of_measure,
-  unit_price
+  FROM invoice_details
 
-FROM invoice_details
+)
+
+SELECT * 
+FROM final
