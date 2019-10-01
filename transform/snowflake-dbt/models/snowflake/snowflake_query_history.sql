301b4e1db01024851b09af577d330683eb25d182
Justin Stark <jjamesstark@gmail.com>
2019-10-01 10:14:24 -0600

    dedupe and naming

--- transform/snowflake-dbt/models/snowflake/snowflake_query_history.sql
+++ transform/snowflake-dbt/models/snowflake/snowflake_query_history.sql
@@ -5,35 +5,42 @@
     )
 }}
 
-with source AS(
-    SELECT *
+WITH source AS(
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY query_id ORDER BY query_id) AS dupcnt
     FROM {{source('snowflake_account_usage','query_history')}}
 )
 
-SELECT 
-query_id 			AS snowflake_query_id,
+, deduped AS(
+    SELECT *
+    FROM source
+    WHERE dupcnt = 1
+) 
 
--- Foreign Keys
-database_id			AS snowflake_database_id,
-schema_id			AS snowflake_schema_id,
-session_id			AS snowflake_session_id,
-warehouse_id		AS snowflake_warehouse_id,
+, renamed AS (
 
+    SELECT 
+        query_id 			AS snowflake_query_id,
 
--- Logical Info
-database_name		AS snowflake_database_name,
-query_text			AS snowflake_query_text,
-role_name			AS snowflake_role_name,
-rows_produced		AS snowflake_rows_produced,
-schema_name			AS snowflake_schema_name,
-user_name			AS snowflake_user_name,
-warehouse_name		AS snowflake_warehouse_name,
+        -- Foreign Keys
+        database_id			AS database_id,
+        schema_id			AS schema_id,
+        session_id			AS snowflake_session_id,
+        warehouse_id		AS warehouse_id,
 
+        -- Logical Info
+        database_name		AS database_name,
+        query_text			AS query_text,
+        role_name			AS snowflake_role_name,
+        rows_produced		AS rows_produced,
+        schema_name			AS schema_name,
+        user_name			AS snowflake_user_name,
+        warehouse_name		AS warehouse_name,
 
--- metadata 
-end_time			AS snowflake_query_end_time,
-start_time			AS snowflake_query_start_time
-FROM source
+        -- metadata 
+        end_time			AS query_end_time,
+        start_time			AS query_start_time
+    FROM deduped
 
 
 {% if is_incremental() %}
@@ -42,3 +49,8 @@ FROM source
   WHERE snowflake_query_start_time > (SELECT MAX(snowflake_query_start_time) from {{ this }})
 
 {% endif %}
+
+)
+
+SELECT * 
+from renamed
