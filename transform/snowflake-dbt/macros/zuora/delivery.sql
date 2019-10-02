bc2f1f6da3cf7df026d9fa62305d02c37739d807
mpeychet <mpeychet@gitlab.com>
2019-10-02 12:33:27 +0200

    change macro

--- transform/snowflake-dbt/macros/zuora/delivery.sql
+++ transform/snowflake-dbt/macros/zuora/delivery.sql
@@ -5,7 +5,7 @@ CASE
     THEN 'GitLab.com'
   WHEN {{product_category_column}} IN ('Ultimate', 'Premium', 'Starter')
     THEN 'self-managed'
-  WHEN {{product_category_column}} IN ('Basic', 'GitHost', 'Other', 'Plus', 'Standard', 'Support')
+  WHEN {{product_category_column}} IN ('Basic', 'GitHost', 'Other', 'Plus', 'Standard', 'Support', 'Trueup')
     THEN 'Others'
 END AS {{output_column_name}}
 
