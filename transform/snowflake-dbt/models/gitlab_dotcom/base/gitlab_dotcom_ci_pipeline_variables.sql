e4370a0a184c6eb525e83dc7bed47666a6237b8f
Eli Kastelein <ekastelein@gitlab.com>
2019-10-04 11:16:43 -0700

    Pass run

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_ci_pipeline_variables.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_ci_pipeline_variables.sql
@@ -14,8 +14,6 @@ WITH source AS (
       id::INTEGER          AS ci_pipeline_variable_id, 
       key                  AS key, 
       value                AS value, 
-      encrypted_value_salt AS encrypted_value_salt, 
-      encrypted_value_iv   AS encrypted_value_iv, 
       pipeline_id::INTEGER AS ci_pipeline_id, 
       variable_type        AS variable_type
 
