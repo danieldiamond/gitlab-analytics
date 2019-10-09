2e009f80e60842304c62fd0be0fa7aef24ec9e94
mpeychet <mpeychet@gitlab.com>
2019-10-09 11:09:28 +0200

    create the surrogate key

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_resource_label_events.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_resource_label_events.sql
@@ -9,10 +9,22 @@ WITH source AS (
 		*
 	FROM {{ source('gitlab_dotcom', 'resource_label_events') }}
 
-), renamed AS (
+), deduplicated AS (
+	
+	SELECT
+	  *,
+		{{ dbt_utils.surrogate_key('action', 'issue_id', 
+															   'merge_request_id', 'epic_id', 
+																 'label_id', 'user_id', 'created_at') }}    AS surrogate_key_id,
+		ROW_NUMBER() OVER (PARTITION BY surrogate_key_id ORDER BY _uploaded_at) AS rank_in_key
+		
+	FROM source
+)
+
+, renamed AS (
 
 	SELECT
-		action::INTEGER                  AS action_id,
+		action::INTEGER                  AS action_type_id,
 		issue_id::INTEGER                AS issue_id,
 		merge_request_id::INTEGER        AS merge_request_id,
 		epic_id::INTEGER                 AS epic_id,
@@ -23,7 +35,8 @@ WITH source AS (
 		reference::VARCHAR               AS referrence,
 		reference_html::VARCHAR          AS reference_html
 		
-	FROM source
+	FROM deduplicated
+	WHERE rank_in_key = 1
 
 )
 
