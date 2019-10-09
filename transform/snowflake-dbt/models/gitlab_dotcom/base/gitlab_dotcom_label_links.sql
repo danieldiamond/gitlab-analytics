35134e6862c75becec1a1c070063daa56dd74e49
Eli Kastelein <ekastelein@gitlab.com>
2019-10-09 10:19:36 -0700

    Keep rank_in_key

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_label_links.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_label_links.sql
@@ -7,6 +7,7 @@ WITH source AS (
 
   SELECT
     *,
+    ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC)             AS rank_in_key,
     DENSE_RANK() OVER (ORDER BY DATEADD(S, _uploaded_at, '1970-01-01')::DATE ) AS rank_in_uploaded_date
   FROM {{ source('gitlab_dotcom', 'label_links') }}
 
@@ -22,7 +23,8 @@ WITH source AS (
       updated_at::TIMESTAMP                          AS label_link_updated_at
 
     FROM source
-    WHERE rank_in_uploaded_date = 1
+    WHERE rank_in_key = 1
+      AND rank_in_uploaded_date = 1
 
 )
 
