58855180900d26f1d005c28489e22c39892d92f4
Taylor A. Murphy <tmurphy@gitlab.com>
2019-10-11 08:42:27 -0500

    lint sql

--- transform/snowflake-dbt/models/sheetload/base/sheetload_osat.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_osat.sql
@@ -6,19 +6,14 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-    TRY_TO_TIMESTAMP_NTZ(timestamp)::DATE                 AS completed_date,
-    'Anonymous'                                           AS employee_name, 
-    ZEROIFNULL(NULLIF("SATISFACTION_SCORE",''))::INTEGER  AS satisfaction_score,
-    ZEROIFNULL(NULLIF("RECOMMEND_TO_FRIEND",''))::INTEGER AS recommend_to_friend
+        TRY_TO_TIMESTAMP_NTZ(timestamp)::DATE                 AS completed_date,
+        'Anonymous'                                           AS employee_name, 
+        ZEROIFNULL(NULLIF("SATISFACTION_SCORE",''))::INTEGER  AS satisfaction_score,
+        ZEROIFNULL(NULLIF("RECOMMEND_TO_FRIEND",''))::INTEGER AS recommend_to_friend
     FROM source
       
 ) 
 
-
-SELECT
-    completed_date, 
-    employee_name,
-    satisfaction_score, 
-    recommend_to_friend
+SELECT *
 FROM renamed
 WHERE completed_date is not NULL
