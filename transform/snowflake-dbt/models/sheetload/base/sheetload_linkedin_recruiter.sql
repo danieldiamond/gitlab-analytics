7b193236beac27a218f77b60891c9d5e49882306
Kathleen Tam <ktam@gitlab.com>
2019-10-15 20:36:10 +0000

    Add in sql script for linkedin_recruiter

---
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_linkedin_recruiter.sql
@@ -0,0 +1,25 @@
+WITH source AS (
+
+    SELECT * 
+    FROM {{ source('sheetload','linkedin_recruiter') }}
+    
+), renamed AS (
+
+    SELECT 
+      seat_holder::VARCHAR                                 AS sourcer,
+      ZEROIFNULL(NULLIF("MESSAGES_SENT",''))::INTEGER      AS messages_sent,
+      ZEROIFNULL(NULLIF("RESPONSES_RECEIVED",''))::INTEGER AS responses_received,
+      ZEROIFNULL(NULLIF("ACCEPTANCES",''))::INTEGER        AS acceptances,
+      ZEROIFNULL(NULLIF("DECLINES",''))::INTEGER           AS declines,
+      ZEROIFNULL(NULLIF("NO_RESPONSE",''))::INTEGER        AS no_response,
+      ZEROIFNULL(NULLIF("RESPONSES_RATE",''))::INTEGER     AS responses_rate,
+      ZEROIFNULL(NULLIF("ACCEPT_RATE",''))::INTEGER        AS accept_rate,
+      month::DATE                                          AS month
+    FROM source
+      
+) 
+
+SELECT *
+FROM renamed
+WHERE month is not NULL 
+  AND sourcer IS NOT NULL; 
