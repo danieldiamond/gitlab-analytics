d85c6c2eb2fb216516abef55329ea7ad12b5ec5c
iweeks <iweeks@gitlab.com>
2019-10-16 13:33:54 -0400

    fix formatting 2

--- transform/snowflake-dbt/models/zuora/xf/zuora_refund_xf.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_refund_xf.sql
@@ -10,12 +10,12 @@ WITH zuora_refund_base AS (
 )
 
 SELECT
-  za.sfdc_entity,
-  za.account_name,
-  za.account_number,
-  za.currency,
-  date_trunc('month',zr.refund_date)::DATE   AS refund_month,
-  zr.refund_amount,
+	za.sfdc_entity,
+	za.account_name,
+	za.account_number,
+	za.currency,
+	date_trunc('month',zr.refund_date)::DATE   AS refund_month,
+	zr.refund_amount,
 	zr.comment,
 	zr.reason_code,
 	zr.source_type,
