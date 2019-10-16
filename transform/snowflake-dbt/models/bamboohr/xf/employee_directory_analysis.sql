ec49e6a661cba1a3690166e360fc9586cec0ee0a
emilie <emilie.burke@gmail.com>
2019-10-16 19:26:46 -0400

    get models working

--- transform/snowflake-dbt/models/bamboohr/xf/employee_directory_analysis.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/employee_directory_analysis.sql
@@ -6,69 +6,10 @@
 
 {% set max_date_in_analysis = "date_trunc('week', dateadd(week, 3, CURRENT_DATE))" %}
 
-WITH employee_directory AS (
+WITH employee_directory_intermediate AS (
 
-    SELECT
-      employee_id,
-      employee_number,
-      first_name,
-      last_name,
-      work_email,
-      hire_date,
-      termination_date,
-      hire_location_factor
-    FROM {{ ref('employee_directory') }}
+   SELECT * FROM {{ref('employee_directory_intermediate')}}
 
-), date_details AS (
-
-    SELECT * FROM {{ ref('date_details') }}
-
-), department_info AS (
-
-    SELECT employee_id,
-          job_title,
-          effective_date,
-          department,
-          division,
-          reports_to,
-          effective_end_date
-    FROM {{ ref('bamboohr_job_info') }}
-
-
-), location_factor AS (
-
-  SELECT *
-  FROM {{ ref('employee_location_factor_snapshots') }}
-
-), cost_center as (
-
-    SELECT *
-    FROM {{ref('cost_center_division_department_mapping')}}
-
-), joined AS (
-
-    SELECT  date_actual,
-            employee_directory.*,
-            department_info.job_title,
-            department_info.department,
-            department_info.division,
-            department_info.reports_to,
-            location_factor.location_factor,
-            CASE WHEN hire_date = date_actual THEN True ELSE False END AS is_hire_date,
-            CASE WHEN termination_date = dateadd('day', 1, date_actual) THEN True ELSE False END AS is_termination_date
-    FROM date_details
-    LEFT JOIN employee_directory
-      ON hire_date::date <= date_actual
-      AND COALESCE(termination_date::date, {{max_date_in_analysis}}) > date_actual
-    LEFT JOIN department_info
-      ON employee_directory.employee_id = department_info.employee_id
-      AND effective_date <= date_actual
-      AND COALESCE(effective_end_date::date, {{max_date_in_analysis}}) > date_actual
-    LEFT JOIN location_factor
-      ON employee_directory.employee_number::varchar = location_factor.bamboo_employee_number::varchar
-      AND valid_from <= date_actual
-      AND COALESCE(valid_to::date, {{max_date_in_analysis}}) > date_actual
-    WHERE employee_directory.employee_id IS NOT NULL
 
 ), cleaned as (
 
@@ -94,7 +35,7 @@ WITH employee_directory AS (
             is_hire_date,
             is_termination_date,
             hire_date
-    FROM joined
+    FROM employee_directory_intermediate
 
 ), final as (
 
--- transform/snowflake-dbt/models/snapshots/employee_location_factor_snapshots.sql
+++ transform/snowflake-dbt/models/snapshots/employee_location_factor_snapshots.sql
@@ -8,12 +8,15 @@ with source as (
 
     SELECT *
     FROM {{ source('snapshots', 'sheetload_employee_location_factor_snapshots') }}
+    WHERE dbt_scd_id != 'fd3dee5ab322f68692b97130f2bfefab'
+    AND "Employee_ID" != 'Not In Comp Calc'
+    AND "Employee_ID" NOT IN ('$72,124')
 
 ), renamed as (
 
     SELECT
          nullif("Employee_ID",'')::varchar as bamboo_employee_number,
-         nullif("Location_Factor",'')::float as location_factor,
+         nullif("Location_Factor",'') as location_factor,
          CASE WHEN "DBT_VALID_FROM"::number::timestamp::date < '2019-07-20'::date
              THEN '2000-01-20'::date
              ELSE "DBT_VALID_FROM"::number::timestamp::date END AS valid_from,
@@ -25,7 +28,7 @@ with source as (
 ), deduplicated as (
 
 SELECT bamboo_employee_number::bigint as bamboo_employee_number,
-        location_factor,
+        location_factor::float as location_factor,
         valid_from,
         valid_to
 FROM renamed
