09fb851ed4db0b2fd4c6ed940551098cf1b48619
Eli Kastelein <ekastelein@gitlab.com>
2019-10-16 09:39:49 -0700

    Coalesce and add tests

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_namespaces.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_namespaces.sql
@@ -63,7 +63,7 @@ WITH source AS (
 
   SELECT
     namespaces.*,
-    gitlab_subscriptions.plan_id
+    COALESCE(gitlab_subscriptions.plan_id, 34) AS plan_id
   FROM namespaces
     LEFT JOIN gitlab_subscriptions
       ON namespaces.namespace_id = gitlab_subscriptions.namespace_id
