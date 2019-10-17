9416d4a56831c7447a4174e1c852f59e9f731133
emilie <emilie.burke@gmail.com>
2019-10-17 12:12:33 -0400

    add weighted models

--- transform/snowflake-dbt/models/bamboohr/rpt/rpt_team_members_out_of_comp_band.sql
+++ transform/snowflake-dbt/models/bamboohr/rpt/rpt_team_members_out_of_comp_band.sql
@@ -20,7 +20,11 @@ WITH employee_directory_intermediate AS (
 
   SELECT employee_directory_intermediate.*,
           comp_band.deviation_from_comp_calc,
-          CASE WHEN comp_band.deviation_from_comp_calc < 
+          CASE WHEN comp_band.deviation_from_comp_calc <= 0 THEN 0
+               WHEN comp_band.deviation_from_comp_calc < 0.01 THEN 0.25
+               WHEN comp_band.deviation_from_comp_calc < 0.05 THEN 0.5
+               WHEN comp_band.deviation_from_comp_calc < 0.1 THEN 0.75
+               ELSE 1 END as weighted_deviated_from_comp_calc
   FROM employee_directory_intermediate
   LEFT JOIN comp_band
     ON employee_directory_intermediate.employee_number::varchar = comp_band.bamboo_employee_number::varchar
@@ -31,8 +35,8 @@ WITH employee_directory_intermediate AS (
 
   SELECT
     date_actual,
-
-    CASE WHEN
+    SUM(weighted_deviated_from_comp_calc)/
+      COUNT(distinct employee_number) as percent_of_employees_outside_of_band
   FROM joined
   WHERE date_actual < CURRENT_DATE
   GROUP BY 1
