fe6b0db20e6523881db007cdce13ec5ec975cfc0
Eli Kastelein <ekastelein@gitlab.com>
2019-10-22 14:57:31 -0700

    Restore file from old commit

---
+++ transform/snowflake-dbt/models/snapshots/base/license_db_licenses_snapshots_base.sql
@@ -0,0 +1,42 @@
+{{ config({
+    "schema": "staging",
+    "alias": "license_db_licenses_snapshots"
+    })
+}}
+
+WITH source AS (
+
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) AS rank_in_key
+  FROM {{ source('snapshots', 'license_db_licenses_snapshots') }}
+
+), renamed AS (
+
+  SELECT DISTINCT
+    dbt_scd_id::VARCHAR                AS license_snapshot_id,
+    id::INTEGER                        AS license_id,
+    company::VARCHAR                   AS company,
+    users_count::INTEGER               AS users_count,
+    email::VARCHAR                     AS email,
+    expires_at::TIMESTAMP              AS license_expires_at,
+    recurly_subscription_id::VARCHAR   AS recurly_subscription_id,
+    plan_name::VARCHAR                 AS plan_name,
+    starts_at::TIMESTAMP               AS starts_at,
+    zuora_subscription_id::VARCHAR     AS zuora_subscription_id,
+    previous_users_count::INTEGER      AS previous_users_count,
+    trueup_quantity::INTEGER           AS trueup_quantity,
+    trueup_from::TIMESTAMP             AS trueup_from,
+    trueup_to::TIMESTAMP               AS trueup_to,
+    plan_code::VARCHAR                 AS plan_code,
+    trial::BOOLEAN                     AS is_trial,
+    created_at::TIMESTAMP              AS created_at,
+    updated_at::TIMESTAMP              AS updated_at,
+    "DBT_VALID_FROM"::TIMESTAMP        AS valid_from,
+    "DBT_VALID_TO"::TIMESTAMP          AS valid_to
+ FROM source
+ WHERE rank_in_key = 1 
+)
+
+SELECT *
+FROM renamed
