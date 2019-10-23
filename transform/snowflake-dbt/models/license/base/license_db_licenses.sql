66e66a001d347919e99bcc63232e73454347c1c6
Eli Kastelein <ekastelein@gitlab.com>
2019-10-23 13:51:35 -0700

    Add NULLIF

--- transform/snowflake-dbt/models/license/base/license_db_licenses.sql
+++ transform/snowflake-dbt/models/license/base/license_db_licenses.sql
@@ -13,24 +13,24 @@ WITH source AS (
 ), renamed AS (
 
   SELECT DISTINCT
-    id::INTEGER                        AS license_id,
-    company::VARCHAR                   AS company,
-    users_count::INTEGER               AS users_count,
-    email::VARCHAR                     AS email,
-    license_md5::VARCHAR               AS license_md5,
-    expires_at::TIMESTAMP              AS license_expires_at,
-    recurly_subscription_id::VARCHAR   AS recurly_subscription_id,
-    plan_name::VARCHAR                 AS plan_name,
-    starts_at::TIMESTAMP               AS starts_at,
-    zuora_subscription_id::VARCHAR     AS zuora_subscription_id,
-    previous_users_count::INTEGER      AS previous_users_count,
-    trueup_quantity::INTEGER           AS trueup_quantity,
-    trueup_from::TIMESTAMP             AS trueup_from,
-    trueup_to::TIMESTAMP               AS trueup_to,
-    plan_code::VARCHAR                 AS plan_code,
-    trial::BOOLEAN                     AS is_trial,
-    created_at::TIMESTAMP              AS created_at,
-    updated_at::TIMESTAMP              AS updated_at
+    id::INTEGER                                  AS license_id,
+    company::VARCHAR                             AS company,
+    users_count::INTEGER                         AS users_count,
+    email::VARCHAR                               AS email,
+    license_md5::VARCHAR                         AS license_md5,
+    expires_at::TIMESTAMP                        AS license_expires_at,
+    recurly_subscription_id::VARCHAR             AS recurly_subscription_id,
+    plan_name::VARCHAR                           AS plan_name,
+    starts_at::TIMESTAMP                         AS starts_at,
+    NULLIF(zuora_subscription_id, '')::VARCHAR   AS zuora_subscription_id,
+    previous_users_count::INTEGER                AS previous_users_count,
+    trueup_quantity::INTEGER                     AS trueup_quantity,
+    trueup_from::TIMESTAMP                       AS trueup_from,
+    trueup_to::TIMESTAMP                         AS trueup_to,
+    plan_code::VARCHAR                           AS plan_code,
+    trial::BOOLEAN                               AS is_trial,
+    created_at::TIMESTAMP                        AS created_at,
+    updated_at::TIMESTAMP                        AS updated_at
  FROM source
  WHERE rank_in_key = 1
 
