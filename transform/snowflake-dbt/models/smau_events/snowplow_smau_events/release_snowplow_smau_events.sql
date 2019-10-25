592ab5b08a4e5af82656f82d951385499f8e3fa3
Eli Kastelein <ekastelein@gitlab.com>
2019-10-24 17:44:24 -0700

    Add regex filters draft

---
+++ transform/snowflake-dbt/models/smau_events/snowplow_smau_events/release_snowplow_smau_events.sql
@@ -0,0 +1,76 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": "event_surrogate_key"
+    })
+}}
+
+{%- set event_ctes = [
+    {
+      "event_name":"environment_page_viewed",
+      "regexp_where_statements":[
+         {
+            "regexp_pattern":"((\/([0-9A-Za-z_.-])*){2,})?\/environments$",
+            "regexp_function":"REGEXP"
+         }
+      ]
+   },
+   {
+      "event_name":"feature_flags_viewed",
+      "regexp_where_statements":[
+         {
+            "regexp_pattern":"((\/([0-9A-Za-z_.-])*){2,})?\/feature_flags$",
+            "regexp_function":"REGEXP"
+         }
+      ]
+   },
+    {
+      "event_name":"release_page_viewed",
+      "regexp_where_statements":[
+         {
+            "regexp_pattern":"((\/([0-9A-Za-z_.-])*){2,})?\/releases$",
+            "regexp_function":"REGEXP"
+         }
+      ]
+   },
+]
+-%}
+
+WITH snowplow_page_views AS (
+
+  SELECT
+    user_snowplow_domain_id,
+    user_custom_id,
+    page_view_start,
+    page_url_path,
+    page_view_id
+  FROM {{ ref('snowplow_page_views_30')}} --TODO
+  WHERE TRUE
+  {% if is_incremental() %}
+    AND page_view_start >= (SELECT MAX(event_date) FROM {{this}})
+  {% endif %}
+
+)
+
+{% for event_cte in event_ctes %}
+
+, {{ smau_events_ctes(event_name=event_cte.event_name, regexp_where_statements=event_cte.regexp_where_statements) }}
+
+{% endfor -%}
+
+, unioned AS (
+
+  {% for event_cte in event_ctes %}
+
+    SELECT *
+    FROM {{ event_cte.event_name }}
+
+    {%- if not loop.last %}
+      UNION
+    {%- endif %}
+
+  {% endfor -%}
+
+)
+
+SELECT *
+FROM unioned
