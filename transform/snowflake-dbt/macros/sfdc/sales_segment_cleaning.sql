b07fd327f78f287241f47f9915f582231b37cf92
Kathleen Tam <ktam@gitlab.com>
2019-10-28 18:55:10 +0000

    Add Temporary fix for the mid market issue.

--- transform/snowflake-dbt/macros/sfdc/sales_segment_cleaning.sql
+++ transform/snowflake-dbt/macros/sfdc/sales_segment_cleaning.sql
@@ -2,6 +2,7 @@
  
 CASE  WHEN lower({{ column_1 }}) = 'smb' THEN 'SMB' 
       WHEN lower({{ column_1 }}) = 'mid-market' THEN 'Mid-Market' 
+      WHEN lower({{ column_1 }}) = 'mid market' THEN 'Mid-Market' 
       WHEN {{ column_1 }} is not null THEN initcap({{ column_1 }}) 
 END
 
