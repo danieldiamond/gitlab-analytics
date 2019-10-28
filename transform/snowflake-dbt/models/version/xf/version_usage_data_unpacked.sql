c976ddced69c79b1e0253198819bea93192f17b1
Eli Kastelein <ekastelein@gitlab.com>
2019-10-28 15:32:15 -0700

    Extra indent

--- transform/snowflake-dbt/models/version/xf/version_usage_data_unpacked.sql
+++ transform/snowflake-dbt/models/version/xf/version_usage_data_unpacked.sql
@@ -2,96 +2,101 @@
 
 WITH usage_data AS (
 
-  SELECT * 
-  FROM {{ ref('version_usage_data') }}
+    SELECT * 
+    FROM {{ ref('version_usage_data') }}
 
 ), licenses AS (
   
-  SELECT *
-  FROM {{ ref('licenses_db_licenses') }}
+    SELECT *
+    FROM {{ ref('licenses_db_licenses') }}
 
+), zuora_subscriptions AS (
+
+    SELECT *  
+    FROM {{ ref('zuora_subscription')}}
+  
 ), joined AS (
 
-  SELECT
-    unpacked.*,
-    licenses.zuora_subscription_id,
+    SELECT
+      unpacked.*,
+      licenses.zuora_subscription_id,
 
 
-  FROM usage_data
-    LEFT JOIN licenses
-      ON usage_data.license_md5 = licenses.license_md5
+    FROM usage_data
+      LEFT JOIN licenses
+        ON usage_data.license_md5 = licenses.license_md5
   
 
 
 ), unpacked AS (
 
-  SELECT
-    id,
-    source_ip,
-    version,
-    installation_type,
-    active_user_count,
-    created_at,
-    mattermost_enabled,
-    uuid,
-    CASE
-      WHEN uuid = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f' THEN 'SaaS'
-      ELSE 'Self-Managed' END                                                       AS ping_source,
-    edition,
-    CONCAT(CONCAT(SPLIT_PART(version, '.', 1), '.'), SPLIT_PART(version, '.', 2))   AS major_version,
-    CASE WHEN LOWER(edition) LIKE '%ee%' THEN 'EE'
-      ELSE 'CE' END                                                                 AS main_edition,
-    CASE WHEN edition LIKE '%CE%' THEN 'Core'
-        WHEN edition LIKE '%EES%' THEN 'Starter'
-        WHEN edition LIKE '%EEP%' THEN 'Premium'
-        WHEN edition LIKE '%EEU%' THEN 'Ultimate'
-        WHEN edition LIKE '%EE Free%' THEN 'Core'
-        WHEN edition LIKE '%EE%' THEN 'Starter'
-      ELSE NULL END                                                                 AS edition_type,
-    hostname,
-    host_id,
-    git_version,
-    gitaly_servers,
-    gitaly_version,
+    SELECT
+      id,
+      source_ip,
+      version,
+      installation_type,
+      active_user_count,
+      created_at,
+      mattermost_enabled,
+      uuid,
+      CASE
+        WHEN uuid = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f' THEN 'SaaS'
+        ELSE 'Self-Managed' END                                                       AS ping_source,
+      edition,
+      CONCAT(CONCAT(SPLIT_PART(version, '.', 1), '.'), SPLIT_PART(version, '.', 2))   AS major_version,
+      CASE WHEN LOWER(edition) LIKE '%ee%' THEN 'EE'
+        ELSE 'CE' END                                                                 AS main_edition,
+      CASE WHEN edition LIKE '%CE%' THEN 'Core'
+          WHEN edition LIKE '%EES%' THEN 'Starter'
+          WHEN edition LIKE '%EEP%' THEN 'Premium'
+          WHEN edition LIKE '%EEU%' THEN 'Ultimate'
+          WHEN edition LIKE '%EE Free%' THEN 'Core'
+          WHEN edition LIKE '%EE%' THEN 'Starter'
+        ELSE NULL END                                                                 AS edition_type,
+      hostname,
+      host_id,
+      git_version,
+      gitaly_servers,
+      gitaly_version,
 
-    f.path                                                                          AS ping_name, 
-    REPLACE(f.path, '.','_')                                                        AS full_ping_name,
-    f.value                                                                         AS ping_value 
-  FROM joined,
-    lateral flatten(input => joined.stats_used, recursive => True) f
-  WHERE IS_OBJECT(f.value) = FALSE
-    AND stats_used IS NOT NULL
-  {% if is_incremental() %}
-      AND created_at > (SELECT max(created_at) FROM {{ this }})
-  {% endif %}
+      f.path                                                                          AS ping_name, 
+      REPLACE(f.path, '.','_')                                                        AS full_ping_name,
+      f.value                                                                         AS ping_value 
+    FROM joined,
+      lateral flatten(input => joined.stats_used, recursive => True) f
+    WHERE IS_OBJECT(f.value) = FALSE
+      AND stats_used IS NOT NULL
+    {% if is_incremental() %}
+        AND created_at > (SELECT max(created_at) FROM {{ this }})
+    {% endif %}
 
 ), final AS (
   
-  SELECT
-    id,
-    source_ip,
-    version,
-    installation_type,
-    active_user_count,
-    created_at,
-    mattermost_enabled,
-    uuid,
-    ping_source,
-    edition,
-    major_version,
-    main_edition,
-    edition_type,
-    hostname,
-    host_id,
-    git_version,
-    gitaly_servers,
-    gitaly_version,
-    {% for stat_name in version_usage_stats_list %}
-      MAX(IFF(full_ping_name = '{{stat_name}}', ping_value::numeric, NULL)) AS {{stat_name}} {{ "," if not loop.last }}
-    {% endfor %}
+    SELECT
+      id,
+      source_ip,
+      version,
+      installation_type,
+      active_user_count,
+      created_at,
+      mattermost_enabled,
+      uuid,
+      ping_source,
+      edition,
+      major_version,
+      main_edition,
+      edition_type,
+      hostname,
+      host_id,
+      git_version,
+      gitaly_servers,
+      gitaly_version,
+      {% for stat_name in version_usage_stats_list %}
+        MAX(IFF(full_ping_name = '{{stat_name}}', ping_value::numeric, NULL)) AS {{stat_name}} {{ "," if not loop.last }}
+      {% endfor %}
     
-  FROM unpacked
-  {{ dbt_utils.group_by(n=18) }}
+    FROM unpacked
+    {{ dbt_utils.group_by(n=18) }}
   
 )
 
