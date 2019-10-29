91a35e75c2db28fe441b172fd3574943a7290568
Eli Kastelein <ekastelein@gitlab.com>
2019-10-29 14:48:26 -0700

    Add all the columns

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_milestones_xf.sql
@@ -0,0 +1,50 @@
+{% set fields_to_mask = ['milestone_title', 'milestone_description'] %}
+
+WITH milestones AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_milestones')}}
+
+),
+
+-- Projects table joins milestones to namespaces
+projects AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_projects')}}
+),
+
+internal_namespaces AS (
+
+    SELECT
+      namespace_id
+    FROM {{ref('gitlab_dotcom_namespace_lineage')}}
+    WHERE ultimate_parent_id IN {{ get_internal_parent_namespaces() }}
+),
+
+
+SELECT
+    milestones.milestone_id,
+    milestones.project_id,
+    milestones.group_id,
+    milestones.start_date,
+    milestones.due_date,
+    milestones.milestone_status,
+    milestones.milestone_created_at,
+    milestones.milestone_updated_at
+   {% for field in fields_to_mask %}
+    CASE
+      WHEN is_confidential = TRUE
+        AND internal_namespaces.namespace_id IS NULL
+        THEN 'confidential - masked'
+      WHEN visibility_level != 'public'
+        AND internal_namespaces.namespace_id IS NULL
+        THEN 'private/internal - masked'
+      ELSE {{field}}
+    END                                          AS {{field}},
+    {% endfor %}
+FROM milestones
+  INNER JOIN projects
+    ON milestones.project_id = projects.project_id
+  LEFT JOIN internal_namespaces
+    ON projects.namespaces = internal_namespaces.namespace_id
