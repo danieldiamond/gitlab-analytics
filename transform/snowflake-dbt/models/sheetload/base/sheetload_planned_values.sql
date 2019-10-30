13ffebf68aa061a82dea516856c99b07155a3f31
Melia Vilain <mvilain@gitlab.com>
2019-10-30 18:38:55 +0000

    Add planned values for ACV, New IACV, and Growth IACV

--- transform/snowflake-dbt/models/sheetload/base/sheetload_planned_values.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_planned_values.sql
@@ -14,7 +14,10 @@ WITH source AS (
       planned_total_arr::INT            AS planned_total_arr,
       sales_efficiency_plan::FLOAT      AS sales_efficiency_plan,
       magic_number_plan::FLOAT          AS magic_number_plan,
-      tcv_plan::INT                     AS tcv_plan
+      tcv_plan::INT                     AS tcv_plan,
+      acv_plan::INT                     AS acv_plan,
+      planned_new_iacv::INT             AS planned_new_iacv,
+      planned_growth_iacv::INT          AS planned_growth_iacv
     FROM source
 
 )
