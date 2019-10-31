c848ce1ead09fd38214999ec7cbf0aa77df85a02
Eli Kastelein <ekastelein@gitlab.com>
2019-10-31 11:26:17 -0700

    Uncomment columns

--- transform/snowflake-dbt/models/version/base/version_hosts.sql
+++ transform/snowflake-dbt/models/version/base/version_hosts.sql
@@ -16,9 +16,9 @@ renamed AS (
     updated_at::TIMESTAMP               AS updated_at,
     star::BOOLEAN                       AS has_star,
     fortune_rank::INTEGER               AS fortune_rank,
-    in_salesforce::BOOLEAN              AS is_in_salesforce
-    --current_usage_data_id // waiting on fresh data https://gitlab.com/gitlab-data/analytics/issues/2696
-    --current_version_check_id // waiting on fresh data https://gitlab.com/gitlab-data/analytics/issues/2696
+    in_salesforce::BOOLEAN              AS is_in_salesforce,
+    current_usage_data_id::INTEGER      AS current_usage_data_id,
+    current_version_check_id::INTEGER   AS ccurrent_version_check_id
 
   FROM source
   WHERE rank_in_key = 1
