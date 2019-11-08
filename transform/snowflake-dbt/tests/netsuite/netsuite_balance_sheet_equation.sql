bb79983b15e4b2f1c08355604a8f1c1e4e2d83a5
iweeks <iweeks@gitlab.com>
2019-11-08 13:13:02 -0500

    add tests

---
+++ transform/snowflake-dbt/tests/netsuite/netsuite_balance_sheet_equation.sql
@@ -0,0 +1,29 @@
+--Tests to see if assets = liabilities + owner's equity
+
+WITH balance_sheet AS (
+
+  SELECT *
+  FROM {{ ref('netsuite_actuals_balance_sheet') }}
+
+), equation AS (
+
+  SELECT
+    accounting_period,
+    SUM(CASE
+          WHEN balance_sheet_grouping_level_3 = '1-assets'
+            THEN actual_amount
+        END)                                                              AS total_assets,
+    SUM(CASE
+          WHEN balance_sheet_grouping_level_3 = '2-liabilities & equity'
+            THEN actual_amount
+        END)                                                              AS total_liabilities_equity,
+    (total_assets - total_liabilities_equity)::INT                        AS balance_sheet_equation
+  FROM balance_sheet
+  GROUP BY 1
+  ORDER BY 1
+
+)
+
+SELECT *
+FROM equation
+WHERE balance_sheet_equation != 0
