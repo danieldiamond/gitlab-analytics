84ee326f90875fe04851f0f0bcfaf198931316c4
Eli Kastelein <ekastelein@gitlab.com>
2019-11-18 13:46:49 -0800

    checkout some of Mathieu's branch

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_issue_links.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_issue_links.sql
@@ -6,10 +6,9 @@
 
 WITH source AS (
 
-  SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) AS rank_in_key
+  SELECT *
   FROM {{ source('gitlab_dotcom', 'issue_links') }}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
@@ -17,12 +16,11 @@ WITH source AS (
       id::INTEGER                      AS issue_link_id,
       source_id::INTEGER               AS source_id,
       target_id::INTEGER               AS target_id,
-      created_at::TIMESTAMP            AS issue_link_created_at,
-      updated_at::TIMESTAMP            AS issue_link_updated_at
+      created_at::TIMESTAMP            AS created_at,
+      updated_at::TIMESTAMP            AS updated_at
 
     FROM source
-    WHERE rank_in_key = 1
-      AND created_at IS NOT NULL
+    WHERE created_at IS NOT NULL
 
 )
 
