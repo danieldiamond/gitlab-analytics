e309d1c24aedd0b1de65d2a0183f18cfe0808bf5
Eli Kastelein <ekastelein@gitlab.com>
2019-11-21 15:06:52 -0800

    Add progress

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions.sql
@@ -28,7 +28,39 @@ WITH source AS (
 
     FROM source
 
+), max_uploaded_at_by_id AS (
+  SELECT
+    namespace_id,
+    MAX(DATEADD('sec', _uploaded_at, '1970-01-01')::DATE) AS uploaded_at
+  FROM {{ source('gitlab_dotcom', 'gitlab_subscriptions') }} -- Source table
+  GROUP BY 1
+
+), windowed AS (
+  SELECT
+    renamed.*,
+
+    FIRST_VALUE(updated_at) OVER (
+        PARTITION BY renamed.namespace_id
+        ORDER BY updated_at
+        ROWS BETWEEN 1 FOLLOWING AND UNBOUNDED FOLLOWING
+    ) AS next_updated_at,
+    updated_at AS valid_from,
+    CASE
+      WHEN next_updated_at IS NOT NULL THEN DATEADD('millisecond', -1, next_updated_at)
+      WHEN id_in_most_recent_task = FALSE THEN max_uploaded_at_by_id.uploaded_at
+    END AS valid_to,
+    CASE
+      WHEN next_updated_at IS NOT NULL THEN FALSE
+      WHEN id_in_most_recent_task = FALSE THEN FALSE
+      ELSE TRUE
+    END AS is_currently_valid
+
+  FROM renamed
+    LEFT JOIN max_uploaded_at_by_id
+      ON renamed.namespace_id = max_uploaded_at_by_id.namespace_id
+  WHERE renamed.namespace_id = 1418911
+  ORDER BY updated_at
 )
 
 SELECT *
-FROM renamed
+FROM windowed
--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_label_links.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_label_links.sql
@@ -7,7 +7,7 @@ WITH source AS (
 
   SELECT *
   FROM {{ source('gitlab_dotcom', 'label_links') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+  --QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
