2ce862a50c76b82721407ec374e9eb687bcf07fe
Taylor A. Murphy <tmurphy@gitlab.com>
2019-11-22 14:38:53 -0600

    point to new image

--- dags/general/snowflake_permissions.py
+++ dags/general/snowflake_permissions.py
@@ -40,7 +40,7 @@ dag = DAG(
 # Task 1
 snowflake_load = KubernetesPodOperator(
     **gitlab_defaults,
-    image="registry.gitlab.com/meltano/meltano/runner:v1.1.0",
+    image="registry.gitlab.com/gitlab-data/data-image/data-image:tayloramurphy-master-patch-12221",
     task_id="snowflake-permissions",
     name="snowflake-permissions",
     secrets=[
