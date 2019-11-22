735e2e85b26b27d220528ccf23e03b9e26e714b5
Eli Kastelein <ekastelein@gitlab.com>
2019-11-21 17:44:36 -0800

    Sync trial change on merge_requests

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_merge_requests_xf.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_merge_requests_xf.sql
@@ -100,9 +100,12 @@ WITH merge_requests AS (
         merge_request_metrics.merged_at)           AS hours_to_merged_status,
 
       CASE
-        WHEN merge_request_created_at >= '2019-11-09'
-          THEN COALESCE(gitlab_subscriptions.plan_id, 34)
-        ELSE gitlab_subscriptions.plan_id            
+        WHEN merge_request_created_at >= '2019-11-09' THEN
+          CASE
+            WHEN merge_request_created_at BETWEEN DATEADD('days', -30, gitlab_subscription_trial_ends_on) AND gitlab_subscription_trial_ends_on
+              THEN 'trial'
+            ELSE COALESCE(gitlab_subscriptions.plan_id, 34)::VARCHAR END
+        ELSE gitlab_subscriptions.plan_id::VARCHAR
       END AS namespace_plan_id_at_merge_request_creation
 
     FROM merge_requests
@@ -121,6 +124,7 @@ WITH merge_requests AS (
       LEFT JOIN gitlab_subscriptions
         ON project_namespace_lineage.ultimate_parent_id = gitlab_subscriptions.namespace_id
         AND merge_requests.merge_request_created_at BETWEEN gitlab_subscriptions.valid_from AND {{ coalesce_to_infinity("gitlab_subscriptions.valid_to") }}
+        -- TODO: check for overlaps
 
 )
 
