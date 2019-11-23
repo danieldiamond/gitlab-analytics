51b75305d033c63c603e42b03adecc6cbac8320a
Eli Kastelein <ekastelein@gitlab.com>
2019-11-22 18:34:24 -0800

    Get members working

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_members.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_members.sql
@@ -3,16 +3,11 @@ WITH source AS (
   SELECT *
   FROM {{ source('gitlab_dotcom', 'members') }}
 
-), source_distinct AS (
+), 
 
-  SELECT
-    {{ dbt_utils.star(from=source('gitlab_dotcom', 'members'), except=["_UPLOADED_AT", "_TASK_INSTANCE"]) }},
-    MIN(DATEADD('sec', _uploaded_at, '1970-01-01')) AS valid_from,
-    MAX(_task_instance) AS max_task_instance
-  FROM source
-  GROUP BY {{ dbt_utils.star(from=source('gitlab_dotcom', 'members'),  except=["_UPLOADED_AT", "_TASK_INSTANCE"]) }}
+{{ source_distinct_rows(source = "source('gitlab_dotcom', 'members')")}}
 
-), renamed AS (
+, renamed AS (
 
     SELECT DISTINCT
 
