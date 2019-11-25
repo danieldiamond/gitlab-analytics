9a24a565fd11db65d5251b8b9502b3bca9e1cf9a
Eli Kastelein <ekastelein@gitlab.com>
2019-11-25 14:17:50 -0800

    Updates to gitlab_subscriptions to do it the old way

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions.sql
@@ -9,7 +9,11 @@ WITH source AS (
   FROM {{ source('gitlab_dotcom', 'gitlab_subscriptions') }}
   WHERE id != 572635 -- This ID has NULL values for many of the important columns.
 
-), renamed AS (
+), 
+
+{{ source_distinct_rows(source=source('gitlab_dotcom', 'gitlab_subscriptions'))}}
+
+, renamed AS (
 
     SELECT DISTINCT
 
@@ -24,16 +28,16 @@ WITH source AS (
       trial::BOOLEAN                                AS is_trial,
       created_at::TIMESTAMP                         AS created_at,
       updated_at::TIMESTAMP                         AS updated_at,
-      updated_at::TIMESTAMP                         AS valid_from
+      valid_from -- Column was added in source_distinct_rows CTE
   
-    FROM source
+    FROM source_distinct
 
 )
 
-
+/* Note: primary key used is namespace_id, not subscription id */
 {{ scd_type_2(
-    primary_key='member_id',
-    primary_key_raw='id',
+    primary_key='namespace_id', 
+    primary_key_raw='namespace_id',
     source_cte='source_distinct',
     source_timestamp='valid_from',
     casted_cte='renamed'
