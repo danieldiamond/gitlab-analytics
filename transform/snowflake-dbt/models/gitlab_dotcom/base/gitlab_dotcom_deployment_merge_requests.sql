9f29960c1ed7176d4d63bfa520bf57d72e04e717
davis_townsend <dtownsend@gitlab.com>
2019-11-25 14:51:53 -0600

    adding base dbt models for deployment_merge_requests,deployments, and environments

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_deployment_merge_requests.sql
@@ -0,0 +1,24 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+  SELECT
+    *
+  FROM {{ source('gitlab_dotcom', 'deployment_merge_requests') }}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY merge_request_id ORDER BY _uploaded_at DESC) = 1
+
+), renamed AS (
+
+    SELECT
+      deployment_id::INTEGER                           AS deployment_id,
+      merge_request_id::INTEGER                        AS merge_request_id,
+    FROM source
+
+)
+
+
+SELECT *
+FROM renamed
