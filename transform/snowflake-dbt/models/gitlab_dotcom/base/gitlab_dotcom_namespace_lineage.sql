158670885d8d6ae0b810892e9df8459a99cebf90
Eli Kastelein <ekastelein@gitlab.com>
2019-11-26 15:40:30 -0800

    Updates to lineage

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_namespace_lineage.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_namespace_lineage.sql
@@ -5,13 +5,20 @@
 
 WITH RECURSIVE namespaces AS (
 
-    SELECT
-      *
+    SELECT *
     FROM {{ref('gitlab_dotcom_namespaces')}}
 
-),
+), gitlab_subscriptions AS (
 
-recursive_namespaces(namespace_id, parent_id, upstream_lineage) AS (
+    SELECT *
+    FROM {{ref('gitlab_dotcom_gitlab_subscriptions')}}
+
+), plans AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_plans')}}
+
+), recursive_namespaces(namespace_id, parent_id, upstream_lineage) AS (
 
   -- Select all namespaces without parents
   SELECT
@@ -32,10 +39,28 @@ recursive_namespaces(namespace_id, parent_id, upstream_lineage) AS (
     INNER JOIN namespaces  AS iter -- Child namespace
       ON anchor.namespace_id = iter.parent_id
 
-  )
+), with_ultimate_parent AS (
 
-SELECT
-  *,
-  -- The last item of the lineage array is the ultimate parent
-  GET(upstream_lineage, ARRAY_SIZE(upstream_lineage)-1) AS ultimate_parent_id
-FROM recursive_namespaces
+  SELECT
+    *,
+    -- The last item of the lineage array is the ultimate parent
+    GET(upstream_lineage, ARRAY_SIZE(upstream_lineage)-1) AS ultimate_parent_id
+  FROM recursive_namespaces
+
+), ultimate_parent_with_plan AS (
+
+  SELECT
+    with_ultimate_parent.*,
+    plans.plan_id  AS ultimate_parent_plan_id,
+    plans.title    AS ultimate_parent_plan_title
+  FROM with_ultimate_parent
+    LEFT JOIN gitlab_subscriptions
+      ON with_ultimate_parent.ultimate_parent_id = gitlab_subscriptions.namespace_id
+      --AND gitlab_subsriptions.is_currently_valid = True
+    LEFT JOIN plans
+      ON COALESCE(gitlab_subscriptions.plan_id, 34) = plans.plan_id
+
+)
+
+SELECT *
+FROM ultimate_parent_with_plan
