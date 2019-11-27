05d695f6af7587ad9abf6e4cf20b77d0f568e716
Eli Kastelein <ekastelein@gitlab.com>
2019-11-27 14:35:38 -0800

    Add 2 new data tests

---
+++ transform/snowflake-dbt/tests/gitlab/gitlab_dotcom_gitlab_subscriptions_valid_from_less_than_valid_to.sql
@@ -0,0 +1,12 @@
+-- valid_from should always be less than valid_to
+
+WITH data AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_gitlab_subscriptions')}}
+
+)
+
+SELECT *
+FROM data
+WHERE valid_from >= valid_to
