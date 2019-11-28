22b6e11d0b4b123e299449f8e5130f42461ab506
mpeychet <mpeychet@gitlab.com>
2019-11-28 17:41:07 +0100

    LINTING

--- transform/snowflake-dbt/tests/gitlab/gitlab_dotcom_gitlab_subscriptions_no_overlaps.sql
+++ transform/snowflake-dbt/tests/gitlab/gitlab_dotcom_gitlab_subscriptions_no_overlaps.sql
@@ -2,17 +2,19 @@
 
 WITH data AS (
   
-  SELECT *
-  FROM {{ref('gitlab_dotcom_gitlab_subscriptions')}}
+    SELECT *
+    FROM {{ref('gitlab_dotcom_gitlab_subscriptions')}}
   
 ), grouped AS (
-  SELECT
-    namespace_id,
-    COUNT(*) AS count
-  FROM data
-    INNER JOIN (SELECT 1) AS a
-      ON '2019-09-01' BETWEEN data.valid_from AND COALESCE(data.valid_to, '9999-12-31')
-   GROUP BY 1
+  
+    SELECT
+      namespace_id,
+      COUNT(*) AS count
+    FROM data
+      INNER JOIN (SELECT 1) AS a
+        ON '2019-09-01' BETWEEN data.valid_from AND COALESCE(data.valid_to, '9999-12-31')
+     GROUP BY 1
+   
  )
  
 SELECT *
