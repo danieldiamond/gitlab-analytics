cae007b26ee8a1e35435302e9d4b082ef4f29ca5
mpeychet <mpeychet@gitlab.com>
2019-11-29 18:00:08 +0100

    todo and snippet base

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_snippets.sql
@@ -0,0 +1,28 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('gitlab_dotcom', 'snippets') }}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
+
+), renamed AS (
+  
+    SELECT
+        id::INTEGER               AS snippet_id,
+        title::VARCHAR            AS snippet_title,
+        author_id::INTEGER        AS author_id,
+        project_id::INTEGER       AS project_id,
+        created_at::TIMESTAMP     AS created_at,
+        updated_at::TIMESTAMP     AS updated_at
+        type::INTEGER             AS type,
+        visibility_level::INTEGER AS visibility_level
+    FROM source
+    
+)
+
+SELECT * 
+FROM renamed
