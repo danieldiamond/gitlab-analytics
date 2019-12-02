f142473d751ec4e61ac2726af28d6e433f36b464
mpeychet <mpeychet@gitlab.com>
2019-12-02 15:19:52 +0100

    add clusters and cluster groups and cluster porjects

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_cluster_projects.sql
@@ -0,0 +1,28 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+  SELECT
+    *
+  FROM {{ source('gitlab_dotcom', 'cluster_projects') }}
+  WHERE QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
+  
+)
+
+, renamed AS (
+  
+  SELECT
+  
+    id::INTEGER           AS cluster_group_id,
+    cluster_id::INTEGER   AS cluster_id,
+    project_id::INTEGER   AS group_id
+
+  FROM source
+  
+)
+
+SELECT * 
+FROM renamed
---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_clusters.sql
@@ -0,0 +1,34 @@
+{{ config({
+    "schema": "staging"
+    })
+}}
+
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('gitlab_dotcom', 'clusters') }}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY issue_id ORDER BY updated_at DESC) = 1
+
+), renamed AS (
+
+    SELECT
+        id::INTEGER                AS cluster_id,
+        user_id::INTEGER           AS user_id,
+        provider_type::VARCHAR     AS provider_type,
+        platform_type::VARCHAR     AS platform_type,
+        created_at::TIMESTAMP      AS created_at,
+        updated_at::TIMESTAMP      AS updated_at,
+        enabled::BOOLEAN           AS is_enabled,
+        name::VARCHAR              AS cluster_name,
+        environment_scope::VARCHAR AS environment_scope,
+        cluster_type::VARCHAR      AS cluster_type,
+        domain::VARCHAR            AS domain,
+        managed::VARCHAR           AS managed
+
+    FROM source
+
+)
+
+
+SELECT *
+FROM renamed
