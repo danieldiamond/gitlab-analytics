ba9c7ff5c50f1d7473e0ce1eef8f415d3835bc3f
Taylor A. Murphy <tmurphy@gitlab.com>
2019-12-02 11:11:35 -0600

    DAG and initial script

---
+++ dags/general/snowflake_password_reset.py
@@ -0,0 +1,55 @@
+import os
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from kube_secrets import *
+from airflow_utils import slack_failed_task, gitlab_defaults
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+
+# Load the env vars into a dict and set Secrets
+env = os.environ.copy()
+
+# Default arguments for the DAG
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 0,
+    "retry_delay": timedelta(minutes=1),
+    "start_date": datetime(2019, 1, 1),
+}
+
+# Set the command for the container
+container_cmd = f"""
+    git clone -b {env['GIT_BRANCH']} --single-branch https://gitlab.com/gitlab-data/analytics.git --depth 1 &&
+    analytics/orchestration/snowflake_password_change.py set_password_reset
+"""
+
+# Create the DAG
+# At 05:30 on Sunday in every 3rd month from January through December
+dag = DAG(
+    "snowflake_password_reset", default_args=default_args, schedule_interval="30 5 * 1-12/3 7"
+)
+
+# Task 1
+snowflake_password = KubernetesPodOperator(
+    **gitlab_defaults,
+    image="registry.gitlab.com/gitlab-data/data-image/data-image:latest",
+    task_id="snowflake-password-reset",
+    name="snowflake-password-reset",
+    secrets=[
+        PERMISSION_BOT_USER,
+        PERMISSION_BOT_PASSWORD,
+        PERMISSION_BOT_ACCOUNT,
+        PERMISSION_BOT_ROLE,
+        PERMISSION_BOT_DATABASE,
+        PERMISSION_BOT_WAREHOUSE,
+    ],
+    env_vars=pod_env_vars,
+    cmds=["/bin/bash", "-c"],
+    arguments=[container_cmd],
+    dag=dag,
+)
