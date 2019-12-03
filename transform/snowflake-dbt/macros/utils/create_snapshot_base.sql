e7bb6dd81d9e733d1b5b41648839afe844444ca3
derekatwood <datwood@gitlab.com>
2019-12-03 17:50:18 -0600

    style, quote, doc

--- transform/snowflake-dbt/macros/utils/create_snapshot_base.sql
+++ transform/snowflake-dbt/macros/utils/create_snapshot_base.sql
@@ -5,10 +5,10 @@ WITH date_spine AS (
     SELECT DISTINCT
       DATE_TRUNC({{ date_part }}, date_day) AS date_actual
     FROM {{ref("date_details")}}
-    WHERE date_day >= {{ date_start }}::DATE
+    WHERE date_day >= '{{ date_start }}'::DATE
       AND date_day <= CURRENT_DATE   
 
-), source AS (
+), base AS (
 
     SELECT *
     FROM {{ source }}
@@ -23,11 +23,11 @@ WITH date_spine AS (
       dbt_valid_from                         AS valid_from,
       dbt_valid_to                           AS valid_to,
       IFF(dbt_valid_to IS NULL, TRUE, FALSE) AS is_currently_valid,
-      source.*
-    FROM source
+      base.*
+    FROM base
     INNER JOIN date_spine
-      ON source.dbt_valid_from::DATE <= date_spine.date_actual
-     AND (source.dbt_valid_to::DATE > date_spine.date_actual OR source.dbt_valid_to IS NULL)
+      ON base.dbt_valid_from::DATE <= date_spine.date_actual
+      AND (base.dbt_valid_to::DATE > date_spine.date_actual OR base.dbt_valid_to IS NULL)
     ORDER BY 2,3      
       
 
