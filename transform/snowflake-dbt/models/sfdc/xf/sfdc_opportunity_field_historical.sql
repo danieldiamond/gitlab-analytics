ee6c3532dc07aa9c2d08fe2daef4ee90d2b62976
derekatwood <datwood@gitlab.com>
2019-12-04 12:41:20 -0600

    documentation

--- transform/snowflake-dbt/models/sfdc/xf/sfdc_opportunity_field_historical.sql
+++ transform/snowflake-dbt/models/sfdc/xf/sfdc_opportunity_field_historical.sql
@@ -1,10 +1,8 @@
 {% set fields_to_use = [
-    'account_owner_team_o__c','amount','business_development_rep__c','closedate',
-    'expected_close_date__c','forecastcategoryname','incremental_acv_2__c',
-    'leadsource','next_steps__c','opportunity_term_new__c','renewal_acv__c',
-    'renewal_amount__c','renewal_forecast_category__c','sales_accepted_date__c',
-    'sales_qualified__c','sales_qualified_date__c','sales_segmentation_employees_o__c',
-    'sales_segmentation_o__c','sql_amount__c','sql_source__c','stagename','swing_deal__c',
+    'amount','closedate','forecastcategoryname','incremental_acv_2__c',
+    'leadsource','renewal_acv__c','renewal_amount__c','sales_accepted_date__c',
+    'sales_qualified_date__c','sales_segmentation_employees_o__c',
+    'sales_segmentation_o__c','sql_source__c','stagename','swing_deal__c',
     'type','ultimate_parent_sales_segment_emp_o__c','ultimate_parent_sales_segment_o__c',
     'upside_iacv__c'
 ] %}
@@ -36,30 +34,25 @@ WITH date_spine AS (
       field_history.opportunity_id,
       field_modified_at                AS valid_to,
       opportunity_field,
-      COALESCE(old_value, 'true null') AS old_value
+      COALESCE(old_value, 'true null') AS old_value --retain record of fields that transitioned from NULl to another state
     FROM {{ref('sfdc_opportunity_field_history')}} field_history
     INNER JOIN first_snapshot
       ON field_history.field_modified_at <= first_snapshot.valid_from
      AND field_history.opportunity_id = first_snapshot.opportunity_id 
     WHERE opportunity_field IN ('{{ fields_to_use | join ("', '") }}')
 
-), base_pivoted AS (
-
-    SELECT *
-    FROM base
-      PIVOT(MAX(old_value) FOR opportunity_field IN ('{{ fields_to_use | join ("', '") }}'))
-      
 ), unioned AS (
 
     SELECT *
-    FROM first_snapshot 
+    FROM first_snapshot
     UNION
     SELECT 
-      base_pivoted.*,
+      *,
       NULL::TIMESTAMP_TZ AS created_at,
-      NULL::TIMESTAMP_TZ AS valid_from
-    FROM base_pivoted
- 
+      NULL::TIMESTAMP_TZ AS valid_from      
+    FROM base
+      PIVOT(MAX(old_value) FOR opportunity_field IN ('{{ fields_to_use | join ("', '") }}'))  
+      
 ), filled AS (
 
     SELECT
@@ -75,9 +68,58 @@ WITH date_spine AS (
       valid_to
     FROM unioned
 
+), cleaned AS (
+
+    SELECT
+      opportunity_id,
+      {% for field in fields_to_use %}
+      IFF({{field}} = 'true null', NULL, {{field}}) AS {{field}},
+      {% endfor %}
+      created_date,
+      valid_from,
+      valid_to
+    FROM filled
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY opportunity_id, DATE_TRUNC('day', valid_from) ORDER BY valid_from DESC) = 1               
+
+), joined AS (
+
+    SELECT
+      date_actual,
+      valid_from,
+      valid_to,
+      IFF(valid_to IS NULL, TRUE, FALSE) AS is_currently_valid,
+      opportunity_id,
+      closedate::DATE                    AS close_date,
+      created_date::DATE                 AS created_date,
+      sql_source__c                      AS generated_source,
+      leadsource                         AS lead_source,
+      COALESCE({{ sales_segment_cleaning('ultimate_parent_sales_segment_emp_o__c') }}, 
+               {{ sales_segment_cleaning('ultimate_parent_sales_segment_o__c') }}) 
+                                         AS parent_segment,
+      sales_accepted_date__c::DATE       AS sales_accepted_date,
+      sales_qualified_date__c::DATE      AS sales_qualified_date,
+      COALESCE({{ sales_segment_cleaning('sales_segmentation_employees_o__c') }}, 
+               {{ sales_segment_cleaning('sales_segmentation_o__c') }}, 'Unknown') 
+                                         AS sales_segment,
+      type                               AS sales_type,
+      {{  sfdc_source_buckets('leadsource') }}         
+      stagename                          AS stage_name,
+      {{sfdc_deal_size('incremental_acv_2__c::FLOAT', 'deal_size')}},
+      forecastcategoryname               AS forecast_category_name,  
+      incremental_acv_2__c::FLOAT        AS forecasted_iacv,       
+      swing_deal__c                      AS is_swing_deal,
+      renewal_acv__c::FLOAT              AS renewal_acv,
+      renewal_amount__c::FLOAT           AS renewal_amount,
+      sales_segmentation_o__c            AS segment,
+      amount::FLOAT                      AS total_contract_value,
+      upside_iacv__c::FLOAT              AS upside_iacv
+    FROM cleaned  
+    INNER JOIN date_spine
+      ON cleaned.valid_from::DATE <= date_spine.date_actual
+      AND (cleaned.valid_to::DATE > date_spine.date_actual OR cleaned.valid_to IS NULL) 
+
 )
 
 SELECT *
-FROM filled
-QUALIFY ROW_NUMBER() OVER (PARTITION BY opportunity_id, DATE_TRUNC('day', valid_from) ORDER BY valid_from DESC) = 1
+FROM joined
 ORDER BY opportunity_id, valid_from
