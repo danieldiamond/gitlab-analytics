4c40e71ce37d132adaff839b69cc0f49b4cc2159
Eli Kastelein <ekastelein@gitlab.com>
2019-12-09 10:32:52 -0800

    New custom schema test

---
+++ transform/snowflake-dbt/macros/tests/test_unique_where_currently_valid.sql
@@ -0,0 +1,18 @@
+{% macro test_unique_where_currently_valid(model, column_name) %}
+
+WITH data AS (
+
+    SELECT
+      {{ column_name }} AS id,
+      COUNT(*)          AS count_valid_rows
+    FROM {{ model }}
+    WHERE is_currently_valid = True
+    GROUP BY 1
+
+),
+
+SELECT COUNT(*)
+FROM data
+WHERE count_valid_rows != 1
+
+{% endmacro %}
