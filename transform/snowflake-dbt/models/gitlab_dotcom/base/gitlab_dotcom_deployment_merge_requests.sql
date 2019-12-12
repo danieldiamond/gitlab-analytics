078ba9b70b271837f273210fe82c78264bb96b8a
davis_townsend <dtownsend@gitlab.com>
2019-12-12 16:20:52 -0600

    update PARTITION use pkey COALESCE because of N:N relationship

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_deployment_merge_requests.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_deployment_merge_requests.sql
@@ -7,15 +7,16 @@ WITH source AS (
 
     SELECT *
     FROM {{ source('gitlab_dotcom', 'deployment_merge_requests') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY merge_request_id ORDER BY _uploaded_at DESC) = 1
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY pkey ORDER BY _uploaded_at DESC) = 1
 
 ), renamed AS (
 
     SELECT
       deployment_id::INTEGER                           AS deployment_id,
       merge_request_id::INTEGER                        AS merge_request_id
+      pkey::INTEGER                                    AS pkey
     FROM source
-    
+
 )
 
 SELECT *
