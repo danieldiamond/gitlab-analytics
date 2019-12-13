90e3920d7992626b94fe52c491058691cab1ab0d
Davis Townsend <dtownsend@gitlab.com>
2019-12-13 16:11:15 +0000

    updating deployment_merge_request pk

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_deployment_merge_requests.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_deployment_merge_requests.sql
@@ -14,7 +14,7 @@ WITH source AS (
     SELECT
       deployment_id::INTEGER                           AS deployment_id,
       merge_request_id::INTEGER                        AS merge_request_id
-      pkey::INTEGER                                    AS pkey
+      MD5(deployment_merge_request_id::VARCHAR)        AS deployment_merge_request_id
     FROM source
 
 )
