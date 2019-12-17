60ee54d8b6ff8f8e34138acc6957a0c028b250d5
Eli Kastelein <ekastelein@gitlab.com>
2019-12-17 13:09:10 -0800

    small tweaks

--- transform/snowflake-dbt/models/version/xf/version_usage_data_action_summary_xf.sql
+++ transform/snowflake-dbt/models/version/xf/version_usage_data_action_summary_xf.sql
@@ -9,7 +9,7 @@ WITH usage_data AS (
       data.id            AS usage_data_id,
       data.license_md5   AS license_md5,
       data.created_at    AS created_at,
-      f.path,
+      f.path             AS stage,
       f.value            AS stage_json
     FROM usage_data AS data,
       LATERAL FLATTEN(INPUT => usage_activity_by_stage, RECURSIVE => False) f
@@ -18,12 +18,12 @@ WITH usage_data AS (
 ), final AS (
 
     SELECT
-      {{ dbt_utils.surrogate_key('by_stage.usage_data_id', 'f.path', 'created_at') }} 
+      {{ dbt_utils.surrogate_key('by_stage.usage_data_id', 'f.path', 'by_stage.created_at') }} 
                     AS unique_key,
       by_stage.usage_data_id,
       by_stage.license_md5,
       by_stage.created_at,
-      by_stage.path AS stage,
+      by_stage.stage,
       f.path        AS action,
       f.value       AS count_users
     FROM by_stage,
