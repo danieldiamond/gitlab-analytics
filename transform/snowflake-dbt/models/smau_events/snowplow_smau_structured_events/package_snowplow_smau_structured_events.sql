90c53f9e3bb7ab0f71f88d386638b57fc686f581
mpeychet <mpeychet@gitlab.com>
2019-12-19 17:40:02 +0100

    first working version of structured events for snowplow events

---
+++ transform/snowflake-dbt/models/smau_events/snowplow_smau_structured_events/package_snowplow_smau_structured_events.sql
@@ -0,0 +1,51 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": "event_surrogate_key"
+    })
+}}
+
+WITH snowplow_structured_events AS (
+
+  SELECT
+    user_snowplow_domain_id,
+    user_custom_id,
+    derived_tstamp,
+    page_url_path,
+    event_id,
+    event_action,
+    event_label
+  FROM {{ ref('snowplow_structured_events')}}
+  WHERE derived_tstamp >= '2019-01-01'
+  {% if is_incremental() %}
+    AND page_view_start >= (SELECT MAX(event_date) FROM {{this}})
+    AND 
+      (
+        (
+          event_action IN 
+            (
+              'registry_repository_delete',
+              'bulk_registry_tag_delete',
+              'registry_tag_delete',
+              'list_repositories',
+              'delete_repository'
+            )
+        )
+      OR
+        (
+          event_label IN
+            (
+              'bulk_registry_tag_delete',
+              'registry_repository_delete',
+              'registry_tag_delete'
+              
+            )
+        (
+              
+            )
+          )
+  {% endif %}
+
+)
+
+SELECT *
+FROM snowplow_structured_events
