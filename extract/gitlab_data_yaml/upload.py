c76b694467222839cf9645a6281b8b40d4fb5ad9
Taylor A. Murphy <tmurphy@gitlab.com>
2019-12-19 09:40:21 -0600

    Lint

--- extract/gitlab_data_yaml/upload.py
+++ extract/gitlab_data_yaml/upload.py
@@ -11,7 +11,9 @@ from gitlabdata.orchestration_utils import (
 
 if __name__ == "__main__":
 
-    file_dict = dict(team="team", location_factors="location_factors", roles="job_families")
+    file_dict = dict(
+        team="team", location_factors="location_factors", roles="job_families"
+    )
 
     logging.basicConfig(stream=sys.stdout, level=20)
 
