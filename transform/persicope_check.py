9cd4d9b16c2e872ac74e07d09e2f809a8996abd2
Taylor A. Murphy <tmurphy@gitlab.com>
2020-01-01 08:39:41 -0600

    Updates to do writes

--- transform/persicope_check.py
+++ transform/persicope_check.py
@@ -15,7 +15,7 @@ periscope_table_dict = dict()
 
 for path in paths_to_check:
     fullpath = f"{parentdirname}/periscope/{path}"
-    for root, files in os.walk(fullpath):
+    for root, dirs, files in os.walk(fullpath):
         for file in files:
             if re.search("sql", file):
                 full_filename = f"{root}/{file}"
@@ -25,27 +25,21 @@ for path in paths_to_check:
                         matches = re.search("(?:from|join)\s+(?:analytics|analytics_staging|boneyard)\.([\_A-z0-9]*)", line.lower())
                         if matches is not None:
                             for match in matches.groups():
-                                curr_list = periscope_table_dict.get(match, set())
-                                curr_list.add(full_filename)
-                                periscope_table_dict[match] = curr_list
-
-print(len(periscope_table_dict.keys()))
-                            
-# periscope_table_dict = {
-#     "table_queried": [
-#         "fully qualified file name"
-#     ]
-# }
+                                curr_list = periscope_table_dict.get(match, [])
+                                simplified_name = re.sub(".*\/analytics\/periscope\/", "", full_filename)
+                                curr_list.append(simplified_name)
+                                periscope_table_dict[match] = list(set(curr_list))
 
 
-# analytics project
+with open('comparison.txt', 'w+') as f:
+    f.write("dbt models to check\r\n")
 
-# dbt_files_changed = [dbt, files, changed]
-
-# files_to_check = []
-
-# for model in dbt_files_changed:
-#     if model in periscope_table_dict.keys():
-#         files_to_check.extend(periscope_table_dict.get(model, []))
-
-# print(files_to_check)
+with open('diff.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        match = periscope_table_dict.get(line.strip(), [])
+        if len(match) > 0:
+            with open('comparison.txt', 'a') as comp:
+                write_string = f"dbt model: {line}. Periscope references: {str(match)} \r\n"
+                comp.write(write_string)
+          
