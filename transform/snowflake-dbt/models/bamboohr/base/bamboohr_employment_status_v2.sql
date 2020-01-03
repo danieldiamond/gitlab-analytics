adc4f44bc121ce2ebcba235f2ad97c5b1e6eee44
Parul Luthra <pluthra@gitlab.com>
2020-01-03 14:14:59 +0000

    Apply suggestion to transform/snowflake-dbt/models/bamboohr/base/bamboohr_employment_status_v2.sql

--- transform/snowflake-dbt/models/bamboohr/base/bamboohr_employment_status_v2.sql
+++ transform/snowflake-dbt/models/bamboohr/base/bamboohr_employment_status_v2.sql
@@ -5,7 +5,9 @@ WITH source AS (
     FROM {{ source('bamboohr', 'employment_status') }}
     ORDER BY uploaded_at DESC
     LIMIT 1
-), intermediate as (
+
+), intermediate AS (
+
         SELECT d.value as data_by_row
         FROM source,
         LATERAL FLATTEN(INPUT => parse_json(jsontext), outer => true) d
