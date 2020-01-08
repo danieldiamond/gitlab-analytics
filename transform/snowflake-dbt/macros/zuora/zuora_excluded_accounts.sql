6013af808062a2765a59741ede4c271a2c1bc122
Eli Kastelein <ekastelein@gitlab.com>
2020-01-08 13:33:37 -0800

    Reorg into a dbt seed

---
+++ transform/snowflake-dbt/macros/zuora/zuora_excluded_accounts.sql
@@ -0,0 +1,19 @@
+{% macro zuora_excluded_accounts() %}
+
+    {%- call statement('get_excluded_accounts', fetch_result=True) %}
+
+        SELECT DISTINCT account_id
+        FROM {{ref('zuora_excluded_accounts')}}
+        WHERE account_id IS NOT NULL
+
+    {%- endcall -%}
+
+    {%- set value_list = load_result('get_excluded_accounts') -%}
+
+    {%- if value_list and value_list['data'] -%}
+      {%- set values = value_list['data'] | map(attribute=0) | join(', ') %}
+    {%- endif -%}
+
+    {{ return(values) }}
+
+ {% endmacro %}
