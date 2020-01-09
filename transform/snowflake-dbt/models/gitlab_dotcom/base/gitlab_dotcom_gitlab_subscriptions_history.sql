b6214c7e5c5231e4c2bf3d7a20429d73a29b871f
Eli Kastelein <ekastelein@gitlab.com>
2020-01-09 12:10:16 -0800

    get working

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions_history.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions_history.sql
@@ -2,7 +2,7 @@ WITH source AS (
 
   SELECT *
   FROM {{ source('gitlab_dotcom', 'gitlab_subscription_histories') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1 -- Each ID should always have the same data.
 
 ), renamed AS (
 
