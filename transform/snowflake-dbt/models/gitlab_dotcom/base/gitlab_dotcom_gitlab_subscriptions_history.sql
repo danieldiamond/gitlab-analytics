b368310ecbc96fe3daa999c371861882f1513afe
Eli Kastelein <ekastelein@gitlab.com>
2020-01-09 12:01:51 -0800

    Fill in base model

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions_history.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_gitlab_subscriptions_history.sql
@@ -8,21 +8,21 @@ WITH source AS (
 
     SELECT
 
-      id
-      gitlab_subscription_created_at
-      gitlab_subscription_updated_at
-      start_date
-      end_date
-      trial_starts_on
-      trial_ends_on
-      namespace_id
-      hosted_plan_id
-      max_seats_used
-      seats
-      trial
-      change_type
-      gitlab_subscription_id
-      created_at
+      id::INTEGER                                      AS gitlab_subscription_history_id,
+      gitlab_subscription_created_at::TIMESTAMP        AS gitlab_subscription_created_at,
+      gitlab_subscription_updated_at::TIMESTAMP        AS gitlab_subscription_updated_at,
+      start_date::TIMESTAMP                            AS start_date,
+      end_date::TIMESTAMP                              AS end_date,
+      trial_starts_on::TIMESTAMP                       AS trial_starts_on,
+      trial_ends_on::TIMESTAMP                         AS trial_ends_on,
+      namespace_id::INTEGER                            AS namespace_id,
+      hosted_plan_id::INTEGER                          AS hosted_plan_id,
+      max_seats_used::INTEGER                          AS max_seats_used,
+      seats::INTEGER                                   AS seats,
+      trial::BOOLEAN                                   AS is_trial,
+      change_type                                      AS todo,
+      gitlab_subscription_id::INTEGER                  AS gitlab_subscription_id,
+      created_at::TIMESTAMP                            AS gitlab_subscription_history_created_at
 
     FROM source
 
