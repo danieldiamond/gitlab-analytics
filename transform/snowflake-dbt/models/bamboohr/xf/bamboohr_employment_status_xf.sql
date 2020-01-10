c44b9e5bd2452ed187eab43c4b488e7d9fb2b8fd
Parul Luthra <pluthra@gitlab.com>
2020-01-10 15:48:03 -0600

    making suggested updates including updating schema to sensitive

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_employment_status_xf.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_employment_status_xf.sql
@@ -1,5 +1,5 @@
 {{ config({
-    "schema": "analytics"
+    "schema": "sensitive"
     })
 }}
 
@@ -7,8 +7,13 @@ WITH bamboohr_employment_status AS (
   
     SELECT *
     FROM {{ ref ('bamboohr_employment_status') }}
+
+WITH bamboohr_employment_status AS (
   
-),employment_log AS (--the emp_status will need to work with people group to create a translation table for different employment statuses   
+    SELECT *
+    FROM {{ ref ('bamboohr_employment_status') }}
+  
+), employment_log AS (
   
    SELECT
     status_id,
@@ -18,31 +23,21 @@ WITH bamboohr_employment_status AS (
     effective_date                                                                      AS valid_from_date,
     LEAD(effective_date) OVER (PARTITION BY employee_id ORDER BY effective_date)        AS valid_to_date,
     LEAD(employment_status) OVER (PARTITION BY employee_id ORDER BY effective_date)     AS next_employment_status,
-    LAG(employment_status) OVER (PARTITION BY employee_id ORDER BY effective_date)      AS previous_employment_status,
-    CASE 
-      WHEN employment_status LIKE '%Leave%' 
-        THEN 'L'
-      WHEN employment_status IN ('Terminated','Suspended') 
-        THEN 'T'
-      WHEN employment_status ='Active' 
-        THEN 'A'
-      ELSE'Other' END                                                                   AS emp_status                   
-    FROM bamboohr_employment_status
-
+    LAG(employment_status) OVER (PARTITION BY employee_id ORDER BY effective_date)      AS previous_employment_status
+    
 ), final AS (  
 
     SELECT
       employee_id,
-      employment_status,
-      emp_status, 
+      employment_status, 
       termination_type,
-            CASE WHEN previous_employment_status ='Terminated' 
-              AND employment_status !='Terminated' THEN 1 ELSE 0 END                    AS is_rehire,
+      CASE WHEN previous_employment_status ='Terminated' 
+        AND employment_status !='Terminated' THEN 'True' ELSE 'False' END               AS is_rehire,
       next_employment_status,           
-      to_date(valid_from_date)                                                          AS valid_from_date,
+      valid_from_date                                                                   AS valid_from_date,
       IFF(employment_status='Terminated'
-            ,to_date(valid_to_date)
-            ,IFNULL(to_date(valid_to_date), last_day(current_date)))                    AS valid_to_date
+            ,valid_to_date
+            ,IFNULL(valid_to_date, last_day(current_date)))                             AS valid_to_date
      FROM employment_log
 )
 
