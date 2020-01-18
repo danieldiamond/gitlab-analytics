40fa09b0d8da5a8aae257c4c124907ff5303fa00
iweeks <iweeks@gitlab.com>
2020-01-18 18:45:28 -0300

    account for moving to gitlab sku

--- transform/snowflake-dbt/models/revenue_mart/mrr_gaap_revenue_mart.sql
+++ transform/snowflake-dbt/models/revenue_mart/mrr_gaap_revenue_mart.sql
@@ -44,6 +44,18 @@ WITH zuora_accts AS (
   FROM {{ ref('date_details') }}
   WHERE day_of_month = 1
 
+), list AS (
+
+	SELECT DISTINCT
+    zuora_acct_period.startdate::DATE AS accounting_period,
+    zuora_product.name                AS product_name,
+    zuora_rpc.name                    AS rate_plan_charge_name
+  FROM zuora_product
+  CROSS JOIN zuora_acct_period
+  CROSS JOIN zuora_rpc
+  WHERE zuora_acct_period.startdate::DATE <= current_date
+  ORDER BY 1 DESC,2,3
+
 ), base_mrr AS (
 
   SELECT
@@ -54,21 +66,6 @@ WITH zuora_accts AS (
     zuora_accts.accountnumber AS account_number,
     zuora_contact.country,
     zuora_accts.currency,
-
-    -- subscription info
-    --zuora_subscriptions_xf.subscription_name,
-    --zuora_subscriptions_xf.subscription_name_slugify,
-    --zuora_subscriptions_xf.subscription_start_date,
-
-    --subscription_lineage info
-    --zuora_subscriptions_xf.exclude_from_renewal_report,
-    --zuora_subscriptions_xf.lineage,
-    --zuora_subscriptions_xf.oldest_subscription_in_cohort,
-    --zuora_subscriptions_xf.subscription_status,
-
-    --cohort info
-    --zuora_subscriptions_xf.cohort_month,
-    --zuora_subscriptions_xf.cohort_quarter
 
     -- rate_plan info
     --zuora_rp.delivery,
@@ -82,11 +79,9 @@ WITH zuora_accts AS (
 
     zuora_product.name AS product_name,
 
-    --date_trunc('month', zuora_subscriptions_xf.subscription_start_date :: date) AS sub_start_month,
-    --date_trunc('month', dateadd('month', -1, zuora_subscriptions_xf.subscription_end_date :: DATE)) AS sub_end_month,
     date_trunc('month', zuora_rpc.effectivestartdate::DATE) AS effective_start_month,
     date_trunc('month', zuora_rpc.effectiveenddate::DATE) AS effective_end_month,
-    --datediff(month, zuora_rpc.effectivestartdate::DATE,zuora_rpc.effectiveenddate::DATE) AS month_interval,
+
     zuora_rpc.effectivestartdate AS effective_start_date,
     zuora_rpc.effectiveenddate AS effective_end_date,
     zuora_rpc.quantity AS quantity,
@@ -101,7 +96,7 @@ WITH zuora_accts AS (
   LEFT JOIN zuora_contact
     ON COALESCE(zuora_accts.soldtocontactid ,zuora_accts.billtocontactid) = zuora_contact.id
   LEFT JOIN zuora_product
-    ON zuora_product.id = zuora_rpc.product_id
+    ON zuora_product.id = zuora_rpc.productid
   WHERE zuora_subscription.status NOT IN ('Draft','Expired')
 
 ), month_base_mrr AS (
@@ -109,38 +104,26 @@ WITH zuora_accts AS (
   SELECT
     --country,
     --account_number,
-    --subscription_name,
-    --subscription_name_slugify,
-    --oldest_subscription_in_cohort,
-    --lineage,
     --rate_plan_name,
     --product_category,
     --delivery,
-    --rate_plan_charge_name,
     date_actual AS accounting_period,
-    --sub_start_month,
-    --sub_end_month,
-    --effective_start_month,
-    --effective_end_month,
-    --effective_start_date,
-    --effective_end_date,
-    --cohort_month,
-    --cohort_quarter,
-    --unit_of_measure,
     product_name,
+		rate_plan_charge_name,
     SUM(mrr) AS mrr,
     SUM(quantity) AS quantity
   FROM base_mrr
   LEFT JOIN date_table
     ON base_mrr.effective_start_date <= date_table.date_actual
     AND (base_mrr.effective_end_date > date_table.date_actual OR base_mrr.effective_end_date IS NULL)
-  {{ dbt_utils.group_by(n=2) }}
+  {{ dbt_utils.group_by(n=3) }}
 
 ), gaap_revenue AS (
 
   SELECT
     zuora_acct_period.startdate::DATE  AS accounting_period,
     zuora_product.name                 AS product_name,
+		zuora_rpc.name						         AS rate_plan_charge_name,
     SUM(zuora_rev_sch.amount)          AS revenue_amt
   FROM zuora_rev_sch
   INNER JOIN zuora_rpc
@@ -148,8 +131,8 @@ WITH zuora_accts AS (
   INNER JOIN zuora_acct_period
     ON zuora_acct_period.id = zuora_rev_sch.accountingperiodid
   LEFT JOIN zuora_product
-    ON zuora_product.id = zuora_rpc.product_id
-  {{ dbt_utils.group_by(n=2) }}
+    ON zuora_product.id = zuora_rev_sch.productid
+  {{ dbt_utils.group_by(n=3) }}
 
 ), current_mrr AS (
 
@@ -166,16 +149,40 @@ WITH zuora_accts AS (
      AND effectivestartdate <= current_date
      AND (effectiveenddate > current_date OR effectiveenddate IS NULL)
 
+), combined AS (
+
+   SELECT
+     list.accounting_period,
+     list.product_name,
+	   list.rate_plan_charge_name,
+     MAX(current_mrr)   AS current_mrr,
+     SUM(mrr)           AS mrr,
+     SUM(revenue_amt)   AS revenue_amt
+   FROM list
+   LEFT JOIN gaap_revenue
+     ON list.accounting_period = gaap_revenue.accounting_period
+     AND list.product_name = gaap_revenue.product_name
+     AND list.rate_plan_charge_name = gaap_revenue.rate_plan_charge_name
+   LEFT JOIN month_base_mrr
+     ON list.accounting_period = month_base_mrr.accounting_period
+	   AND list.product_name = month_base_mrr.product_name
+	   AND list.rate_plan_charge_name = month_base_mrr.rate_plan_charge_name
+   LEFT JOIN current_mrr
+   {{ dbt_utils.group_by(n=3) }}
+
 )
 
 SELECT
-  month_base_mrr.accounting_period,
+  accounting_period,
   product_name,
-  MAX(current_mrr)   AS current_mrr,
-  SUM(mrr)           AS mrr,
-  SUM(revenue_amt)   AS revenue_amt
-FROM month_base_mrr
-LEFT JOIN gaap_revenue
-  ON month_base_mrr.mrr_month = gaap_revenue.accounting_period
-LEFT JOIN current_mrr
-{{ dbt_utils.group_by(n=2) }}
+  rate_plan_charge_name,
+  current_mrr,
+  CASE
+    WHEN rate_plan_charge_name = '#movingtogitlab'
+      THEN revenue_amt
+    ELSE mrr
+  END  											AS mrr,
+  revenue_amt
+FROM combined
+WHERE revenue_amt IS NOT NULL
+  OR mrr IS NOT NULL
