4af365cff38872aab79dd864d61de8b7f7ccd629
Eli Kastelein <ekastelein@gitlab.com>
2020-01-20 12:57:13 -0800

    remove macro since only using twice

--- transform/snowflake-dbt/macros/snowplow/sanitize_unstructured_event.sql
+++
@@ -1,9 +0,0 @@
-{% macro sanitize_unstructured_event() -%}
-
-  CASE
-    WHEN event_name IN ('submit_form', 'focus_form', 'change_form')
-      THEN 'masked'
-    ELSE nullif(jsontext['unstruct_event']::STRING, '')   
-  END
-
-{%- endmacro %}
