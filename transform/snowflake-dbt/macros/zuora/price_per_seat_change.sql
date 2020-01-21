3d8b5190d4829c9a7ad86faf49f1db5f3e3f8d31
Kathleen Tam <ktam@gitlab.com>
2020-01-21 22:16:43 +0000

    Apply suggestion to transform/snowflake-dbt/macros/zuora/price_per_seat_change.sql

--- transform/snowflake-dbt/macros/zuora/price_per_seat_change.sql
+++ transform/snowflake-dbt/macros/zuora/price_per_seat_change.sql
@@ -5,7 +5,7 @@ CASE
     WHEN NOT (       -- Only compare prices per seat when the unit of measure of the original and new plans is seats
         ARRAY_CONTAINS('Seats'::VARIANT, {{ original_unit_of_measure }}) AND
         ARRAY_SIZE({{ original_unit_of_measure }}) = 1 AND
-        ARRAY_CONTAINS('Seats'::variant, {{ new_unit_of_measure }}) AND
+        ARRAY_CONTAINS('Seats'::VARIANT, {{ new_unit_of_measure }}) AND
         ARRAY_SIZE({{ new_unit_of_measure }}) = 1
     )
         THEN NULL
