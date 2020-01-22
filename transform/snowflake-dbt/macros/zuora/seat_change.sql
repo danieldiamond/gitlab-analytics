8c7ee7011cba119d8da09a708f78104f359d0528
Jean Del Rosario <jpeguero@gitlab.com>
2020-01-22 14:14:49 -0400

    Remove check for array_size in retention macros

--- transform/snowflake-dbt/macros/zuora/seat_change.sql
+++ transform/snowflake-dbt/macros/zuora/seat_change.sql
@@ -3,9 +3,7 @@
     CASE
         WHEN NOT (       -- Only compare prices per seat when the unit of measure of the original and new plans is seats
             {{ original_unit_of_measure }} = ARRAY_CONSTRUCT('Seats') AND
-            ARRAY_SIZE({{ original_unit_of_measure }}) = 1 AND
-            {{ new_unit_of_measure }} = ARRAY_CONSTRUCT('Seats') AND
-            ARRAY_SIZE({{ new_unit_of_measure }}) = 1
+            {{ new_unit_of_measure }} = ARRAY_CONSTRUCT('Seats')
         )
             THEN 'Not Valid'
         WHEN {{ original_seat_quantity }} = {{ new_seat_quantity }}
