4318e79ed760f48c95cce08288b1227093d794f4
mpeychet <mpeychet@gitlab.com>
2020-01-22 11:40:58 +0100

    addressign comments

--- transform/snowflake-dbt/models/zuora/xf/zuora_subscription_periods.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_subscription_periods.sql
@@ -26,7 +26,7 @@ WITH zuora_account AS (
   
 )
 
-, subscription_joined_with_accounts AS (
+, subsription_joined_with_accounts AS (
 
     SELECT DISTINCT
       zuora_subscription.subscription_id,
@@ -37,6 +37,7 @@ WITH zuora_account AS (
       zuora_subscription.zuora_renewal_subscription_name_slugify,
       zuora_subscription.renewal_term,
       zuora_subscription.renewal_term_period_type,
+      
       zuora_subscription.account_id, 
       zuora_account.account_number,
       zuora_account.account_name,
@@ -46,9 +47,9 @@ WITH zuora_account AS (
       term_start_date                 AS subscription_version_term_start_date,
       MIN(term_start_date) 
       OVER (PARTITION BY subscription_name_slugify 
-            ORDER BY zuora_subscription.version
-            ROWS BETWEEN 1 FOLLOWING
-                   AND UNBOUNDED FOLLOWING)   AS min_following_subscription_version_term_start_date
+            ORDER BY zuora_subscription.version DESC 
+            ROWS BETWEEN UNBOUNDED PRECEDING
+                   AND 1 PRECEDING)   AS min_following_subscription_version_term_start_date
     FROM zuora_subscription
     INNER JOIN zuora_account
       ON zuora_subscription.account_id = zuora_account.account_id
@@ -58,29 +59,28 @@ WITH zuora_account AS (
 , subscription_joined_with_charges AS (
   
     SELECT DISTINCT
-      subscription_joined_with_accounts.subscription_id,
-      subscription_joined_with_accounts.subscription_name,
-      subscription_joined_with_accounts.subscription_name_slugify,    
-      subscription_joined_with_accounts.subscription_status, 
-      subscription_joined_with_accounts.version,
-      subscription_joined_with_accounts.zuora_renewal_subscription_name_slugify,
-      subscription_joined_with_accounts.account_id, 
-      subscription_joined_with_accounts.account_number,
-      subscription_joined_with_accounts.account_name,
-      subscription_joined_with_accounts.subscription_start_date, 
-      subscription_joined_with_accounts.subscription_version_end_date,
-      subscription_joined_with_accounts.subscription_version_term_end_date,
-      subscription_joined_with_accounts.subscription_version_term_start_date,
+      subsription_joined_with_accounts.subscription_id,
+      subsription_joined_with_accounts.subscription_name,
+      subsription_joined_with_accounts.subscription_name_slugify,    
+      subsription_joined_with_accounts.subscription_status, 
+      subsription_joined_with_accounts.version,
+      subsription_joined_with_accounts.zuora_renewal_subscription_name_slugify,
+      subsription_joined_with_accounts.account_id, 
+      subsription_joined_with_accounts.account_number,
+      subsription_joined_with_accounts.account_name,
+      subsription_joined_with_accounts.subscription_start_date, 
+      subsription_joined_with_accounts.subscription_version_end_date,
+      subsription_joined_with_accounts.subscription_version_term_start_date,
+      subsription_joined_with_accounts.subscription_version_term_end_date,
       SUM(tcv) AS tcv
       
-    FROM subscription_joined_with_accounts
+    FROM subsription_joined_with_accounts
     INNER JOIN zuora_rate_plan
-      ON subscription_joined_with_accounts.subscription_id = zuora_rate_plan.subscription_id
+      ON subsription_joined_with_accounts.subscription_id = zuora_rate_plan.subscription_id
     INNER JOIN zuora_rate_plan_charge
       ON zuora_rate_plan.rate_plan_id = zuora_rate_plan_charge.rate_plan_id
         -- remove refunded subscriptions
-      AND mrr > 0
-      AND tcv > 0
+        AND mrr > 0 AND tcv > 0
     WHERE (subscription_version_term_start_date  < min_following_subscription_version_term_start_date
       OR min_following_subscription_version_term_start_date IS NULL)
       -- remove cancelled subscription
@@ -93,11 +93,11 @@ SELECT
   *,
   CASE
     -- manual linked subscription
-    WHEN zuora_renewal_subscription_name_slugify IS NOT NULL
-      THEN TRUE
+    WHEN zuora_renewal_subscription_name_slugify IS NOT NULL THEN TRUE
     -- new version available, got renewed
-    WHEN LAG(subscription_name_slugify, 1) OVER (PARTITION BY subscription_name_slugify ORDER BY version DESC) IS NOT NULL
+    WHEN LEAD(subscription_name_slugify) OVER (PARTITION BY subscription_name_slugify ORDER BY version) IS NOT NULL
       THEN TRUE
     ELSE FALSE
   END AS is_renewed
 FROM subscription_joined_with_charges
+ORDER BY subscription_version_start_date
