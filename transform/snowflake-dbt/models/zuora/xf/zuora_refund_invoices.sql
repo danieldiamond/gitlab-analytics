630f8bbbe85215350a5f8644524b8195b1a6270c
Eli Kastelein <ekastelein@gitlab.com>
2020-01-22 14:42:27 -0800

    Fix to get it working

--- transform/snowflake-dbt/models/zuora/xf/zuora_refund_invoices.sql
+++ transform/snowflake-dbt/models/zuora/xf/zuora_refund_invoices.sql
@@ -11,23 +11,22 @@ WITH invoices AS (
     SELECT DISTINCT
       invoices.invoice_id,
       invoices.invoice_date,
+      invoices.amount,
+      invoices.account_id,
       DATE_TRUNC('month', invoices.invoice_date)::DATE AS refund_month,
       zuora_account.crm_id,
       zuora_account.sfdc_entity,
       zuora_account.account_name,
       zuora_account.account_number,
       zuora_account.currency,
-      SUM(COALESCE(before_and_after.amount, 0)) OVER (
-        PARTITION BY zuora_account.crm_id
-        ORDER BY invoice.invoice_date
-      ) AS before_and_after_amount_sum
+      SUM(COALESCE(before_and_after.amount, 0))        AS before_and_after_amount_sum
     FROM invoices
       LEFT JOIN zuora_account
         ON invoices.account_id = zuora_account.account_id
       LEFT JOIN invoices AS before_and_after
         ON invoices.account_id = before_and_after.account_id
         AND before_and_after.invoice_date BETWEEN DATEADD('days', -60, invoices.invoice_date) AND DATEADD('days', 60, invoices.invoice_date)
-    {{ dbt_utils.group_by(8) }}
+    {{ dbt_utils.group_by(10) }}
 )
 
 SELECT
