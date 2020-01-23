c4be1b0e0a59d3b7b9ef15222985c1d90fdcad29
Eli Kastelein <ekastelein@gitlab.com>
2020-01-22 16:00:30 -0800

    Initial commit: switch back

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_merge_requests_xf.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_merge_requests_xf.sql
@@ -9,14 +9,12 @@ WITH merge_requests AS (
       updated_at  AS merge_request_updated_at
     FROM {{ref('gitlab_dotcom_merge_requests')}}
 
-), label_states AS (
+), label_links AS (
 
-    SELECT
-      label_id,
-      merge_request_id
-    FROM {{ref('gitlab_dotcom_label_states_xf')}}
-    WHERE merge_request_id IS NOT NULL
-      AND latest_state = 'added'
+    SELECT *
+    FROM {{ref('gitlab_dotcom_label_links')}}
+    WHERE is_currently_valid = True
+      AND target_type = 'MergeRequest'
 
 ), all_labels AS (
 
@@ -29,10 +27,10 @@ WITH merge_requests AS (
       merge_requests.merge_request_id,
       ARRAY_AGG(LOWER(masked_label_title)) WITHIN GROUP (ORDER BY masked_label_title ASC) AS labels
     FROM merge_requests
-    LEFT JOIN label_states
-      ON merge_requests.merge_request_id = label_states.merge_request_id
+    LEFT JOIN label_links
+      ON merge_requests.merge_request_id = label_links.target_id
     LEFT JOIN all_labels
-      ON label_states.label_id = all_labels.label_id
+      ON label_links.label_id = all_labels.label_id
     GROUP BY merge_requests.merge_request_id
 
 ),  latest_merge_request_metric AS (
