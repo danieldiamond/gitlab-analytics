e77d021c2fea4f4e3ed031bc7a399e98ccb70187
Michael Walker <mwalker@gitlab.com>
2020-01-24 14:50:29 -0700

    narrow down the number of metrics to those that will most likely not crash the Cloud Function

--- extract/prometheus/src/execute.py
+++ extract/prometheus/src/execute.py
@@ -1,10 +1,10 @@
 import argparse
-from datetime import datetime
 import json
 import logging
 import sys
 import os
 from os import environ as env
+import pendulum
 import yaml
 
 from gitlabdata.orchestration_utils import (
@@ -16,12 +16,8 @@ from api import Prometheus
 
 # The api expects a specific timezone format
 def fixup_datetime_string_format(datetime_string):
-    if ":" == datetime_string[-3:-2]:
-        datetime_string = datetime_string[:-3] + datetime_string[-2:]
-    parsed_datetime = datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S%z")
-
-    # These are all UTC times, so we can just hack off the timezone and replace it with Z
-    return parsed_datetime.isoformat()[:-6] + "Z"
+    pendulum_datetime = pendulum.parse(datetime_string)
+    return pendulum_datetime.isoformat()[:-6] + "Z"
 
 
 if __name__ == "__main__":
@@ -41,34 +37,40 @@ if __name__ == "__main__":
     config_dict = env.copy()
     snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
-    metrics_to_load = [
-        "gitlab_workhorse_http_request_duration_seconds_sum",
-        "gitlab_workhorse_http_requests_total",
-        "http_request_duration_seconds_sum",
-        "http_request_duration_seconds_count",
-        "sidekiq_jobs_completion_seconds_sum",
-        "sidekiq_jobs_failed_total",
-        "sidekiq_jobs_queue_duration_seconds_sum",
-        "registry_http_request_duration_seconds_sum",
-        "registry_storage_action_seconds_sum",
-        "job_queue_duration_seconds_sum",
-        "gitlab_runner_jobs_total",
-        "gitlab_runner_failed_jobs_total",
-    ]
+    metrics_to_load = {
+        # "gitlab_workhorse_http_request_duration_seconds_bucket",
+        # "gitlab_workhorse_http_requests_total",
+        # "http_request_duration_seconds_bucket",
+        # "http_request_duration_seconds_count",
+        # "sidekiq_jobs_completion_seconds_bucket",
+        # "sidekiq_jobs_queue_duration_seconds_bucket",
+        # "registry_http_request_duration_seconds_bucket",
+        # "registry_storage_action_seconds_bucket",
+        "sidekiq_jobs_failed_total": "sidekiq_jobs_failed_total",
+        "job_queue_duration_seconds_bucket": "job_queue_duration_seconds_bucket",
+        "gitlab_runner_jobs_total": "gitlab_runner_jobs_total",
+        "gitlab_runner_failed_jobs_total": "gitlab_runner_failed_jobs_total",
+        "avg_over_time(slo_observation_status[1d])": "slo_observation_status",
+        "gitlab_service_errors:ratio": "gitlab_service_errors_ratio",
+    }
 
     start = fixup_datetime_string_format(args.start)
     end = fixup_datetime_string_format(args.end)
 
-    for metric in metrics_to_load:
-        logging.info(f"loading {metric} from prometheus for {args.start} to {args.end}")
-        file_name = f"{metric.upper()}.json"
+    for metric_name, table_name in metrics_to_load:
+        logging.info(
+            f"loading {metric_name} from prometheus for {args.start} to {args.end}"
+        )
+        file_name = f"{metric_name.upper()}.json"
         with open(file_name, "w") as outfile:
-            metric_data = prometheus_client.get_metric(start, end, metric, args.token)
+            metric_data = prometheus_client.get_metric(
+                start, end, metric_name, args.token
+            )
             json.dump(metric_data, outfile)
 
         snowflake_stage_load_copy_remove(
             file_name,
-            f"raw.prometheus.{metric}",
-            f"raw.prometheus.{metric}",
+            f"raw.prometheus.{table_name}",
+            f"raw.prometheus.{table_name}",
             snowflake_engine,
         )
