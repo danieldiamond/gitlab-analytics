702551bc28e54facf90df49f78baabc3de43a146
Parul Luthra <pluthra@gitlab.com>
2020-01-27 15:08:21 -0600

    updating pivot macro

---
+++ transform/snowflake-dbt/macros/utils/pivot.sql
@@ -0,0 +1,45 @@
+{% macro pivot(
+               column,
+               values,
+               then_value,
+               alias=True,
+               agg='sum',
+               cmp='=',
+               prefix='',
+               suffix='',
+               else_value=0,
+               quote_identifiers=True) %}
+
+  {% for v in values %}
+    {{ agg }}(
+      case
+      when {{ column }} {{ cmp }} '{{ v }}'
+        then {{ then_value }}
+      else {{ else_value }}
+      end
+    )
+    {% if alias %}
+      {% if quote_identifiers %}
+            as {{ adapter.quote(prefix ~ v ~ suffix) }}
+      {% else %}
+        as {{prefix ~ v ~ suffix }}
+      {% endif %}
+    {% endif %}
+    {% if not loop.last %},{% endif %}
+  {% endfor %}
+{% endmacro %}
+
+
+
+{# Arguments:
+    column: Column name, required
+    values: List of row values to turn into columns, required
+    then_value: Value to use if comparison succeeds
+    alias: Whether to create column aliases, default is True
+    agg: SQL aggregation function, default is sum
+    cmp: SQL value comparison, default is =
+    prefix: Column alias prefix, default is blank
+    suffix: Column alias postfix, default is blank  
+    else_value: Value to use if comparison fails, default is 0
+    quote_identifiers: Whether to surround column aliases with double quotes, default is true 
+    #}
--- transform/snowflake-dbt/models/bamboohr/rpt/rpt_headcount_aggregation.sql
+++ transform/snowflake-dbt/models/bamboohr/rpt/rpt_headcount_aggregation.sql
@@ -8,13 +8,13 @@ With overall_headcount_pivoted AS (
    
     SELECT 
         DATE_TRUNC('month',month_date)                  AS month_date,
-        {{ pivot_macro_in_bamboohr(
+        
+        
+        {{ pivot(
             'metric',
-            pivot_macro_in_bamboohr.get_column_values(ref('bamboohr_headcount_aggregation_intermediate'), 'metric')
-        ) }}
-
-        {# {{ pivot_macro_in_bamboohr('metric', ('headcount_start','headcount_end')) }} #}
-
+            get_column_values(ref('bamboohr_headcount_aggregation_intermediate'), 'metric'),
+            'total_count'
+        ) }} 
 
     FROM {{ ref('bamboohr_headcount_aggregation_intermediate') }}
     GROUP BY DATE_TRUNC('month',month_date)
