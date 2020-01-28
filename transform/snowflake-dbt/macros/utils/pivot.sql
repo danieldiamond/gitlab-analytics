11defc8763fc3725094aa9cdf0db2ef976115939
Parul Luthra <pluthra@gitlab.com>
2020-01-27 18:35:11 -0600

    updating macro read.me file

--- transform/snowflake-dbt/macros/utils/pivot.sql
+++ transform/snowflake-dbt/macros/utils/pivot.sql
@@ -30,16 +30,3 @@
 {% endmacro %}
 
 
-
-{# Arguments:
-    column: Column name, required
-    values: List of row values to turn into columns, required
-    then_value: Value to use if comparison succeeds
-    alias: Whether to create column aliases, default is True
-    agg: SQL aggregation function, default is sum
-    cmp: SQL value comparison, default is =
-    prefix: Column alias prefix, default is blank
-    suffix: Column alias postfix, default is blank  
-    else_value: Value to use if comparison fails, default is 0
-    quote_identifiers: Whether to surround column aliases with double quotes, default is true 
-    #}
