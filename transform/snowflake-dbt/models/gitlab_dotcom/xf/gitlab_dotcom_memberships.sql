0b32edae670947c0f81c24eb6625e86c7b1a5db6
Eli Kastelein <ekastelein@gitlab.com>
2020-01-29 12:25:02 -0800

    Spacing

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
@@ -4,22 +4,33 @@ WITH members AS (
     FROM {{ref('gitlab_dotcom_members')}}
     WHERE is_currently_valid = True
 
-), project_group_links AS (
+), 
+
+project_group_links AS (
 
   SELECT *
-  FROM {{ref('gitlab_dotcom_members')}}
+  FROM {{ref('gitlab_dotcom_project_group_links')}}
   WHERE is_currently_valid = True
 
-), project_group_links_unnested AS (
+), 
+
+project_group_links_unnested AS (
 
   SELECT
-    project_group_links.project_id,
+    project_group_links.groups_id,
     project_group_links.group_access,
     memmbers.user_id
   FROM project_group_links
     INNER JOIN members
       ON project_group_links.project_id = members.source_id
       AND members.member_source_type = 'Project'
+
+),
+
+unioned AS (
+
+  SELECT *
+  FROM members
 
 )
 
