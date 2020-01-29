c3d37ecd70df2fd6dfe99bea817a83c6e1229dc2
Eli Kastelein <ekastelein@gitlab.com>
2020-01-29 12:31:57 -0800

    Fleshing out the unnested

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
@@ -8,22 +8,41 @@ WITH members AS (
 
 project_group_links AS (
 
-  SELECT *
-  FROM {{ref('gitlab_dotcom_project_group_links')}}
-  WHERE is_currently_valid = True
+    SELECT *
+    FROM {{ref('gitlab_dotcom_project_group_links')}}
+    WHERE is_currently_valid = True
 
 ), 
 
-project_group_links_unnested AS (
+namespace_lineage AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_namespace_lineage')}}
+
+),
+
+projects AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_projects')}}
+
+),
+
+project_group_links_unnested AS ( -- Where groups are invited to projects.
 
   SELECT
-    project_group_links.groups_id,
+    namespace_lineage.ultimate_parent_id, -- Ultimate parent group of the project.
+    project_group_links.project_id
     project_group_links.group_access,
-    memmbers.user_id
+    members.user_id
   FROM project_group_links
     INNER JOIN members
       ON project_group_links.project_id = members.source_id
       AND members.member_source_type = 'Project'
+    INNER JOIN projects
+      ON project_group_links.project_id = projects.project_id
+    LEFT JOIN namespace_lineage
+      ON projects.namespace_id = namespace_lineage.namespace_id
 
 ),
 
