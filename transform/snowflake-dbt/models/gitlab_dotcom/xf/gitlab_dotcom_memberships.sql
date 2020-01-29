e960a205aa528a55b26278784cb7e8fc02ee6433
Eli Kastelein <ekastelein@gitlab.com>
2020-01-29 12:20:18 -0800

    Start unnesting

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
@@ -8,9 +8,16 @@ WITH members AS (
   SELECT *
   FROM {{ref('gitlab_dotcom_members')}}
 
+), project_group_links_unnested AS (
+
+  SELECT
+    project_group_links.project_id,
+    memmbers.user_id
+  FROM project_group_links
+    INNER JOIN members
+      ON project_group_links.project_id = members.source_id
+      AND members.member_source_type = 'Project'
+
 )
 
-project_group_links_unnested AS (
-
-  
-)
+SELECT * FROM project_group_links_unnested
