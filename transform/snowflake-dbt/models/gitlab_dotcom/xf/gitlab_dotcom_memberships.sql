59c745a694417d71cea1d87603a973131b1fb731
Eli Kastelein <ekastelein@gitlab.com>
2020-01-29 12:22:14 -0800

    Refine with WHERE

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
@@ -2,16 +2,19 @@ WITH members AS (
 
     SELECT *
     FROM {{ref('gitlab_dotcom_members')}}
+    WHERE is_currently_valid = True
 
 ), project_group_links AS (
 
   SELECT *
   FROM {{ref('gitlab_dotcom_members')}}
+  WHERE is_currently_valid = True
 
 ), project_group_links_unnested AS (
 
   SELECT
     project_group_links.project_id,
+    project_group_links.group_access,
     memmbers.user_id
   FROM project_group_links
     INNER JOIN members
