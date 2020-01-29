76d0f9a251b7e506035512d940e8bff792a3e799
Eli Kastelein <ekastelein@gitlab.com>
2020-01-29 12:50:15 -0800

    Get the union working

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_memberships.sql
@@ -4,6 +4,8 @@ WITH members AS (
     FROM {{ref('gitlab_dotcom_members')}}
     WHERE is_currently_valid = True
 
+    LIMIT 10000 --TODO
+
 ), 
 
 project_group_links AS (
@@ -28,29 +30,71 @@ projects AS (
 
 ),
 
+group_members AS (
+
+    SELECT *
+    FROM members
+    WHERE member_source_type = 'Namespace'
+
+),
+
+project_members AS (
+
+    SELECT *
+    FROM members
+    WHERE member_source_type = 'Project'
+
+),
+
 project_group_links_unnested AS ( -- Where groups are invited to projects.
 
-  SELECT
-    namespace_lineage.ultimate_parent_id, -- Ultimate parent group of the project.
-    project_group_links.project_id
-    project_group_links.group_access,
-    members.user_id
-  FROM project_group_links
-    INNER JOIN members
-      ON project_group_links.project_id = members.source_id
-      AND members.member_source_type = 'Project'
-    INNER JOIN projects
-      ON project_group_links.project_id = projects.project_id
-    LEFT JOIN namespace_lineage
-      ON projects.namespace_id = namespace_lineage.namespace_id
+    SELECT
+      projects.namespace_id, -- The group that the project belongs to
+      project_group_links.project_group_link_id,
+      project_group_links.project_id,
+      project_group_links.group_access AS access_level,
+      group_members.user_id
+    FROM project_group_links
+      INNER JOIN group_members
+        ON project_group_links.project_id = group_members.source_id
+      INNER JOIN projects
+        ON project_group_links.project_id = projects.project_id
 
 ),
 
 unioned AS (
 
-  SELECT *
-  FROM members
+  SELECT
+    source_id          AS namespace_id,
+    user_id,
+    access_level,
+    source_id          AS membership_source,
+    'group_membership' AS membership_type
+  FROM group_members
 
+  UNION 
+
+  SELECT
+    source_id AS namespace_id, --TODO
+    user_id,
+    access_level,
+    source_id            AS membership_source,
+    'project_membership' AS membership_type
+  FROM project_members
+
+  UNION
+
+  SELECT 
+    namespace_id,
+    user_id,
+    access_level,
+    project_group_link_id AS membership_source,
+    'project_group_link'  AS membership_type
+
+  FROM project_group_links_unnested
 )
+)
+
+
 
 SELECT * FROM project_group_links_unnested
