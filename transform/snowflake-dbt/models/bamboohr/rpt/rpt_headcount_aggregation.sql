8688fdb31308d62d5f016bed2bd481fb2f972ea2
Parul Luthra <pluthra@gitlab.com>
2020-01-29 12:33:16 -0600

    adding windows function to aggregate for the last 12 months also adding a base table to make sure each diversity field has the same number of month_date records to aggregate appropriately

--- transform/snowflake-dbt/models/bamboohr/rpt/rpt_headcount_aggregation.sql
+++ transform/snowflake-dbt/models/bamboohr/rpt/rpt_headcount_aggregation.sql
@@ -4,7 +4,14 @@
     })
 }}
 
-With overall_headcount_pivoted AS (
+With date_details AS (
+
+    SELECT distinct(DATE_TRUNC('month',date_actual))    AS month_date,
+            'join'                                      AS join_field
+    FROM {{ref('date_details')}} 
+    WHERE date_actual <= {{max_date_in_bamboo_analyses()}}
+    
+), overall_headcount_pivoted AS (
    
     SELECT 
         DATE_TRUNC('month',month_date)                  AS month_date,  
@@ -48,29 +55,63 @@ With overall_headcount_pivoted AS (
         (headcount_start + headcount_end)/2                                 AS average_headcount
     FROM gender_headcount_pivoted
 
-), final AS(
+), diversity_fields AS (
 
     SELECT 
-      aggregated.month_date,
-      aggregated.diversity_field,
-      aggregated.aggregation_type,
+        DISTINCT diversity_field, 
+        aggregation_type,
+        'join'                                                              AS join_field
+    FROM aggregated
+
+), base AS (
+
+    SELECT 
+        date_details.*,
+        diversity_field,
+        aggregation_type
+    FROM date_details
+    LEFT JOIN diversity_fields
+      ON date_details.join_field = diversity_fields.join_field
+
+), intermediate AS(
+
+    SELECT 
+      base.month_date,
+      base.diversity_field,
+      base.aggregation_type,
       aggregated.headcount_start,
       aggregated.headcount_end,  
       aggregated.hires,
-      AVG(a2.average_headcount)                                             AS rolling_12_month_headcount,    
-      SUM(a2.total_separated)                                               AS rolling_12_month_separations,
-      SUM(a2.voluntary_separations)                                         AS rolling_12_month_voluntary_separations,
-      SUM(a2.involuntary_separations)                                       AS rolling_12_month_involuntary_separations,
-      IFF(AVG(a2.average_headcount)<sum(a2.total_separated), null,
-          1 -  (SUM(a2.total_separated) / avg(a2.average_headcount) ))      AS retention 
-FROM aggregated
-LEFT JOIN aggregated a2 
-  ON a2.month_date BETWEEN DATEADD('month', -11, aggregated.month_date) AND aggregated.month_date
-  AND a2.aggregation_type = aggregated.aggregation_type
-  AND a2.diversity_field = aggregated.diversity_field
-GROUP BY 1,2,3,4,5,6
+      AVG(aggregated.average_headcount) 
+        OVER (PARTITION BY base.diversity_field, base.aggregation_type
+        ORDER BY base.month_date DESC
+        ROWS BETWEEN CURRENT ROW AND 11 FOLLOWING)                              AS rolling_12_month_headcount,                            
+      SUM(total_separated) 
+        OVER (PARTITION BY base.diversity_field, base.aggregation_type 
+        ORDER BY base.month_date DESC  
+        ROWS BETWEEN CURRENT ROW AND 11 FOLLOWING )                             AS rolling_12_month_separations,
+      SUM(voluntary_separations) 
+        OVER (PARTITION BY base.diversity_field, base.aggregation_type 
+        ORDER BY base.month_date DESC  
+        ROWS BETWEEN CURRENT ROW AND 11 FOLLOWING )                             AS rolling_12_month_voluntary_separations,
+      SUM(involuntary_separations) 
+        OVER (PARTITION BY base.diversity_field, base.aggregation_type 
+        ORDER BY base.month_date DESC  
+        ROWS BETWEEN CURRENT ROW AND 11 FOLLOWING )                             AS rolling_12_month_involuntary_separations
+FROM base
+LEFT JOIN aggregated 
+  ON base.month_date = aggregated.month_date
+  AND base.diversity_field = aggregated.diversity_field
+  AND base.aggregation_type = aggregated.aggregation_type
+
+), final AS (
+
+    SELECT 
+      intermediate.*,
+      1 - (rolling_12_month_separations/rolling_12_month_headcount)             AS retention
+    FROM intermediate
 
 )
 
-SELECT *
-FROM final
+SELECT * 
+FROM final 
