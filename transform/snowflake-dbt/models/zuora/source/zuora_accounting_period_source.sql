f868e40d17b39f0afa6d27a63262ba72672df39a
iweeks <iweeks@gitlab.com>
2020-01-30 17:11:37 -0300

    fix names

--- transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
@@ -7,7 +7,16 @@ WITH source AS (
 
     SELECT
       --Primary Keys
-<<<<<<< HEAD
+      id::VARCHAR                       AS accounting_period_id,
+
+      --Info
+      enddate::TIMESTAMP_TZ             AS end_date,
+      fiscalyear::NUMBER                AS fiscal_year,
+      name::VARCHAR                     AS accounting_period_name,
+      startdate::TIMESTAMP_TZ           AS accounting_period_start_date,
+      status::VARCHAR                   AS accounting_period_status,
+      updatedbyid::VARCHAR              AS updated_by_id,
+      updateddate::TIMESTAMP_TZ         AS updated_date,
       id::VARCHAR                       AS accounting_period_id,
 
       --Info
@@ -18,19 +27,7 @@ WITH source AS (
       status::VARCHAR                   AS accounting_period_status,
       updatedbyid::VARCHAR              AS updated_by_id,
       updateddate::TIMESTAMP_TZ         AS updated_date
-=======
-      id::VARCHAR                     AS accounting_period_id,
 
-      --Info
-      enddate::TIMESTAMP_TZ           AS end_date,
-      fiscalyear::NUMBER              AS fiscal_year,
-      name::VARCHAR                   AS accounting_period_name,
-      startdate::TIMESTAMP_TZ         AS accounting_period_start_date,
-      status::VARCHAR                 AS accounting_period_status,
-      updatedbyid::VARCHAR            AS updated_by_id,
-      updateddate::TIMESTAMP_TZ       AS updated_date
-
->>>>>>> 7df100be211277b1c18b33e3fb5dae00989cc1a4
     FROM source
 
 )
--- transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
@@ -7,7 +7,16 @@ WITH source AS (
 
     SELECT
       --Primary Keys
-<<<<<<< HEAD
+
+      id::VARCHAR                       AS product_id,
+
+      --Info
+      name::VARCHAR                     AS product_name,
+      sku::VARCHAR                      AS sku,
+      description::VARCHAR              AS product_description,
+      category::VARCHAR                 AS category,
+      updatedbyid::VARCHAR              AS updated_by_id,
+      updateddate::TIMESTAMP_TZ         AS updated_date,
       id::VARCHAR                       AS product_id,
 
       --Info
@@ -17,18 +26,7 @@ WITH source AS (
       category::VARCHAR                 AS category,
       updatedbyid::VARCHAR              AS updated_by_id,
       updateddate::TIMESTAMP_TZ         AS updated_date
-=======
-      id::VARCHAR                   AS product_id,
 
-      --Info
-      name::VARCHAR                 AS product_name,
-      sku::VARCHAR                  AS sku,
-      description::VARCHAR          AS product_description,
-      category::VARCHAR             AS category,
-      updatedbyid::VARCHAR          AS updated_by_id,
-      updateddate::TIMESTAMP_TZ     AS updated_date
-
->>>>>>> 7df100be211277b1c18b33e3fb5dae00989cc1a4
     FROM source
 
 )
