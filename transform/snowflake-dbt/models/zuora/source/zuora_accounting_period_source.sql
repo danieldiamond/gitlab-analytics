13248ce1c352797893092858e7f953e338d288a1
Taylor Murphy <tmurphy@gitlab.com>
2020-01-30 13:04:48 -0600

    Update spacing

--- transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
@@ -1,29 +1,24 @@
-{{config({
-    "schema": "staging"
-  })
-}}
-
 WITH source AS (
 
-  SELECT *
-  FROM {{ source('zuora', 'accounting_period') }}
+    SELECT *
+    FROM {{ source('zuora', 'accounting_period') }}
 
 ), renamed AS (
 
-  SELECT
-    --Primary Keys
-    id::VARCHAR                            AS accounting_period_id,
+    SELECT
+      --Primary Keys
+      id::VARCHAR                     AS accounting_period_id,
 
-    --Info
-	enddate::TIMESTAMP_TZ				   AS end_date,
-	fiscalyear::NUMBER					   AS fiscal_year,
-	name::VARCHAR						   AS accounting_period_name,
-	startdate::TIMESTAMP_TZ				   AS accounting_period_start_date,
-	status::VARCHAR						   AS accounting_period_status,
-    updatedbyid::VARCHAR                   AS updated_by_id,
-    updateddate::TIMESTAMP_TZ              AS updated_date
+      --Info
+      enddate::TIMESTAMP_TZ           AS end_date,
+      fiscalyear::NUMBER              AS fiscal_year,
+      name::VARCHAR                   AS accounting_period_name,
+      startdate::TIMESTAMP_TZ         AS accounting_period_start_date,
+      status::VARCHAR                 AS accounting_period_status,
+      updatedbyid::VARCHAR            AS updated_by_id,
+      updateddate::TIMESTAMP_TZ       AS updated_date
 
-  FROM source
+    FROM source
 
 )
 
--- transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
@@ -1,28 +1,23 @@
-{{config({
-    "schema": "staging"
-  })
-}}
-
 WITH source AS (
 
-  SELECT *
-  FROM {{ source('zuora', 'product') }}
+    SELECT *
+    FROM {{ source('zuora', 'product') }}
 
 ), renamed AS (
 
-  SELECT
-    --Primary Keys
-    id::VARCHAR                            AS product_id,
+    SELECT
+      --Primary Keys
+      id::VARCHAR                   AS product_id,
 
-    --Info
-	name::VARCHAR						   AS product_name,
-	sku::VARCHAR						   AS sku,
-	description::VARCHAR				   AS product_description,
-	category::VARCHAR					   AS category,
-    updatedbyid::VARCHAR                   AS updated_by_id,
-    updateddate::TIMESTAMP_TZ              AS updated_date
+      --Info
+      name::VARCHAR                 AS product_name,
+      sku::VARCHAR                  AS sku,
+      description::VARCHAR          AS product_description,
+      category::VARCHAR             AS category,
+      updatedbyid::VARCHAR          AS updated_by_id,
+      updateddate::TIMESTAMP_TZ     AS updated_date
 
-  FROM source
+    FROM source
 
 )
 
