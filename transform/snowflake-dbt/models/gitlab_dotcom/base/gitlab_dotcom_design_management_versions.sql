5beef3fac28b43f293c61bd4bbcf2b58fcb66d74
Mathieu Peychet <mpeychet@gitlab.com>
2020-01-30 09:21:21 +0000

    Apply suggestion to transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_design_management_versions.sql

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_design_management_versions.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_design_management_versions.sql
@@ -7,7 +7,7 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-      MD5(id)::VARCHAR                            AS version_id,
+      id::INTEGER                                 AS version_id,
       issue_id::INTEGER                           AS issue_id,
       created_at::TIMESTAMP                       AS created_at,
       author_id::INTEGER                          AS author_id
