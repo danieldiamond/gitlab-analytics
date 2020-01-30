ab23103b8d40f736cef8ed329337d16061eb5a9c
Taylor Murphy <tmurphy@gitlab.com>
2020-01-30 14:42:56 -0600

    Switch to upgrades containers

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -8,8 +8,8 @@ from airflow.contrib.kubernetes.pod import Resources
 from airflow.operators.slack_operator import SlackAPIPostOperator
 
 REPO = "https://gitlab.com/gitlab-data/analytics.git"
-DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:latest"
-DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:latest"
+DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:upgrades"
+DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:upgrades"
 
 
 def split_date_parts(day: date, partition: str) -> List[dict]:
