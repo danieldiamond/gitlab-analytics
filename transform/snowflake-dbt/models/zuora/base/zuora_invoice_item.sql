af91bea5cf1dd6dc4495f6e3ce800948e8680497
Taylor Murphy <tmurphy@gitlab.com>
2020-01-30 09:31:26 -0600

    Add new source model layer

--- transform/snowflake-dbt/models/zuora/base/zuora_invoice_item.sql
+++ transform/snowflake-dbt/models/zuora/base/zuora_invoice_item.sql
@@ -1,56 +1,10 @@
-{{config({
-    "schema": "staging"
-  })
-}}
-
 WITH source AS (
 
     SELECT *
-    FROM {{ source('zuora', 'invoice_item') }}
+    FROM {{ ref('zuora_invoice_item_source') }}
 
-), renamed AS (
-
-    SELECT
-      -- keys
-      id                     AS invoice_item_id,
-      invoiceid              AS invoice_id,
-      appliedtoinvoiceitemid AS applied_to_invoice_item_id,
-      rateplanchargeid       AS rate_plan_charge_id,
-      subscriptionid         AS subscription_id,
-
-
-      -- invoice item metadata
-      accountingcode         AS accounting_code,
-      productid              AS product_id,
-      --revrecstartdate        AS revenue_recognition_start_date,
-      serviceenddate         AS service_end_date,
-      servicestartdate       AS service_start_date,
-
-
-      -- financial info
-      chargeamount           AS charge_amount,
-      chargedate             AS charge_date,
-      chargename             AS charge_name,
-      processingtype         AS processing_type,
-      quantity               AS quantity,
-      sku                    AS sku,
-      taxamount              AS tax_amount,
-      taxcode                AS tax_code,
-      taxexemptamount        AS tax_exempt_amount,
-      taxmode                AS tax_mode,
-      uom                    AS unit_of_measure,
-      unitprice              AS unit_price,
-
-      -- metadata
-      createdbyid            AS created_by_id,
-      createddate            AS created_date,
-      updatedbyid            AS updated_by_id,
-      updateddate            AS updated_date
-
-
-    FROM source
-    WHERE deleted = FALSE
 )
 
 SELECT *
-FROM renamed
+FROM source
+WHERE is_deleted = FALSE
--- transform/snowflake-dbt/models/zuora/base/zuora_rate_plan_charge.sql
+++ transform/snowflake-dbt/models/zuora/base/zuora_rate_plan_charge.sql
@@ -1,94 +1,11 @@
--- depends_on: {{ ref('zuora_excluded_accounts') }}
-
--- values to consider renaming:
--- mrr
--- dmrc
--- dtcv
--- tcv
--- uom
-
-{{config({
-    "schema": "staging"
-  })
-}}
-
-
 WITH source AS (
 
 	SELECT *
-    FROM {{ source('zuora', 'rate_plan_charge') }}
+    FROM {{ ref('zuora_rate_plan_charge_source') }}
 
-), renamed AS(
-
-	SELECT
-	    id                              									AS rate_plan_charge_id,
-		name                            									AS rate_plan_charge_name,
-		--keys	
-		originalid                      									AS original_id,
-		rateplanid                      									AS rate_plan_id,
-		productrateplanchargeid         									AS product_rate_plan_charge_id,
-
-		--recognition
-		revenuerecognitionrulename      									AS revenue_recognition_rule_name,
-		revreccode                      									AS revenue_recognition_code,
-		revrectriggercondition          									AS revenue_recognition_trigger_condition,
-
-		-- info
-		effectivestartdate              									AS effective_start_date,
-		effectiveenddate                									AS effective_end_date,
-		date_trunc('month', effectivestartdate)::DATE 						AS effective_start_month,
-  		add_months(date_trunc('month', effectiveenddate)::DATE, -1)::DATE 	AS effective_end_month,
-		enddatecondition                									AS end_date_condition,
-
-		mrr,
-		quantity                        									AS quantity,
-		tcv,
-		uom																	AS unit_of_measure,
-
-        accountid                                                           AS account_id,
-		accountingcode                  									AS accounting_code,
-		applydiscountto                 									AS apply_discount_to,
-		billcycleday                    									AS bill_cycle_day,
-		billcycletype                   									AS bill_cycle_type,
-		billingperiod                   									AS billing_period,
-		billingperiodalignment          									AS billing_period_alignment,
-		chargedthroughdate              									AS charged_through_date,
-		chargemodel                     									AS charge_model,
-		chargenumber                    									AS rate_plan_charge_number,
-		chargetype                      									AS charge_type,
-		description                     									AS description,
-		discountlevel                   									AS discount_level,
-		dmrc                            									AS delta_mrc, -- delta monthly recurring charge
-		dtcv                            									AS delta_tcv, -- delta total contract value
-
-		islastsegment                   									AS is_last_segment,
-		listpricebase                   									AS list_price_base,
-		--numberofperiods                 									AS number_of_periods,
-		overagecalculationoption        									AS overage_calculation_option,
-		overageunusedunitscreditoption  									AS overage_unused_units_credit_option,
-		processedthroughdate            									AS processed_through_date,
-		
-		segment                         									AS segment,
-		specificbillingperiod           									AS specific_billing_period,
-		specificenddate                 									AS specific_end_date,
-		triggerdate                     									AS trigger_date,
-		triggerevent                    									AS trigger_event,
-		uptoperiods                     									AS up_to_period,
-		uptoperiodstype                 									AS up_to_periods_type,
-		version                         									AS version,
-
-		--ext1, ext2, ext3, ... ext13
-
-		--metadata
-		createdbyid                     									AS created_by_id,
-		createddate                     									AS created_date,
-		updatedbyid                     									AS updated_by_id,
-		updateddate                     									AS updated_date
-
-	FROM source
-	WHERE deleted = FALSE
-	  AND account_id NOT IN ({{ zuora_excluded_accounts() }})
 )
 
 SELECT *
-FROM renamed
+FROM source
+WHERE is_deleted = FALSE
+  AND account_id NOT IN ({{ zuora_excluded_accounts() }})
