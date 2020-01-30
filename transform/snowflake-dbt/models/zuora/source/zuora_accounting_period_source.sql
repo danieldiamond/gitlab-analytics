670d2cbee445155ace41ac9c7bc34687110b32b3
iweeks <iweeks@gitlab.com>
2020-01-30 17:27:48 -0300

    naming

--- transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_accounting_period_source.sql
@@ -16,16 +16,6 @@ WITH source AS (
       startdate::TIMESTAMP_TZ           AS accounting_period_start_date,
       status::VARCHAR                   AS accounting_period_status,
       updatedbyid::VARCHAR              AS updated_by_id,
-      updateddate::TIMESTAMP_TZ         AS updated_date,
-      id::VARCHAR                       AS accounting_period_id,
-
-      --Info
-      enddate::TIMESTAMP_TZ             AS end_date,
-      fiscalyear::NUMBER                AS fiscal_year,
-      name::VARCHAR                     AS accounting_period_name,
-      startdate::TIMESTAMP_TZ           AS accounting_period_start_date,
-      status::VARCHAR                   AS accounting_period_status,
-      updatedbyid::VARCHAR              AS updated_by_id,
       updateddate::TIMESTAMP_TZ         AS updated_date
 
     FROM source
--- transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
+++ transform/snowflake-dbt/models/zuora/source/zuora_product_source.sql
@@ -7,16 +7,6 @@ WITH source AS (
 
     SELECT
       --Primary Keys
-
-      id::VARCHAR                       AS product_id,
-
-      --Info
-      name::VARCHAR                     AS product_name,
-      sku::VARCHAR                      AS sku,
-      description::VARCHAR              AS product_description,
-      category::VARCHAR                 AS category,
-      updatedbyid::VARCHAR              AS updated_by_id,
-      updateddate::TIMESTAMP_TZ         AS updated_date,
       id::VARCHAR                       AS product_id,
 
       --Info
