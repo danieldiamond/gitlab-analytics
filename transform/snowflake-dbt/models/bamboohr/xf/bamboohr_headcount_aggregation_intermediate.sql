f47d62817d22dabc64c81dd2637293f5696d7c51
emilie <emilie.burke@gmail.com>
2020-01-30 19:48:01 -0500

    update column set

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_headcount_aggregation_intermediate.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_headcount_aggregation_intermediate.sql
@@ -1,26 +1,28 @@
-With dates AS (
-      
-    SELECT 
+{% set repeated_column_names = "gender, ethnicity, nationality, region," %}
+
+with dates AS (
+
+    SELECT
       date_actual                                 AS start_date,
       LAST_DAY(date_actual)                       AS end_date
     FROM {{ ref ('date_details') }}
-    WHERE date_day <= LAST_DAY(current_date) 
-       AND day_of_month = 1 
+    WHERE date_day <= LAST_DAY(current_date)
+       AND day_of_month = 1
        AND date_actual >= '2013-07-01' -- min employment_status_date in bamboohr_employment_status model
 
 ), mapping AS (
-  
-  SELECT * 
+
+  SELECT *
   FROM {{ref('bamboohr_id_employee_number_mapping')}}
-    
+
 ), bamboohr_employment_status_xf as (
 
-  SELECT * 
+  SELECT *
   FROM {{ref('bamboohr_employment_status_xf')}}
 
-), employees AS (  
-  
-    SELECT 
+), employees AS (
+
+    SELECT
       bamboohr_employment_status_xf.*,
       COALESCE(mapping.gender,'unidentified')             AS gender,
       COALESCE(mapping.ethnicity, 'unidentified')         AS ethnicity,
@@ -28,133 +30,108 @@ With dates AS (
       COALESCE(mapping.region, 'unidentified')            AS region,
       ROW_NUMBER() OVER (PARTITION BY bamboohr_employment_status_xf.employee_id ORDER BY valid_from_date) AS employee_status_event
     FROM bamboohr_employment_status_xf
-    LEFT JOIN mapping 
+    LEFT JOIN mapping
        ON bamboohr_employment_status_xf.employee_id = mapping.employee_id
-      
+
 ),headcount_start AS (
- 
-    SELECT 
-      start_date                                          AS month_date,            
-      gender,
-      ethnicity,
-      nationality,
-      region,
+
+    SELECT
+      start_date                                          AS month_date,
+      {{repeated_column_names}}
       COUNT(employee_id)                                  AS headcount_start
     FROM dates
     LEFT JOIN employees
       ON dates.start_date BETWEEN valid_from_date AND valid_to_date
-    GROUP BY 1,2,3,4,5      
-  
+    GROUP BY 1,2,3,4,5
+
 ), headcount_end AS (
-  
-    SELECT 
+
+    SELECT
       end_date,
-      gender,
-      ethnicity,
-      nationality,
-      region,
-      COUNT(employee_id)                                  AS headcount_end  
+      {{repeated_column_names}}
+      COUNT(employee_id)                                  AS headcount_end
   FROM dates
-  LEFT JOIN employees 
+  LEFT JOIN employees
     ON dates.end_date BETWEEN valid_from_date AND valid_to_date
-  GROUP BY 1,2,3,4,5  
-  
+  GROUP BY 1,2,3,4,5
+
 ), separated AS (
-  
-    SELECT 
+
+    SELECT
       DATE_TRUNC('month',dates.start_date)                AS separation_month,
-      gender,
-      ethnicity,
-      nationality,
-      region,
+      {{repeated_column_names}}
       COUNT(employee_id)                                  AS total_separated,
       SUM(IFF(termination_type = 'Voluntary',1,0))        AS voluntary_separation,
-      SUM(IFF(termination_type = 'Involuntary',1,0))      AS involuntary_separation  
+      SUM(IFF(termination_type = 'Involuntary',1,0))      AS involuntary_separation
   FROM dates
-  LEFT JOIN employees 
-    ON DATE_TRUNC('month',dates.start_date) = DATE_TRUNC('month',valid_from_date) 
+  LEFT JOIN employees
+    ON DATE_TRUNC('month',dates.start_date) = DATE_TRUNC('month',valid_from_date)
   WHERE employment_status='Terminated'
   GROUP BY 1,2,3,4,5
-  
+
 ), hires AS (
 
-    SELECT 
+    SELECT
       DATE_TRUNC('month', dates.start_date)          AS hire_month,
-      gender,
-      ethnicity,
-      nationality,
-      region,
+      {{repeated_column_names}}
       SUM(IFF(employee_status_event = 1,1,0))        AS total_hired
-    FROM dates 
-    LEFT JOIN employees 
-       ON DATE_TRUNC('month',dates.start_date) = DATE_TRUNC('month',valid_from_date) 
+    FROM dates
+    LEFT JOIN employees
+       ON DATE_TRUNC('month',dates.start_date) = DATE_TRUNC('month',valid_from_date)
     WHERE employment_status<>'Terminated'
-    GROUP BY 1,2,3,4,5    
-  
+    GROUP BY 1,2,3,4,5
+
 ), aggregated AS (
 
     SELECT
       month_date,
-      gender,
-      ethnicity,
-      nationality,
-      region,
+      {{repeated_column_names}}
       headcount_start               AS total_count,
      'headcount_start'              AS metric
-    FROM headcount_start                                
-    
+    FROM headcount_start
+
     UNION ALL
-  
+
     SELECT
        headcount_end.*,
        'headcount_end'              AS metric
     FROM headcount_end
- 
+
     UNION ALL
-  
-    SELECT 
+
+    SELECT
        hires.*,
        'hires'                      AS metric
     FROM hires
-  
+
     UNION ALL
 
-    SELECT  
-      separation_month, 
-      gender,
-      ethnicity,
-      nationality,
-      region,
+    SELECT
+      separation_month,
+      {{repeated_column_names}}
       total_separated,
       'total_separated'             AS metric
     FROM separated
-  
+
     UNION ALL
-  
+
     SELECT
-      separation_month, 
-      gender,
-      ethnicity,
-      nationality,
-      region,
+      separation_month,
+      {{repeated_column_names}}
       voluntary_separation,
       'voluntary_separations'       AS metric
     FROM separated
-  
+
     UNION ALL
-  
+
     SELECT
-      separation_month, 
-      gender,
-      ethnicity,
-      nationality,
-      region,
+      separation_month,
+      {{repeated_column_names}}
       involuntary_separation,
       'involuntary_separations'     AS metric
   FROM separated
-  
+
  )
- 
- SELECT * 
+
+ SELECT *
  FROM aggregated
- 
