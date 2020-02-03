7176e1a01d8030cc950b69b15eccff8596d40396
Michael Walker <mwalker@gitlab.com>
2020-02-03 16:02:14 -0700

    Add base model for engineering pop_mr source

---
+++ transform/snowflake-dbt/models/engineering/base/engineering_part_of_product_merge_requests.sql
@@ -0,0 +1,11 @@
+WITH source AS (
+    SELECT *
+    FROM {{ source('engineering', 'part_of_product_merge_requests') }
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY JSONTEXT['plain_diff_path'] ORDER BY uploaded_at DESC) = 1
+)
+SELECT 
+    JSONTEXT['added_lines']::bigint     AS added_lines,
+    JSONTEXT['real_size']::varchar       AS real_size, --this occasionally has `+` - ie `374+`
+    JSONTEXT['removed_lines']::bigint   AS removed_lines,
+    JSONTEXT['plain_diff_path']::varchar AS plain_diff_url_path
+FROM source
