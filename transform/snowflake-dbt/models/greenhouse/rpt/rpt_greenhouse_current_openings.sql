2d18bb24ccc1e12204dd0d40cdfb711286c43001
Parul Luthra <pluthra@gitlab.com>
2020-02-03 16:16:44 -0600

    removing config at top of rpts

--- transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_current_openings.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_current_openings.sql
@@ -1,8 +1,3 @@
-{{ config({
-    "schema": "analytics"
-    })
-}}
-
 WITH greenhouse_openings AS (
 
     SELECT * 
--- transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_hired_employees_opening_ids.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_hired_employees_opening_ids.sql
@@ -1,8 +1,3 @@
-{{ config({
-    "schema": "analytics"
-    })
-}}
-
 WITH employees AS (
 
     SELECT * 
