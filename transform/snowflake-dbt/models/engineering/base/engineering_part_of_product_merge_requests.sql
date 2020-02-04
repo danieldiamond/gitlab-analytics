c116d886475e69164768d8304303253b86c1eac5
Michael Walker <mwalker@gitlab.com>
2020-02-04 21:21:37 +0000

    Apply suggestion to transform/snowflake-dbt/models/engineering/base/engineering_part_of_product_merge_requests.sql

--- transform/snowflake-dbt/models/engineering/base/engineering_part_of_product_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/base/engineering_part_of_product_merge_requests.sql
@@ -4,7 +4,7 @@ WITH source AS (
     QUALIFY ROW_NUMBER() OVER (PARTITION BY jsontext['plain_diff_path'] ORDER BY ARRAY_SIZE(jsontext['merge_request_diffs']) DESC, uploaded_at DESC) = 1
 )
 SELECT 
-    jsontext['added_lines']::bigint      AS added_lines,
+    jsontext['added_lines']::BIGINT      AS added_lines,
     jsontext['real_size']::varchar       AS real_size, --this occasionally has `+` - ie `374+`
     jsontext['removed_lines']::bigint    AS removed_lines,
     jsontext['plain_diff_path']::varchar AS plain_diff_url_path
