07362f3ce2effb6354a99b0fa0eb47db16aea931
Parul Luthra <pluthra@gitlab.com>
2020-02-03 21:46:13 -0600

    updating model to unpivot eeoc fields and using eeoc field name and eeoc field value in the metrics

--- transform/snowflake-dbt/models/greenhouse/rpt/greenhouse_diversity_lifecycle_gender.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/greenhouse_diversity_lifecycle_gender.sql
@@ -1,6 +1,4 @@
-
-{% set repeated_column_names = "month_date, gender" %}
-
+{% set repeated_column_names = "month_date, eeoc_field_name, eeoc_values " %}
 
 WITH date_details AS (
   
@@ -10,13 +8,12 @@ WITH date_details AS (
     FROM {{ ref ('date_details') }}
     WHERE date_actual <= {{max_date_in_bamboo_analyses()}}
     AND day_of_month = 1 
-    AND date_actual >= '2018-09-01'
+    AND date_actual >= '2018-09-01' -- 1st date we started capturing eeoc data
 
 ), applications AS (
     
     SELECT * 
     FROM {{ ref ('greenhouse_applications') }}
-    WHERE applied_at >= '2018-09-01'
 
 ), offers AS (
     
@@ -37,8 +34,8 @@ WITH date_details AS (
 ), eeoc_fields AS (
 
     SELECT 
-      DISTINCT eeoc_field_name        AS eeoc_field_name,
-      'join'                          AS join_field
+      DISTINCT eeoc_field_name                                  AS eeoc_field_name,
+      'join'                                                    AS join_field
     FROM eeoc
 
 ), base AS (
@@ -57,7 +54,7 @@ WITH date_details AS (
       applications.candidate_id,
       eeoc.eeoc_values,
       offers.offer_id,
-      iff(offers.offer_status = 'accepted',1,0)                            AS accepted_offer
+      iff(offers.offer_status = 'accepted',1,0)                             AS accepted_offer
     FROM applications
     LEFT JOIN base
       ON date_trunc('month',applications.applied_at) = base.month_date
@@ -71,79 +68,83 @@ WITH date_details AS (
 ), offers_aggregated AS (
   
     SELECT 
-      DATE_TRUNC('month', offers.sent_at)                                   AS month_date, 
-      COALESCE(candidate_gender,'Decline To Self Identify')                 AS gender,
-      COUNT(DISTINCT(offer_id))                                             AS number_of_offers, 
-      SUM(IFF(offer_status = 'accepted',1,0))                               AS accepted_offers,
-      accepted_offers/number_of_offers                                      AS offer_acceptance_rate,
-      AVG(IFF(offer_status ='accepted',
+      base.*,
+      candidate_id,
+      offer_id,
+      eeoc.eeoc_values,
+      IFF(offer_status = 'accepted',1,0)                                    AS accepted_offer,
+      IFF(offer_status ='accepted',
             DATEDIFF('day', applications.applied_at, offers.sent_at),
-            NULL))                                                          AS avg_apply_to_accept_days
-    FROM offers
+            NULL)                                                           AS apply_to_accept_days
+    FROM base
+    LEFT JOIN offers 
+      ON DATE_TRUNC('month', offers.sent_at) = base.month_date
     LEFT JOIN applications    
       ON offers.application_id = applications.application_id
     LEFT JOIN eeoc            
       ON offers.application_id = eeoc.application_id
-    WHERE sent_at IS NOT NULL
-    GROUP BY 1,2    
-  
+      AND eeoc.eeoc_field_name = base.eeoc_field_name
+    WHERE base.month_date IS NOT NULL
+
 ), aggregated AS (
 
     SELECT
       {{repeated_column_names}},
-      ratio_to_report(COUNT(candidate_id))
-        OVER (PARTITION BY month_date)                                      AS metric_total,
+      RATIO_TO_REPORT(COUNT(DISTINCT(candidate_id)))
+        OVER (PARTITION BY month_date, eeoc_field_name)                     AS metric_total,
       'percent_of_applicants'                                               AS recruiting_metric
     FROM candidates_aggregated
-    GROUP BY 1,2
+    GROUP BY 1,2,3
   
     UNION ALL
   
     SELECT 
       {{repeated_column_names}},
-      SUM(hired)/SUM(total_applications)                                    AS metric_total,             
+      SUM(accepted_offer)/COUNT(DISTINCT(candidate_id))                     AS metric_total,             
       'applicaton_to_offer_percent'                                         AS recruiting_metric 
     FROM candidates_aggregated
-    GROUP BY 1,2  
+    GROUP BY 1,2,3  
 
     UNION ALL
   
     SELECT 
       {{repeated_column_names}},
-      RATIO_TO_REPORT(number_of_offers) 
-        OVER (PARTITION BY month_date)                                      AS metric_total,
-      'percent_of_offers'                                                   AS recruiting_metric
+      RATIO_TO_REPORT(COUNT(DISTINCT(offer_id)))
+        OVER (PARTITION BY month_date, eeoc_field_name)                     AS metric_total,
+      'percent_of_extended_offers'                                          AS recruiting_metric
     FROM offers_aggregated
+    GROUP BY 1,2,3
+
+    UNION ALL
+  
+    SELECT 
+      {{repeated_column_names}},
+      RATIO_TO_REPORT(SUM(accepted_offer)) 
+        OVER (PARTITION BY month_date, eeoc_field_name)                     AS metric_total,
+      'percent_of_accepted_offers'                                          AS recruiting_metric
+    FROM offers_aggregated
+    GROUP BY 1,2,3
 
     UNION ALL
 
     SELECT 
       {{repeated_column_names}},
-      offer_acceptance_rate                                                 AS metric_total,  
+      SUM(accepted_offer)/ COUNT(DISTINCT(offer_id))                        AS metric_total,  
       'offer_acceptance_rate_based_on_offer_month'                          AS recruiting_metric
     FROM offers_aggregated
+    GROUP BY 1,2,3
 
     UNION ALL
 
     SELECT 
       {{repeated_column_names}},
-      avg_apply_to_accept_days                                              AS metric_total,
+      AVG(apply_to_accept_days)                                             AS metric_total,
       'avg_apply_to_accept_days'                                            AS recruiting_metric
     FROM offers_aggregated
-
-), final AS(
-
-    SELECT 
-      date_details.month_date,
-      gender,
-      metric_total,
-      recruiting_metric
-    FROM date_details
-    LEFT JOIN aggregated
-    ON date_details.month_date = aggregated.month_date
-    WHERE date_details.month_date < DATE_TRUNC('month', CURRENT_DATE)
+    GROUP BY 1,2,3 
 
 )
 
 SELECT *
-FROM final 
+FROM aggregated
+WHERE month_date < DATE_TRUNC('month', CURRENT_DATE)
