f13963f2209ec0a86e636a13f8ec9f9163bd2203
Taylor Murphy <tmurphy@gitlab.com>
2020-02-07 14:12:09 -0600

    Switch containers

--- dags/airflow_utils.py
+++ dags/airflow_utils.py
@@ -8,7 +8,7 @@ from airflow.contrib.kubernetes.pod import Resources
 from airflow.operators.slack_operator import SlackAPIPostOperator
 
 REPO = "https://gitlab.com/gitlab-data/analytics.git"
-DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:latest"
+DATA_IMAGE = "registry.gitlab.com/gitlab-data/data-image/data-image:tayloramurphy-master-patch-04386"
 DBT_IMAGE = "registry.gitlab.com/gitlab-data/data-image/dbt-image:latest"
 
 
