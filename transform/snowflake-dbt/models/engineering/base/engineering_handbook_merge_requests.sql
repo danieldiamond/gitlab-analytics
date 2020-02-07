a6f1eb19075de2e7b85b8625ff372f5b494c9ccc
Michael Walker <mwalker@gitlab.com>
2020-02-07 11:43:07 -0500

    get the dbt models to at least run

--- transform/snowflake-dbt/models/engineering/base/engineering_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/base/engineering_handbook_merge_requests.sql
@@ -1,10 +1,10 @@
 WITH filtered_attributes AS (
 
     SELECT
-      json_text['']::ARRAY AS file_diffs,
-      json_text['']::VARCHAR AS source_branch_name,
-      json_text['']::ARRAY AS merge_request_version_diffs,
-      json_text['']::VARCHAR AS plain_diff_url_path
+      jsontext['diff_files']::ARRAY AS file_diffs,
+      jsontext['branch_name']::VARCHAR AS source_branch_name,
+      jsontext['merge_request_diffs']::ARRAY AS merge_request_version_diffs,
+      jsontext['plain_diff_path']::VARCHAR AS plain_diff_url_path
     FROM  {{ source('engineering', 'handbook_merge_requests') }}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY jsontext['plain_diff_path'] 
       ORDER BY ARRAY_SIZE(jsontext['merge_request_diffs']) DESC, uploaded_at DESC) = 1
--- transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
@@ -1,9 +1,11 @@
 WITH diff_id_broken_out AS (
     
     SELECT 
-      {{ dbt_utils.star(from=ref('engineering_specific_handbook_merge_requests'), except=['merge_request_version_url_path']) }},
+      handbook_file_edited,
+      plain_mr_diff_url_path,
+      source_branch_name,
       SPLIT(merge_request_version_url_path, 'diff_id=')[1]::BIGINT AS DIFF_ID
-    FROM {{ ref('engineering', 'engineering_specific_handbook_merge_requests') }}
+    FROM {{ ref('engineering_specific_handbook_merge_requests') }}
 
 ), joined_to_mr AS (
 
@@ -14,10 +16,10 @@ WITH diff_id_broken_out AS (
     FROM 
       diff_id_broken_out di
       JOIN
-      {{ ref('gitlab_dotcom', 'gitlab_dotcom_merge_request_diffs')}} diff
+      {{ ref('gitlab_dotcom_merge_request_diffs') }} diff
       ON (diff.merge_request_diff_id = di.DIFF_ID)
       JOIN
-      {{ ref('gitlab_dotcom', 'gitlab_dotcom_merge_requests')}} mr
+      {{ ref('gitlab_dotcom_merge_requests') }} mr
       ON (diff.merge_request_id = mr.merge_request_id)
 
 )
