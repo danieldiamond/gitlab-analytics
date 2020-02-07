aa06c226422002c148c1b573bf1125b97f2d9eb2
Michael Walker <mwalker@gitlab.com>
2020-02-07 15:01:49 -0500

    align handbook mr DBT models to SQL and DBT conventions

--- transform/snowflake-dbt/models/engineering/ephemeral/engineering_specific_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/ephemeral/engineering_specific_handbook_merge_requests.sql
@@ -3,27 +3,32 @@
     })
 }}
 
-WITH exploded_file_paths AS (
+WITH base AS (
+
+    SELECT *
+    FROM {{ ref('engineering_handbook_merge_requests') }}
+
+), exploded_file_paths AS ( -- explodes the files in the list of diffs
     
     SELECT 
-      d.value:file_path AS handbook_merge_request_file_edited,
-      base.plain_diff_url_path,
-      base.merge_request_version_diffs,
-      base.source_branch_name
-    FROM {{ ref('engineering_handbook_merge_requests') }} AS base,
-    TABLE(FLATTEN(INPUT => file_diffs, outer => true)) AS d
-    WHERE d.value:file_path LIKE ANY ('%/handbook/engineering/%', '%/handbook/support/%') 
+      file_diffs.value:file_path::VARCHAR AS handbook_merge_request_file_edited,
+      base.plain_diff_url_path            AS plain_diff_url_path,
+      base.merge_request_version_diffs    AS merge_request_version_diffs,
+      base.source_branch_name             AS source_branch_name
+    FROM base
+    INNER JOIN TABLE(FLATTEN(INPUT => file_diffs, outer => true)) AS file_diffs
+    WHERE file_diffs.value:file_path LIKE ANY ('%/handbook/engineering/%', '%/handbook/support/%') 
 
-), current_mr_diff AS (
+), current_mr_diff AS ( -- keeps only the latest MR diff
 
     SELECT 
-      fp.handbook_merge_request_file_edited  AS handbook_file_edited,
-      fp.plain_diff_url_path                 AS plain_mr_diff_url_path,
-      fp.source_branch_name                  AS source_branch_name,
-      d.value:version_path                   AS merge_request_version_url_path
-    FROM exploded_file_paths AS fp,
-    TABLE(FLATTEN(INPUT => merge_request_version_diffs, outer => true)) AS d
-    WHERE d.value:latest::boolean
+      exploded_file_paths.handbook_merge_request_file_edited  AS handbook_file_edited,
+      exploded_file_paths.plain_diff_url_path                 AS plain_mr_diff_url_path,
+      exploded_file_paths.source_branch_name                  AS source_branch_name,
+      mr_diffs.value:version_path::VARCHAR                    AS merge_request_version_url_path
+    FROM exploded_file_paths
+    INNER JOIN TABLE(FLATTEN(INPUT => merge_request_version_diffs, outer => true)) AS mr_diffs
+    WHERE mr_diffs.value:latest::boolean
 
 )
 SELECT * FROM current_mr_diff
--- transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
@@ -10,42 +10,43 @@ WITH diff_id_broken_out AS (
 ), joined_to_mr AS (
 
     SELECT 
-      mr.merge_request_state,
-      mr.updated_at,
-      di.handbook_file_edited,
-      di.plain_mr_diff_url_path,
-      CASE WHEN
-        di.handbook_file_edited like '%/handbook/engineering/development/%' THEN 'development'
-          WHEN
-        di.handbook_file_edited like '%/handbook/engineering/infrastructure/%' THEN 'infrastructure'
-          WHEN
-        di.handbook_file_edited like '%/handbook/engineering/security/%' THEN 'security'
-          WHEN
-        di.handbook_file_edited like '%/handbook/support/%' THEN 'support'
-          WHEN
-        di.handbook_file_edited like '%/handbook/engineering/ux/%' THEN 'ux'
-          WHEN 
-        di.handbook_file_edited like '%/handbook/engineering/%' THEN 'engineering'
-          ELSE NULL 
-      END AS department
+      mr.merge_request_state    AS gitlab_db_merge_request_state,
+      mr.updated_at             AS gitlab_db_merge_request_updated_at,
+      di.plain_mr_diff_url_path AS gitlab_api_plain_mr_diff_url_path,
+      CASE 
+        WHEN di.handbook_file_edited like '%/handbook/engineering/development/%' 
+          THEN 'development'
+        WHEN di.handbook_file_edited like '%/handbook/engineering/infrastructure/%' 
+          THEN 'infrastructure'
+        WHEN di.handbook_file_edited like '%/handbook/engineering/security/%' 
+          THEN 'security'
+        WHEN di.handbook_file_edited like '%/handbook/support/%' 
+          THEN 'support'
+        WHEN di.handbook_file_edited like '%/handbook/engineering/ux/%' 
+          THEN 'ux'
+        WHEN di.handbook_file_edited like '%/handbook/engineering/%' 
+          THEN 'engineering'
+        ELSE NULL 
+      END                       AS gitlab_api_merge_request_file_department
 
     FROM 
-      diff_id_broken_out di
-      JOIN
-      {{ ref('gitlab_dotcom_merge_request_diffs') }} diff
+      diff_id_broken_out AS di
+      INNER JOIN
+      {{ ref('gitlab_dotcom_merge_request_diffs') }} AS diff
       ON (diff.merge_request_diff_id = di.DIFF_ID)
-      JOIN
-      {{ ref('gitlab_dotcom_merge_requests') }} mr
+      INNER JOIN
+      {{ ref('gitlab_dotcom_merge_requests') }} AS mr
       ON (diff.merge_request_id = mr.merge_request_id)
 
+), renamed AS (
+
+    SELECT
+      gitlab_db_merge_request_state                                AS merge_request_state,
+      gitlab_db_merge_request_updated_at                           AS merge_request_edited_at,
+      gitlab_api_plain_mr_diff_url_path                            AS plain_merge_request_diff_url_path,
+      ARRAY_AGG(DISTINCT gitlab_api_merge_request_file_department) AS merge_request_department_list
+    FROM joined_to_mr
+    GROUP BY 1, 2, 3 
+
 )
-SELECT
-  merge_request_state,
-  updated_at,
-  plain_mr_diff_url_path,
-  ARRAY_AGG(DISTINCT department) AS department_list
-FROM joined_to_mr
-GROUP BY 
-  plain_mr_diff_url_path,
-  updated_at,
-  merge_request_state  
+SELECT * FROM renamed
