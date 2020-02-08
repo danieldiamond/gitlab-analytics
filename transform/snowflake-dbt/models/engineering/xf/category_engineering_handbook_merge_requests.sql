63a9617fda4a8c592b2d18077c476b9ce7cb6f37
Michael Walker <mwalker@gitlab.com>
2020-02-08 18:07:06 -0500

    make engineering handbook changes xf model easier to read

--- transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/xf/category_engineering_handbook_merge_requests.sql
@@ -1,14 +1,14 @@
-WITH gitlab_dotcom_mr_diffs AS (
+WITH mr_diffs AS (
 
     SELECT *
     FROM {{ ref('gitlab_dotcom_merge_request_diffs') }}
 
-), gitlab_dotcom_merge_requests AS (
+), merge_requests AS (
 
     SELECT *
     FROM {{ ref('gitlab_dotcom_merge_requests') }}
 
-), diff_id_broken_out AS (
+), mr_files AS (
     
     SELECT 
       handbook_file_edited,
@@ -20,39 +20,39 @@ WITH gitlab_dotcom_mr_diffs AS (
 ), joined_to_mr AS (
 
     SELECT 
-      gitlab_dotcom_merge_requests.merge_request_state    AS gitlab_db_merge_request_state,
-      gitlab_dotcom_merge_requests.updated_at             AS gitlab_db_merge_request_updated_at,
-      diff_id_broken_out.plain_mr_diff_url_path           AS gitlab_api_plain_mr_diff_url_path,
+      gitlab_dotcom_merge_requests.merge_request_state    AS merge_request_state,
+      gitlab_dotcom_merge_requests.updated_at             AS merge_request_updated_at,
+      diff_id_broken_out.plain_mr_diff_url_path           AS plain_mr_diff_url_path,
       CASE 
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/engineering/development/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/engineering/development/%' 
           THEN 'development'
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/engineering/infrastructure/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/engineering/infrastructure/%' 
           THEN 'infrastructure'
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/engineering/security/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/engineering/security/%' 
           THEN 'security'
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/support/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/support/%' 
           THEN 'support'
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/engineering/ux/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/engineering/ux/%' 
           THEN 'ux'
-        WHEN diff_id_broken_out.handbook_file_edited like '%/handbook/engineering/%' 
+        WHEN LOWER(mr_files.handbook_file_edited) like '%/handbook/engineering/%' 
           THEN 'engineering'
         ELSE NULL 
       END                       AS gitlab_api_merge_request_file_department
     FROM 
-      diff_id_broken_out
+      mr_files
       INNER JOIN
-      gitlab_dotcom_mr_diffs
-      ON (gitlab_dotcom_mr_diffs.merge_request_diff_id = diff_id_broken_out.diff_id)
+      mr_diffs
+      ON (mr_diffs.merge_request_diff_id = mr_files.diff_id)
       INNER JOIN
-      gitlab_dotcom_merge_requests
-      ON (gitlab_dotcom_mr_diffs.merge_request_id = gitlab_dotcom_merge_requests.merge_request_id)
+      merge_requests
+      ON (mr_diffs.merge_request_id = merge_requests.merge_request_id)
 
 ), renamed AS (
 
     SELECT
-      gitlab_db_merge_request_state                                AS merge_request_state,
-      gitlab_db_merge_request_updated_at                           AS merge_request_edited_at,
-      gitlab_api_plain_mr_diff_url_path                            AS plain_merge_request_diff_url_path,
+      merge_request_state,
+      merge_request_updated_at                                     AS merge_request_edited_at,
+      plain_mr_diff_url_path                            AS plain_merge_request_diff_url_path,
       ARRAY_AGG(DISTINCT gitlab_api_merge_request_file_department) AS merge_request_department_list
     FROM joined_to_mr
     GROUP BY 1, 2, 3 
