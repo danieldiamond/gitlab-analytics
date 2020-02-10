ecadcbc6cb4c6bbca31bd7154233d1010f795e3c
Michael Walker <mwalker@gitlab.com>
2020-02-10 16:27:41 -0700

    use csv to classify handbook MR files

--- transform/snowflake-dbt/models/handbook/xf/category_engineering_handbook_merge_requests.sql
+++ transform/snowflake-dbt/models/handbook/xf/category_engineering_handbook_merge_requests.sql
@@ -17,44 +17,35 @@ WITH mr_diffs AS (
       SPLIT(merge_request_version_url_path, 'diff_id=')[1]::BIGINT AS diff_id
     FROM {{ ref('handbook_merge_requests_files') }}
 
+), file_classifications AS (
+
+    SELECT 
+      handbook_path,
+      file_classification
+    FROM {{ ref('handbook_file_classification_mapping') }}
+
 ), joined_to_mr AS (
 
     SELECT 
-      merge_requests.merge_request_state    AS merge_request_state,
-      merge_requests.updated_at             AS merge_request_updated_at,
+      merge_requests.merge_request_state        AS merge_request_state,
+      merge_requests.updated_at                 AS merge_request_updated_at,
       mr_files.plain_mr_diff_url_path           AS plain_mr_diff_url_path,
-      CASE 
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/engineering/development/%' 
-          THEN 'development'
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/engineering/infrastructure/%' 
-          THEN 'infrastructure'
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/engineering/security/%' 
-          THEN 'security'
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/support/%' 
-          THEN 'support'
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/engineering/ux/%' 
-          THEN 'ux'
-        WHEN LOWER(mr_files.handbook_file_edited) LIKE '%/handbook/engineering/%' 
-          THEN 'engineering'
-        ELSE NULL 
-      END                       AS gitlab_api_merge_request_file_department
-    FROM 
-      mr_files
-      INNER JOIN
-      mr_diffs
-      ON (mr_diffs.merge_request_diff_id = mr_files.diff_id)
-      INNER JOIN
-      merge_requests
-      ON (mr_diffs.merge_request_id = merge_requests.merge_request_id)
-    WHERE gitlab_api_merge_request_file_department IS NOT NULL 
+      file_classifications.file_classification  AS file_classification
+    FROM mr_files
+    INNER JOIN mr_diffs
+      ON ( mr_diffs.merge_request_diff_id = mr_files.diff_id)
+    INNER JOIN merge_requests
+      ON ( mr_diffs.merge_request_id = merge_requests.merge_request_id)
+    INNER JOIN file_classifications
+      ON ( LOWER(mr_files.handbook_file_edited) LIKE '%' || file_classifications.handbook_path || '%' )
 
 ), renamed AS (
 
     SELECT
       merge_request_state,
-      merge_request_updated_at                                     AS merge_request_edited_at,
-      plain_mr_diff_url_path                                       AS plain_merge_request_diff_url_path,
-      ARRAY_AGG(DISTINCT gitlab_api_merge_request_file_department) AS merge_request_department_list
+      merge_request_updated_at                  AS merge_request_edited_at,
+      plain_mr_diff_url_path                    AS plain_merge_request_diff_url_path,
+      ARRAY_AGG(DISTINCT file_classification)   AS merge_request_department_list
     FROM joined_to_mr
     GROUP BY 1, 2, 3 
 
