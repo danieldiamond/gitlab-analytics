580b9635980fcaf5e202ca4f9cc2cd5ecc1efff9
Parul Luthra <pluthra@gitlab.com>
2020-02-11 15:11:43 -0600

    updating to aggregate information at week level

--- transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_offer_stage.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_offer_stage.sql
@@ -14,18 +14,32 @@ WITH gh_offers AS (
     SELECT 
         offer_id, 
         offer_custom_field,
-        offer_custom_field_display_value
+        offer_custom_field_display_value                                                    AS candidate_country
     FROM {{ref('greenhouse_offer_custom_fields')}} 
-    WHERE offer_custom_field IN ('Candidate City', 'Candidate State', 'Candidate Country')
+    WHERE offer_custom_field = 'Candidate Country'
 
-), greenhouse_location_fields AS (
-    
+), zuora_regions AS (
+
     SELECT *
-    FROM greenhouse_offer_custom_fields
-    PIVOT(MAX(offer_custom_field_display_value) FOR offer_custom_field IN ('Candidate City', 'Candidate State', 'Candidate Country'))
-          as p (offer_id, candidate_city, candidate_state, candidate_country)
+     FROM {{ref('zuora_country_geographic_region')}}
 
-), final AS (
+), location_cleaned AS (
+
+    SELECT
+        offer_id,
+        candidate_country,
+        IFF(LOWER(LEFT(candidate_country,12))= 'united state', 
+            'North America',
+            COALESCE(z1.geographic_region,z2.geographic_region, z3.geographic_region))       AS geographic_region      
+    FROM greenhouse_offer_custom_fields
+    LEFT JOIN zuora_regions z1 
+      ON LOWER(z1.country_name_in_zuora) = LOWER(greenhouse_offer_custom_fields.candidate_country)
+    LEFT JOIN zuora_regions z2 
+      ON LOWER(z2.iso_alpha_2_code) = LOWER(greenhouse_offer_custom_fields.candidate_country)
+    LEFT JOIN zuora_regions z3 
+      ON LOWER(z3.iso_alpha_3_code) = LOWER(greenhouse_offer_custom_fields.candidate_country) 
+
+), data_set AS (
 
     SELECT 
       gh_offers.offer_id,
@@ -35,13 +49,24 @@ WITH gh_offers AS (
       gh_offers.created_at,
       gh_offers.sent_at,
       gh_offers.start_date,
-      candidate_state,
-      candidate_country
+      geographic_region
     FROM gh_offers 
     INNER JOIN gh_applications 
       ON gh_offers.application_id = gh_applications.application_id
-    LEFT JOIN greenhouse_location_fields 
-      ON gh_offers.offer_id = greenhouse_location_fields.offer_id
+    INNER JOIN location_cleaned
+      ON location_cleaned.offer_id = gh_offers.offer_id
+
+), final AS (
+
+    SELECT 
+      DATE_TRUNC(WEEK,start_date)                               AS start_week,
+      geographic_region,
+      COUNT(offer_id)                                           AS candidates_estimated_to_start,
+      SUM(IFF(offer_status = 'accepted',1,0))                   AS accepted_offers_to_start
+    FROM data_set
+    WHERE geographic_region = 'North America'
+    GROUP BY 1,2
+    ORDER BY 1 DESC
 
 )
 
