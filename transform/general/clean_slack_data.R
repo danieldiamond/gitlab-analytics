fd73054aa73abdb4a5a0e78974bf53cb94061f93
Parul Luthra <pluthra@gitlab.com>
2020-02-13 16:37:28 +0000

    Apply suggestion to transform/general/clean_slack_data.R

--- transform/general/clean_slack_data.R
+++ transform/general/clean_slack_data.R
@@ -1,35 +1,3 @@
-setwd("~/scratch/slack_stats")
-if(nzchar(system.file(package = 'tidyverse')) == F) {
-  install.packages('tidyverse')
-}
-library(tidyverse)
-
-file_path <- paste0('./data/raw/', dir('data/raw'))
-
-raw <- read_csv(file_path,
-                col_types = cols(.default = 'c',
-                                 Date = 'D'))
-
-clean_names <- function(list_names) {
-  
-  list_names %>%
-    tolower() %>%
-    str_replace('\\,', ' in') %>%
-    str_replace('\\-', '\\_') %>%
-    str_replace_all('\\s', '\\_')
-  
-}
-
-clean <- as_tibble(raw, .name_repair = clean_names)
-file_name <- paste0('sheetload_gitlab_slack_data_', Sys.Date(), '.csv')
-
-write_csv(clean, paste0('./data/clean/', file_name))
-
-
-
-##in the case tidyverse doesn't install, use the below
-
-install.packages('tibble')
 install.packages('readr')
 install.packages('stringr')
                  
