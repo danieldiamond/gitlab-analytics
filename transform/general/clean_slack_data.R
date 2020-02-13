744fc3673c093febaf75e17bf1c3dc693c5b64f2
Parul Luthra <pluthra@gitlab.com>
2020-02-13 02:39:42 +0000

    adding additional code to get packages installed to run the code

--- transform/general/clean_slack_data.R
+++ transform/general/clean_slack_data.R
@@ -24,3 +24,34 @@ clean <- as_tibble(raw, .name_repair = clean_names)
 file_name <- paste0('sheetload_gitlab_slack_data_', Sys.Date(), '.csv')
 
 write_csv(clean, paste0('./data/clean/', file_name))
+
+
+
+##in the case tidyverse doesn't install, use the below
+
+install.packages('tibble')
+install.packages('readr')
+install.packages('stringr')
+                 
+   
+library(stringr)
+library(readr)
+library(tibble)
+
+setwd("/Users/parulluthra/scratch/")
+file_path <- paste0('./data/raw/', dir('data/raw'))
+raw <- read_csv(file_path,
+                col_types = cols(.default = 'c',
+                                 Date = 'D'))
+
+
+clean_names <- function(list_names) {
+  list_names %>%
+    tolower() %>%
+    str_replace('\\,', ' in') %>%
+    str_replace('\\-', '\\_') %>%
+    str_replace_all('\\s', '\\_')
+}
+clean <- as_tibble(raw, .name_repair = clean_names)
+file_name <- paste0('sheetload_gitlab_slack_data_', Sys.Date(), '.csv')
+write_csv(clean, paste0('./data/clean/', file_name))
