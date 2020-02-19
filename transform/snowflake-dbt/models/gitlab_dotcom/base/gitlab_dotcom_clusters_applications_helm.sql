0b19d1377faadeb14c2ef216b6e346627ff7eb81
Eli Kastelein <ekastelein@gitlab.com>
2020-02-19 13:00:54 -0800

    Add helm base model

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_clusters_applications_helm.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_clusters_applications_helm.sql
@@ -1,24 +1,21 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('gitlab_dotcom', 'clusters') }}
+    FROM {{ source('gitlab_dotcom', 'clusters_applications_helm') }}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-), renamed AS (
+), 
 
+renamed AS 
+    
     SELECT
-      id::INTEGER                AS cluster_id,
-      user_id::INTEGER           AS user_id,
-      provider_type::INTEGER     AS provider_type_id,
-      platform_type::INTEGER     AS platform_type_id,
-      created_at::TIMESTAMP      AS created_at,
-      updated_at::TIMESTAMP      AS updated_at,
-      enabled::BOOLEAN           AS is_enabled,
-      environment_scope::VARCHAR AS environment_scope,
-      cluster_type::INTEGER      AS cluster_type_id,
-      domain::VARCHAR            AS domain,
-      managed::VARCHAR           AS managed
-
+      id::INTEGER              AS clusters_application_helm_id,
+      cluster_id::INTEGER      AS cluster_id,
+      created_at::TIMESTAMP    AS created_at,
+      updated_at::TIMESTAMP    AS updated_at,
+      status::INTEGER          AS status,
+      version::VARCHAR         AS version,
+      status_reason::VARCHAR   AS status_reason
     FROM source
 
 )
