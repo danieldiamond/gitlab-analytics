d1cd0f372a12bcefcb29c42eb592b15354ab202a
Kathleen Tam <ktam@gitlab.com>
2020-02-19 02:36:58 +0000

    Add a new dbt data model transform/snowflake-dbt/models/handbook/xf/category_handbook_engineering_merge_requests_path_count.sql that will have all the counts per path per department (+ Eng Division)

---
+++ transform/snowflake-dbt/models/handbook/xf/category_handbook_engineering_merge_requests_path_count.sql
@@ -0,0 +1,35 @@
+{{ config({
+    "materialized": "view"
+    })
+}}
+
+WITH category_handbook_engineering_merge_requests AS (
+
+    SELECT *
+    FROM {{ ref('category_handbook_engineering_merge_requests') }}
+
+), handbook_engineering_merge_request_path_count_department AS (
+
+    SELECT
+        merge_request_state,
+        merge_request_updated_at,
+        merge_request_created_at,
+        merge_request_last_edited_at,
+        merge_request_merged_at,
+        merge_request_iid,
+        merge_request_path,
+        CASE WHEN LOWER(merge_request_path) LIKE '%/handbook/engineering/%' THEN 1 
+             WHEN LOWER(merge_request_path) LIKE '%/handbook/support/%' THEN 1 
+             ELSE 0 END AS engineering,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/ux/%',1,0) AS path_count_ux,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/security/%',1,0) AS path_count_security,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/infrastructure/%',1,0) AS path_count_infrastructure,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/development/%',1,0) AS path_count_development,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/quality/%',1,0) AS path_count_quality,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/support/%',1,0) AS path_count_support
+    FROM category_handbook_engineering_merge_requests
+
+)
+
+SELECT *
+FROM handbook_engineering_merge_request_path_count_department
