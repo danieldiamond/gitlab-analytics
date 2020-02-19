c4dda9c8adaf2437a0adcd42d96df82fec86d8d5
Eli Kastelein <ekastelein@gitlab.com>
2020-02-19 14:54:36 -0800

    fix last commit

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -13,6 +13,125 @@
     "key_to_parent_group": "cluster_group_id",
     "primary_key": "clusters_applications_helm_id",
     "is_representative_of_stage": "True"
+  },
+  {
+    "event_name": "ci_builds",
+    "table_name": "gitlab_dotcom_ci_builds",
+    "key_to_parent_object": "ci_build_project_id",
+    "primary_key": "ci_build_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "ci_pipeline_schedules",
+    "table_name": "gitlab_dotcom_ci_pipeline_schedules",
+    "key_to_parent_object": "project_id",
+    "primary_key": "ci_pipeline_schedule_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "ci_pipelines",
+    "table_name": "gitlab_dotcom_ci_pipelines",
+    "key_to_parent_object": "project_id",
+    "primary_key": "ci_pipeline_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "ci_stages",
+    "table_name": "gitlab_dotcom_ci_stages",
+    "key_to_parent_object": "project_id",
+    "primary_key": "ci_stage_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "ci_triggers",
+    "table_name": "gitlab_dotcom_ci_triggers",
+    "key_to_parent_object": "project_id",
+    "primary_key": "ci_trigger_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "deployments",
+    "table_name": "gitlab_dotcom_deployments",
+    "key_to_parent_object": "project_id",
+    "primary_key": "deployment_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "environments",
+    "table_name": "gitlab_dotcom_environments",
+    "key_to_parent_object": "project_id",
+    "primary_key": "environment_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "issues",
+    "table_name": "gitlab_dotcom_issues",
+    "key_to_parent_object": "project_id",
+    "primary_key": "issue_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "labels",
+    "table_name": "gitlab_dotcom_labels",
+    "key_to_parent_object": "project_id",
+    "primary_key": "label_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "lfs_objects",
+    "table_name": "gitlab_dotcom_lfs_objects_projects",
+    "key_to_parent_object": "project_id",
+    "primary_key": "lfs_object_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "merge_requests",
+    "table_name": "gitlab_dotcom_merge_requests",
+    "key_to_parent_object": "project_id",
+    "primary_key": "merge_request_id",
+    "is_representative_of_stage": "True"
+  },
+  {
+    "event_name": "milestones",
+    "table_name": "gitlab_dotcom_milestones",
+    "key_to_parent_object": "project_id",
+    "primary_key": "milestone_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "notes",
+    "table_name": "gitlab_dotcom_notes",
+    "key_to_parent_object": "project_id",
+    "primary_key": "note_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "project_auto_devops",
+    "table_name": "gitlab_dotcom_project_auto_devops",
+    "key_to_parent_object": "project_id",
+    "primary_key": "project_auto_devops_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "releases",
+    "table_name": "gitlab_dotcom_releases",
+    "key_to_parent_object": "project_id",
+    "primary_key": "release_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "snippets",
+    "table_name": "gitlab_dotcom_snippets",
+    "key_to_parent_object": "project_id",
+    "primary_key": "snippet_id",
+    "is_representative_of_stage": "False"
+  },
+  {
+    "event_name": "todos",
+    "table_name": "gitlab_dotcom_todos",
+    "key_to_parent_object": "project_id",
+    "primary_key": "todo_id",
+    "is_representative_of_stage": "False"
   }
 ]
 -%}
