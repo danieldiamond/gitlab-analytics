2f135de9fe4e469c4601d75614a99c5e4f5ff60f
Eli Kastelein <ekastelein@gitlab.com>
2020-02-19 14:53:45 -0800

    change the join to work in different ways

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -9,128 +9,10 @@
   {
     "event_name": "clusters_applications_helm",
     "table_name": "gitlab_dotcom_clusters_applications_helm_xf",
-    "key_to_parent_object": "cluster_project_id",
+    "key_to_parent_project": "cluster_project_id",
+    "key_to_parent_group": "cluster_group_id",
     "primary_key": "clusters_applications_helm_id",
     "is_representative_of_stage": "True"
-  },
-  {
-    "event_name": "ci_builds",
-    "table_name": "gitlab_dotcom_ci_builds",
-    "key_to_parent_object": "ci_build_project_id",
-    "primary_key": "ci_build_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "ci_pipeline_schedules",
-    "table_name": "gitlab_dotcom_ci_pipeline_schedules",
-    "key_to_parent_object": "project_id",
-    "primary_key": "ci_pipeline_schedule_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "ci_pipelines",
-    "table_name": "gitlab_dotcom_ci_pipelines",
-    "key_to_parent_object": "project_id",
-    "primary_key": "ci_pipeline_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "ci_stages",
-    "table_name": "gitlab_dotcom_ci_stages",
-    "key_to_parent_object": "project_id",
-    "primary_key": "ci_stage_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "ci_triggers",
-    "table_name": "gitlab_dotcom_ci_triggers",
-    "key_to_parent_object": "project_id",
-    "primary_key": "ci_trigger_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "deployments",
-    "table_name": "gitlab_dotcom_deployments",
-    "key_to_parent_object": "project_id",
-    "primary_key": "deployment_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "environments",
-    "table_name": "gitlab_dotcom_environments",
-    "key_to_parent_object": "project_id",
-    "primary_key": "environment_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "issues",
-    "table_name": "gitlab_dotcom_issues",
-    "key_to_parent_object": "project_id",
-    "primary_key": "issue_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "labels",
-    "table_name": "gitlab_dotcom_labels",
-    "key_to_parent_object": "project_id",
-    "primary_key": "label_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "lfs_objects",
-    "table_name": "gitlab_dotcom_lfs_objects_projects",
-    "key_to_parent_object": "project_id",
-    "primary_key": "lfs_object_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "merge_requests",
-    "table_name": "gitlab_dotcom_merge_requests",
-    "key_to_parent_object": "project_id",
-    "primary_key": "merge_request_id",
-    "is_representative_of_stage": "True"
-  },
-  {
-    "event_name": "milestones",
-    "table_name": "gitlab_dotcom_milestones",
-    "key_to_parent_object": "project_id",
-    "primary_key": "milestone_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "notes",
-    "table_name": "gitlab_dotcom_notes",
-    "key_to_parent_object": "project_id",
-    "primary_key": "note_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "project_auto_devops",
-    "table_name": "gitlab_dotcom_project_auto_devops",
-    "key_to_parent_object": "project_id",
-    "primary_key": "project_auto_devops_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "releases",
-    "table_name": "gitlab_dotcom_releases",
-    "key_to_parent_object": "project_id",
-    "primary_key": "release_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "snippets",
-    "table_name": "gitlab_dotcom_snippets",
-    "key_to_parent_object": "project_id",
-    "primary_key": "snippet_id",
-    "is_representative_of_stage": "False"
-  },
-  {
-    "event_name": "todos",
-    "table_name": "gitlab_dotcom_todos",
-    "key_to_parent_object": "project_id",
-    "primary_key": "todo_id",
-    "is_representative_of_stage": "False"
   }
 ]
 -%}
@@ -190,7 +72,7 @@ SELECT
   ultimate_namespace.namespace_id, 
   ultimate_namespace.namespace_created_at,
   projects.project_id,
-  projects.created_at                                  AS project_created_at,
+  projects.project_created_at,
   {{ event_cte.event_name }}.created_at                AS event_created_at,
   {{ event_cte.is_representative_of_stage }}::BOOLEAN  AS is_representative_of_stage,
   
@@ -224,9 +106,15 @@ SELECT
              project_created_at, 
              event_created_at)/(24 * 7))               AS weeks_since_project_creation
 FROM {{ event_cte.event_name }}
-  INNER JOIN projects ON {{ event_cte.event_name }}.{{event_cte.key_to_parent_object}} = projects.project_id
+  /* Join with project project. */
+  LEFT JOIN projects
+    ON {{ event_cte.event_name }}.{{event_cte.key_to_parent_project}} = projects.project_id
+  /* Join with parent group. */
+  LEFT JOIN namespaces
+    ON {{ event_cte.event_name }}.{{event_cte.key_to_parent_group}} = namespaces.namespace_id
+  /* Join on either the project or the group's ultimate namespace. */
   INNER JOIN namespaces AS ultimate_namespace 
-    ON projects.ultimate_parent_id = ultimate_namespace.namespace_id
+    ON COALESCE(projects.ultimate_parent_id, namespaces.namespace_ultimate_parent_id) = ultimate_namespace.namespace_id
   LEFT JOIN gitlab_subscriptions
     ON ultimate_namespace.namespace_id = gitlab_subscriptions.namespace_id
     AND {{ event_cte.event_name }}.created_at BETWEEN gitlab_subscriptions.valid_from 
