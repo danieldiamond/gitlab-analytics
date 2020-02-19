4cf7871e7521c0d25d7ac1c9c83a3366b0be6cc1
Kathleen Tam <ktam@gitlab.com>
2020-02-19 02:58:01 +0000

    Reorganized the columns in transform/snowflake-dbt/models/handbook/xf/category_handbook_engineering_merge_requests_path_count.sql to match the SQL Style Guide

--- transform/snowflake-dbt/models/handbook/xf/category_handbook_engineering_merge_requests_path_count.sql
+++ transform/snowflake-dbt/models/handbook/xf/category_handbook_engineering_merge_requests_path_count.sql
@@ -11,22 +11,30 @@ WITH category_handbook_engineering_merge_requests AS (
 ), handbook_engineering_merge_request_path_count_department AS (
 
     SELECT
+        -- Foreign Keys 
+        merge_request_iid,
+
+        -- Logical Information
+        merge_request_path,
         merge_request_state,
-        merge_request_updated_at,
+        CASE WHEN LOWER(merge_request_path) LIKE '%/handbook/engineering/%' THEN 1 
+             WHEN LOWER(merge_request_path) LIKE '%/handbook/support/%' THEN 1 
+             ELSE 0 END                                                                     AS path_count_engineering,
+
+             -- Engineering departments 
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/development/%',1,0)      AS path_count_development,    
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/infrastructure/%',1,0)   AS path_count_infrastructure,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/quality/%',1,0)          AS path_count_quality,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/security/%',1,0)         AS path_count_security,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/support/%',1,0)                      AS path_count_support,
+        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/ux/%',1,0)               AS path_count_ux
+
+        -- Metadata 
         merge_request_created_at,
         merge_request_last_edited_at,
         merge_request_merged_at,
-        merge_request_iid,
-        merge_request_path,
-        CASE WHEN LOWER(merge_request_path) LIKE '%/handbook/engineering/%' THEN 1 
-             WHEN LOWER(merge_request_path) LIKE '%/handbook/support/%' THEN 1 
-             ELSE 0 END AS path_count_engineering,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/ux/%',1,0) AS path_count_ux,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/security/%',1,0) AS path_count_security,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/infrastructure/%',1,0) AS path_count_infrastructure,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/development/%',1,0) AS path_count_development,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/engineering/quality/%',1,0) AS path_count_quality,
-        IFF(LOWER(merge_request_path) LIKE '%/handbook/support/%',1,0) AS path_count_support
+        merge_request_updated_at,
+
     FROM category_handbook_engineering_merge_requests
 
 )
