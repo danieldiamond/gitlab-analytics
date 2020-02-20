264f1e045e7c0eff88261a12776f4829011dd9f9
Parul Luthra <pluthra@gitlab.com>
2020-02-20 08:59:40 -0600

    adding finance fields

--- transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_current_openings.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_current_openings.sql
@@ -18,10 +18,19 @@ WITH greenhouse_openings AS (
     SELECT * 
     FROM {{ref('greenhouse_organizations')}}
 
+
+), greenhouse_finance_id AS (
+
+    SELECT * 
+    FROM {{ref('greenhouse_job_custom_fields')}}
+    WHERE job_custom_field = 'Finance_ID'
+
 ), aggregated AS (
 
     SELECT 
       greenhouse_openings.job_id,
+      greenhouse_finance_id.job_custom_field_float_value,
+      job_custom_field_display_value,
       greenhouse_jobs.job_created_at, 
       greenhouse_jobs.job_status,
       greenhouse_openings.opening_id, 
@@ -35,6 +44,8 @@ WITH greenhouse_openings AS (
       ON greenhouse_openings.job_id = greenhouse_jobs.job_id 
     LEFT JOIN greenhouse_department 
       ON greenhouse_department.department_id = greenhouse_jobs.department_id
+    LEFT join greenhouse_finance_id 
+      ON greenhouse_finance_id.job_id = greenhouse_jobs.job_id  
     WHERE greenhouse_jobs.job_closed_at IS NULL
       AND greenhouse_jobs.job_opened_at IS NOT NULL 
 )
