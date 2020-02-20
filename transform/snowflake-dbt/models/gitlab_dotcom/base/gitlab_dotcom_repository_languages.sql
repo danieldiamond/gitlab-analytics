535ba8360edbddec2c2f3ab5c35114c4c240e63e
mpeychet <mpeychet@gitlab.com>
2020-02-20 12:16:08 +0100

    repo language init

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_repository_languages.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_repository_languages.sql
@@ -1,7 +1,7 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('gitlab_dotcom', 'releases') }}
+    FROM {{ source('gitlab_dotcom', 'repository_languages') }}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
 
 ), renamed AS (
