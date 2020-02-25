fd057e99b5190c9bff572c64e7387d4aff3f355d
derekatwood <datwood@gitlab.com>
2020-02-25 15:02:40 -0600

    remove unnecessary test and fix spacing

--- transform/snowflake-dbt/models/handbook/rpt/rpt_engineering_handbook_update_frequency_monthly.sql
+++ transform/snowflake-dbt/models/handbook/rpt/rpt_engineering_handbook_update_frequency_monthly.sql
@@ -11,17 +11,17 @@ WITH category_handbook_engineering_merge_requests_count AS (
 ), handbook_engineering_total_count_department AS (
 
     SELECT
-        DATE_TRUNC('MONTH', merge_request_merged_at)    AS month_merged_at,
-        SUM(mr_count_engineering)                       AS mr_count_engineering,
-        SUM(mr_count_ux)                                AS mr_count_ux,
-        SUM(mr_count_security)                          AS mr_count_security,
-        SUM(mr_count_infrastructure)                    AS mr_count_infrastructure,
-        SUM(mr_count_development)                       AS mr_count_development,
-        SUM(mr_count_quality)                           AS mr_count_quality,
-        SUM(mr_count_support)                           AS mr_count_support
+      DATE_TRUNC('MONTH', merge_request_merged_at)    AS month_merged_at,
+      SUM(mr_count_engineering)                       AS mr_count_engineering,
+      SUM(mr_count_ux)                                AS mr_count_ux,
+      SUM(mr_count_security)                          AS mr_count_security,
+      SUM(mr_count_infrastructure)                    AS mr_count_infrastructure,
+      SUM(mr_count_development)                       AS mr_count_development,
+      SUM(mr_count_quality)                           AS mr_count_quality,
+      SUM(mr_count_support)                           AS mr_count_support
     FROM category_handbook_engineering_merge_requests_count
     WHERE merge_request_state = 'merged'
-        AND merge_request_merged_at IS NOT NULL
+      AND merge_request_merged_at IS NOT NULL
     GROUP BY 1
 
 )
