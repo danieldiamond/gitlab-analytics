ee34743f529399def2c8662388f954d15668d36e
Ken Aguilar <kaguilar@gitlab.com>
2020-02-25 02:18:21 +0000

    Update column names transform/snowflake-dbt/models/handbook/rpt/rpt_engineering_handbook_update_frequency_monthly.sql

--- transform/snowflake-dbt/models/handbook/rpt/rpt_engineering_handbook_update_frequency_monthly.sql
+++ transform/snowflake-dbt/models/handbook/rpt/rpt_engineering_handbook_update_frequency_monthly.sql
@@ -12,13 +12,13 @@ WITH category_handbook_engineering_merge_requests_count AS (
 
     SELECT
         DATE_TRUNC('MONTH', merge_request_merged_at)    AS month_merged_at,
-        SUM(mr_count_engineering)                       AS is_mr_engineering,
-        SUM(mr_count_ux)                                AS is_mr_ux,
-        SUM(mr_count_security)                          AS is_mr_security,
-        SUM(mr_count_infrastructure)                    AS is_mr_infrastructure,
-        SUM(mr_count_development)                       AS is_mr_development,
-        SUM(mr_count_quality)                           AS is_mr_quality,
-        SUM(mr_count_support)                           AS is_mr_support
+        SUM(mr_count_engineering)                       AS mr_count_engineering,
+        SUM(mr_count_ux)                                AS mr_count_ux,
+        SUM(mr_count_security)                          AS mr_count_security,
+        SUM(mr_count_infrastructure)                    AS mr_count_infrastructure,
+        SUM(mr_count_development)                       AS mr_count_development,
+        SUM(mr_count_quality)                           AS mr_count_quality,
+        SUM(mr_count_support)                           AS mr_count_support
     FROM category_handbook_engineering_merge_requests_count
     WHERE merge_request_state = 'merged'
         AND merge_request_merged_at IS NOT NULL
