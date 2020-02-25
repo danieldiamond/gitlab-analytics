1a92dd30dac5c89718d53e1c27e684fd7cc389b3
mpeychet <mpeychet@gitlab.com>
2020-02-25 12:38:53 +0100

    add testst

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_repository_languages.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_repository_languages.sql
@@ -7,7 +7,7 @@ WITH source AS (
 ), renamed AS (
   
     SELECT
-      MD5(project_programming_language_id)::VARCHAR AS project_programming_language_id,
+      MD5(project_programming_language_id)::VARCHAR AS repository_language_id,
       project_id::INTEGER                           AS project_id,
       programming_language_id::INTEGER              AS programming_language_id
     FROM source
