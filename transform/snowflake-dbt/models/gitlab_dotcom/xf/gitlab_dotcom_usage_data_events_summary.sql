7c893ce83825e0a6cf7c34c39d6ee53f8f989141
Eli Kastelein <ekastelein@gitlab.com>
2020-02-26 10:13:52 -0800

    add join

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events_summary.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events_summary.sql
@@ -2,6 +2,7 @@ WITH usage_data_events AS (
 
     SELECT *
     FROM {{ref('gitlab_dotcom_usage_data_events')}}
+    WHERE event_created_at IS NOT NULL --TODO: add test earlier
     LIMIT 10000 --TODO
 
 ),
@@ -28,7 +29,9 @@ spine_cols AS (
 spine AS (
     
     SELECT DISTINCT
-      date_actual,
+      DATE_TRUNC('day', date_actual)::DATE    AS event_day,
+      DATE_TRUNC('week', date_actual)::DATE   AS event_week,
+      DATE_TRUNC('month', date_actual)::DATE  AS event_month,
       event_name,
       stage_name,
       plan_name_at_event_date,
@@ -42,12 +45,13 @@ spine AS (
 
 final AS (
   SELECT DISTINCT
-    DATE_TRUNC('day', event_created_at)::DATE   AS event_day,
-    DATE_TRUNC('week', event_created_at)::DATE  AS event_week,
-    DATE_TRUNC('month', event_created_at)::DATE AS event_month,
-    event_name,
-    stage_name,
-    plan_name_at_event_date,
+    spine.event_day,
+    spine.event_week,
+    spine.event_month,
+    spine.event_name,
+    spine.stage_name,
+    spine.plan_name_at_event_date,
+
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_day, event_name)   AS unique_users_by_event_and_day,
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_week, event_name)  AS unique_users_by_event_and_week,
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_month, event_name) AS unique_users_by_event_and_month,
@@ -61,8 +65,12 @@ final AS (
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_day, stage_name, plan_name_at_event_date)   AS unique_users_by_stage_and_day_and_plan,
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_week, stage_name, plan_name_at_event_date)  AS unique_users_by_stage_and_week_and_plan,
     COUNT(DISTINCT user_id) OVER (PARTITION BY event_month, stage_name, plan_name_at_event_date) AS unique_users_by_stage_and_month_and_plan
-  FROM usage_data_events
-  WHERE event_created_at IS NOT NULL
+  FROM spine
+    LEFT JOIN usage_data_events
+      ON spine.event_day = DATE_TUNC('day', usage_data_events.event_created_at)
+      AND spine.event_name = usage_data_events.event_name
+      AND spine.stage_name = usage_data_events.stage_name
+      AND spine.plan_name_at_event_date = usage_data_events.plan_name_at_event_date
 )
 
 SELECT *
