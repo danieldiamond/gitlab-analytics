0fcffcb84dc494d9ead4805c27c219afdb493703
Michael Walker <mwalker@gitlab.com>
2020-02-27 14:13:31 -0700

    use count * to measure gitlab_com row counts to be more general

--- extract/postgres_pipeline/postgres_pipeline/main.py
+++ extract/postgres_pipeline/postgres_pipeline/main.py
@@ -339,7 +339,7 @@ def main(file_path: str, load_type: str) -> None:
             swap_temp_table(snowflake_engine, real_table_name, table_name)
 
         if loaded:
-            count_query = f"SELECT COUNT(id) FROM {table_name} WHERE id IS NOT NULL"
+            count_query = f"SELECT COUNT(*) FROM {table_name}"
             count = query_executor(snowflake_engine, count_query)[0][0]
             append_to_xcom_file({table_name: count})
         else:
