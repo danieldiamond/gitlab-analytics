d35eca5b98d2e43614ac340738e5d17f1a667d5d
Mathieu Peychet <mpeychet@gitlab.com>
2020-02-27 08:40:05 +0000

    Apply suggestion to transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -283,6 +283,7 @@ WITH gitlab_subscriptions AS (
       FROM {{ event_cte.source_cte_name }}
     {% endif %}
     WHERE created_at IS NOT NULL
+
     {% if is_incremental() %}
 
       AND created_at >= (SELECT MAX(event_created_at) FROM {{this}} WHERE event_name = '{{ event_cte.event_name }}')
