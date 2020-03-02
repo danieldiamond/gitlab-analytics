9ad9ccf633cdf49c44515c8690917181c9ea4174
Parul Luthra <pluthra@gitlab.com>
2020-03-02 15:08:40 -0500

    updating intermediate to get a kpi level breakout

--- transform/snowflake-dbt/models/bamboohr/xf/bamboohr_headcount_intermediate_v2.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/bamboohr_headcount_intermediate_v2.sql
@@ -175,16 +175,15 @@ with dates AS (
 
     SELECT
       DATE_TRUNC(month,start_date)      AS month_date,
-      'department_breakout'             AS breakout_type, 
-      department,
-      division,
-      eeoc_field_name,                                                       
-      eeoc_value,     
+      'kpi_breakout'                    AS breakout_type, 
+      null                              AS department,
+      null                              AS division,
+      null                              AS eeoc_field_name,                                                       
+      null                              AS eeoc_value,     
       {{repeated_metric_columns}}
     FROM dates 
     LEFT JOIN intermediate
       ON DATE_TRUNC(month, start_date) = DATE_TRUNC(month, date_actual)
-    WHERE department IS NOT NULL
     {{ dbt_utils.group_by(n=6) }}  
 
 ) 
