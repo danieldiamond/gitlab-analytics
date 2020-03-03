4611df37e11bd058a632f2ba420463d3cb7e41ff
Parul Luthra <pluthra@gitlab.com>
2020-03-03 15:24:38 -0500

    updating formatting

--- transform/snowflake-dbt/models/snapshots/xf/comp_band_deviation_snapshots.sql
+++ transform/snowflake-dbt/models/snapshots/xf/comp_band_deviation_snapshots.sql
@@ -14,13 +14,17 @@ WITH source AS (
     SELECT
       NULLIF("Employee_ID",'')::VARCHAR                                     AS bamboo_employee_number,
       deviation_from_comp_calc                                              AS original_value_deviation_from_comp_calc,
-      CASE WHEN NULLIF(deviation_from_comp_calc, '') ='Exec'    THEN '0.00'
-           WHEN NULLIF(deviation_from_comp_calc, '') ='#DIV/0!' THEN NULL
-           WHEN deviation_from_comp_calc LIKE '%'               THEN NULLIF(REPLACE(deviation_from_comp_calc,'%',''),'') 
-           ELSE NULLIF(deviation_from_comp_calc, '') END                    AS deviation_from_comp_calc_cl,
-      CASE WHEN "DBT_VALID_FROM"::NUMBER::TIMESTAMP::DATE < '2019-10-18'::date
-           THEN '2000-01-20'::DATE
-           ELSE "DBT_VALID_FROM"::NUMBER::TIMESTAMP::DATE END               AS valid_from,
+      CASE 
+        WHEN NULLIF(deviation_from_comp_calc, '') ='Exec'    
+          THEN '0.00'
+        WHEN NULLIF(deviation_from_comp_calc, '') ='#DIV/0!' 
+          THEN NULL
+        WHEN deviation_from_comp_calc LIKE '%'               
+          THEN NULLIF(REPLACE(deviation_from_comp_calc,'%',''),'') 
+        ELSE NULLIF(deviation_from_comp_calc, '') END                       AS deviation_from_comp_calc_cl,
+      IFF("DBT_VALID_FROM"::NUMBER::TIMESTAMP::DATE < '2019-10-18'::date,
+           '2000-01-20'::DATE,
+           "DBT_VALID_FROM"::NUMBER::TIMESTAMP::DATE)                       AS valid_from,
       "DBT_VALID_TO"::NUMBER::TIMESTAMP::DATE                               AS valid_to
     FROM source
     WHERE deviation_from_comp_calc_cl IS NOT NULL
@@ -28,10 +32,10 @@ WITH source AS (
   ), deduplicated AS (
 
     SELECT DISTINCT   
-      bamboo_employee_number::BIGINT                                              AS bamboo_employee_number,
+      bamboo_employee_number::BIGINT                                       AS bamboo_employee_number,
       IFF(CONTAINS(original_value_deviation_from_comp_calc,'%') = True,
-        ROUND(deviation_from_comp_calc_cl/100::FLOAT, 2),
-        ROUND(deviation_from_comp_calc_cl::FLOAT, 2))                             AS deviation_from_comp_calc,
+          ROUND(deviation_from_comp_calc_cl/100::FLOAT, 2),
+          ROUND(deviation_from_comp_calc_cl::FLOAT, 2))                    AS deviation_from_comp_calc,
     valid_from,
     valid_to
     FROM renamed
