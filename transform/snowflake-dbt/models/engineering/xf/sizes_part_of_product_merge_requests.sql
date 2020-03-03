30d5cbbd1235d3fcf57699adce1a9637afa5b9df
Michael Walker <mwalker@gitlab.com>
2020-03-03 11:25:05 -0700

    join pop_mrs data to product_projects to get project ids

--- transform/snowflake-dbt/models/engineering/xf/sizes_part_of_product_merge_requests.sql
+++ transform/snowflake-dbt/models/engineering/xf/sizes_part_of_product_merge_requests.sql
@@ -16,9 +16,23 @@ WITH split_diff_path AS (
       product_merge_request_files_changed,
       product_merge_request_files_changed_truncated,
       product_merge_request_lines_removed,
-      ARRAY_TO_STRING(ARRAY_SLICE(product_merge_request_diff_url_split, 0, -1), '-')::VARCHAR AS product_merge_request_project,
+      TRIM(ARRAY_TO_STRING(ARRAY_SLICE(product_merge_request_diff_url_split, 0, -1), '-'), '/')::VARCHAR AS product_merge_request_project,
       REGEXP_REPLACE(GET(product_merge_request_diff_url_split, product_merge_request_diff_url_size - 1), '[^0-9]+', '')::bigint AS product_merge_request_id
     FROM split_diff_path
+
+), project_id_merged_in AS (
+
+    SELECT
+      product_merge_request_lines_added,
+      product_merge_request_files_changed,
+      product_merge_request_files_changed_truncated,
+      product_merge_request_lines_removed,
+      product_merge_request_project
+      product_projects.project_id AS product_merge_request_project_id,
+      product_merge_request_iid
+    FROM id_split_out
+    INNER JOIN {{ ref('projects_part_of_product') }} product_projects
+      ON product_projects.project_path = id_split_out.project_path
 
 )
 SELECT * 
