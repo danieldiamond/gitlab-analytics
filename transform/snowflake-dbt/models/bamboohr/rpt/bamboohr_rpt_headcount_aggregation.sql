f6cd4fad388af6266178596829a96356a1f1ca3f
Parul Luthra <pluthra@gitlab.com>
2020-03-04 01:44:01 -0500

    updating rpt to account for situations where the value is less than 4 to not appear and updated schema.yml

--- transform/snowflake-dbt/models/bamboohr/rpt/bamboohr_rpt_headcount_aggregation.sql
+++ transform/snowflake-dbt/models/bamboohr/rpt/bamboohr_rpt_headcount_aggregation.sql
@@ -9,6 +9,10 @@
                               " %}
 
 
+{% set ratio_to_report_partition_statement = "OVER (PARTITION BY base.breakout_type, base.department, base.division, base.eeoc_field_name
+                                              ORDER BY base.month_date)
+                              " %}
+
 with source AS (
 
     SELECT *
@@ -25,7 +29,7 @@ with source AS (
       eeoc_value
     FROM source
 
-)
+), intermediate AS (
 
    SELECT
       base.month_date,
@@ -47,8 +51,6 @@ with source AS (
       SUM(COALESCE(involuntary_separation,0)) {{partition_statement}}               AS rolling_12_month_involuntary_separations,
       IFF(rolling_12_month_headcount< rolling_12_month_separations, null,
         1 - (rolling_12_month_separations/NULLIF(rolling_12_month_headcount,0)))    AS retention,
-       {# RATIO_TO_REPORT(headcount_average) {{partition_statement}}                    AS percent_of_headcount,
-      RATIO_TO_REPORT(hire_count) {{partition_statement}}                           AS percent_of_hires #}
 
       headcount_average_leader,
       hired_leaders,
@@ -60,7 +62,26 @@ with source AS (
 
       headcount_average_contributor,
       hired_contributor,
-      separated_contributor
+      separated_contributor,
+
+      MIN(headcount_average)  {{ratio_to_report_partition_statement}}               AS min_headcount_average,
+      MIN(hire_count) {{ratio_to_report_partition_statement}}                       AS min_hire_count,
+      MIN(headcount_average_leader) {{ratio_to_report_partition_statement}}         AS min_headcount_leader,
+      MIN(headcount_average_manager) {{ratio_to_report_partition_statement}}        AS min_headcount_manager,
+      MIN(headcount_average_contributor) {{ratio_to_report_partition_statement}}    AS min_headcount_contributor,
+
+
+      RATIO_TO_REPORT(headcount_average) 
+        {{ratio_to_report_partition_statement}}                                     AS percent_of_headcount,
+      RATIO_TO_REPORT(hire_count) 
+        {{ratio_to_report_partition_statement}}                                     AS percent_of_hires,
+      RATIO_TO_REPORT(headcount_average_leader) 
+        {{ratio_to_report_partition_statement}}                                     AS percent_of_headcount_leaders,
+      RATIO_TO_REPORT(headcount_average_manager) 
+        {{ratio_to_report_partition_statement}}                                     AS percent_of_headcount_manager,     
+      RATIO_TO_REPORT(headcount_average_contributor) 
+        {{ratio_to_report_partition_statement}}                                     AS percent_of_headcount_contributor
+      
     FROM base
     LEFT JOIN source  
       ON base.month_date = source.month_date
@@ -69,4 +90,51 @@ with source AS (
       AND base.division = source.division
       AND base.eeoc_field_name = source.eeoc_field_name
       AND base.eeoc_value = source.eeoc_value
-    WHERE base.month_date < date_trunc('month', CURRENT_DATE)   
+    WHERE base.month_date < DATE_TRUNC('month', CURRENT_DATE)   
+
+ ), final AS (
+     
+    SELECT   
+      month_date,
+      breakout_type, 
+      department,
+      division,
+      eeoc_field_name,
+      eeoc_value,
+      IFF(headcount_start <4, null,headcount_start)                         AS headcount_start,
+      IFF(headcount_end <4, null, headcount_end)                            AS headcount_end,
+      IFF(headcount_average <4, null, headcount_average)                    AS headcount_average,
+      IFF(hire_count <4, null, hire_count)                                  AS hire_count,
+      IFF(separation_count <4, null, separation_count)                      AS separation_count,
+      
+      rolling_12_month_headcount,
+      rolling_12_month_separations,
+      rolling_12_month_voluntary_separations,
+      rolling_12_month_involuntary_separations,
+      rolling_12_month_voluntary_separations/ rolling_12_month_headcount    AS voluntary_separation_rate,
+      rolling_12_month_involuntary_separations/ rolling_12_month_headcount  AS involuntary_separation_rate,
+      retention,
+
+      IFF(headcount_average_leader < 4, null,headcount_average_leader)      AS headcount_leader,
+      IFF(hired_leaders < 4, null, hired_leaders)                           AS hired_leaders,
+      IFF(separated_leaders < 4, null, separated_leaders)                   AS separated_leaders,
+
+      IFF(headcount_average_manager < 4, null, headcount_average_manager)   AS headcount_manager,
+      IFF(hired_manager < 4, null, hired_manager)                           AS hired_manager,
+      IFF(separated_manager < 4, null, separated_manager)                   AS separated_manager,
+      IFF(headcount_average_contributor < 4, null, 
+            headcount_average_contributor)                                  AS headcount_contributor,
+      IFF(hired_contributor < 4, null, hired_contributor)                   AS hired_contributor,
+      IFF(separated_contributor < 4, null, separated_contributor)           AS separated_contributor,
+
+      IFF(min_headcount_average <2, null, percent_of_headcount)             AS percent_of_headcount,
+      IFF(min_hire_count <2, null, percent_of_hires)                        AS percent_of_hires,
+      IFF(min_headcount_leader <2, null, percent_of_headcount_leaders)      AS percent_of_headcount_leaders,
+      IFF(min_headcount_manager <2, null, percent_of_headcount_manager)     AS percent_of_headcount_manager,
+      IFF(min_headcount_contributor <2, null, percent_of_headcount_leaders) AS percent_of_headcount_contributor
+    FROM intermediate   
+
+)
+
+ SELECT * 
+ FROM final
