115b9d83f3338038a871b530aa637eab9e488552
mpeychet <mpeychet@gitlab.com>
2020-03-05 13:57:39 +0100

    troubleshoot

--- transform/snowflake-dbt/models/snowplow/sessionized_funnels/sessionized_saas_funnel_90.sql
+++ transform/snowflake-dbt/models/snowplow/sessionized_funnels/sessionized_saas_funnel_90.sql
@@ -21,12 +21,7 @@ saas_funnel_subscription_start_page AS (
       page_url_path,
       page_url_query,
       snowplow_page_views_90.referer_url_path,
-      CASE
-        WHEN (snowplow_page_views_90.referer_url_host = 'about.gitlab.com' AND page_url_path = '/subscriptions/new'
-           AND RLIKE(page_url_query, '(.)*plan_id=(2c92a0ff5a840412015aa3cde86f2ba6|2c92a0fd5a840403015aa6d9ea2c46d6|2c92a0fc5a83f01d015aa6db83c45aac)(.)*'))
-        THEN 'SaaS Subscription Page'
-          ELSE 'Others'
-      END AS page_type
+      min_tstamp
     FROM snowplow_page_views_90
     LEFT JOIN snowplow_sessions_90
       ON snowplow_page_views_90.session_id = snowplow_sessions_90.session_id
@@ -48,12 +43,14 @@ saas_funnel_subscription_start_page AS (
     SELECT 
         saas_funnel_subscription_start_page.session_id,
         saas_funnel_subscription_start_page.min_tstamp,
-        saas_funnel_subscription_start_page.session_id IS NOT NULL AS subscription_funnel_start_page,
-        saas_funnel_subscription_success_page.session_id IS NOT NULL AS subscription_success_page
+        saas_funnel_subscription_start_page.session_id IS NOT NULL   AS subscription_funnel_start_page,
+        MIN(saas_funnel_subscription_start_page.min_tstamp)          AS subscription_funnel_start_min_tsamp,
+        saas_funnel_subscription_success_page.session_id IS NOT NULL AS subscription_funnel_success_page,
+        MIN(saas_funnel_subscription_success_page.min_tstamp)        AS subscription_funnel_success_min_tsamp
     FROM saas_funnel_subscription_start_page
     LEFT JOIN saas_funnel_subscription_success_page 
         ON saas_funnel_subscription_start_page.session_id = saas_funnel_subscription_success_page.session_id
-
+    GROUP BY 1,2,3,5
 )
 
 SELECT *
