e6569f1e0bf452e827af9830b8d91c8699272fb2
Justin Stark <jstark@gitlab.com>
2020-03-06 20:46:05 +0000

    Update ga360_session_xf.sql

--- transform/snowflake-dbt/models/google_analytics_360/xf/ga360_session_xf.sql
+++ transform/snowflake-dbt/models/google_analytics_360/xf/ga360_session_xf.sql
@@ -14,9 +14,9 @@ WITH sessions AS (
 	    
 	    --sessions
 	    sessions.*
-		
-		--client_id dimension
-		, clientid_dim.dimension_value 	AS client_id
+	    
+	    --client_id dimension
+	    , clientid_dim.dimension_value 	AS client_id
 		
 	FROM sessions
 	LEFT JOIN session_custom_dims AS clientid_dim
