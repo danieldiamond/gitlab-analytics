f00a52cf85bc98988e312399bef12d4460c571bb
jjstark <jstark@gitlab.com>
2020-03-06 12:39:51 -0800

    models and project config

---
+++ transform/snowflake-dbt/models/google_analytics_360/xf/ga360_session_custom_dimension_xf.sql
@@ -0,0 +1,23 @@
+WITH session_custom_dims AS (
+
+	SELECT *
+	FROM {{ ref('ga360_session_custom_dimension') }}
+
+), ga_index_names AS (
+
+	SELECT * 
+	FROM  {{ ref('google_analytics_custom_dimension_indexes') }}
+
+), named_dims AS(
+
+	SELECT 
+		session_custom_dims.*, 
+		ga_index_names.name	AS dimension_name
+	FROM session_custom_dims
+	LEFT JOIN ga_index_names 
+		ON session_custom_dims.dimension_index = ga_index_names.index
+
+)
+
+SELECT *
+FROM named_dims
---
+++ transform/snowflake-dbt/models/google_analytics_360/xf/ga360_session_xf.sql
@@ -0,0 +1,23 @@
+WITH sessions AS (
+
+	SELECT *
+	FROM {{ ref('ga360_session') }}
+
+), session_custom_dims AS (
+
+	SELECT *
+	FROM {{ ref('ga360_session_custom_dimension') }}
+
+), sessions_xf AS(
+
+	SELECT
+		sessions.*
+		, clientid_dim.dimension_value 	AS client_id
+	FROM sessions
+	LEFT JOIN session_custom_dims AS clientid_dim
+		ON sessions.visit_id = clientid_dim.visit_id
+			and clientid_dim.dimension_index = 6 --clientID
+)
+
+SELECT *
+FROM sessions_xf
