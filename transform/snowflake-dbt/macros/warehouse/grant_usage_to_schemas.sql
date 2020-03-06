52ef5c4a478f6c2130512c03e61d1e5da5806a5d
Taylor Murphy <tmurphy@gitlab.com>
2020-03-06 13:50:08 -0600

    Large refactor

--- transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
+++ transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
@@ -6,17 +6,8 @@
 		{%- set roles = [rolename] -%}
 	{% else %}
 		{%- set roles = [
-				'analyst_growth',
-				'analyst_finance',
-				'analyst_core',
-				'analyst_sensitive',
-				'engineer',
-				'intern',
-				'loader',
-				'product_manager',
-				'reporter',
-				'reporter_sensitive',
-				'transformer'
+				'dbt_analytics',
+                'dbt_analytics_sensitive'
 			] -%}
 	{% endif %}
 
@@ -36,7 +27,7 @@
 			grant select on all views in schema {{ schema_name }}_staging to role {{ role_name }};
 		{%- endif -%}
 
-		{%- if target.name == 'prod' and role_name in ('analyst_sensitive','reporter_sensitive') -%}
+		{%- if target.name == 'prod' and role_name in ('dbt_analytics_sensitive') -%}
 			grant usage on schema {{ schema_name }}_sensitive to role {{ role_name }};
 			grant select on all tables in schema {{ schema_name }}_sensitive to role {{ role_name }};
 			grant select on all views in schema {{ schema_name }}_sensitive to role {{ role_name }};
