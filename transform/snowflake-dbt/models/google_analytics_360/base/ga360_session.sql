fa5ab21c3a8bb11b18ba7e5e856f46f509f00ec4
Justin Stark <jstark@gitlab.com>
2020-03-06 20:47:19 +0000

    Update ga360_session.sql

--- transform/snowflake-dbt/models/google_analytics_360/base/ga360_session.sql
+++ transform/snowflake-dbt/models/google_analytics_360/base/ga360_session.sql
@@ -7,12 +7,12 @@ WITH source AS (
 
 	SELECT
 	    --Keys
-	    visit_id::FLOAT					AS visit_id, 
-	    visitor_id::VARCHAR				AS visitor_id, 
+	    visit_id::FLOAT                 AS visit_id, 
+	    visitor_id::VARCHAR             AS visitor_id, 
 	    
 	    --Info
-	    date::DATE						AS session_date,
-	    visit_start_time::TIMESTAMP_TZ	AS visit_start_time
+	    date::DATE                      AS session_date,
+	    visit_start_time::TIMESTAMP_TZ  AS visit_start_time
 	FROM source
 
 )
