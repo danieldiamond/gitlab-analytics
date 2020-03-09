50c584e99d8d4f0c4cd7203ec451b22f7f8a6326
Michael Walker <mwalker@gitlab.com>
2020-03-09 14:06:27 -0600

    add qualtrics initial model revisions

---
+++ transform/snowflake-dbt/models/qualtrics/base/qualtrics_distribution.sql
@@ -0,0 +1,22 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('qualtrics', 'distribution') }}
+
+), intermediate AS (
+
+      SELECT d.value as data_by_row
+      FROM source,
+      LATERAL FLATTEN(INPUT => parse_json(jsontext), outer => true) d
+
+), parsed AS (
+
+    SELECT 
+      data_by_row['recipients']['mailingListId']::VARCHAR   AS mailing_list_id,
+      data_by_row['id']::VARCHAR                            AS distribution_id,
+      data_by_row['surveyLink']['surveyId']::VARCHAR        AS survey_id,
+      data_by_row['sendDate']::TIMESTAMP                    AS mailing_sent_at
+    FROM intermediate
+
+)
+SELECT * from parsed
