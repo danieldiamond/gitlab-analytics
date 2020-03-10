b80c95d272c036cde5e245997fa8452faffefa58
Taylor Murphy <tmurphy@gitlab.com>
2020-03-10 11:03:48 -0500

    Simplify grants

--- transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
+++ transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
@@ -1,38 +1,33 @@
-{%- macro grant_usage_to_schemas(schema_name, rolename) -%}
+{%- macro grant_usage_to_schemas() -%}
 
 	{%- set schema_name = target.schema -%}
 
-	{% if rolename %}
-		{%- set roles = [rolename] -%}
-	{% else %}
-		{%- set roles = [
-                'dbt_analytics',
-                'dbt_analytics_sensitive'
-			] -%}
-	{% endif %}
+    {#
+        This works in conjunction with the Permifrost roles.yml file. 
+        This will only run on production and mainly covers our bases so that
+        new models created will be immediately available for querying to the 
+        roles listed.
+    #}
+    
+    {%- set non_sensitive = 'dbt_analytics' -%}
+    {%- set sensitive = 'dbt_analytics_sensitive' -%}
 
-	{%- for role_name in roles -%}
+    {%- if target.name == 'prod' -%}
+        grant usage on schema {{ schema_name }} to role {{ non_sensitive }};
+        grant select on all tables in schema {{ schema_name }} to role {{ non_sensitive }};
+        grant select on all views in schema {{ schema_name }} to role {{ non_sensitive }};
 
-		{%- if target.name == 'prod' -%}
-			grant usage on schema {{ schema_name }} to role {{ role_name }};
-			grant select on all tables in schema {{ schema_name }} to role {{ role_name }};
-			grant select on all views in schema {{ schema_name }} to role {{ role_name }};
+        grant usage on schema {{ schema_name }}_meta to role {{ non_sensitive }};
+        grant select on all tables in schema {{ schema_name }}_meta to role {{ non_sensitive }};
+        grant select on all views in schema {{ schema_name }}_meta to role {{ non_sensitive }};
 
-			grant usage on schema {{ schema_name }}_meta to role {{ role_name }};
-			grant select on all tables in schema {{ schema_name }}_meta to role {{ role_name }};
-			grant select on all views in schema {{ schema_name }}_meta to role {{ role_name }};
+        grant usage on schema {{ schema_name }}_staging to role {{ non_sensitive }};
+        grant select on all tables in schema {{ schema_name }}_staging to role {{ non_sensitive }};
+        grant select on all views in schema {{ schema_name }}_staging to role {{ non_sensitive }};
 
-			grant usage on schema {{ schema_name }}_staging to role {{ role_name }};
-			grant select on all tables in schema {{ schema_name }}_staging to role {{ role_name }};
-			grant select on all views in schema {{ schema_name }}_staging to role {{ role_name }};
-		{%- endif -%}
-
-		{%- if target.name == 'prod' and role_name in ('dbt_analytics_sensitive') -%}
-			grant usage on schema {{ schema_name }}_sensitive to role {{ role_name }};
-			grant select on all tables in schema {{ schema_name }}_sensitive to role {{ role_name }};
-			grant select on all views in schema {{ schema_name }}_sensitive to role {{ role_name }};
-		{%- endif -%}
-
-	{%- endfor -%}
+        grant usage on schema {{ schema_name }}_sensitive to role {{ sensitive }};
+        grant select on all tables in schema {{ schema_name }}_sensitive to role {{ sensitive }};
+        grant select on all views in schema {{ schema_name }}_sensitive to role {{ sensitive }};
+    {%- endif -%}
 
 {%- endmacro -%} 
