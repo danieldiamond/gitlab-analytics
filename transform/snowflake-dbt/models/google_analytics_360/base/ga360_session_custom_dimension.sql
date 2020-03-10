b6d99eeea456a82a0e65b7a6271aeec43e5a3bc4
Justin Stark <jstark@gitlab.com>
2020-03-10 16:44:13 +0000

    Update ga360_session_custom_dimension.sql

--- transform/snowflake-dbt/models/google_analytics_360/base/ga360_session_custom_dimension.sql
+++ transform/snowflake-dbt/models/google_analytics_360/base/ga360_session_custom_dimension.sql
@@ -6,13 +6,13 @@ WITH source AS (
 ), renamed AS(
 
 	SELECT
-	  --Keys
-	  index::FLOAT					    AS dimension_index,
-	  visit_id::FLOAT					AS visit_id,
-	  visitor_id::VARCHAR				AS visitor_id,
-
-	  --Info 
-	  value::VARCHAR					AS dimension_value,
+	--Keys
+	  index::FLOAT                      AS dimension_index,
+	  visit_id::FLOAT                   AS visit_id,
+	  visitor_id::VARCHAR               AS visitor_id,
+	  
+	  --Info
+	  value::VARCHAR                    AS dimension_value,
 	  visit_start_time::TIMESTAMP_TZ    AS visit_start_time
 
 	FROM source
