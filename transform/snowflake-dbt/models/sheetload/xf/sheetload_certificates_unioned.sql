1d57a10f8568812a85cc400f49b0ae7a625b737e
emilie <emilie.burke@gmail.com>
2020-03-11 14:26:14 -0400

    add additional docs

--- transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
+++ transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
@@ -1,3 +1,8 @@
+{{ config({
+    "schema": "analytics"
+    })
+}}
+
 {{ dbt_utils.union_relations(
     relations=[ref('sheetload_ally_certificate'),
                ref('sheetload_values_certificate'),
