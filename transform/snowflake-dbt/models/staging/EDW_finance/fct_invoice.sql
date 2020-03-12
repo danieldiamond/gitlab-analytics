ab303e5015671048c9a9ce32472aff399bf09a36
msendal <msendal@gitlab.com>
2020-03-12 17:39:15 +0100

    update fct invoice and try calculating mrr from invoice charged value

--- transform/snowflake-dbt/models/staging/EDW_finance/fct_invoice.sql
+++ transform/snowflake-dbt/models/staging/EDW_finance/fct_invoice.sql
@@ -1,31 +1,35 @@
 WITH zuora_invoice AS (
-
     SELECT *
     FROM {{ ref('zuora_invoice') }}
 
 ), zuora_invoice_item AS (
 
-    SELECT *
-    FROM {{ ref('zuora_invoice_item') }}
+SELECT *
+FROM {{ ref('zuora_invoice_item') }}
 
-), zuora_product AS (
+    ), invoice_data AS (
+select zuora_invoice.INVOICE_ID,
+    zuora_invoice.ACCOUNT_ID,
+    zuora_invoice.DUE_DATE::DATE AS invoice_due_date,
+    zuora_invoice.INVOICE_NUMBER,
+    zuora_invoice.INVOICE_DATE::DATE AS invoice_date,
+    zuora_invoice.STATUS as invoice_status,
+    POSTED_DATE::DATE
+    invoice_item_id,
+    zuora_invoice_item.SUBSCRIPTION_ID,
+    zuora_invoice_item.SKU,
+    zuora_invoice_item.service_start_Date::DATE as service_start_Date,
+    zuora_invoice_item.SERVICE_END_DATE::DATE as SERVICE_END_DATE,
+    datediff(month, zuora_invoice_item.SERVICE_START_DATE::DATE, zuora_invoice_item.SERVICE_END_DATE) as service_months,
+    zuora_invoice_item.CHARGE_AMOUNT,
+    zuora_invoice_item.CHARGE_DATE::DATE,
+    zuora_invoice_item.CHARGE_NAME,
+    zuora_invoice_item.RATE_PLAN_CHARGE_ID
+from zuora_invoice
+    join zuora_invoice_item
+ON zuora_invoice_item.invoice_id = zuora_invoice.invoice_id
+    )
 
-    SELECT *
-    FROM {{ ref('zuora_product_source') }}
-    WHERE is_deleted = FALSE
-
-)
-SELECT
-      zuora_invoice.invoice_number,
-      zuora_invoice_item.invoice_item_id,
-      zuora_invoice_item.rate_plan_charge_id,
-      zuora_invoice.invoice_date::DATE              AS invoice_date,
-      zuora_invoice_item.service_start_date::DATE   AS service_start_date,
-      zuora_invoice_item.service_end_date::DATE     AS service_end_date,
-      --zuora_invoice.amount_without_tax              AS invoice_amount_without_tax,
-      zuora_invoice_item.charge_amount              AS invoice_item_charge_amount,
-      zuora_invoice_item.tax_amount                 AS invoice_item_tax_amount,
-      zuora_invoice.status                          AS invoice_status
-    FROM zuora_invoice_item
-    INNER JOIN zuora_invoice
-      ON zuora_invoice_item.invoice_id = zuora_invoice.invoice_id
+select *,
+       iff(service_months = 0, 0, charge_amount / service_months) as mrr
+from invoice_data
