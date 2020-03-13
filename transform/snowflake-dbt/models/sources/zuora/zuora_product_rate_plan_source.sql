9c649477368a55e897a3cc6d6c9c705b5c6e6fcd
msendal <msendal@gitlab.com>
2020-03-13 12:53:48 +0100

    add source model for zuora product rate plan

---
+++ transform/snowflake-dbt/models/sources/zuora/zuora_product_rate_plan_source.sql
@@ -0,0 +1,35 @@
+with source as (
+
+    select * from zuora_stitch_schema.productrateplan
+
+),
+
+renamed as (
+
+    select
+
+        id as product_rate_plan_id,
+        productid as product_id,
+        rateplansku__c as rate_plan_sku,
+
+        rows_allowed__c as rows_allowed,
+        integrations_enabled__c as integrations_enabled,
+        log_days__c as log_days,
+
+        name,
+        description,
+        effectiveenddate::date as effective_end_date,
+        effectivestartdate::date as effective_start_date,
+
+        deleted,
+
+        createdbyid as created_by_id,
+        createddate as created_at,
+        updatedbyid as updated_by_id,
+        updateddate as updated_date
+
+    from source
+
+)
+
+select * from renamed
