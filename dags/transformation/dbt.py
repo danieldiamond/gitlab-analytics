bc0cf34df91f829fcea971a6383eca2e4d0f9fce
Michael Walker <mwalker@gitlab.com>
2020-03-13 10:45:11 -0600

    add LOAD credentials for loading DBT info to Snowflake

--- dags/transformation/dbt.py
+++ dags/transformation/dbt.py
@@ -25,6 +25,10 @@ from kube_secrets import (
     SNOWFLAKE_TRANSFORM_SCHEMA,
     SNOWFLAKE_TRANSFORM_WAREHOUSE,
     SNOWFLAKE_USER,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,
 )
 
 # Load the env vars into a dict and set Secrets
@@ -223,6 +227,10 @@ dbt_source_freshness = KubernetesPodOperator(
         SNOWFLAKE_TRANSFORM_ROLE,
         SNOWFLAKE_TRANSFORM_WAREHOUSE,
         SNOWFLAKE_TRANSFORM_SCHEMA,
+        SNOWFLAKE_LOAD_PASSWORD,
+        SNOWFLAKE_LOAD_ROLE,
+        SNOWFLAKE_LOAD_USER,
+        SNOWFLAKE_LOAD_WAREHOUSE,
     ],
     env_vars=pod_env_vars,
     arguments=[dbt_source_cmd],
