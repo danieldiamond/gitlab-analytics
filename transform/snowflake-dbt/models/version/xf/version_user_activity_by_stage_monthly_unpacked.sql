4eb9a51653be1a703214ea1c475c5ddfdc178fbe
mpeychet <mpeychet@gitlab.com>
2020-03-17 17:01:42 +0100

    smaller set of columns

--- transform/snowflake-dbt/models/version/xf/version_user_activity_by_stage_monthly_unpacked.sql
+++ transform/snowflake-dbt/models/version/xf/version_user_activity_by_stage_monthly_unpacked.sql
@@ -3,74 +3,17 @@
 WITH usage_data AS (
 
     SELECT {{ dbt_utils.star(from=ref('version_usage_data'), except=['LICENSE_ID', 'LICENSE_STARTS_AT', 'LICENSE_EXPIRES_AT']) }}
-    FROM {{ ref('version_usage_data') }}
-
-), licenses AS ( -- Licenses app doesn't alter rows after creation so the snapshot is not necessary.
-
-    SELECT *
-    FROM {{ ref('license_db_licenses') }}
-
-), zuora_subscriptions AS (
-
-    SELECT *
-    FROM {{ ref('zuora_subscription')}}
-
-), zuora_accounts AS (
-
-    SELECT *
-    FROM {{ ref('zuora_account')}}
-
-), joined AS (
-
-    SELECT
-      usage_data.*,
-      licenses.license_id,
-      licenses.zuora_subscription_id,
-      licenses.company,
-      licenses.plan_code                      AS license_plan_code,
-      licenses.starts_at                      AS license_starts_at,
-      licenses.license_expires_at,
-      zuora_subscriptions.subscription_status AS zuora_subscription_status,
-      zuora_accounts.crm_id                   AS zuora_crm_id
-
-    FROM usage_data
-      LEFT JOIN licenses
-        ON usage_data.license_md5 = licenses.license_md5
-      LEFT JOIN zuora_subscriptions
-        ON licenses.zuora_subscription_id = zuora_subscriptions.subscription_id
-      LEFT JOIN zuora_accounts
-        ON zuora_subscriptions.account_id = zuora_accounts.account_id
-    WHERE licenses.email IS NULL
-      OR NOT (email LIKE '%@gitlab.com' AND LOWER(company) LIKE '%gitlab%') -- Exclude internal tests licenses.
+    FROM {{ ref('version_usage_data_unpacked') }}
 
 ), unpacked_stage_json AS (
 
     SELECT
-      {{ dbt_utils.star(from=ref('version_usage_data'), except=['stats_used']) }},
-      CASE
-        WHEN uuid = 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f' THEN 'SaaS'
-        ELSE 'Self-Managed'
-      END                                                                             AS ping_source,
-      CONCAT(CONCAT(SPLIT_PART(version, '.', 1), '.'), SPLIT_PART(version, '.', 2))   AS major_version,
-      CASE WHEN LOWER(edition) LIKE '%ee%' THEN 'EE'
-        ELSE 'CE' END                                                                 AS main_edition,
-      CASE WHEN edition LIKE '%CE%' THEN 'Core'
-          WHEN edition LIKE '%EES%' THEN 'Starter'
-          WHEN edition LIKE '%EEP%' THEN 'Premium'
-          WHEN edition LIKE '%EEU%' THEN 'Ultimate'
-          WHEN edition LIKE '%EE Free%' THEN 'Core'
-          WHEN edition LIKE '%EE%' THEN 'Starter'
-        ELSE NULL END                                                                 AS edition_type,
-      license_plan_code,
-      company,
-      zuora_subscription_id,
-      zuora_subscription_status,
-      zuora_crm_id,
+      *,
       f.key                                                                           AS stage_name,
       f.value                                                                         AS stage_activity_count_json
 
-    FROM joined,
-      lateral flatten(input => joined.usage_activity_by_stage) f
+    FROM usage_data,
+      lateral flatten(input => usage_data.usage_activity_by_stage) f
     WHERE IS_OBJECT(f.value) = TRUE
       AND stats_used IS NOT NULL
     {% if is_incremental() %}
@@ -80,7 +23,12 @@ WITH usage_data AS (
 ), final AS (
 
     SELECT
-      {{ dbt_utils.star(from=ref('version_usage_data'), except=['stats_used']) }},
+      unpacked_stage_json.id,
+      unpacked_stage_json.version,
+      unpacked_stage_json.created_at,
+      unpacked_stage_json.license_id,
+      unpacked_stage_json.uuid,
+      unpacked_stage_json.edition,      
       unpacked_stage_json.ping_source,
       unpacked_stage_json.major_version,
       unpacked_stage_json.main_edition,
