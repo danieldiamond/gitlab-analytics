812d2a00b81f141c471e9e15409adffe622d4f59
Taylor Murphy <tmurphy@gitlab.com>
2020-03-18 15:21:10 -0500

    Configuration and formatting

--- transform/snowflake-dbt/models/sources/license/license_db_add_ons_source.sql
+++ transform/snowflake-dbt/models/sources/license/license_db_add_ons_source.sql
@@ -1,18 +1,18 @@
 WITH source AS (
 
-  SELECT *
-  FROM {{ source('license', 'add_ons') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+    SELECT *
+    FROM {{ source('license', 'add_ons') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
-  SELECT DISTINCT
-    id::INTEGER             AS add_on_id,
-    name::VARCHAR           AS add_on_name,
-    code::VARCHAR           AS add_on_code,
-    created_at::TIMESTAMP   AS created_at,
-    updated_at::TIMESTAMP   AS updated_at
- FROM source
+    SELECT DISTINCT
+      id::INTEGER             AS add_on_id,
+      name::VARCHAR           AS add_on_name,
+      code::VARCHAR           AS add_on_code,
+      created_at::TIMESTAMP   AS created_at,
+      updated_at::TIMESTAMP   AS updated_at
+    FROM source
 
 )
 
--- transform/snowflake-dbt/models/sources/license/license_db_granted_add_ons_source.sql
+++ transform/snowflake-dbt/models/sources/license/license_db_granted_add_ons_source.sql
@@ -1,19 +1,19 @@
 WITH source AS (
 
-  SELECT *
-  FROM {{ source('license', 'granted_add_ons') }}
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+    SELECT *
+    FROM {{ source('license', 'granted_add_ons') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
-  SELECT DISTINCT
-    id::INTEGER            AS granted_add_on_id,
-    license_id::INTEGER    AS license_id,
-    add_on_id::INTEGER     AS add_on_id,
-    quantity::INTEGER      AS quantity,
-    created_at::TIMESTAMP  AS created_at,
-    updated_at::TIMESTAMP  AS updated_at
- FROM source
+    SELECT DISTINCT
+      id::INTEGER            AS granted_add_on_id,
+      license_id::INTEGER    AS license_id,
+      add_on_id::INTEGER     AS add_on_id,
+      quantity::INTEGER      AS quantity,
+      created_at::TIMESTAMP  AS created_at,
+      updated_at::TIMESTAMP  AS updated_at
+    FROM source
 
 )
 
