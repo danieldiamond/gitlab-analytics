bf8a38c82a1b6d41a392ea933f0b2ffb2d47f5ca
mpeychet <mpeychet@gitlab.com>
2020-03-19 16:08:28 +0100

    add incrementality

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_secure_stage_ci_jobs.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_secure_stage_ci_jobs.sql
@@ -8,6 +8,11 @@ WITH ci_builds AS (
   
   SELECT *
   FROM {{ ref('gitlab_dotcom_ci_builds') }}
+  {% if is_incremental() %}
+
+    WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
+
+  {% endif %}
 
 ), projects AS (
   
