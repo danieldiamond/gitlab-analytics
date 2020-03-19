67e22b81d246c724d1e4fecde90b2cf1583ced4e
mpeychet <mpeychet@gitlab.com>
2020-03-19 11:32:00 +0100

    add fields

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_secure_stage_ci_jobs.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_secure_stage_ci_jobs.sql
@@ -9,6 +9,21 @@ WITH ci_builds AS (
   SELECT *
   FROM {{ ref('gitlab_dotcom_ci_builds') }}
 
+), projects AS (
+  
+  SELECT *
+  FROM {{ ref('gitlab_dotcom_projects') }}
+
+), namespace_lineage AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_namespace_lineage')}}
+
+), gitlab_subscriptions AS (
+
+    SELECT *
+    FROM {{ref('gitlab_dotcom_gitlab_subscriptions_snapshots_namespace_id_base')}}
+
 ), secure_ci_builds AS (
   
   SELECT 
@@ -35,8 +50,33 @@ WITH ci_builds AS (
           ci_build_name LIKE '%license_management%' OR
           ci_build_name LIKE '%license_scanning%' OR
           ci_build_name LIKE '%sast%'
-          
+        )
 )
 
+, joined AS (
+  
+  SELECT 
+    secure_ci_builds.*,
+    namespace_lineage.namespace_is_internal      AS is_internal_job,
+    namespace_lineage.ultimate_parent_id,
+    namespace_lineage.ultimate_parent_plan_id,
+    namespace_lineage.ultimate_parent_plan_title,
+    namespace_lineage.ultimate_parent_plan_is_paid,
+
+    CASE
+      WHEN gitlab_subscriptions.is_trial
+        THEN 'trial'
+      ELSE COALESCE(gitlab_subscriptions.plan_id, 34)::VARCHAR
+    END AS plan_id_at_job_creation
+  FROM secure_ci_builds
+  LEFT JOIN projects
+    ON secure_ci_builds.ci_build_project_id = projects.project_id
+  LEFT JOIN namespace_lineage
+    ON projects.namespace_id = namespace_lineage.namespace_id
+  LEFT JOIN gitlab_subscriptions
+    ON namespace_lineage.ultimate_parent_id = gitlab_subscriptions.namespace_id
+    AND issues.created_at BETWEEN gitlab_subscriptions.valid_from AND {{ coalesce_to_infinity("gitlab_subscriptions.valid_to") }}
+  
+)
 SELECT *
 FROM secure_ci_builds
