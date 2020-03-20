2a4f1f34100e1d967bfdeb4d1ad016832932a33d
mpeychet <mpeychet@gitlab.com>
2020-03-20 15:15:21 +0100

    linting

--- transform/snowflake-dbt/models/snowplow/sessionized_funnels/sessionized_saas_funnel_90.sql
+++ transform/snowflake-dbt/models/snowplow/sessionized_funnels/sessionized_saas_funnel_90.sql
@@ -25,8 +25,9 @@ saas_funnel_subscription_start_page AS (
     FROM snowplow_page_views_90
     LEFT JOIN snowplow_sessions_90
       ON snowplow_page_views_90.session_id = snowplow_sessions_90.session_id
-    WHERE  (snowplow_page_views_90.referer_url_host = 'about.gitlab.com' AND page_url_path = '/subscriptions/new'
-      AND RLIKE(page_url_query, '(.)*plan_id=(2c92a0ff5a840412015aa3cde86f2ba6|2c92a0fd5a840403015aa6d9ea2c46d6|2c92a0fc5a83f01d015aa6db83c45aac)(.)*'))
+    WHERE  snowplow_page_views_90.referer_url_host = 'about.gitlab.com' 
+      AND page_url_path = '/subscriptions/new'
+      AND RLIKE(page_url_query, '(.)*plan_id=(2c92a0ff5a840412015aa3cde86f2ba6|2c92a0fd5a840403015aa6d9ea2c46d6|2c92a0fc5a83f01d015aa6db83c45aac)(.)*')
       
 )
 
@@ -35,6 +36,7 @@ saas_funnel_subscription_start_page AS (
     SELECT *
     FROM snowplow_page_views_90
     WHERE (RLIKE(snowplow_page_views_90.page_url_path, '/subscriptions/([a-zA-Z0-9\-]{1,})/success/create_subscription') 
+      --SaaS packages have these 3 plan_id
       AND RLIKE(snowplow_page_views_90.referer_url_query, '(.)*plan_id=(2c92a0ff5a840412015aa3cde86f2ba6|2c92a0fd5a840403015aa6d9ea2c46d6|2c92a0fc5a83f01d015aa6db83c45aac)(.)*'))
 )
 
