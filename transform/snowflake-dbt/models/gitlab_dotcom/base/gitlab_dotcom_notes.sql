c8979634a4427e4f06db707b2e2df7773e738ef5
Eli Kastelein <ekastelein@gitlab.com>
2020-03-20 11:41:47 -0700

    Remove temp exclusion

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_notes.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_notes.sql
@@ -15,8 +15,7 @@ WITH source AS (
   WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
 
   {% endif %}
-  --QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
-  LIMIT 100000 --TEMP
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ), renamed AS (
 
