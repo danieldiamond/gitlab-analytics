8a558da413d957ff5d4f4e3bf31b36db5bfbbb8b
msendal <msendal@gitlab.com>
2020-03-20 14:39:58 +0100

    fix subscription_id source

--- transform/snowflake-dbt/models/staging/finance/fct_charge.sql
+++ transform/snowflake-dbt/models/staging/finance/fct_charge.sql
@@ -8,17 +8,17 @@ WITH zuora_rate_plan AS (
     SELECT *
     FROM {{ ref('zuora_rate_plan_charge_source') }}
 
-), zuora_revenue_schedule_item_amount as(
+), zuora_revenue_schedule_item as(
     SELECT RATE_PLAN_CHARGE_ID,
            REVENUE_SCHEDULE_ITEM_AMOUNT
-    FROM {{ ref('zuora_rate_plan_charge_source') }}
-    )
-    zuora_base_charges AS (
+    FROM {{ ref('zuora_revenue_schedule_item_source') }}
+
+), base_charges AS (
 
     SELECT
       zuora_rate_plan_charge.rate_plan_charge_id as charge_id,
       zuora_rate_plan_charge.product_id,
-      zuora_rate_plan_charge.subscription_id,
+      zuora_rate_plan.subscription_id,
       zuora_rate_plan_charge.account_id,
       zuora_rate_plan_charge.rate_plan_charge_number,
       zuora_rate_plan_charge.rate_plan_charge_name,
@@ -35,11 +35,12 @@ WITH zuora_rate_plan AS (
       zuora_rate_plan_charge.discount_level,
       zuora_rate_plan_charge.segment                      AS rate_plan_charge_segment,
       zuora_rate_plan_charge.version                      AS rate_plan_charge_version,
-      zuora_revenue_schedule_item_amount.REVENUE_SCHEDULE_ITEM_AMOUNT as revenue_amt,
+      zuora_revenue_schedule_item.REVENUE_SCHEDULE_ITEM_AMOUNT AS revenue_amt
     FROM zuora_rate_plan
     INNER JOIN zuora_rate_plan_charge
       ON zuora_rate_plan.rate_plan_id = zuora_rate_plan_charge.rate_plan_id
-          inner join zuora_revenue_schedule_item_amount on zuora_revenue_schedule_item_amount.rate_plan_charge_id=zuora_rate_plan_charge.rate_plan_charge_id
+          INNER JOIN zuora_revenue_schedule_item
+          ON zuora_revenue_schedule_item.rate_plan_charge_id=zuora_rate_plan_charge.rate_plan_charge_id
 
 ), final AS (
 
