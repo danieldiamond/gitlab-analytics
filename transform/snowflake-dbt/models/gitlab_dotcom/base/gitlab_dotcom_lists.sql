d7e63a48032f8daf6f7c1112eec73b74ae6ba2b0
mpeychet <mpeychet@gitlab.com>
2020-03-25 18:13:17 +0100

    add base models

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_lists.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_lists.sql
@@ -7,16 +7,16 @@ WITH source AS (
 ), renamed AS (
 
       SELECT
-        id::INTEGER               AS list_id
-        board_id::INTEGER         AS board_id
-        label_id::INTEGER         AS label_id
-        list_type::INTEGER        AS list_type
-        created_at::TIMESTAMP     AS created_at
-        updated_at::TIMESTAMP     AS updated_at
-        user_id::INTEGER          AS user_id
-        milestone_id::INTEGER     AS milestone_id
-        max_issue_count::INTEGER  AS max_issue_count
-        max_issue_weight::INTEGER AS max_issue_weight
+        id::INTEGER               AS list_id,
+        board_id::INTEGER         AS board_id,
+        label_id::INTEGER         AS label_id,
+        list_type::INTEGER        AS list_type,
+        created_at::TIMESTAMP     AS created_at,
+        updated_at::TIMESTAMP     AS updated_at,
+        user_id::INTEGER          AS user_id,
+        milestone_id::INTEGER     AS milestone_id,
+        max_issue_count::INTEGER  AS max_issue_count,
+        max_issue_weight::INTEGER AS max_issue_weight,
         limit_metric::VARCHAR     AS limit_metric
       FROM source
 
