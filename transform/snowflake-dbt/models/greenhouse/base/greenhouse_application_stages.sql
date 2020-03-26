5db352702aa7abcd785d848de0f399a433b6574d
Parul Luthra <pluthra@gitlab.com>
2020-03-26 19:01:26 -0400

    updating stages to reflect milestones

--- transform/snowflake-dbt/models/greenhouse/base/greenhouse_application_stages.sql
+++ transform/snowflake-dbt/models/greenhouse/base/greenhouse_application_stages.sql
@@ -17,7 +17,40 @@ WITH source as (
 
 	FROM source
 
+), intermediate AS (
+
+      SELECT 
+      renamed.application_id,
+      stage_id,
+      application_stage_name,
+      IFF(stage_id IN (45 --application_review
+                   ,7 --screen
+                   ,8 -- Screen
+                   ,26 --screening
+                   ,22 --HM Interview
+                   ,46 --HIring Manager Interview
+                   ,23 --Team Interviews
+                   ,47 --Team Interview    
+                   ,33 --face to face
+                   ,48 --executive interview
+                   ,58 --references
+                   ,59 --Reference Check    
+                   ,53 --background check
+                   ,61 --offer
+                   ),True, False)               AS is_milestone_stage,
+        CASE WHEN stage_id IN (7,8,26) 
+               THEN 'Screen'
+             WHEN stage_id IN (23,47)  
+               THEN 'Team Interview'
+             WHEN stage_id IN (41, 57) 
+               THEN 'Take Home Assessment'
+             WHEN stage_id IN (22, 46) 
+               THEN 'Hiring Manager Review'
+             WHEN stage_id in (58,59)
+               THEN 'Reference Check'
+             ELSE application_stage_name END    AS stages_cleaned             
+    FROM renamed 
 )
 
 SELECT *
-FROM renamed
+FROM intermediate
