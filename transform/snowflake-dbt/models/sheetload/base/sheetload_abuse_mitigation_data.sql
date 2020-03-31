1cd5a82a5658c8fef239a9cfa057d5f6515db004
Ken Aguilar <kaguilar@gitlab.com>
2020-03-31 01:49:50 +0000

    Update transform/snowflake-dbt/models/sheetload/base/sheetload_abuse_mitigation_data.sql, transform/snowflake-dbt/models/sheetload/base/schema.yml, transform/snowflake-dbt/models/sheetload/base/sources.yml files

---
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_abuse_mitigation_data.sql
@@ -0,0 +1,30 @@
+WITH source AS (
+    
+    SELECT * 
+    FROM {{ source('sheetload','abuse_mitigation_data') }}
+
+), final AS (
+    
+    SELECT 
+        NULLIF(email_domain, '')::VARCHAR                                 AS email_domain,
+        NULLIF(account_creation_date, '')::VARCHAR::DATE                  AS account_creation_date,
+        NULLIF(account_creation_time, '')::VARCHAR::TIME                  AS account_creation_time,
+        NULLIF(account_creation_timestamp, '')::VARCHAR::DATETIME         AS account_creation_timestamp,
+        NULLIF(category, '')::VARCHAR                                     AS category,
+        NULLIF(description, '')::VARCHAR                                  AS description,
+        NULLIF(automation, '')::VARCHAR                                   AS automation,
+        TRY_TO_NUMBER(mitigation_week)                                    AS mitigation_week,
+        TRY_TO_NUMBER(mitigation_month)                                   AS mitigation_month,
+        NULLIF(mitigation_date, '')::VARCHAR::DATE                        AS mitigation_date,
+        NULLIF(mitigation_time, '')::VARCHAR::TIME                        AS mitigation_time,
+        NULLIF(mitigation_timestamp, '')::VARCHAR::DATETIME               AS mitigation_timestamp,
+        NULLIF(timezone, '')::VARCHAR                                     AS timezone,
+        TRY_TO_DECIMAL(time_to_mitigate)                                  AS time_to_mitigate,
+        TRY_TO_NUMBER(rule_id)                                            AS rule_id,
+        NULLIF(rule_name, '')::VARCHAR                                    AS rule_name
+    FROM source
+
+) 
+
+SELECT * 
+FROM final
