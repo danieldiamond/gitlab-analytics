641478e1c1d5c5ee762b4503dda76747e1bf1746
mpeychet <mpeychet@gitlab.com>
2020-04-02 15:04:19 +0200

    sort columns

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
@@ -20,14 +20,24 @@ WITH date_details AS (
 )
 
 SELECT 
-  DATEADD('month', -1, date_day)            AS smau_month,
+  DATEADD('month', -1, date_day)                  AS smau_month,
+  
+  -- ids 
   user_id,
   namespace_id,
+  
+  -- event data
   event_name,
   stage_name,
-  COUNT(*)                                  AS event_count,
-  COuNT(DISTINCT TO_DATE(event_created_at)) AS event_day_count
+  
+  --metadata
+  DATEDIFF('day', user_created_at, date_day)      AS days_since_user_creation,
+  DATEDIFF('day', namespace_created_at, date_day) AS days_since_namespace_creation,
+  
+  COUNT(*)                                        AS event_count,
+  COUNT(DISTINCT TO_DATE(event_created_at))       AS event_day_count
 FROM date_details
-LEFT JOIN gitlab_dotcom_usage_data_events ON gitlab_dotcom_usage_data_events.event_created_at BETWEEN DATEADD('day', -28, date_details.date_day) AND date_day 
-WHERE day_of_month = 1 AND date_day < current_date
+LEFT JOIN gitlab_dotcom_usage_data_events 
+  ON gitlab_dotcom_usage_data_events.event_created_at BETWEEN DATEADD('day', -28, date_details.date_day) AND date_day 
+WHERE day_of_month = 1
 GROUP BY 1,2,3,4,5
