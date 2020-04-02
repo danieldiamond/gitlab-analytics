45e1b9b7c906a792cd5f927d65c0f03ef10987de
Mathieu Peychet <mpeychet@gitlab.com>
2020-04-02 13:02:03 +0000

    Apply suggestion to transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_monthly_stage_active_users.sql
@@ -28,6 +28,8 @@ SELECT
   COUNT(*)                                  AS event_count,
   COuNT(DISTINCT TO_DATE(event_created_at)) AS event_day_count
 FROM date_details
-LEFT JOIN gitlab_dotcom_usage_data_events ON gitlab_dotcom_usage_data_events.event_created_at BETWEEN DATEADD('day', -28, date_details.date_day) AND date_day 
+LEFT JOIN gitlab_dotcom_usage_data_events 
+  ON gitlab_dotcom_usage_data_events.event_created_at 
+  BETWEEN DATEADD('day', -28, date_details.date_day) AND date_day 
 WHERE day_of_month = 1 AND date_day < current_date
 GROUP BY 1,2,3,4,5
