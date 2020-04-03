017ae205c8aa1ab1b6508b2701bca26d1f21dd2f
Michael Walker <mwalker@gitlab.com>
2020-04-03 12:03:15 -0600

    create new base model for sheetload_release_blog_posts

---
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_release_blog_posts.sql
@@ -0,0 +1,17 @@
+with source as (
+
+    SELECT * FROM {{ source('sheetload', 'release_blog_posts') }}
+
+), renamed as (
+
+    SELECT 
+      release::VARCHAR              AS release,
+      base_items::INTEGER           AS base_items,
+      bug_items::INTEGER            AS bug_items,
+      performance_items::INTEGER    AS performance_items
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
