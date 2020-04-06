069f14159e19d30a215b4dd2a30dd0ad4c9ec229
Eli Kastelein <ekastelein@gitlab.com>
2020-04-06 14:11:49 -0700

    Add comma

--- transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
+++ transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
@@ -9,7 +9,7 @@ WITH source AS (
     FROM {{ ref('version_usage_data_source') }}
     WHERE uuid IS NOT NULL
       AND hostname NOT IN ( -- Staging data has no current use cases for analysis.
-        'staging.gitlab.com'
+        'staging.gitlab.com',
         'dr.gitlab.com'
       )
 
