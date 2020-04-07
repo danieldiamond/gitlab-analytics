529476f41ed08a7bcf163684391e930cd7b21827
Eli Kastelein <ekastelein@gitlab.com>
2020-04-07 11:39:04 -0700

    Add minor version

--- transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
+++ transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
@@ -5,7 +5,10 @@
 
 WITH source AS (
 
-    SELECT *
+    SELECT
+      *,
+      SPLIT_PART(version, '.', 1)::INT AS major_version,
+      SPLIT_PART(version, '.', 2)::INT AS minor_version
     FROM {{ ref('version_usage_data_source') }}
     WHERE uuid IS NOT NULL
 
