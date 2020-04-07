c49c8808c1a9eceb3f309e515965b3f246a19744
Eli Kastelein <ekastelein@gitlab.com>
2020-04-07 14:31:32 -0700

    Add NULLIF

--- transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
+++ transform/snowflake-dbt/models/staging/version/intermediate/version_usage_data.sql
@@ -2,16 +2,12 @@ WITH source AS (
 
     SELECT
       *,
-      LTRIM(version, 'v')                           AS cleaned_version,
+      LTRIM(NULLIF(version, ''), 'v')               AS cleaned_version,
       SPLIT_PART(cleaned_version, '.', 1)::INT      AS major_version,
       SPLIT_PART(cleaned_version, '.', 2)::INT      AS minor_version,
       major_version || '.' || minor_version         AS major_minor_version
     FROM {{ ref('version_usage_data_source') }}
-    WHERE uuid NOT IN (
-      '13880013', -- Bad `version` values
-      '13718000',
-      '14040899'
-    )
+    WHERE version NOT LIKE ('%VERSION%') -- Messy data that's not worth parsing.
 
 )
 
