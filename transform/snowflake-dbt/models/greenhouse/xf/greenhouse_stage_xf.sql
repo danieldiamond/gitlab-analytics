71fa3d68f5dac694729bb6110b18b1870b931251
Parul Luthra <pluthra@gitlab.com>
2020-04-07 16:17:41 -0400

    adding application submitted as a stage

--- transform/snowflake-dbt/models/greenhouse/xf/greenhouse_stage_xf.sql
+++ transform/snowflake-dbt/models/greenhouse/xf/greenhouse_stage_xf.sql
@@ -3,30 +3,10 @@
         })
     }} #}
 
-    WITH stages AS (
 
-        SELECT *
-        FROM {{ ref ('greenhouse_application_stages') }}
-        WHERE stage_entered_on IS NOT NULL
-    
-    ), recruiting_xf AS (
-
-    SELECT * 
-    FROM {{ ref ('greenhouse_recruiting_xf') }}
-
-    ), stages_intermediate AS (
-    
-        SELECT 
-        stages.application_id,
-        candidate_id,
-        stages.stages_cleaned                                                          AS application_stage,
-        stages.is_milestone_stage,
-        DATE_TRUNC(MONTH, application_date)                                            AS application_month,
-        IFF(application_stage_name = 'Offer',offer_sent_date, stages.stage_entered_on) AS stage_entered_on,
-        IFF(application_stage_name = 'Offer', offer_resolved_date, 
-            COALESCE(stages.stage_exited_on, CURRENT_DATE()))                           AS stage_exited_on,
-        requisition_id,
-        application_status                                                              AS current_application_status,
+{% set repeated_column_names = 
+        "requisition_id,
+        application_status,
         job_name,
         department_name,
         division_modified,
@@ -38,8 +18,52 @@
         rejection_reason_name,
         rejection_reason_type,
         is_hired_in_bamboo,
-        time_to_offer            AS app_to_offer_turntime,
-        ROW_NUMBER() OVER (PARTITION BY stages.application_id, candidate_id ORDER BY stage_entered_on DESC) AS row_number_stages_desc
+        time_to_offer" %}
+
+
+WITH stages AS (
+
+    SELECT *
+    FROM {{ ref ('greenhouse_application_stages') }}
+    WHERE stage_entered_on IS NOT NULL
+    
+), recruiting_xf AS (
+
+    SELECT * 
+    FROM {{ ref ('greenhouse_recruiting_xf') }}
+
+
+), applications AS (
+
+        SELECT 
+        application_id,
+        candidate_id,
+        'Application Submitted'                                                         AS application_stage,
+        TRUE                                                                            AS is_milestone_stage,
+        DATE_TRUNC(MONTH, application_date)                                             AS application_month,
+        application_date                                                                AS stage_entered_on,
+        null                                                                            AS stage_exited_on,
+        1                                                                               AS row_number_stages_asc,
+        null                                                                            AS row_number_stages_desc,
+        {{repeated_column_names}}
+        FROM recruiting_xf 
+
+    ), stages_intermediate AS (
+    
+        SELECT 
+        stages.application_id,
+        candidate_id,
+        stages.stages_cleaned                                                           AS application_stage,
+        stages.is_milestone_stage,
+        DATE_TRUNC(MONTH, application_date)                                             AS application_month,
+        IFF(application_stage_name = 'Offer',offer_sent_date, stages.stage_entered_on)  AS stage_entered_on,
+        IFF(application_stage_name = 'Offer', offer_resolved_date, 
+            COALESCE(stages.stage_exited_on, CURRENT_DATE()))                           AS stage_exited_on,
+        ROW_NUMBER() OVER (PARTITION BY stages.application_id, candidate_id 
+                            ORDER BY stage_entered_on)                                  AS row_number_stages_asc,
+        ROW_NUMBER() OVER (PARTITION BY stages.application_id, candidate_id 
+                            ORDER BY stage_entered_on DESC)                             AS row_number_stages_desc,
+        {{repeated_column_names}}
         FROM stages
         LEFT JOIN recruiting_xf 
         ON recruiting_xf.application_id = stages.application_id
@@ -49,30 +73,23 @@
         SELECT 
         application_id,
         candidate_id,
-        application_status                                                              AS application_stage,
+        IFF(application_status = 'hired','Hired', 'Rejected')                           AS application_stage,
         TRUE                                                                            AS is_milestone_stage,
         DATE_TRUNC(MONTH, application_date)                                             AS application_month,
         IFF(application_status = 'hired',candidate_target_hire_date, rejected_at)       AS stage_entered_on,
         IFF(application_status = 'hired',candidate_target_hire_date, rejected_at)       AS stage_exited_on,
-        requisition_id,
-        application_status                                                              AS current_application_status,
-        job_name,
-        department_name,
-        division_modified,
-        source_name,
-        source_type,
-        sourcer_name,
-        candidate_recruiter,
-        candidate_coordinator,
-        rejection_reason_name,
-        rejection_reason_type,
-        is_hired_in_bamboo,
-        time_to_offer                                                                   AS app_to_offer_turntime,
-        1 AS row_number_stages_desc
+        null                                                                            AS row_number_stages_asc,
+        1                                                                               AS row_number_stages_desc,
+        {{repeated_column_names}}
         FROM recruiting_xf 
         WHERE application_status in ('hired', 'rejected')
     
     ), all_stages AS (
+
+        SELECT * 
+        FROM applications 
+
+        UNION ALL
     
         SELECT * 
         FROM stages_intermediate
@@ -87,8 +104,9 @@
         SELECT 
         application_id,
         candidate_id,
-        MIN(stage_entered_on)                                   AS min_stage_entered_on,
-        MAX(stage_exited_on)                                    AS max_stage_exited_on,
+        MIN(stage_entered_on)                                       AS min_stage_entered_on,
+        MAX(stage_exited_on)                                        AS max_stage_exited_on,
+        SUM(IFF(application_stage = 'Application Submitted',1,0))   AS hit_application_submitted,
         SUM(IFF(application_stage = 'Application Review',1,0))  AS hit_application_review,
         SUM(IFF(application_stage = 'Assessment',1,0))          AS hit_assessment,
         SUM(IFF(application_stage = 'Screen',1,0))              AS hit_screening,
@@ -109,24 +127,13 @@
         application_stage, 
         is_milestone_stage,
         application_month,
-        DATE_TRUNC(MONTH,stage_entered_on) AS month_stage_entered_on,
-        DATE_TRUNC(MONTH, stage_exited_on) AS month_stage_exited_on,
-        DATEDIFF(DAY, stage_entered_on, COALESCE(stage_exited_on, CURRENT_DATE())) AS days_in_stage,
-        DATEDIFF(DAY, min_stage_entered_on, max_stage_exited_on) AS days_in_pipeline,
-        requisition_id,
-        current_application_status,
-        job_name,
-        department_name,
-        division_modified,
-        source_name,
-        source_type,
-        sourcer_name,
-        candidate_recruiter,
-        candidate_coordinator,
-        rejection_reason_name,
-        rejection_reason_type,
-        is_hired_in_bamboo,
-        app_to_offer_turntime,
+        stage_entered_on,
+        stage_exited_on,
+        DATE_TRUNC(MONTH,stage_entered_on)                                          AS month_stage_entered_on,
+        DATE_TRUNC(MONTH,stage_exited_on)                                           AS month_stage_exited_on,
+        DATEDIFF(DAY, stage_entered_on, COALESCE(stage_exited_on, CURRENT_DATE()))  AS days_in_stage,
+        DATEDIFF(DAY, min_stage_entered_on, max_stage_exited_on)                    AS days_in_pipeline,
+        {{repeated_column_names}},
         hit_application_review,
         hit_assessment,
         hit_screening,
@@ -134,20 +141,19 @@
         hit_reference_check,
         hit_offer,
         hit_hired,
-        CASE WHEN application_stage IN ('hired', 'rejected') 
-               THEN 1
-             WHEN hit_hired = 1 OR hit_rejected = 1 
-               THEN row_number_stages_desc+1
-             ELSE row_number_stages_desc END                    AS row_number_stages_desc
+        hit_rejected,
+        IFF(application_stage IN ('Hired','Rejected'), 1,
+            ROW_NUMBER() OVER (PARTITION BY all_stages.application_id, all_stages.candidate_id 
+                ORDER BY stage_entered_on DESC))                              AS row_number_stages_desc
         FROM all_stages
         LEFT JOIN stages_hit 
-        ON all_stages.application_id = stages_hit.application_id
-        AND all_stages.candidate_id = stages_hit.candidate_id
+          ON all_stages.application_id = stages_hit.application_id
+          AND all_stages.candidate_id = stages_hit.candidate_id
         
     )
 
     SELECT *,
       ROW_NUMBER() OVER (PARTITION BY application_id, candidate_id ORDER BY row_number_stages_desc DESC) AS row_number_stages_asc,
-      LEAD(application_stage) OVER (PARTITION BY application_id, candidate_id ORDER BY row_number_stages_desc DESC) AS next_stage,
+      LEAD(application_stage) OVER (PARTITION BY application_id, candidate_id ORDER BY row_number_stages_asc) AS next_stage,
       IFF(row_number_stages_desc = 1, TRUE, FALSE) AS current_stage
     FROM final
