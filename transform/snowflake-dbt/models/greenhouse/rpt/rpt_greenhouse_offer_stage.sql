9bfff48e7694379eb05cc08ac892104bdecc44eb
Parul Luthra <pluthra@gitlab.com>
2020-04-08 13:23:39 -0400

    removing where clause filtering for north america

--- transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_offer_stage.sql
+++ transform/snowflake-dbt/models/greenhouse/rpt/rpt_greenhouse_offer_stage.sql
@@ -72,7 +72,6 @@ WITH recruiting_xf AS (
       COUNT(offer_id)                                           AS candidates_estimated_to_start,
       SUM(IFF(offer_status = 'accepted',1,0))                   AS accepted_offers_to_start
     FROM data_set
-    WHERE geographic_region = 'North America'
     GROUP BY 1,2
     ORDER BY 1 DESC
 
