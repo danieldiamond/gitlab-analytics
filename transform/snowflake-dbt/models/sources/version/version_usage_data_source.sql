03264d4bc5e2ff916f49e314c139d5572af07ba7
Eli Kastelein <ekastelein@gitlab.com>
2020-04-09 14:00:02 -0700

    Revert todos

--- transform/snowflake-dbt/models/sources/version/version_usage_data_source.sql
+++ transform/snowflake-dbt/models/sources/version/version_usage_data_source.sql
@@ -11,12 +11,7 @@ WITH source AS (
     {% if is_incremental() %}
     WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
     {% endif %}
-
-    --TEMP
-    --QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
-    LIMIT 100000
-
-    
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
 ),
 
