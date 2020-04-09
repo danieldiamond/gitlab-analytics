e8f4b2233f274afcedb0b0d522fa4ec56efe2549
Michael Walker <mwalker@gitlab.com>
2020-04-09 16:45:03 -0600

    fix json field parsing in qualtrics nps survey model

--- transform/snowflake-dbt/models/sources/qualtrics/qualtrics_nps_survey_responses.sql
+++ transform/snowflake-dbt/models/sources/qualtrics/qualtrics_nps_survey_responses.sql
@@ -18,12 +18,11 @@ WITH source AS (
 ), response_parsed AS (
 
     SELECT 
-      data_by_row["responseId"]::VARCHAR  AS response_id,
-      data_by_row["values"]::VARIANT      AS response_values
+      data_by_row['responseId']::VARCHAR  AS response_id,
+      data_by_row['values']::VARIANT      AS response_values
     FROM parsed
     QUALIFY ROW_NUMBER() OVER (PARTITION BY response_id ORDER BY uploaded_at DESC) = 1
 
-  
 )
 SELECT * 
 FROM response_parsed
