8045af244702338140ae68ec5fa49a9c5237cb60
Jean Del Rosario <jpeguero@gitlab.com>
2020-04-13 10:21:35 -0400

    Add total columns

--- transform/snowflake-dbt/models/sources/google_analytics_360/ga360_session.sql
+++ transform/snowflake-dbt/models/sources/google_analytics_360/ga360_session.sql
@@ -12,7 +12,17 @@ WITH source AS (
 	    
 	  --Info
 	  date::DATE                      AS session_date,
-	  visit_start_time::TIMESTAMP_TZ  AS visit_start_time
+	  visit_start_time::TIMESTAMP_TZ  AS visit_start_time,
+      visit_number::FLOAT             AS visit_number,
+      total_visits::FLOAT             AS total_visits,
+      total_pageviews::FLOAT          AS total_pageviews,
+      total_screenviews::FLOAT        AS total_screenviews,
+      total_unique_screenviews::FLOAT AS total_unique_screenviews,
+      total_hits::FLOAT               AS total_hits,
+      total_new_visits::FLOAT         AS total_new_visits,
+      total_time_on_screen::FLOAT     AS total_time_on_screen,
+      total_time_on_site::FLOAT       AS total_time_on_site
+
 	FROM source
 
 )
