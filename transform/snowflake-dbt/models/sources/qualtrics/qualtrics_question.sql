b8e338100a03f1663bb6be14665ad393e754753e
Michael Walker <mwalker@gitlab.com>
2020-04-13 15:18:00 -0600

    fix qualtrics question model spacing

--- transform/snowflake-dbt/models/sources/qualtrics/qualtrics_question.sql
+++ transform/snowflake-dbt/models/sources/qualtrics/qualtrics_question.sql
@@ -7,18 +7,18 @@ WITH source AS (
 
 ), questions AS (
 
-  SELECT d.value AS data_by_row
-  FROM source,
-  LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext), outer => true) d
+    SELECT d.value AS data_by_row
+    FROM source,
+    LATERAL FLATTEN(INPUT => PARSE_JSON(jsontext), outer => true) d
   
 ), parsed AS (
 
-  SELECT 
-    data_by_row['survey_id']::VARCHAR               AS survey_id,
-    data_by_row['QuestionID']::VARCHAR              AS question_id,
-    data_by_row['QuestionDescription']::VARCHAR     AS question_description,
-    data_by_row['Choices']::ARRAY                   AS answer_choices
-  FROM questions
+    SELECT 
+      data_by_row['survey_id']::VARCHAR               AS survey_id,
+      data_by_row['QuestionID']::VARCHAR              AS question_id,
+      data_by_row['QuestionDescription']::VARCHAR     AS question_description,
+      data_by_row['Choices']::ARRAY                   AS answer_choices
+    FROM questions
 
 )
 SELECT * 
