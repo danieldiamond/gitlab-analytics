76f0074615560790457651d541af0c781ea67a65
msendal <msendal@gitlab.com>
2020-04-15 11:49:29 +0200

    add customer_id to account

--- transform/snowflake-dbt/models/staging/common/dim_accounts.sql
+++ transform/snowflake-dbt/models/staging/common/dim_accounts.sql
@@ -12,14 +12,21 @@ WITH zuora_account AS (
 
     SELECT DISTINCT account_id
     FROM {{ref('zuora_excluded_accounts')}}
+), sfdc_account AS (
+
+    SELECT *
+    FROM {{ ref('sfdc_account_source') }}
+
 )
+
 
 SELECT
     zuora_account.account_id,
+    zuora_account.crm_id,
+    sfdc_account.ULTIMATE_PARENT_ACCOUNT_ID AS customer_id,
     zuora_account.account_number,
     zuora_account.account_name,
     zuora_account.status     AS account_status,
-    zuora_account.crm_id,
     zuora_account.parent_id,
     zuora_account.sfdc_account_code,
     zuora_account.currency   AS account_currency,
@@ -29,3 +36,5 @@ SELECT
 FROM zuora_account
      LEFT JOIN zuora_contact
     ON COALESCE(zuora_account.sold_to_contact_id, zuora_account.bill_to_contact_id) = zuora_contact.contact_id
+    LEFT JOIN sfdc_account
+    ON sfdc_account.account_id = zuora_account.crm_id
--- transform/snowflake-dbt/models/staging/common/dim_customers.sql
+++ transform/snowflake-dbt/models/staging/common/dim_customers.sql
@@ -1,9 +1,3 @@
-/*
- Parent: An accumulation of Accounts under an organization with common ownership.
- In the case of the U.S. government, we count U.S. government major agencies as a unique parent account.
- (In Salesforce this is the Ultimate Parent Account field)
- */
-
 WITH sfdc_account AS (
 
     SELECT *
@@ -19,13 +13,13 @@ WITH sfdc_account AS (
     SELECT *
     FROM {{ ref('sfdc_record_type_source') }}
 
-) joined AS (
+)
 
     SELECT
       sfdc_account.account_id AS customer_id,
       sfdc_account.account_name AS customer_name,
       sfdc_account.record_type_id AS record_type_id,
-      sfdc_account.gitlab_entity
+      sfdc_account.gitlab_entity,
       sfdc_account.federal_account AS federal_account,
       sfdc_account.gitlab_com_user,
       sfdc_account.account_owner,
@@ -43,8 +37,5 @@ WITH sfdc_account AS (
     LEFT JOIN sfdc_record_type
       ON sfdc_account.record_type_id = sfdc_record_type.record_type_id
     where account_id = ULTIMATE_PARENT_ACCOUNT_ID
-)
 
-SELECT *
-FROM joined
 
