219aa9e089a6cb36901c0143403dbecb59a54649
Eli Kastelein <ekastelein@gitlab.com>
2020-04-16 11:10:34 -0700

    sync

--- transform/snowflake-dbt/tests/version/version_releases_has_newest_release.sql
+++ transform/snowflake-dbt/tests/version/version_releases_has_newest_release.sql
@@ -1,18 +1,19 @@
 WITH max_usage_ping AS (
 
-  SELECT MAX(created_at) AS max_timestamp
+  SELECT MAX(created_at) AS max_usage_ping
   FROM {{ ref('version_usage_data_unpacked') }}
 
 ),
 
 max_release_date AS (
 
-  SELECT MAX(release_date) AS max_timestamp
+  SELECT MAX(release_date) AS max_release_date
   FROM {{ ref('version_releases') }} 
 
 )
 
 /* Version releases should always have a higher max timestamp */
-SELECT COUNT(*)
-FROM max_usage_ping, max_release_date
-WHERE max_usage_ping.max_timestamp >= max_release_date.max_timestamp
+SELECT *
+FROM max_usage_ping
+  INNER JOIN max_release_date
+    ON max_usage_ping.max_usage_ping >= max_release_date.max_release_date
