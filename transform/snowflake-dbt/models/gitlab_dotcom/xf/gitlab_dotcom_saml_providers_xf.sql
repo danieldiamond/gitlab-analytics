f67a7d9da83729c633359f5ba783ecb455d8794d
mpeychet <mpeychet@gitlab.com>
2020-04-22 13:21:46 +0200

    add _xf

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_saml_providers_xf.sql
@@ -0,0 +1,28 @@
+WITH saml_providers AS (
+  
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_saml_providers') }}
+
+)
+
+, identities AS (
+  
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_identities') }}
+
+)
+
+, joined AS (
+  
+    SELECT 
+      saml_providers.*,
+      COUNT(DISTINCT user_id) AS saml_provider_user_count,
+      MIN(created_at)         AS firstt_saml_provider_created_at
+    FROM saml_providers
+    LEFT JOIN identities 
+      ON saml_providers.saml_provider_id = identities.saml_provider_id
+    {{ dbt_utils.group_by(n=8) }}
+)
+
+SELECT *
+FROM joined
