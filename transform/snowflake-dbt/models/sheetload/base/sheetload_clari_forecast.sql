2436ed58950620e8c51050ac37510e8a3e88b0e0
Justin Stark <jstark@gitlab.com>
2020-04-22 21:34:11 +0000

    Update sheetload_clari_forecast.sql

--- transform/snowflake-dbt/models/sheetload/base/sheetload_clari_forecast.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_clari_forecast.sql
@@ -8,16 +8,16 @@ WITH source AS (
     SELECT
          "User"::VARCHAR                          AS user,
          "Email"::varchar                         AS email,
-         "CRM_User_ID"::varchar                   AS crm_user_id
-         "Sales_Team_Role"::varchar                          AS role
-         "Parent_Role"::varchar                   AS parent_role
-         "Timeframe"::varchar                     AS timeframe
-         "Field"::varchar                         AS field
-         "Week"::INTEGER                          AS week
-         "Start_Day"::DATE                        AS start_date
-         "End_Day"::DATE                          AS end_day
-         NULLIF("Data_Type"::varchar)                     AS data_type
-         NULLIF("Data_Value"::varchar)                    AS data_value
+         "CRM_User_ID"::varchar                   AS crm_user_id,
+         "Sales_Team_Role"::varchar               AS role,
+         "Parent_Role"::varchar                   AS parent_role,
+         "Timeframe"::varchar                     AS timeframe,
+         "Field"::varchar                         AS field,
+         "Week"::INTEGER                          AS week,
+         "Start_Day"::DATE                        AS start_date,
+         "End_Day"::DATE                          AS end_day,
+         NULLIF("Data_Type"::varchar)             AS data_type,
+         NULLIF("Data_Value"::varchar)            AS data_value
     FROM source
 
 )
