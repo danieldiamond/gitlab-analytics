41e910e6de3c289706fd8b8d8012b125e0a7e796
Justin Stark <jstark@gitlab.com>
2020-04-23 18:45:38 +0000

    Update fct_charges.sql

--- transform/snowflake-dbt/models/staging/common/fct_charges.sql
+++ transform/snowflake-dbt/models/staging/common/fct_charges.sql
@@ -16,26 +16,26 @@ WITH zuora_rate_plan AS (
 ), base_charges AS (
 
     SELECT
-      zuora_rate_plan_charge.rate_plan_charge_id AS charge_id,
+      zuora_rate_plan_charge.rate_plan_charge_id        AS charge_id,
       zuora_rate_plan_charge.product_id,
       zuora_rate_plan.subscription_id,
       zuora_rate_plan_charge.account_id,
       zuora_rate_plan_charge.rate_plan_charge_number,
       zuora_rate_plan_charge.rate_plan_charge_name,
       TO_NUMBER(TO_CHAR(zuora_rate_plan_charge.effective_start_date, 'YYYYMMDD'),'99999999')
-                                    AS effective_start_date_id,
+                                                        AS effective_start_date_id,
       TO_NUMBER(TO_CHAR(zuora_rate_plan_charge.effective_end_date, 'YYYYMMDD'), '99999999')
-                                    AS effective_end_date_id,
+                                                        AS effective_end_date_id,
       zuora_rate_plan_charge.unit_of_measure,
       zuora_rate_plan_charge.quantity,
       zuora_rate_plan_charge.mrr,
       zuora_rate_plan_charge.delta_tcv,
-      zuora_rate_plan.rate_plan_name AS rate_plan_name,
+      zuora_rate_plan.rate_plan_name                    AS rate_plan_name,
       {{ product_category('zuora_rate_plan.rate_plan_name') }},
       {{ delivery('product_category')}},
       zuora_rate_plan_charge.discount_level,
-      zuora_rate_plan_charge.segment AS rate_plan_charge_segment,
-      zuora_rate_plan_charge.version AS rate_plan_charge_version
+      zuora_rate_plan_charge.segment                    AS rate_plan_charge_segment,
+      zuora_rate_plan_charge.version                    AS rate_plan_charge_version
     FROM zuora_rate_plan
     INNER JOIN zuora_rate_plan_charge
       ON zuora_rate_plan.rate_plan_id = zuora_rate_plan_charge.rate_plan_id
