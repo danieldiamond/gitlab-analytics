0146dfb2ef31b9aaa72269dfe301d93ff5e71162
Michael Walker <mwalker@gitlab.com>
2020-04-23 16:45:00 -0600

    generate numbers using row numbers instead of generator

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details.sql
@@ -16,18 +16,22 @@ WITH source AS (
   {% endif %}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-), details_parsed AS (
+), numbers AS (
+
+    SELECT ROW_NUMBER() OVER (ORDER BY id DESC) rn
+    FROM {{ source('gitlab_dotcom', 'audit_events') }}
+    ORDER BY rn ASC
+    LIMIT 11
+
+
+),details_parsed AS (
 
     SELECT
       id::INTEGER                                                   AS audit_event_id,
       REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, rn, 'c', 1) AS key_name,
       REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, rn, 'c', 2) AS key_value
     FROM source
-    INNER JOIN
-    (
-        select seq4() + 1 as rn
-        from table(generator(rowcount => 11)) v 
-    )
+    INNER JOIN numbers
     WHERE key_name IS NOT NULL
 
 )
