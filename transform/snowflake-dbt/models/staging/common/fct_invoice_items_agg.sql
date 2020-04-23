5e90f257e3a2e6ffc2e75980291ad577cbd09a6f
Justin Stark <jstark@gitlab.com>
2020-04-23 18:40:57 +0000

    Update fct_invoice_items_agg.sql

--- transform/snowflake-dbt/models/staging/common/fct_invoice_items_agg.sql
+++ transform/snowflake-dbt/models/staging/common/fct_invoice_items_agg.sql
@@ -13,7 +13,7 @@ WITH zuora_invoice AS (
   SELECT
     zuora_invoice_item.RATE_PLAN_CHARGE_ID            AS charge_id,
     zuora_invoice_item.SKU AS SKU,
-    MIN(zuora_invoice_item.service_start_Date::DATE) AS service_start_date,
+    MIN(zuora_invoice_item.service_start_Date::DATE)  AS service_start_date,
     MAX(zuora_invoice_item.SERVICE_END_DATE::DATE)    AS service_end_date,
     SUM(zuora_invoice_item.CHARGE_AMOUNT)             AS charge_amount_sum,
     SUM(zuora_invoice_item.tax_amount)                AS tax_amount_sum
