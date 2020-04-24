2d388406dce4a4fa1c177cc84a420d96076824b2
Michael Walker <mwalker@gitlab.com>
2020-04-24 16:17:42 -0600

    add primitive count checking to pulling MR information

--- extract/gitlab_api_mrs/src/execute.py
+++ extract/gitlab_api_mrs/src/execute.py
@@ -7,9 +7,11 @@ from typing import Dict, Any, List
 
 import pandas as pd
 import requests
+from sqlalchemy.engine.base import Engine
 
 from api import GitLabAPI
 from gitlabdata.orchestration_utils import (
+    query_executor,
     snowflake_engine_factory,
     snowflake_stage_load_copy_remove,
 )
@@ -25,6 +27,25 @@ def get_product_project_ids() -> List[str]:
     return csv["project_id"].unique()
 
 
+def verify_mr_information(
+    pulled_mrs: int, project_id: int, snowflake_engine: Engine, start: str, end: str
+) -> None:
+    """
+    """
+    count_query = f"""
+        SELECT count(*) 
+        FROM ANALYTICS.ANALYTICS.GITLAB_DOTCOM_MERGE_REQUESTS_XF 
+        WHERE updated_at BETWEEN '{start}' AND '{end}'
+        AND project_id = {project_id}
+    """
+    result_set = query_executor(snowflake_engine, count_query)
+    checked_mr_count = result_set[0][0]
+    if checked_mr_count != pulled_mrs:
+        logging.warn(
+            f"Project {project_id} MR counts didn't match: pulled {pulled_mrs}, see {checked_mr_count} in database."
+        )
+
+
 if __name__ == "__main__":
 
     logging.basicConfig(stream=sys.stdout, level=20)
@@ -35,6 +56,9 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         logging.error("Script ran without specifying extract.")
         sys.exit(1)
+
+    start = config_dict["START"]
+    end = config_dict["END"]
 
     extract_name = sys.argv[1]
 
@@ -66,11 +90,14 @@ if __name__ == "__main__":
     api_token = env["GITLAB_COM_API_TOKEN"]
     api_client = GitLabAPI(api_token)
 
+    load_database: str = configuration["SNOWFLAKE_LOAD_DATABASE"]
+
     for project_id in configuration["project_ids"]:
         logging.info(f"Extracting project {project_id}.")
-        mr_urls = api_client.get_urls_for_mrs_for_project(
-            project_id, config_dict["START"], config_dict["END"]
-        )
+        mr_urls = api_client.get_urls_for_mrs_for_project(project_id, start, end)
+
+        verify_mr_information(len(mr_urls), project_id, snowflake_engine, start, end)
+
         wrote_to_file = False
 
         with open(file_name, "w") as out_file:
@@ -83,7 +110,7 @@ if __name__ == "__main__":
         if wrote_to_file:
             snowflake_stage_load_copy_remove(
                 file_name,
-                f"raw.{schema}.{stage}",
-                f"raw.{schema}.{extract_name}_merge_requests",
+                f"{load_database}.{schema}.{stage}",
+                f"{load_database}.{schema}.{extract_name}_merge_requests",
                 snowflake_engine,
             )
