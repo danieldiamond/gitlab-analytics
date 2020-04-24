a09cd456d78d54a629d647fe40730a85150f086f
msendal <msendal@gitlab.com>
2020-04-24 12:34:41 +0200

    remove unused columns for ARR

--- transform/snowflake-dbt/models/staging/common/dim_customers.sql
+++ transform/snowflake-dbt/models/staging/common/dim_customers.sql
@@ -9,11 +9,6 @@ WITH sfdc_account AS (
     SELECT *
     FROM {{ ref('sfdc_users_source') }}
 
-), sfdc_record_type AS (
-
-    SELECT *
-    FROM {{ ref('sfdc_record_type_source') }}
-
 ), ultimate_parent_account AS (
 
     SELECT
@@ -42,7 +37,9 @@ WITH sfdc_account AS (
 )
 
 SELECT
-  sfdc_account.account_id                 AS customer_id,
+  sfdc_account.account_id                 AS crm_id,
+  IFF(sfdc_account.is_deleted, master_records.sfdc_master_record_id, sfdc_account.account_id )
+                                          AS customer_id,
   sfdc_account.account_name               AS customer_name,
   sfdc_account.billing_country            AS customer_country,
   ultimate_parent_account.account_id      AS ultimate_parent_account_id,
@@ -56,17 +53,7 @@ SELECT
   sfdc_account.account_owner_team,
   sfdc_account.account_type,
   sfdc_users.name                         AS technical_account_manager,
-  sfdc_record_type.record_type_name,
-  sfdc_record_type.business_process_id,
-  sfdc_record_type.record_type_label,
-  sfdc_record_type.record_type_description,
-  sfdc_record_type.record_type_modifying_object_type,
-  sfdc_account.is_deleted                 AS is_deleted,
-  CASE
-    WHEN sfdc_account.is_deleted
-      THEN master_records.sfdc_master_record_id
-    ELSE NULL
-    END                                   AS merged_to_account_id
+  sfdc_account.is_deleted                 AS is_deleted
 FROM sfdc_account
 LEFT JOIN master_records
   ON sfdc_account.account_id = master_records.account_id
@@ -74,8 +61,7 @@ LEFT JOIN ultimate_parent_account
   ON ultimate_parent_account.account_id = sfdc_account.ultimate_parent_account_id
 LEFT OUTER JOIN sfdc_users
   ON sfdc_account.technical_account_manager_id = sfdc_users.id
-LEFT JOIN sfdc_record_type
-  ON sfdc_account.record_type_id = sfdc_record_type.record_type_id
+
 
 
 
