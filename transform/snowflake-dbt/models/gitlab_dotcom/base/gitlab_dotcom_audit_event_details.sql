8367a782607b75426030b1b92778a2e8c616f4c4
Michael Walker <mwalker@gitlab.com>
2020-04-24 09:52:23 -0600

    use series macro from dbt for audit event details

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details.sql
@@ -16,23 +16,19 @@ WITH source AS (
   {% endif %}
   QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-), numbers AS (
+), sequence AS (
 
-    SELECT ROW_NUMBER() OVER (ORDER BY id DESC) rn
-    FROM {{ source('gitlab_dotcom', 'audit_events') }}
-    ORDER BY rn ASC
-    LIMIT 11
-
+    {{ dbt_utils.generate_series(upper_bound=11) }}
 
 ), details_parsed AS (
 
     SELECT
-      id::INTEGER                                                   AS audit_event_id,
-      REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, rn, 'c', 1) AS key_name,
-      REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, rn, 'c', 2) AS key_value,
-      updated_at::TIMESTAMP                                         AS updated_at
+      id::INTEGER                                                                 AS audit_event_id,
+      REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, generated_number, 'c', 1) AS key_name,
+      REGEXP_SUBSTR(details, '\\:([a-z_]*)\\: (.*)', 1, generated_number, 'c', 2) AS key_value,
+      updated_at::TIMESTAMP                                                       AS updated_at
     FROM source
-    INNER JOIN numbers
+    INNER JOIN sequence
     WHERE key_name IS NOT NULL
 
 )
