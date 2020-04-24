0526060fadae417103d39a363ea2af66615e4221
msendal <msendal@gitlab.com>
2020-04-24 12:55:29 +0200

    incorporate subscription to customer join; change customer_id definition

--- transform/snowflake-dbt/models/staging/common/dim_accounts.sql
+++ transform/snowflake-dbt/models/staging/common/dim_accounts.sql
@@ -14,17 +14,11 @@ WITH zuora_account AS (
       account_id
     FROM {{ref('zuora_excluded_accounts')}}
 
-), sfdc_account AS (
-
-    SELECT *
-    FROM {{ ref('sfdc_account_source') }}
-
 )
 
 SELECT
   zuora_account.account_id,
   zuora_account.crm_id,
-  sfdc_account.account_id       AS customer_id,
   zuora_account.account_number,
   zuora_account.account_name,
   zuora_account.status          AS account_status,
@@ -41,5 +35,3 @@ SELECT
 FROM zuora_account
 LEFT JOIN zuora_contact
   ON COALESCE(zuora_account.sold_to_contact_id, zuora_account.bill_to_contact_id) = zuora_contact.contact_id
-LEFT JOIN sfdc_account
-  ON sfdc_account.account_id = zuora_account.crm_id
