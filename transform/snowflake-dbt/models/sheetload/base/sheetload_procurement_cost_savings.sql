b9b1cf5ed6ad65801ecda6bc17730233f3f9eae6
Kathleen Tam <ktam@gitlab.com>
2020-04-25 03:19:54 +0000

    add data type transformation

--- transform/snowflake-dbt/models/sheetload/base/sheetload_procurement_cost_savings.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_procurement_cost_savings.sql
@@ -6,11 +6,11 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-      calendar_month::VARCHAR                              AS calendar_month,
-      savings::VARCHAR                                     AS savings,
-      rolling_12_month_savings_without_audit::VARCHAR      AS rolling_12_month_savings_without_audit,
-      rolling_12_month_savings_with_audit::VARCHAR         AS rolling_12_month_savings_with_audit,
-      target::VARCHAR                                      AS target
+      TRY_TO_DATE(calendar_month::VARCHAR)                                      AS calendar_month,
+      TRY_TO_DECIMAL(savings::VARCHAR,14,2)                                     AS savings,
+      TRY_TO_DECIMAL(rolling_12_month_savings_without_audit::VARCHAR,14,2)      AS rolling_12_month_savings_without_audit,
+      TRY_TO_DECIMAL(rolling_12_month_savings_with_audit::VARCHAR,14,2)         AS rolling_12_month_savings_with_audit,
+      TRY_TO_DECIMAL(target::VARCHAR,14,2)                                      AS target
     FROM source
 
 )
