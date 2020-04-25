36475db2a40702ab2a5b1767d61f7fa3a2a26a55
Kathleen Tam <ktam@gitlab.com>
2020-04-25 01:20:42 +0000

    add sheetload model for procurement cost savings

---
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_procurement_cost_savings.sql
@@ -0,0 +1,20 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('sheetload', 'procurement_cost_savings') }}
+
+), renamed AS (
+
+    SELECT
+      calendar_month::DATE                                               AS calendar_month,
+      TRY_TO_DECIMAL(savings, 14, 2)                                     AS savings,
+      TRY_TO_DECIMAL(rolling_12_month_savings_without_audit, 14, 2)      AS rolling_12_month_savings_without_audit,
+      TRY_TO_DECIMAL(rolling_12_month_savings_with_audit, 14, 2)         AS rolling_12_month_savings_with_audit,
+      TRY_TO_DECIMAL(target, 14, 2)                                    AS target
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
+
