d63f0d2492cb69ff32614702ff9681a803c960e8
Parul Luthra <pluthra@gitlab.com>
2020-04-26 00:25:31 -0400

    updating hire_calculation

--- transform/snowflake-dbt/models/staging/greenhouse/xf/greenhouse_hires.sql
+++ transform/snowflake-dbt/models/staging/greenhouse/xf/greenhouse_hires.sql
@@ -11,7 +11,7 @@ WITH applications AS (
     FROM  {{ ref ('greenhouse_offers_source') }}
     WHERE offer_status = 'accepted'
 
-), bamboo as (
+), bamboo_status as (
  
     SELECT *,
       ROW_NUMBER() OVER (PARTITION BY employee_id ORDER BY valid_from_date) AS bamboo_row_number
@@ -27,47 +27,41 @@ WITH applications AS (
     SELECT 
       bamboohr_mapping.employee_id,
       bamboohr_mapping.greenhouse_candidate_id,
-      valid_from_date,
-      hire_date,
-      region,
-      bamboo_row_number,
-      is_rehire
+      bamboohr_mapping.hire_date,
+      bamboo_status.valid_from_date,
+      bamboohr_mapping.region,
+      bamboo_status.bamboo_row_number,
+      bamboo_status.is_rehire
     FROM bamboohr_mapping
-    LEFT JOIN bamboo 
-      ON bamboo.employee_id = bamboohr_mapping.employee_id
-      AND (bamboo_row_number = 1 OR is_rehire=TRUE)
+    LEFT JOIN bamboo_status 
+      ON bamboo_status.employee_id = bamboohr_mapping.employee_id
+    WHERE bamboo_status.bamboo_row_number = 1 OR bamboo_status.is_rehire= 'True'
 
 ), final AS (
 
     SELECT 
       applications.application_id,  
-      candidate_id, 
-      employee_id,
-      offers.start_date                                         AS candidate_target_hire_date, 
-      region,
-      greenhouse_candidate_row_number,
-      bamboo_row_number,
-      valid_from_date,
-      hire_date,
-      CASE WHEN hire_date <='2018-12-01' 
-      --because we don't always have all job statuses we are using the candidate target hire date
-            THEN candidate_target_hire_date
-           WHEN candidate_target_hire_date > '2018-12-01' 
-                AND bamboo_row_number = 1 
-                AND greenhouse_candidate_row_number =1
+      applications.candidate_id, 
+      bamboo_hires.employee_id,
+      offers.start_date                                             AS candidate_target_hire_date, 
+      bamboo_hires.region,
+      applications.greenhouse_candidate_row_number,
+      bamboo_hires.bamboo_row_number,
+      bamboo_hires.valid_from_date,
+      bamboo_hires.hire_date,
+      CASE WHEN candidate_target_hire_date IS NULL 
+            THEN valid_from_date
+           WHEN candidate_target_hire_date IS NOT NULL 
+                AND applications.greenhouse_candidate_row_number =1 
+                AND bamboo_hires.bamboo_row_number = 1 
+                AND ABS(DATEDIFF(day,candidate_target_hire_date, bamboo_hires.hire_date))<120
             THEN hire_date
-           ELSE candidate_target_hire_date END                 AS bamboo_hire_date,
+        ELSE candidate_target_hire_date END                     AS hire_date_mod
       CASE WHEN bamboo_row_number=1 AND greenhouse_candidate_row_number = 1 
             THEN 'hire'
            WHEN is_rehire = TRUE 
             THEN 'rehire'
-           WHEN valid_from_date IS NULL AND hire_date IS NOT NULL 
-            THEN 'hire'
            WHEN greenhouse_candidate_row_number>1 
-                AND hire_date IS NOT NULL 
-            THEN 'transfer'
-           WHEN greenhouse_candidate_row_number = 1 
-                AND candidate_target_hire_date > bamboo_hire_date 
             THEN 'transfer'
            ELSE NULL END                                       AS hire_type
     FROM applications
