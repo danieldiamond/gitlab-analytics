8b768df043deae9166d50685e4ac9f71772fe812
Michael Walker <mwalker@gitlab.com>
2020-04-27 16:50:38 -0600

    fix some bugs with audit_event_details new models

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details_pii.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_audit_event_details_pii.sql
@@ -6,14 +6,14 @@
 WITH base AS (
 
     SELECT * 
-    FROM {{ ref('gitlab_dotcom', 'audit_event_details') }}
+    FROM {{ ref('gitlab_dotcom_audit_event_details') }}
 
 ), audit_event_pii AS (
 
     SELECT
       audit_event_id,
       key_name,
-      {{ nohash_sensitive_columns('gitlab_dotcom','audit_event_details') }},
+      {{ nohash_sensitive_columns('gitlab_dotcom_audit_event_details', 'key_value') }},
       updated_at
     FROM base
     WHERE key_name = 'target_details'
--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_audit_event_details_clean.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_audit_event_details_clean.sql
@@ -5,7 +5,7 @@ WITH non_pii_details AS (
       key_name,
       key_value,
       updated_at
-    FROM {{ ref('gitlab_dotcom', 'audit_event_details') }}
+    FROM {{ ref('gitlab_dotcom_audit_event_details') }}
     WHERE key_name != 'target_details'
 
 ), pii_details AS (
@@ -15,7 +15,7 @@ WITH non_pii_details AS (
       key_name,
       key_value_hash AS key_value,
       updated_at
-    FROM {{ ref('gitlab_dotcom', 'audit_event_details_pii') }}
+    FROM {{ ref('gitlab_dotcom_audit_event_details_pii') }}
 
 ), unioned AS (
 
