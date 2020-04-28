bc23109bce3e1938167fb44b5422bba488676b6c
Parul Luthra <pluthra@gitlab.com>
2020-04-28 16:07:03 -0400

    updating columns to add in buddy experience score, added in hire month based on join to name, and changing the columns to be null instead of 0

--- transform/snowflake-dbt/models/sheetload/base/sheetload_osat.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_osat.sql
@@ -7,13 +7,32 @@ WITH source AS (
 
     SELECT
         TRY_TO_TIMESTAMP_NTZ(timestamp)::DATE                 AS completed_date,
-        'Anonymous'                                           AS employee_name, 
-        ZEROIFNULL(NULLIF("SATISFACTION_SCORE",''))::INTEGER  AS satisfaction_score,
-        ZEROIFNULL(NULLIF("RECOMMEND_TO_FRIEND",''))::INTEGER AS recommend_to_friend
+        employee_name::VARCHAR                                AS employee_name,
+        division::VARCHAR                                     AS division,
+        NULLIF("SATISFACTION_SCORE",'')::INTEGER              AS satisfaction_score,
+        NULLIF("RECOMMEND_TO_FRIEND",'')::INTEGER             AS recommend_to_friend,
+        NULLIF(ONBOARDING_BUDDY_EXPERIENCE_SCORE,'')::INTEGER AS buddy_experience_score
     FROM source
-      
-) 
+ 
+), bamboohr AS (
 
+    SELECT *
+    FROM {{ ref ('bamboohr_id_employee_number_mapping') }}
+  
+), final AS (
+
+    SELECT
+      completed_date,
+      DATE_TRUNC(month, bamboohr.hire_date) AS hire_month,
+      division
+      satisfaction_score,
+      recommend_to_friend,
+      buddy_experience_score
+    FROM renamed
+    LEFT JOIN bamboohr
+      ON renamed.employee_name = CONCAT(first_name,' ',last_name)
+    WHERE completed_date IS NOT NULL
+
+)
 SELECT *
-FROM renamed
-WHERE completed_date is not NULL
+FROM final
