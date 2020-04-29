7e95b65140608f68009e598981e140acd0a0a937
msendal <msendal@gitlab.com>
2020-04-29 15:05:42 +0200

    add service_type logic, add, rename dates to date_ids

--- transform/snowflake-dbt/models/staging/common/fct_charges.sql
+++ transform/snowflake-dbt/models/staging/common/fct_charges.sql
@@ -26,6 +26,10 @@ WITH zuora_rate_plan AS (
                                                         AS effective_start_date_id,
       TO_NUMBER(TO_CHAR(zuora_rate_plan_charge.effective_end_date, 'YYYYMMDD'), '99999999')
                                                         AS effective_end_date_id,
+      TO_NUMBER(TO_CHAR(zuora_rate_plan_charge.effective_start_month, 'YYYYMMDD'),'99999999')
+                                                        AS effective_start_month_id,
+      TO_NUMBER(TO_CHAR(zuora_rate_plan_charge.effective_end_month, 'YYYYMMDD'), '99999999')
+                                                        AS effective_end_month_id,
       zuora_rate_plan_charge.unit_of_measure,
       zuora_rate_plan_charge.quantity,
       zuora_rate_plan_charge.mrr,
@@ -33,6 +37,11 @@ WITH zuora_rate_plan AS (
       zuora_rate_plan.rate_plan_name                    AS rate_plan_name,
       {{ product_category('zuora_rate_plan.rate_plan_name') }},
       {{ delivery('product_category')}},
+      CASE
+        WHEN lower(rate_plan_name) like '%support%'
+          THEN 'Support Only'
+        ELSE 'Full Service'
+      END                                               AS service_type,
       zuora_rate_plan_charge.discount_level,
       zuora_rate_plan_charge.segment                    AS rate_plan_charge_segment,
       zuora_rate_plan_charge.version                    AS rate_plan_charge_version
@@ -43,29 +52,26 @@ WITH zuora_rate_plan AS (
 ), latest_invoiced_charge_version_in_segment AS (
 
     SELECT
-      rate_plan_charge_id
+      rate_plan_charge_id,
+      ROW_NUMBER() OVER (
+          PARTITION BY rate_plan_charge_number
+          ORDER BY rate_plan_charge_segment, rate_plan_charge_version, service_start_date
+      ) AS segment_version_order,
+      IFF(ROW_NUMBER() OVER (
+          PARTITION BY rate_plan_charge_number, rate_plan_charge_segment
+          ORDER BY rate_plan_charge_version DESC, service_start_date DESC) = 1,
+          TRUE, FALSE
+      ) AS is_last_segment_version
     FROM base_charges
     INNER JOIN zuora_invoice_item AS invoice_item_source
       ON base_charges.charge_id = invoice_item_source.rate_plan_charge_id
-    QUALIFY ROW_NUMBER() OVER (
-        PARTITION BY
-          rate_plan_charge_number,
-          rate_plan_charge_segment
-        ORDER BY
-          rate_plan_charge_version DESC,
-          service_start_date DESC)= 1
 
 ), final AS (
 
     SELECT
       base_charges.*,
-      ROW_NUMBER() OVER (
-        PARTITION BY rate_plan_charge_number
-        ORDER BY
-          rate_plan_charge_segment,
-          rate_plan_charge_version
-      )                                                                         AS segment_version_order,
-      latest_invoiced_charge_version_in_segment.rate_plan_charge_id IS NOT NULL AS is_last_version_segment
+      latest_invoiced_charge_version_in_segment.segment_version_order,
+      latest_invoiced_charge_version_in_segment.is_last_segment_version
     FROM base_charges
     LEFT JOIN latest_invoiced_charge_version_in_segment
       ON latest_invoiced_charge_version_in_segment.rate_plan_charge_id = base_charges.charge_id
