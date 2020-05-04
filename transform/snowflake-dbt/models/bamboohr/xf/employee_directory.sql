ca80461df285b7a0af830ed7b1b6192f892dbc90
Parul Luthra <pluthra@gitlab.com>
2020-05-04 12:35:56 -0400

    removing contract employee

--- transform/snowflake-dbt/models/bamboohr/xf/employee_directory.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/employee_directory.sql
@@ -49,36 +49,42 @@ WITH bamboohr_directory AS (
        valid_from_date as rehire_date
     FROM {{ref('bamboohr_employment_status_xf')}}
     WHERE is_rehire='True'
+
+), final AS (
+
+    SELECT DISTINCT
+      mapping.employee_id,
+      mapping.employee_number,
+      mapping.first_name,
+      mapping.last_name,
+      bamboohr_directory.work_email,
+      iff(rehire.is_rehire = 'True', initial_hire.hire_date, mapping.hire_date) AS hire_date,
+      rehire.rehire_date,
+      mapping.termination_date,
+      department_info.last_job_title,
+      department_info.last_supervisor,
+      department_info.last_department,
+      department_info.last_division,
+      cost_center.cost_center,
+      location_factor.hire_location_factor
+    FROM mapping
+    LEFT JOIN bamboohr_directory
+    ON bamboohr_directory.employee_id = mapping.employee_id
+    LEFT JOIN department_info
+    ON mapping.employee_id = department_info.employee_id
+    LEFT JOIN cost_center
+    ON trim(department_info.last_department)=trim(cost_center.department)
+    AND trim(department_info.last_division)=trim(cost_center.division)
+    LEFT JOIN location_factor
+    ON location_factor.bamboo_employee_number = mapping.employee_number
+    LEFT JOIN initial_hire 
+    ON initial_hire.employee_id = mapping.employee_id
+    LEFT JOIN rehire
+    ON rehire.employee_id = mapping.employee_id
+    WHERE mapping.hire_date < date_trunc('week', dateadd(week, 3, CURRENT_DATE))
+
+
 )
-
-SELECT distinct
-        mapping.employee_id,
-        mapping.employee_number,
-        mapping.first_name,
-        mapping.last_name,
-        bamboohr_directory.work_email,
-        iff(rehire.is_rehire = 'True', initial_hire.hire_date, mapping.hire_date) AS hire_date,
-        rehire.rehire_date,
-        mapping.termination_date,
-        department_info.last_job_title,
-        department_info.last_supervisor,
-        department_info.last_department,
-        department_info.last_division,
-        cost_center.cost_center,
-        location_factor.hire_location_factor
-FROM mapping
-LEFT JOIN bamboohr_directory
-  ON bamboohr_directory.employee_id = mapping.employee_id
-LEFT JOIN department_info
-  ON mapping.employee_id = department_info.employee_id
-LEFT JOIN cost_center
-  ON trim(department_info.last_department)=trim(cost_center.department)
- AND trim(department_info.last_division)=trim(cost_center.division)
-LEFT JOIN location_factor
-  ON location_factor.bamboo_employee_number = mapping.employee_number
-LEFT JOIN initial_hire 
-  ON initial_hire.employee_id = mapping.employee_id
-LEFT JOIN rehire
-  ON rehire.employee_id = mapping.employee_id
-WHERE mapping.hire_date < date_trunc('week', dateadd(week, 3, CURRENT_DATE))
-
+SELECT * 
+FROM final
+WHERE employee_id != 42071
