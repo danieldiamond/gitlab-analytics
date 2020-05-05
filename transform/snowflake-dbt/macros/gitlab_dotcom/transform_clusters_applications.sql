a467bd99148437b25dd881197355d4b32354916d
Eli Kastelein <ekastelein@gitlab.com>
2020-05-05 16:39:53 -0700

    switch existing to macro

--- transform/snowflake-dbt/macros/gitlab_dotcom/transform_clusters_applications.sql
+++ transform/snowflake-dbt/macros/gitlab_dotcom/transform_clusters_applications.sql
@@ -1,5 +1,51 @@
-{%- macro transform_clusters_applications(model_name) -%}
+{%- macro transform_clusters_applications(base_model) -%}
 
+  WITH base AS (
 
+      SELECT *
+      FROM {{ ref('base_model')}}
+
+  ),
+
+  clusters AS (
+
+      SELECT *
+      FROM {{ ref('gitlab_dotcom_clusters') }}
+
+  ),
+
+  cluster_groups AS (
+
+      SELECT *
+      FROM {{ ref('gitlab_dotcom_cluster_groups')}}
+
+  ),
+
+  cluster_projects AS (
+
+      SELECT *
+      FROM {{ ref('gitlab_dotcom_cluster_projects')}}
+
+  ),
+
+  final AS (
+
+      SELECT
+        base.*,
+        clusters.user_id,
+        cluster_groups.cluster_group_id,
+        cluster_projects.cluster_project_id
+      FROM base
+        INNER JOIN clusters
+          ON base.cluster_id = clusters.cluster_id
+        LEFT JOIN cluster_groups
+          ON clusters.cluster_id = cluster_groups.cluster_id
+        LEFT JOIN cluster_projects
+          ON clusters.cluster_id = cluster_projects.cluster_id
+
+  )
+
+  SELECT *
+  FROM final
 
 {%- endmacro -%}
