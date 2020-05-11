c01fa69e05d07a00f4b82a3e6712b6deaeb0d69a
Jean Del Rosario <jpeguero@gitlab.com>
2020-05-11 12:12:57 -0400

    Add session hit to analytics schema

---
+++ transform/snowflake-dbt/models/sources/google_analytics_360/ga360_session_hit.sql
@@ -0,0 +1,31 @@
+{{ config(schema='analytics') }}
+
+WITH source AS (
+
+	SELECT *
+	FROM {{ source('google_analytics_360', 'session_hit') }}
+
+), renamed AS(
+
+	SELECT
+	  --Keys
+	  visit_id::FLOAT                 AS visit_id, 
+	  visitor_id::VARCHAR             AS visitor_id, 
+	    
+	  --Info
+	  visit_start_time::TIMESTAMP_TZ  AS visit_start_time,
+      hit_number::INTEGER             AS hit_number,
+	  is_entrance::BOOLEAN            AS is_entrance,
+	  is_exit::BOOLEAN                AS is_exit,
+	  referer::VARCHAR                AS referer,
+	  data_source::VARCHAR            AS data_source,
+	  page_hostname::VARCHAR          AS hostname,
+	  page_page_path::VARCHAR         AS page_path,
+	  page_page_title::VARCHAR        AS page_title
+
+	FROM source
+
+)
+
+SELECT *
+FROM renamed
