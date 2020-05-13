9c3c683eca4fcc0be1438c56849e1d14a8fb6044
Eli Kastelein <ekastelein@gitlab.com>
2020-05-13 11:21:10 -0700

    undo last commit

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_resource_milestone_events.sql
@@ -0,0 +1,36 @@
+{# {{ config({
+    "materialized": "incremental",
+    "unique_key": "resource_label_event_id"
+    })
+}} #}
+
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('gitlab_dotcom', 'resource_milestone_events') }}
+    {% if is_incremental() %}
+      WHERE created_at >= (SELECT MAX(created_at) FROM {{this}})
+    {% endif %}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
+
+)
+
+, renamed AS (
+
+    SELECT
+      id                                             AS resource_milestone_event_id,
+      {# action::INTEGER                                AS action_type_id,
+      {{ resource_event_action_type('action') }}     AS action_type, #}
+      user_id::INTEGER                               AS user_id,
+      issue_id::INTEGER                              AS issue_id,
+      merge_request_id::INTEGER                      AS merge_request_id,
+      milestone_id::INTEGER                          AS milestone_id,
+      state 
+
+      created_at::TIMESTAMP                          AS created_at
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
