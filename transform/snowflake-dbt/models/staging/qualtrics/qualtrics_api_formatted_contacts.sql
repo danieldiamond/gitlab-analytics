d0ef640820c1fa2951527bf216d91a3dc0aa501f
Michael Walker <mwalker@gitlab.com>
2020-05-13 17:03:01 -0500

    fix up last name and language for qualtrics mailing contacts

--- transform/snowflake-dbt/models/staging/qualtrics/qualtrics_api_formatted_contacts.sql
+++ transform/snowflake-dbt/models/staging/qualtrics/qualtrics_api_formatted_contacts.sql
@@ -1,17 +1,22 @@
+{{ config({
+        "schema": "sensitive"
+    })
+}}
+
 WITH users AS (
 
     SELECT *
-    FROM {{ ref('gitlab_dotcom', 'gitlab_dotcom_users') }}
+    FROM {{ ref('gitlab_dotcom_users') }}
 
 ), renamed AS (
 
     SELECT
       user_id                                                AS user_id,
       IFNULL(first_name, SPLIT_PART(users_name, ' ', 1))     AS first_name,
-      IFNULL(last_name,  LTRIM(users_name, SPLIT_PART(users_name, ' ', 1) || ' '))     AS last_name,
+      LTRIM(IFNULL(last_name,  LTRIM(users_name, SPLIT_PART(users_name, ' ', 1))))     AS last_name,
       IFNULL(notification_email, public_email)               AS email_address,
       NULL                                                   AS phone_number,
-      IFNULL(NULLIF(preferred_language, 'nan'), 'EN')        AS language
+      UPPER(IFNULL(NULLIF(preferred_language, 'nan'), 'en')) AS language
     FROM users
 
 )
