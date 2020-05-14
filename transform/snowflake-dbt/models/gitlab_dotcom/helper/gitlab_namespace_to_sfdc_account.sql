d714866c359844f36d4fccd420e09d4a436b616d
mpeychet <mpeychet@gitlab.com>
2020-05-14 15:37:20 +0200

    first working version

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/helper/gitlab_namespace_to_sfdc_account.sql
@@ -0,0 +1,62 @@
+WITH zuora_base_mrr AS (
+
+    SELEECT * 
+    FROM  {{ ref('zuora_base_mrr') }} 
+
+)
+
+, zuora_account AS (
+
+    SELECT * 
+    FROM {{ ref('zuora_account')}}
+)
+
+, charges AS (
+
+    SELECT * 
+    FROM {{ ref('customers_db_charges_xf')}}
+)
+
+, namespaces AS (
+
+    SELECT * 
+    FROM {{ ref('gitlab_dotcom_namespaces_xf')}}
+)
+
+, plans AS (
+
+    SELECT * 
+    FROM {{ ref('gitlab_dotcom_plans')}}
+)
+
+, data AS (
+
+  SELECT 
+    zuora_base_mrr.account_name,
+    zuora_base_mrr.account_number,
+    zuora_base_mrr.subscription_name_slugify,
+    zuora_account.crm_id,
+    current_customer_id,
+    current_gitlab_namespace_id,
+    zuora_base_mrr.product_category,
+    namespace_id,
+    np.plan_id,
+    plans.plan_name,
+    lower(zuora_base_mrr.product_category) = plans.plan_name AS valid
+  FROM zuora_base_mrr
+  LEFT JOIN zuora_account
+    ON zuora_base_mrr.account_number  = zuora_account.account_number 
+  LEFT JOIN charges
+    ON zuora_base_mrr.subscription_name_slugify = charges.subscription_name_slugify
+      AND zuora_base_mrr.rate_plan_charge_id = charges.rate_plan_charge_id
+  LEFT JOIN namespaces 
+    ON charges.current_gitlab_namespace_id = np.namespace_id
+  LEFT JOIN plans
+    ON namespaces.plan_id = plans.plan_id::VARCHAR
+  WHERE TRUE
+    AND zuora_base_mrr.delivery = 'SaaS'
+  
+)
+
+SELECT *
+FROM data
