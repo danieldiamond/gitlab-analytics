f7dbc1b986f9dd602fff19211164280edc58491e
Emilie Schario <eburke@gitlab.com>
2020-05-15 17:59:48 +0000

    formatting again

--- transform/snowflake-dbt/models/sheetload/base/sheetload_company_call_attendance.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_company_call_attendance.sql
@@ -8,7 +8,7 @@ WITH source AS (
 	    SELECT
 	        "Date"::DATE     					      AS call_date,
 	        "Topic"::VARCHAR           				  AS meeting_topic,
-			DAYNAME(call_date)    					  AS day_of_the_week,
+            DAYNAME(call_date)    					  AS day_of_the_week,
 	        "Start_Time"           					  AS call_time,
 	        "Participants"        					  AS count_of_participants
 
