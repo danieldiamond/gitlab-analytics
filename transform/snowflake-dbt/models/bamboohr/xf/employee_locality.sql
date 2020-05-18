1b4626f68b066bbbf2f2dd1e88ca9ad48451f6d2
Parul Luthra <pluthra@gitlab.com>
2020-05-18 13:27:25 -0400

    removing changes

--- transform/snowflake-dbt/models/bamboohr/xf/employee_locality.sql
+++ transform/snowflake-dbt/models/bamboohr/xf/employee_locality.sql
@@ -14,12 +14,6 @@ WITH locality AS (
     FROM {{ ref('location_factors_yaml_historical') }}
     WHERE LOWER(area) LIKE '%everywhere else%'
 
-), location_factor_mexico AS (
-
-    SELECT *
-    FROM {{ ref('location_factors_yaml_historical') }}
-    WHERE country ='Mexico'
-      AND area IN ('Everywhere else','All')
 )
 
 SELECT 
@@ -28,8 +22,7 @@ SELECT
   locality.updated_at,
   locality.locality                                              AS bamboo_locality,
   COALESCE(location_factor_yaml.location_factor, 
-           location_factor_yaml_everywhere_else.location_factor,
-           location_factor_mexico.location_factor)              AS location_factor
+           location_factor_yaml_everywhere_else.location_factor) AS location_factor
 FROM locality
 LEFT JOIN location_factor_yaml
   ON LOWER(locality.locality) = LOWER(CONCAT(location_factor_yaml.area,', ', location_factor_yaml.country))
@@ -37,7 +30,3 @@ LEFT JOIN location_factor_yaml
 LEFT JOIN location_factor_yaml_everywhere_else
   ON LOWER(locality.locality) = LOWER(CONCAT(location_factor_yaml_everywhere_else.area,', ', location_factor_yaml_everywhere_else.country))
   AND locality.updated_at = location_factor_yaml_everywhere_else.snapshot_date
-LEFT JOIN location_factor_mexico
-  ON IFF(locality.locality LIKE '%Mexico%', 'Mexico',NULL) = location_factor_mexico.country
-  AND locality.updated_at = location_factor_mexico.snapshot_date
---The naming convention for Mexico is all in bamboohr and everywhere else in the location factor yaml file accounting for these cases by making this the last coalesce
