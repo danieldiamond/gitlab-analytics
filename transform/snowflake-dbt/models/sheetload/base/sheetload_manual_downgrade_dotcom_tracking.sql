6e40fe02ce7c78b0561810c2d2af059ad9f6ac0e
derekatwood <datwood@gitlab.com>
2020-05-18 09:21:31 -0500

    change type

--- transform/snowflake-dbt/models/sheetload/base/sheetload_manual_downgrade_dotcom_tracking.sql
+++ transform/snowflake-dbt/models/sheetload/base/sheetload_manual_downgrade_dotcom_tracking.sql
@@ -7,7 +7,7 @@ WITH source AS (
 
     SELECT
       namespace_id::INTEGER AS namespace_id,
-      downgraded_date::DATE AS downgraded_date
+      downgraded_date::VARCHAR AS downgraded_date
     FROM source  
 
 )
