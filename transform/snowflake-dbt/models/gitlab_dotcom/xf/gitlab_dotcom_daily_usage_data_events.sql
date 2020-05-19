8fac5392bd1dc9d3840d6748e7b433bdc196b07c
mpeychet <mpeychet@gitlab.com>
2020-05-19 18:03:38 +0200

    a test

--- transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events.sql
@@ -9,7 +9,7 @@ WITH usage_data AS (
 
     SELECT
       {{ dbt_utils.star(from=ref('gitlab_dotcom_usage_data_events'), except=["event_created_at", "namespace_id", "namespace_created_at"]) }},
-      TO_DATE(event_created_at),
+      TO_DATE(event_created_at) AS event_date,
       COUNT(*)
     FROM usage_data
     {{ dbt_utils.group_by(n=20) }}
---
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events_90.sql
@@ -0,0 +1,1 @@
+{{ schema_union_limit('analytics', 'gitlab_dotcom_usage_data_events', 'event_date', 90) }}
