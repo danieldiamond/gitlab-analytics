3e861f0aa7be8f59f61057207f072e5eb0f2be77
mpeychet <mpeychet@gitlab.com>
2020-05-19 16:30:43 +0200

    first working model

---
+++ transform/snowflake-dbt/models/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events.sql
@@ -0,0 +1,20 @@
+WITH usage_data AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_usage_data_events') }}
+
+)
+
+, aggregated AS (
+
+    SELECT
+      {{ dbt_utils.star(from=ref('gitlab_dotcom_usage_data_events'), except=["event_created_at", "namespace_id", "namespace_created_at"]) }},
+      TO_DATE(event_created_at),
+      COUNT(*)
+    FROM usage_data
+    {{ dbt_utils.group_by(n=20) }}
+
+)
+
+SELECT *
+FROM aggregated
