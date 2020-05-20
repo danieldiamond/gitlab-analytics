f1e63b0239020d8de3473c6e96b6d47d5aa2dcdb
Eli Kastelein <ekastelein@gitlab.com>
2020-05-20 12:11:20 -0700

    Initial commit: Copy from source

---
+++ transform/snowflake-dbt/macros/utils/snapshot_timestamp_with_deletes_strategy.sql
@@ -0,0 +1,32 @@
+{% macro snapshot_timestamp_with_deletes_strategy(node, snapshotted_rel, current_rel, config, target_exists) %}
+    {% set primary_key = config['unique_key'] %}
+    {% set updated_at = config['updated_at'] %}
+
+    {% set row_changed_expr -%}
+        ({{ snapshotted_rel }}.{{ updated_at }} < {{ current_rel }}.{{ updated_at }}
+            -- this indicates that the source record was hard-deleted
+            or {{ current_rel }}.{{ primary_key }} is null
+        )
+    {%- endset %}
+
+    {% set scd_id_expr = snapshot_hash_arguments([primary_key, updated_at]) %}
+
+    {% do return({
+        "unique_key": primary_key,
+        "updated_at": updated_at,
+        "row_changed": row_changed_expr,
+        "scd_id": scd_id_expr
+    }) %}
+{% endmacro %}
+
+
+{% macro snapshot_string_as_time(timestamp) -%}
+    {{ adapter_macro('snapshot_string_as_time', timestamp) }}
+{%- endmacro %}
+
+
+{% macro default__snapshot_string_as_time(timestamp) %}
+    {% do exceptions.raise_not_implemented(
+        'snapshot_string_as_time macro not implemented for adapter '+adapter.type()
+    ) %}
+{% endmacro %}
