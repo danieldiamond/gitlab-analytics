ad552a63f8ff5faae4165161be6bce919d2b4b83
Eli Kastelein <ekastelein@gitlab.com>
2020-05-21 15:33:12 -0700

    reset customers

--- transform/snowflake-dbt/snapshots/customers/customers_db_customers_snapshots.sql
+++ transform/snowflake-dbt/snapshots/customers/customers_db_customers_snapshots.sql
@@ -8,12 +8,9 @@
         )
     }}
 
-
-    SELECT *, 0.1 AS eli_temp_col --TODO
+    SELECT *
     FROM {{ source('customers', 'customers_db_customers') }}
-    WHERE _task_instance IN (SELECT MAX(_task_instance) FROM {{ source('customers', 'customers_db_customers') }}) --TODO: macro?
-      AND id NOT IN (9983, 177735) --temp to test hard deletes
+    WHERE _task_instance IN (SELECT MAX(_task_instance) FROM {{ source('customers', 'customers_db_customers') }})
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-    
 {% endsnapshot %}
