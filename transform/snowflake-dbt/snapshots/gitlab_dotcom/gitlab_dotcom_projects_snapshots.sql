e04de8201c68af2f0e8133014523d319a75f400e
Eli Kastelein <ekastelein@gitlab.com>
2020-05-22 15:10:29 -0700

    revert

--- transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_projects_snapshots.sql
+++ transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_projects_snapshots.sql
@@ -3,14 +3,23 @@
     {{
         config(
           unique_key='id',
-          strategy='timestamp_with_deletes',
+          strategy='timestamp',
           updated_at='updated_at',
         )
     }}
+    
+    WITH source as (
 
+    	SELECT 
+        *, 
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) as projects_rank_in_key
+      
+      FROM {{ source('gitlab_dotcom', 'projects') }}
+
+    )
+    
     SELECT *
-    FROM {{ source('gitlab_dotcom', 'projects') }}
-    WHERE _task_instance IN (SELECT MAX(_task_instance) FROM  {{ source('gitlab_dotcom', 'projects') }})
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
-
+    FROM source
+    WHERE projects_rank_in_key = 1
+        
 {% endsnapshot %}
