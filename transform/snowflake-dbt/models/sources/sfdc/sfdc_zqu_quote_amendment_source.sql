256e7496a3483ba771f6902e0f54cfc563937ad5
msendal <msendal@gitlab.com>
2020-05-27 17:26:58 +0200

    reformat code

--- transform/snowflake-dbt/models/sources/sfdc/sfdc_zqu_quote_amendment_source.sql
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_zqu_quote_amendment_source.sql
@@ -6,15 +6,12 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
-      id::VARCHAR AS zqu_quote_amendment_id,
+      id::VARCHAR                             AS zqu_quote_amendment_id,
 
       charge_summary_sub_total__c::FLOAT      AS charge_summary_sub_total,
-      createdbyid::VARCHAR                    AS created_by_id,
-      createddate::TIMESTAMP_TZ               AS created_date,
-      isdeleted::BOOLEAN                      AS is_deleted,
-      lastmodifiedbyid::VARCHAR               AS last_modified_by_id,
       license_amount__c::FLOAT                AS license_amount,
       NAME::VARCHAR                           AS zqu_quote_amendment_name,
+      professional_services_amount__c::FLOAT  AS professional_services_amount,
       true_up_amount__c::FLOAT                AS true_up_amount,
       zqu__autorenew__c::VARCHAR              AS zqu__auto_renew,
       zqu__cancellationpolicy__c::VARCHAR     AS zqu__cancellation_policy,
@@ -27,18 +24,24 @@ WITH source AS (
       zqu__renewalterm__c::FLOAT              AS zqu__renewal_term,
       zqu__status__c::VARCHAR                 AS zqu__status,
       zqu__totalamount__c::FLOAT              AS zqu__total_amount,
-      _sdc_extracted_at::TIMESTAMP_TZ         AS sdc_extracted_at,
-      _sdc_table_version::NUMBER              AS sdc_table_version,
       zqu__quote__c::VARCHAR                  AS zqu__quote,
       zqu__type__c::VARCHAR                   AS zqu__type,
-      _sdc_received_at::TIMESTAMP_TZ          AS sdc_received_at,
-      lastmodifieddate::TIMESTAMP_TZ          AS last_modified_date,
-      professional_services_amount__c::FLOAT  AS professional_services_amount,
-      systemmodstamp::TIMESTAMP_TZ            AS system_mod_stamp,
       zqu__description__c::VARCHAR            AS zqu__description,
       zqu__termstartdate__c::TIMESTAMP_TZ     AS zqu__term_start_date,
+
+
+      --metadata
+      createdbyid::VARCHAR                    AS created_by_id,
+      createddate::TIMESTAMP_TZ               AS created_date,
+      isdeleted::BOOLEAN                      AS is_deleted,
+      lastmodifiedbyid::VARCHAR               AS last_modified_by_id,
+      lastmodifieddate::TIMESTAMP_TZ          AS last_modified_date,
+      _sdc_received_at::TIMESTAMP_TZ          AS sdc_received_at,
+      _sdc_extracted_at::TIMESTAMP_TZ         AS sdc_extracted_at,
+      _sdc_table_version::NUMBER              AS sdc_table_version,
       _sdc_batched_at::TIMESTAMP_TZ           AS sdc_batched_at,
-      _sdc_sequence::NUMBER                   AS sdc_sequence
+      _sdc_sequence::NUMBER                   AS sdc_sequence,
+      systemmodstamp::TIMESTAMP_TZ            AS system_mod_stamp
 
 )
 
