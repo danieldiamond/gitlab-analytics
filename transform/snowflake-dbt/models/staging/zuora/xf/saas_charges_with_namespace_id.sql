75213f3a7f53928f1883ff0c15f28c8566486693
mpeychet <mpeychet@gitlab.com>
2020-05-28 18:41:04 +0200

    worked and tested

--- transform/snowflake-dbt/models/staging/zuora/xf/saas_charges_with_namespace_id.sql
+++ transform/snowflake-dbt/models/staging/zuora/xf/saas_charges_with_namespace_id.sql
@@ -1,12 +1,4 @@
-WITH saas_zuora_charges AS (
-  
-    SELECT * 
-    FROM {{ ref('fct_charges') }}
-    WHERE delivery = 'SaaS'
-  
-)
-
-, zuora_base_mrr AS (
+WITH zuora_base_mrr AS (
   
     SELECT * 
     FROM {{ ref('zuora_base_mrr') }}
@@ -52,7 +44,7 @@ WITH saas_zuora_charges AS (
 , joined AS (
 
     SELECT 
-      saas_zuora_charges.charge_id,
+      zuora_base_mrr.charge_id,
       dim_subscriptions.subscription_name_slugify,
       dim_accounts.account_id                                AS zuora_account_id,
       COALESCE(merged_accounts.crm_id, dim_customers.crm_id) AS sfdc_account_id,
@@ -62,17 +54,13 @@ WITH saas_zuora_charges AS (
                 dim_customers.ultimate_parent_account_name)  AS ultimate_parent_account_name,
       customers_db_charges.current_customer_id,
       namespaces.namespace_id
-    FROM saas_zuora_charges
-    INNER JOIN zuora_base_mrr
-      ON saas_zuora_charges.charge_id = zuora_base_mrr.rate_plan_charge_id
+    FROM zuora_base_mrr
     LEFT JOIN customers_db_charges 
-      ON saas_zuora_charges.charge_id = customers_db_charges.rate_plan_charge_id
+      ON saas_zuora_charges.rate_plan_charge_id = customers_db_charges.rate_plan_charge_id
     LEFT JOIN namespaces
       ON customers_db_charges.current_gitlab_namespace_id = namespaces.namespace_id
-    LEFT JOIN dim_subscriptions
-      ON saas_zuora_charges.subscription_id = dim_subscriptions.subscription_id
     LEFT JOIN dim_accounts
-      ON saas_zuora_charges.account_id = dim_accounts.account_id
+      ON zuora_base_mrr.account_number = dim_accounts.account_number
     LEFT JOIN dim_customers
       ON dim_accounts.crm_id = dim_customers.crm_id
     LEFT JOIN dim_customers AS merged_accounts
