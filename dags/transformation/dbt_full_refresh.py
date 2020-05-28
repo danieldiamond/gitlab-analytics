0f98c50f680f7c31c8b9b1d1eb3fda4fcf6ea171
msendal <msendal@gitlab.com>
2020-05-28 13:44:20 +0200

    fix descriprion

--- dags/transformation/dbt_full_refresh.py
+++ dags/transformation/dbt_full_refresh.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import datetime
 
 from airflow import DAG
@@ -36,12 +37,19 @@ default_args = {
 }
 
 # Create the DAG
-dag = DAG("dbt_full_refresh", default_args=default_args, schedule_interval=None)
+dag = DAG(
+    "dbt_full_refresh",
+    default_args=default_args,
+    schedule_interval=None,
+    description="Before running this DAG set dbt model for full refresh in Airflow Variable named DBT_MODEL_TO_FULL_REFRESH",
+)
 
 # read model for full-refresh from Airflow Variable
 dbt_model_to_full_refresh = Variable.get(
     "DBT_MODEL_TO_FULL_REFRESH", default_var="test"
 )
+
+logging.info(dbt_model_to_full_refresh)
 
 # dbt-full-refresh
 dbt_full_refresh_cmd = f"""
@@ -54,7 +62,6 @@ dbt_full_refresh = KubernetesPodOperator(
     image=DBT_IMAGE,
     task_id="dbt-full-refresh",
     name="dbt-full-refresh",
-    description="Before running this DAG set dbt model for full refresh in Airflow Variable named DBT_MODEL_TO_FULL_REFRESH",
     secrets=[
         SNOWFLAKE_ACCOUNT,
         SNOWFLAKE_USER,
