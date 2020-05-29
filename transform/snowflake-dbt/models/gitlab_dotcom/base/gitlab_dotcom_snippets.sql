91fc1ccf44abc437b9ebf7543f1a5fad1a4de9b7
mpeychet <mpeychet@gitlab.com>
2020-05-29 17:06:43 +0200

    seensitive

--- transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_snippets.sql
+++ transform/snowflake-dbt/models/gitlab_dotcom/base/gitlab_dotcom_snippets.sql
@@ -1,7 +1,19 @@
+{{ config({
+    "materialized": "incremental",
+    "unique_key": "snippet_id",
+    "schema": "sensitive"
+    })
+}}
+
 WITH source AS (
 
     SELECT *
     FROM {{ source('gitlab_dotcom', 'snippets') }}
+    {% if is_incremental() %}
+
+    WHERE updated_at >= (SELECT MAX(updated_at) FROM {{this}})
+
+    {% endif %}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
 
 ), renamed AS (
@@ -13,6 +25,7 @@ WITH source AS (
       created_at::TIMESTAMP     AS created_at,
       updated_at::TIMESTAMP     AS updated_at,
       type::VARCHAR             AS snippet_type,
+      file_name::VARCHAR        AS file_name,
       visibility_level::INTEGER AS visibility_level
       
     FROM source
