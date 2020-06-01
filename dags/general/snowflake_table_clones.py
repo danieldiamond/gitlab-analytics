c5ede0c5ecf77d2fa071aed09e56bb3003e4d329
msendal <msendal@gitlab.com>
2020-06-01 15:06:19 +0200

    fix timestamp of clone

--- dags/general/snowflake_table_clones.py
+++ dags/general/snowflake_table_clones.py
@@ -29,18 +29,18 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
     "start_date": datetime(2020, 6, 1),
 }
+# Clone should be taken at 5PM PST which is 0pm UTC (default Snowflake timezone)
+clone_timestamp = datetime.now().date() - timedelta(days=1)
 
-arr_data_mart_timestamp = (
-    datetime.now().date() - timedelta(days=1) + timedelta(hours=17)
-)
-target_table = "arr_data_mart_{0}".format(arr_data_mart_timestamp.strftime("%Y%m%d"))
-arr_data_mart_timestamp = arr_data_mart_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+target_table = "arr_data_mart_{0}".format(clone_timestamp.strftime("%Y%m%d"))
+clone_timestamp = clone_timestamp.strftime("%Y-%m-%d %H:%M:%S")
 timestamp_format = "yyyy-mm-dd hh24:mi:ss"
 
 # Set the command for the container
 clone_cmd = f"""
     {clone_repo_cmd} &&
-    python analytics/orchestration/manage_snowflake.py create_table_clone --database ANALYTICS --source_schema ANALYTICS --source_table arr_data_mart --target_schema analytics_clones target_table {target_table} timestamp_format {timestamp_format}" 
+    cd analytics/orchestration/ &&
+    python manage_snowflake.py create_table_clone --database ANALYTICS --source_schema ANALYTICS --source_table ARR_DATA_MART --target_schema ANALYTICS_CLONES target_table {target_table} timestamp_format {timestamp_format}" 
 """
 
 # Create the DAG
