02d881a7cda96985ef449862cd17371ba96fd084
parmstrong <parmstrong@gitlab.com>
2020-06-02 19:26:02 +0200

    Added in concurrency in the correct place, had to move a few things around for that, and corrected a typo.

--- dags/extract/gitlab_dbs.py
+++ dags/extract/gitlab_dbs.py
@@ -61,7 +61,7 @@ standard_secrets = [
 ]
 
 validation_schedule_interval = "0 1 * * 0"
-every_eigth_hour = "0 */8 * * *"
+every_eighth_hour = "0 */8 * * *"
 every_day_at_four = "0 4 */1 * *"
 
 # Dictionary containing the configuration values for the various Postgres DBs
@@ -84,7 +84,7 @@ config_dict = {
     "customers": {
         "dag_name": "customers",
         "env_vars": {"DAYS": "1"},
-        "extract_schedule_interval": every_eigth_hour,
+        "extract_schedule_interval": every_eighth_hour,
         "secrets": [
             CUSTOMERS_DB_USER,
             CUSTOMERS_DB_PASS,
@@ -129,7 +129,7 @@ config_dict = {
     "license": {
         "dag_name": "license",
         "env_vars": {"DAYS": "1"},
-        "extract_schedule_interval": every_eigth_hour,
+        "extract_schedule_interval": every_eighth_hour,
         "secrets": [LICENSE_DB_USER, LICENSE_DB_PASS, LICENSE_DB_HOST, LICENSE_DB_NAME],
         "start_date": datetime(2019, 5, 30),
         "sync_schedule_interval": every_day_at_four,
@@ -139,7 +139,7 @@ config_dict = {
     "version": {
         "dag_name": "version",
         "env_vars": {"DAYS": "1", "AVG_CYCLE_ANALYTICS_ID": "1"},
-        "extract_schedule_interval": every_eigth_hour,
+        "extract_schedule_interval": every_eighth_hour,
         "secrets": [VERSION_DB_USER, VERSION_DB_PASS, VERSION_DB_HOST, VERSION_DB_NAME],
         "start_date": datetime(2019, 5, 30),
         "sync_schedule_interval": every_day_at_four,
@@ -238,33 +238,34 @@ for source_name, config in config_dict.items():
         "retry_delay": timedelta(minutes=3),
         "start_date": config["start_date"],
     }
-    sync_dag = DAG(
-        f"{config['dag_name']}_db_sync",
-        default_args=sync_dag_args,
-        schedule_interval=config["sync_schedule_interval"],
-    )
 
-    with sync_dag:
-
-        scd_affinity = {
-            "nodeAffinity": {
-                "requiredDuringSchedulingIgnoredDuringExecution": {
-                    "nodeSelectorTerms": [
-                        {
-                            "matchExpressions": [
-                                {"key": "pgp", "operator": "In", "values": ["scd"]}
-                            ]
-                        }
-                    ]
-                }
+    scd_affinity = {
+        "nodeAffinity": {
+            "requiredDuringSchedulingIgnoredDuringExecution": {
+                "nodeSelectorTerms": [
+                    {
+                        "matchExpressions": [
+                            {"key": "pgp", "operator": "In", "values": ["scd"]}
+                        ]
+                    }
+                ]
             }
         }
+    }
 
-        scd_tolerations = [
-            {"key": "scd", "operator": "Equal", "value": "true", "effect": "NoSchedule"}
-        ]
+    scd_tolerations = [
+        {"key": "scd", "operator": "Equal", "value": "true", "effect": "NoSchedule"}
+    ]
 
-        if config["dag_name"] == "gitlab_com":
+    if config["dag_name"] == "gitlab_com":
+
+        sync_dag = DAG(
+            f"{config['dag_name']}_db_sync",
+            default_args=sync_dag_args,
+            schedule_interval=config["sync_schedule_interval"],
+            concurrency=1,
+        )
+        with sync_dag:
             file_path = f"analytics/extract/postgres_pipeline/manifests/{config['dag_name']}_db_manifest.yaml"
             table_list = extract_table_list_from_manifest(file_path)
             for table in table_list:
@@ -282,7 +283,6 @@ for source_name, config in config_dict.items():
                     arguments=[sync_cmd],
                     do_xcom_push=True,
                     xcom_push=True,
-                    task_concurrency=1,
                 )
 
                 # SCD Task
@@ -303,11 +303,16 @@ for source_name, config in config_dict.items():
                     tolerations=scd_tolerations,
                     do_xcom_push=True,
                     xcom_push=True,
-                    task_concurrency=1,
                 )
 
                 sync_extract >> scd_extract
-        else:
+    else:
+        sync_dag = DAG(
+            f"{config['dag_name']}_db_sync",
+            default_args=sync_dag_args,
+            schedule_interval=config["sync_schedule_interval"],
+        )
+        with sync_dag:
             # SCD Task
             scd_cmd = generate_cmd(config["dag_name"], "--load_type scd")
 
