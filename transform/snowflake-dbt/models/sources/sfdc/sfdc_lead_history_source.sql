b125162aeebf6971b54a037b4c67021ccf1d3507
parmstrong <parmstrong@gitlab.com>
2020-06-04 12:36:23 +0200

    Adding in lead history source model

---
+++ transform/snowflake-dbt/models/sources/sfdc/sfdc_lead_history_source.sql
@@ -0,0 +1,27 @@
+WITH base AS (
+
+    SELECT *
+    FROM {{ source('salesforce', 'lead_history') }}
+
+), renamed AS (
+
+    SELECT
+      opportunityid         AS opportunity_id,
+      id                    AS lead_history_id,
+      leadid                AS lead_id
+      createddate           AS field_modified_at,
+      LOWER(field)          AS lead_field,
+      newvalue__fl          AS new_value_float,
+      newvalue__st          AS new_value_string,
+      newvalue__bo          AS new_value_boolean,
+      newvalue__de          AS new_value_decimal,
+      newvalue              as new_value,
+      oldvalue              AS old_value,
+      isdeleted             AS is_deleted,
+      createdbyid           AS created_by_id
+    FROM base
+
+)
+
+SELECT *
+FROM renamed
