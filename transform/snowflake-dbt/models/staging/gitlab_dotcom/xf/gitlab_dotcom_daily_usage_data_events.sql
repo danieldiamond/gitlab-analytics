2a892bc985e7e8280cdd49c7ab29f978fc1a5947
mpeychet <mpeychet@gitlab.com>
2020-06-05 11:04:04 +0200

    remove useless fields

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_daily_usage_data_events.sql
@@ -8,11 +8,11 @@ WITH usage_data AS (
 , aggregated AS (
 
     SELECT
-      {{ dbt_utils.star(from=ref('gitlab_dotcom_usage_data_events'), except=["EVENT_CREATED_AT", "PARENT_ID"]) }},
+      {{ dbt_utils.star(from=ref('gitlab_dotcom_usage_data_events'), except=["EVENT_CREATED_AT", "PARENT_TYPE", "PARENT_ID", "PARENT_CREATED_AT"]) }},
       TO_DATE(event_created_at) AS event_date,
       COUNT(*)                  AS event_count
     FROM usage_data
-    {{ dbt_utils.group_by(n=18) }}
+    {{ dbt_utils.group_by(n=16) }}
 
 )
 
