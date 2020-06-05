69fc99abf3a09627176cb7fc320710ee850841c4
Michael Walker <mwalker@gitlab.com>
2020-06-05 17:56:57 -0500

    fix dim_products to use correct sources

--- transform/snowflake-dbt/models/sources/zuora/zuora_product_rate_plan_charge_tier_source.sql
+++ transform/snowflake-dbt/models/sources/zuora/zuora_product_rate_plan_charge_tier_source.sql
@@ -1,15 +1,14 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('zuora', 'rate_plan_charge_tier') }}
+    FROM {{ source('zuora', 'product_rate_plan_charge_tier') }}
 
 ), renamed AS (
 
     SELECT 
-      rateplanchargeid        AS rate_plan_charge_id,
       productrateplanchargeid AS product_rate_plan_charge_id,
-      price,
-      currency
+      currency                AS currency,
+      price                   AS price
     FROM source
     
 )
--- transform/snowflake-dbt/models/staging/common/dim_products.sql
+++ transform/snowflake-dbt/models/staging/common/dim_products.sql
@@ -11,18 +11,19 @@ WITH zuora_product AS (
 ), zuora_product_rate_plan_charge AS (
 
     SELECT *
-    FROM {{ ref('zuora_rate_plan_charge_source') }}
+    FROM {{ ref('zuora_product_rate_plan_charge_source') }}
 
 ), zuora_product_rate_plan_charge_tier AS (
 
     SELECT *
-    FROM {{ ref('zuora_rate_plan_charge_tier_source') }}
+    FROM {{ ref('zuora_product_rate_plan_charge_tier_source') }}
     
 ), joined AS (
 
     SELECT
+      zuora_product_rate_plan.product_rate_plan_name       AS product_rate_plan_name,
       zuora_product.product_id                             AS product_id,
-      zuora_product_rate_plan_charge.rate_plan_charge_name AS product_rate_plan_charge_name,
+      zuora_product_rate_plan_charge.product_rate_plan_charge_name AS product_rate_plan_charge_name,
       zuora_product.product_name                           AS product_name,
       zuora_product.sku                                    AS product_sku,
       zuora_product.category                               AS product_category,
@@ -34,14 +35,14 @@ WITH zuora_product AS (
     JOIN zuora_product_rate_plan_charge
       ON zuora_product_rate_plan.product_rate_plan_id = zuora_product_rate_plan_charge.product_rate_plan_id
     JOIN zuora_product_rate_plan_charge_tier
-      ON zuora_product_rate_plan_charge.rate_plan_charge_id = zuora_product_rate_plan_charge_tier.rate_plan_charge_id
+      ON zuora_product_rate_plan_charge.product_rate_plan_charge_id = zuora_product_rate_plan_charge_tier.product_rate_plan_charge_id
     WHERE zuora_product.is_deleted = FALSE
       AND zuora_product.effective_start_date <= CURRENT_DATE
       AND zuora_product.effective_end_date > CURRENT_DATE
       AND zuora_product_rate_plan_charge_tier.currency = 'USD'
       AND zuora_product_rate_plan_charge_tier.price != 0
-    {{ dbt_utils.group_by(n=6) }}
-    ORDER BY 1, 2
+    {{ dbt_utils.group_by(n=7) }}
+    ORDER BY 1, 3
 
 )
 
