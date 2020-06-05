fb63780a3f9feaccf194606dd3976ac25e1cc6ba
Michael Walker <mwalker@gitlab.com>
2020-06-05 17:00:28 -0500

    add to dim_products

---
+++ transform/snowflake-dbt/models/sources/zuora/zuora_rate_plan_charge_tier_source.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('zuora', 'rate_plan_charge_tier') }}
+
+), renamed AS (
+
+    SELECT 
+      rateplanchargeid  AS rate_plan_charge_id,
+      price,
+      currency
+    FROM source
+    
+)
+
+SELECT *
+FROM renamed
--- transform/snowflake-dbt/models/staging/common/dim_products.sql
+++ transform/snowflake-dbt/models/staging/common/dim_products.sql
@@ -4,18 +4,49 @@ WITH zuora_product AS (
     FROM {{ ref('zuora_product_source') }}
 
 ), zuora_product_rate_plan AS (
+    
     SELECT *
     FROM {{ ref('zuora_product_rate_plan_source') }}
+
+), zuora_product_rate_plan_charge AS (
+
+    SELECT *
+    FROM {{ ref('zuora_rate_plan_charge_source') }}
+
+), zuora_product_rate_plan_charge_tier AS (
+
+    SELECT *
+    FROM {{ ref('zuora_rate_plan_charge_tier_source') }}
+    
+), joined AS (
+
+    SELECT
+      zuora_product.product_id                             AS product_id,
+      zuora_product_rate_plan_charge.rate_plan_charge_name AS product_rate_plan_charge_name,
+      zuora_product.product_name                           AS product_name,
+      zuora_product.sku                                    AS product_sku,
+      zuora_product.category                               AS product_category,
+      zuora_product_rate_plan.product_rate_plan_name like '%reporter_access%'   AS is_reporter_license,
+      MIN(zuora_product_rate_plan_charge_tier.price)       AS billing_list_price
+    FROM zuora_product
+    JOIN zuora_product_rate_plan 
+      ON zuora_product.product_id = zuora_product_rate_plan.product_id
+    JOIN zuora_product_rate_plan_charge
+      ON zuora_product_rate_plan.product_rate_plan_id = zuora_product_rate_plan_charge.product_rate_plan_id
+    JOIN zuora_product_rate_plan_charge_tier
+      ON zuora_product_rate_plan_charge.rate_plan_charge_id = zuora_product_rate_plan_charge_tier.rate_plan_charge_id
+    WHERE zuora_product.is_deleted = FALSE
+      AND zuora_product.effective_start_date <= CURRENT_DATE
+      AND zuora_product.effective_end_date > CURRENT_DATE
+      AND zuora_product_rate_plan_charge_tier.currency = 'USD'
+      AND zuora_product_rate_plan_charge_tier.price != 0
+    {{ dbt_utils.group_by(n=6) }}
+    ORDER BY 1, 2
+
 )
 
-SELECT DISTINCT
-  zuora_product.product_id,
-  zuora_product.product_name,
-  zuora_product.sku,
-  zuora_product.category,
-  zuora_product_rate_plan.product_rate_plan_name like '%reporter_access%'   AS is_reporter_license
-FROM zuora_product
-  LEFT JOIN zuora_product_rate_plan ON zuora_product_rate_plan.product_id = zuora_product.product_id
-WHERE zuora_product.is_deleted = FALSE
+SELECT *
+FROM joined
+
 
 
