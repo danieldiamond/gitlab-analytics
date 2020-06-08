d164a3868a379984021d44006551c72761380d97
mpeychet <mpeychet@gitlab.com>
2020-06-08 14:12:49 +0200

    add snapshot

---
+++ transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_issues.sql
@@ -0,0 +1,22 @@
+{% snapshot gitlab_dotcom_members_snapshots %}
+
+    {{
+        config(
+          unique_key='id',
+          strategy='timestamp',
+          updated_at='updated_at',
+        )
+    }}
+    
+    WITH source AS (
+
+    	SELECT 
+        *
+      FROM {{ source('gitlab_dotcom', 'issues') }}
+      QUALIFY (ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1)
+    )
+    
+    SELECT *
+    FROM source
+    
+{% endsnapshot %}
