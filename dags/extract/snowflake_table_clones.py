b3c6da2cec2b0262e5560e622af5c22015e63179
msendal <msendal@gitlab.com>
2020-06-08 21:59:40 +0200

    change dag start time

--- dags/extract/snowflake_table_clones.py
+++ dags/extract/snowflake_table_clones.py
@@ -57,7 +57,7 @@ default_args = {
     "owner": "airflow",
     "retries": 1,
     "retry_delay": timedelta(minutes=1),
-    "start_date": datetime(2020, 6, 1),
+    "start_date": datetime(2020, 6, 7),
 }
 
 # Create the DAG
