186d655bda704967f5d46cf9c4053b5f378c482c
parmstrong <parmstrong@gitlab.com>
2020-06-11 16:02:51 +0200

    Corrected credentials select and cleaned out the testing stuff

--- orchestration/big_query_client.py
+++ orchestration/big_query_client.py
@@ -19,32 +19,23 @@ class BigQueryClient:
 
     def get_clients(self, gapi_keyfile: str = None,) -> (Client, BigQueryStorageClient):
         """
-            Designed to work with a service account which has its configuration pointed to by the
-            GOOGLE_APPLICATION_CREDENTIALS environment variable
             :return:
         """
-        #credentials, project_id = google.auth.default(
-        #    scopes=["https://www.googleapis.com/auth/cloud-platform"]
-        #)
         # Get the gcloud storage client and authenticate
-
-        info(os.system('gcloud config list'))
-        info(os.system('echo $GCP_SERVICE_CREDS'))
         scope = ["https://www.googleapis.com/auth/cloud-platform"]
 
         keyfile = safe_load(gapi_keyfile or env["GCP_SERVICE_CREDS"])
-        info(keyfile)
+
         credentials = service_account.Credentials.from_service_account_info(keyfile)
         scoped_credentials = credentials.with_scopes(scope)
-        info(credentials)
+
         bq_client = Client(
             credentials=scoped_credentials,
-            #project='gitlab-analysis',
         )
 
         bq_storage_client = BigQueryStorageClient(
+                credentials=scoped_credentials
         )
-        info(os.system('gcloud config list'))
         return bq_client, bq_storage_client
 
     def get_dataframe_from_sql(self, sql_statement: str) -> pd.DataFrame:
