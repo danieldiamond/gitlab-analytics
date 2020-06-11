af80a7e3660779f3c21765403612330b739a1c60
parmstrong <parmstrong@gitlab.com>
2020-06-11 16:05:14 +0200

    Added a bit of logging and cleaned imports

--- orchestration/big_query_client.py
+++ orchestration/big_query_client.py
@@ -1,26 +1,24 @@
 import pandas as pd
-import google.auth
 from google.oauth2 import service_account
 from google.cloud.bigquery import Client
 from google.cloud.bigquery_storage_v1beta1 import BigQueryStorageClient
 from yaml import safe_load
-from logging import error, info, basicConfig, getLogger, warning
+from logging import info
 
 from os import environ as env
-import os
 
 
 config_dict = env.copy()
 
 class BigQueryClient:
     def __init__(self):
-
         self.bq_client, self.bq_storage_client = self.get_clients()
 
     def get_clients(self, gapi_keyfile: str = None,) -> (Client, BigQueryStorageClient):
         """
             :return:
         """
+        info("Getting BigQuery clients")
         # Get the gcloud storage client and authenticate
         scope = ["https://www.googleapis.com/auth/cloud-platform"]
 
@@ -36,6 +34,7 @@ class BigQueryClient:
         bq_storage_client = BigQueryStorageClient(
                 credentials=scoped_credentials
         )
+        info("BigQuery clients retrieved")
         return bq_client, bq_storage_client
 
     def get_dataframe_from_sql(self, sql_statement: str) -> pd.DataFrame:
