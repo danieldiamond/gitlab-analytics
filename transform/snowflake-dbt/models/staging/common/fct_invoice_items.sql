6dd8e1dd1013e1a8a247eef071afb24a1b07400c
Michael Walker <mwalker@gitlab.com>
2020-06-11 09:46:36 -0500

    add invoice_item_id to fct_invoice_items

--- transform/snowflake-dbt/models/staging/common/fct_invoice_items.sql
+++ transform/snowflake-dbt/models/staging/common/fct_invoice_items.sql
@@ -11,8 +11,9 @@ WITH zuora_invoice_item AS (
 ), invoice_charges AS (
 
     SELECT
+      zuora_invoice_item.id                         AS invoice_item_id,
       zuora_invoice_item.rate_plan_charge_id        AS charge_id,
-      zuora_invoice.invoice_number,
+      zuora_invoice.invoice_number                  AS invoice_number,
       zuora_invoice.account_id                      AS invoice_account_id,
       zuora_invoice.invoice_date::DATE              AS invoice_date,
       zuora_invoice_item.service_start_date::DATE   AS service_start_date,
