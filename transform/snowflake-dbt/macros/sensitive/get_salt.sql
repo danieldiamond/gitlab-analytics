d67713ab26c300756aa145ff5b961249a44ade93
Michael Walker <mwalker@gitlab.com>
2020-06-17 16:19:11 -0500

    rename salt env names so they are together alphabetically

--- transform/snowflake-dbt/macros/sensitive/get_salt.sql
+++ transform/snowflake-dbt/macros/sensitive/get_salt.sql
@@ -1,11 +1,11 @@
 {% macro get_salt(column_name) %}
 
     {%- if 'name' in column_name -%}
-        {{ return( env_var("NAME_SALT") ) }}
+        {{ return( env_var("SALT_NAME") ) }}
     {%- elif 'email' in column_name -%}
-        {{ return( env_var("EMAIL_SALT") ) }}
+        {{ return( env_var("SALT_EMAIL") ) }}
     {%- elif 'ip' in column_name -%}
-        {{ return( env_var("IP_SALT") ) }}
+        {{ return( env_var("SALT_IP") ) }}
     {%- else -%}
         {{ return( env_var("SALT") ) }}
     {%- endif -%}
