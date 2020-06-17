e7367ef31314469ccc6ed1339f7e73928d5ece4d
Michael Walker <mwalker@gitlab.com>
2020-06-17 16:16:34 -0500

    fix salt method

--- transform/snowflake-dbt/macros/sensitive/get_salt.sql
+++ transform/snowflake-dbt/macros/sensitive/get_salt.sql
@@ -1,1 +1,13 @@
 {% macro get_salt(column_name) %}
+
+    {%- if 'name' in column_name -%}
+        {{ return( env_var("NAME_SALT") ) }}
+    {%- elif 'email' in column_name -%}
+        {{ return( env_var("EMAIL_SALT") ) }}
+    {%- elif 'ip' in column_name -%}
+        {{ return( env_var("IP_SALT") ) }}
+    {%- else -%}
+        {{ return( env_var("SALT") ) }}
+    {%- endif -%}
+
+{% endmacro %}
--- transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
+++ transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
@@ -1,5 +1,5 @@
 {% macro hash_of_column(column) %}
 
-    sha2(TRIM(LOWER({{column|lower}} || '{{ env_var("salt") }}'))) AS {{column|lower}}_hash,
+    sha2(TRIM(LOWER({{column|lower}} || '{{ get_salt(column|lower) }}'))) AS {{column|lower}}_hash,
 
 {% endmacro %}
