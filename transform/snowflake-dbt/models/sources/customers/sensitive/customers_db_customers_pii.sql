8efbdfc58cc580d6687277628f94101180fddb07
msendal <msendal@gitlab.com>
2020-06-17 13:46:31 +0200

    fix typos from test

--- transform/snowflake-dbt/models/sources/customers/sensitive/customers_db_customers_pii.sql
+++ transform/snowflake-dbt/models/sources/customers/sensitive/customers_db_customers_pii.sql
@@ -1,14 +1,12 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ ref('customers_db_customers') }}
+    FROM {{ ref('customers_db_customers_source') }}
 
 ), customers_db_customers_pii AS (
 
     SELECT
       customer_id,
-      {{ nohash_sensitive_columns('customers_db_customers', 'customer_last_name') }},
-      {{ nohash_sensitive_columns('customers_db_customers', 'customer_first_name') }},
       {{ nohash_sensitive_columns('customers_db_customers', 'customer_email') }}
     FROM source
 
