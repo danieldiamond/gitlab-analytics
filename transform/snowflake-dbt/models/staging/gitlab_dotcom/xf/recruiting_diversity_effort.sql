abccd8bc46a340ddfb1be1c7cd0233e5c3221757
Parul Luthra <pluthra@gitlab.com>
2020-06-18 10:41:54 -0400

    formatting

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
@@ -1,8 +1,8 @@
 WITH issues AS (
   
-  SELECT *
-  FROM {{ ref ('gitlab_dotcom_issues_xf') }}
-  WHERE project_id =16492321 --Recruiting for Open Positions
+    SELECT *
+    FROM {{ ref ('gitlab_dotcom_issues_xf') }}
+    WHERE project_id =16492321 --Recruiting for Open Positions
 
 ), users AS (
 
@@ -28,34 +28,31 @@ WITH issues AS (
 
 ), intermediate AS (
 
-  SELECT 
-    issues.issue_title,
-    issues.issue_IID,
-    issues.issue_created_at,
-    DATE_TRUNC(week,issue_created_at) as issue_created_week,
-    issues.issue_closed_at,
-    DATE_TRUNC(week,issues.issue_closed_at) as issue_closed_week,
-    IFF(issue_closed_at IS NOT NULL,1,0)  AS is_issue_closed,
-    issues.state,
-    agg_assignee.assignee,
-    IFF(CONTAINS(ltrim(issue_description), '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
-   * [x] Yes
-   * [ ] No'::varchar) = True ,'Used Diversity Strings',null) AS used_diversity_booleanstrings,
-
-    IFF(CONTAINS(issue_description, '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
-   * [ ] Yes
-   * [ ] No'::varchar) = True ,'No Answer',null) AS no_answer,
-
-    IFF(CONTAINS(trim(issue_description), '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
-   * [ ] Yes
-   * [x] No'::varchar) = True ,'Did not Use',null) AS did_not_use
-  FROM issues
-  LEFT JOIN agg_assignee 
-    ON agg_assignee.issue_id = issues.issue_id
-  WHERE project_id = 16492321
-    AND issue_title like '%Weekly Check-in:%'
-    AND issue_title not like '%Test%'
-  ORDER BY used_Diversity_booleanstrings
+    SELECT 
+      issues.issue_title,
+      issues.issue_iid,
+      issues.issue_created_at,
+      DATE_TRUNC(week,issue_created_at)                         AS issue_created_week,
+      issues.issue_closed_at,
+      DATE_TRUNC(week,issues.issue_closed_at)                   AS issue_closed_week,
+      IFF(issue_closed_at IS NOT NULL,1,0)                      AS is_issue_closed,
+      issues.state,
+      agg_assignee.assignee,
+      IFF(CONTAINS(issue_description, '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
+    * [x] Yes
+    * [ ] No'::varchar) = True ,'used diversity strings',null)  AS used_diversity_booleanstrings,
+      IFF(CONTAINS(issue_description, '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
+    * [ ] Yes
+    * [ ] No'::varchar) = True ,'no answer',null)               AS no_answer,
+        IFF(CONTAINS(trim(issue_description), '2. **Sourcer/Recruiter:** I used [Diversity Boolean strings](https://docs.google.com/spreadsheets/d/1Hs3UVEpgYOJgvV8Nlyb0Cl5P6_8IlAlxeLQeXz64d8Y/edit#gid=0) or used other methods, including provided best practices, to ensure 90% of my outbound sourcing efforts were directed towards individuals from underrepresented groups.
+    * [ ] Yes
+    * [x] No'::varchar) = True ,'did not use',null)             AS did_not_use
+    FROM issues
+    LEFT JOIN agg_assignee 
+      ON agg_assignee.issue_id = issues.issue_id
+    WHERE project_id = 16492321
+      AND issue_title LIKE '%Weekly Check-in:%'
+      AND issue_title NOT LIKE '%Test%'
   
 )
 
