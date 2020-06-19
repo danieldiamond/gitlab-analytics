e8e5ed17a9fdc4ee0bc6bf4c0b9baf8be599f245
mpeychet <mpeychet@gitlab.com>
2020-06-19 12:02:17 +0200

    add alert management

--- transform/snowflake-dbt/models/smau_events/snowplow_smau_pageviews_events/monitor_snowplow_smau_pageviews_events.sql
+++ transform/snowflake-dbt/models/smau_events/snowplow_smau_pageviews_events/monitor_snowplow_smau_pageviews_events.sql
@@ -6,6 +6,15 @@
 
 {%- set event_ctes = [
    {
+      "event_name":"alert_management_viewed",
+      "regexp_where_statements":[
+         {
+            "regexp_pattern":"((\/([0-9A-Za-z_.-])*){2,})?\/alert_management",
+            "regexp_function":"REGEXP"
+         }
+      ]
+   },
+   {
       "event_name":"envrionments_viewed",
       "regexp_where_statements":[
          {
