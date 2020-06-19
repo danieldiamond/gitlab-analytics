cbc7e745ec3813616f71c91f2441ae049cbecfd9
Michael Walker <mwalker@gitlab.com>
2020-06-19 11:08:29 -0500

    obfuscate salts from query history

--- transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
+++ transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
@@ -1,5 +1,11 @@
 {% macro hash_of_column(column) %}
 
-    sha2(TRIM(LOWER({{column|lower}} || '{{ get_salt(column|lower) }}'))) AS {{column|lower}}_hash,
+    sha2(
+        TRIM(
+            LOWER(
+                {{column|lower}} || ENCRYPT('{{ get_salt(column|lower) }}', '{{ get_salt(column|lower) }}')::VARCHAR 
+            )
+        )
+    ) AS {{column|lower}}_hash,
 
 {% endmacro %}
