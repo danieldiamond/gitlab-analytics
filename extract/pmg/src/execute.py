0ba7b8f998aadf1916435c6d3f6b0b87b04c9d2a
parmstrong <parmstrong@gitlab.com>
2020-06-20 19:17:33 +0200

    Return list of file names and write all of them to table

--- extract/pmg/src/execute.py
+++ extract/pmg/src/execute.py
@@ -42,7 +42,7 @@ def get_pmg_reporting_data_query(start_date: datetime, end_date: datetime) -> st
     )
 
 
-def write_date_json(date: datetime, df: DataFrame) -> None:
+def write_date_json(date: datetime, df: DataFrame) -> str:
     """ Just here so we can log in the list comprehension """
     file_name = f"pmg_reporting_data_{date}.json"
     print(f"Writing file {file_name}")
@@ -50,6 +50,8 @@ def write_date_json(date: datetime, df: DataFrame) -> None:
     df.to_json(file_name, orient="records", date_format="iso")
 
     print(f"{file_name} written")
+
+    return file_name
 
 
 if __name__ == "__main__":
@@ -67,11 +69,11 @@ if __name__ == "__main__":
 
     df_by_date = bq.get_dataframe_from_sql(sql_statement).groupby("date")
 
-    [write_date_json(date, df) for date, df in df_by_date]
+    written_files = [write_date_json(date, df) for date, df in df_by_date]
 
-    snowflake_stage_load_copy_remove(
-        "pmg_reporting_data_2020-06-15.json",
-        "pmg.pmg_load",
-        "pmg.paid_digital",
-        snowflake_engine,
-    )
+    [
+        snowflake_stage_load_copy_remove(
+            file_name, "pmg.pmg_load", "pmg.paid_digital", snowflake_engine,
+        )
+        for file_name in written_files
+    ]
