c5237c8b5d3037c986320f4770351d89c29c7b8c
Michael Walker <mwalker@gitlab.com>
2020-06-22 16:47:27 -0500

    fix yaml path

--- orchestration/test/test_drop_snowflake_objects.py
+++ orchestration/test/test_drop_snowflake_objects.py
@@ -2,5 +2,5 @@ from orchestration.drop_snowflake_objects import get_list_of_dbs_to_keep
 
 
 def test_dbs_to_keep():
-    dbs_to_keep = get_list_of_dbs_to_keep()
+    dbs_to_keep = get_list_of_dbs_to_keep(yaml_path="load/snowflake/roles.yml")
     assert "analytics" in dbs_to_keep
