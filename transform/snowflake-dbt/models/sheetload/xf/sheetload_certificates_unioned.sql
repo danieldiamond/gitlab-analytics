c7c5a578a38bab14997acb59335536c0f8c2ab1b
Jacie Bandur <jbandur@gitlab.com>
2020-06-23 21:33:39 +0000

    update sheetload_certificates_unioned.sql

--- transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
+++ transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
@@ -7,5 +7,6 @@
     relations=[ref('sheetload_ally_certificate'),
                ref('sheetload_values_certificate'),
                ref('sheetload_communication_certificate'),
+               ref('ic_collaboration_competency'),
                ref('sheetload_compensation_certificate')]
 ) }}
