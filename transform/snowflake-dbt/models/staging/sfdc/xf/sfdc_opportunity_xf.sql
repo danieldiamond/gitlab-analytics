2f302d1911bd0de8eb776fffc772a49a74e5b690
Robert Kohnke <rkohnke@gitlab.com>
2020-06-25 21:04:08 +0000

    Adding Technical Evaluation date to opportunity xf table.

--- transform/snowflake-dbt/models/staging/sfdc/xf/sfdc_opportunity_xf.sql
+++ transform/snowflake-dbt/models/staging/sfdc/xf/sfdc_opportunity_xf.sql
@@ -64,6 +64,7 @@ WITH sfdc_opportunity AS (
       sfdc_opportunity.stage_name,
       sfdc_opportunity_stage.is_active                                                             AS stage_is_active,
       sfdc_opportunity_stage.is_closed                                                             AS stage_is_closed,
+      sfdc_opportunity.x3_technical_evaluation_date__c                                             AS technical_evaluation_date,
       sfdc_opportunity.order_type,
       
       -- opportunity information
