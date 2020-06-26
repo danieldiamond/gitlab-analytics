b2a3acd6de421a5d41576c2f237d82e7d1e16f10
Michael Walker <mwalker@gitlab.com>
2020-06-26 12:44:08 -0500

    more formatting for sheetload

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_ally_certificate_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_ally_certificate_source.sql
@@ -1,17 +1,23 @@
+{{
+    config({
+      "schema": "sensitive"
+    })
+}}
+
 WITH source AS (
 
-	SELECT *
-	FROM {{ source('sheetload', 'ally_certificate') }}
+    SELECT *
+    FROM {{ source('sheetload', 'ally_certificate') }}
 
 ), renamed AS (
 
-	SELECT
-		"Timestamp"::TIMESTAMP::DATE                                                                 AS date_completed,
-		"Score"                                                                                      AS score,
-		"First_&_Last_Name"                                                                          AS submitter_name,
-		"Email_Address_(GitLab_team_members,_please_use_your_GitLab_email_address)"::STRING          AS submitter_email,
-		"_UPDATED_AT"                                                                                AS last_updated_at
-	FROM source
+    SELECT
+      "Timestamp"::TIMESTAMP::DATE                                                                 AS date_completed,
+      "Score"                                                                                      AS score,
+      "First_&_Last_Name"                                                                          AS submitter_name,
+      "Email_Address_(GitLab_team_members,_please_use_your_GitLab_email_address)"::STRING          AS submitter_email,
+      "_UPDATED_AT"                                                                                AS last_updated_at
+    FROM source
 
 )
 
--- transform/snowflake-dbt/models/sources/sheetload/sheetload_values_certificate_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_values_certificate_source.sql
@@ -1,3 +1,9 @@
+{{
+    config({
+      "schema": "sensitive"
+    })
+}}
+
 WITH source AS (
 
 	SELECT *
