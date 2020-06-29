5c1b072aaf14923a396ce86a00118d41beac6e3a
Jacie Bandur <jbandur@gitlab.com>
2020-06-29 21:25:29 +0000

    Update sheetload_certificates_unioned.sql with additional 5 value ka's for ic's

--- transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
+++ transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
@@ -8,5 +8,10 @@
                ref('sheetload_values_certificate'),
                ref('sheetload_communication_certificate'),
                ref('sheetload_compensation_certificate'),
+               ref('sheetload_ic_results_competency'),
+               ref('sheetload_ic_efficiency_competency'),
+               ref('sheetload_ic_dib_comptency'),
+               ref('sheetload_ic_iteration_competency'),
+               ref('sheetload_ic_transparency_competency'),
                ref('sheetload_ic_collaboration_competency')]
 ) }}
