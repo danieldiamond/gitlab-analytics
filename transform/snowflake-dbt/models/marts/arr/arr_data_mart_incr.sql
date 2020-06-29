dbcee7f6bdb357e7023868720bc7654716e1091a
msendal <msendal@gitlab.com>
2020-06-29 12:07:15 +0200

    add incr changes;

--- transform/snowflake-dbt/models/marts/arr/arr_data_mart_incr.sql
+++ transform/snowflake-dbt/models/marts/arr/arr_data_mart_incr.sql
@@ -115,7 +115,7 @@ WITH fct_charges AS (
 ), charges_month_by_month AS (
 
       SELECT
-        '{{ var('valid_at') }}'::DATE AS snapshot_date,
+        '{{ var('valid_at', '{{ get_current_date() }}' ) }}'::DATE AS snapshot_date,
         charges_agg.*,
         dim_dates.date_id,
         dateadd('month', -1, dim_dates.date_actual)  AS reporting_month
