e976d66ea59b35a7496866f0480c8785b8791dda
Parul Luthra <pluthra@gitlab.com>
2020-06-29 14:45:49 +0000

    adding to union all file

--- transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
+++ transform/snowflake-dbt/models/sheetload/xf/sheetload_certificates_unioned.sql
@@ -7,5 +7,6 @@
     relations=[ref('sheetload_ally_certificate'),
                ref('sheetload_values_certificate'),
                ref('sheetload_communication_certificate'),
-               ref('sheetload_compensation_certificate')]
+               ref('sheetload_compensation_certificate'),
+               ref('sheetload_ic_collaboration_competency')]
 ) }}
