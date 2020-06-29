4752c9735c77f69929343518977e263d6347157e
parmstrong <parmstrong@gitlab.com>
2020-06-29 15:14:35 +0200

    Added actual tests, just that the project & api endpoints are correct.

--- orchestration/test/test_big_query_client.py
+++ orchestration/test/test_big_query_client.py
@@ -2,9 +2,23 @@ import os
 
 import pytest
 
-from google.cloud.bigquery import Client
+from orchestration.big_query_client import BigQueryClient
 
-from orchestration.big_query_client import get_client
+bq_client = BigQueryClient().get_client()
 
-def test_connection():
-    assert get_client() == Client
+
+def get_api_endpoint(client):
+    return client._connection.API_BASE_URL
+
+
+def get_project(client):
+    return client.project
+
+
+def test_project():
+    assert get_project(bq_client) == "gitlab-analysis"
+
+
+def test_endpoint():
+    assert get_api_endpoint(bq_client) == "https://bigquery.googleapis.com"
+
