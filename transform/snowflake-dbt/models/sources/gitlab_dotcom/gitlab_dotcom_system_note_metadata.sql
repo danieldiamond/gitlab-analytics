247d23982907b271e24056cd3d1de1c256fb8f3b
mpeychet <mpeychet@gitlab.com>
2020-07-02 13:54:50 +0200

    add system_note_metadata

---
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/gitlab_dotcom_system_note_metadata.sql
@@ -0,0 +1,23 @@
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('gitlab_dotcom', 'system_note_metadata') }}
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
+
+), renamed AS (
+
+    SELECT
+      id::INTEGER                           AS system_note_metadata,
+      note_id::INTEGER                      AS note_id,
+      commit_count::INTEGER                 AS commit_count,
+      action::INTEGER                       AS action_type,
+      description_version_id::INTEGER       AS description_version_id,
+      created_at::TIMESTAMP                 AS created_at,
+      updated_at::TIMESTAMP                 AS updated_at
+
+    FROM source
+
+)
+
+SELECT  *
+FROM renamed
