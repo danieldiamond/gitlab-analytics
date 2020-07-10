7d6f2edaac63009c53b451f36ec435a62835168a
mpeychet <mpeychet@gitlab.com>
2020-07-10 11:55:39 +0200

    add 3 new events

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_usage_data_events.sql
@@ -135,6 +135,15 @@
     "is_representative_of_stage": "False"
   },
   {
+    "event_name": "epic_notes",
+    "source_cte_name": "epic_notes",
+    "user_column_name": "note_author_id",
+    "key_to_parent_project": "project_id",
+    "primary_key": "author_id",
+    "stage_name": "plan",
+    "is_representative_of_stage": "False"
+  },
+  {
     "event_name": "groups",
     "source_cte_name": "group_members",
     "user_column_name": "user_id",
@@ -160,6 +169,15 @@
     "primary_key": "issue_id",
     "stage_name": "plan",
     "is_representative_of_stage": "True"
+  },
+  {
+    "event_name": "issue_notes",
+    "source_cte_name": "issue_notes",
+    "user_column_name": "note_author_id",
+    "key_to_parent_project": "project_id",
+    "primary_key": "author_id",
+    "stage_name": "plan",
+    "is_representative_of_stage": "False"
   },
   {
     "event_name": "issue_resource_label_events",
@@ -232,6 +250,15 @@
     "primary_key": "merge_request_id",
     "stage_name": "create",
     "is_representative_of_stage": "True"
+  },
+  {
+    "event_name": "merge_requests_notes",
+    "source_cte_name": "merge_requests_notes",
+    "user_column_name": "note_author_id",
+    "key_to_parent_project": "project_id",
+    "primary_key": "author_id",
+    "stage_name": "plan",
+    "is_representative_of_stage": "False"
   },
   {
     "event_name": "milestones",
