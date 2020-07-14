98825b704063cb1c032817782aae273c4d42c342
Jean Del Rosario <jpeguero@gitlab.com>
2020-07-14 13:19:21 -0400

    Change references of tables to ref format

--- transform/snowflake-dbt/models/marts/exports/users_emails/paid_saas_users.sql
+++ transform/snowflake-dbt/models/marts/exports/users_emails/paid_saas_users.sql
@@ -1,4 +1,57 @@
-WITH all_gitlab_user_information AS (
+WITH users AS (
+
+    SELECT *
+    FROM {{ source('gitlab_dotcom', 'users') }}
+    
+), memberships AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_memberships') }}
+  
+), plans AS (
+  
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_plans') }}
+
+), zuora_subscription_product_category AS (
+
+  -- Return all the subscription information 
+  SELECT DISTINCT
+    mrr_month,
+    account_id,
+    account_number,
+    crm_id,
+    subscription_id,
+    product_category,
+    delivery
+  FROM {{ ref('zuora_monthly_recurring_revenue') }}
+
+), zuora_contacts_information AS (
+
+  -- Get the Zuora Contact information to check which columns are good 
+  SELECT DISTINCT
+    contact_id,
+    account_id,
+    first_name,
+    last_name,
+    work_email,
+    personal_email  
+  FROM {{ ref('zuora_contact') }} 
+
+), salesforce_contacts_information AS (
+
+  -- Get the Salesforce Contact information to check which columns are good 
+  SELECT DISTINCT
+      account_id,
+      contact_id                                                                                                                                                AS user_id, 
+      contact_name                                                                                                                                              AS full_name,
+      SPLIT_PART(TRIM(contact_name), ' ', 1)                                                                                                                    AS first_name,
+      ARRAY_TO_STRING(ARRAY_SLICE(SPLIT(TRIM(contact_name), ' '), 1, 10), ' ')                                                                                  AS last_name,
+      contact_email                                                                                                                                             AS email, 
+      IFF(((inactive_contact = TRUE) OR (has_opted_out_email = TRUE) OR (invalid_email_address = TRUE) OR (email_is_bounced = TRUE)), 'Inactive', 'Active')     AS state 
+  FROM {{ ref('sfdc_contact_source') }}
+
+), all_gitlab_user_information AS (
 
   SELECT
         id                                                               AS user_id,
@@ -8,19 +61,9 @@ WITH all_gitlab_user_information AS (
         username,
         notification_email, 
         state 
-    FROM "RAW".tap_postgres.gitlab_db_users
+    FROM users
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 
-), memberships AS (
-
-    SELECT *
-    FROM "ANALYTICS".analytics.gitlab_dotcom_memberships
-  
-), plans AS (
-  
-    SELECT *
-    FROM analytics_staging.gitlab_dotcom_plans
-  
 ), saas_paid_users AS (
   
     SELECT DISTINCT
@@ -36,19 +79,6 @@ WITH all_gitlab_user_information AS (
     LEFT JOIN plans ON memberships.ultimate_parent_plan_id::VARCHAR = plans.plan_id::VARCHAR
     WHERE memberships.ultimate_parent_plan_id::VARCHAR IN ('2', '3', '4')
   
-), zuora_subscription_product_category AS (
-
-  -- Return all the subscription information 
-  SELECT DISTINCT
-    mrr_month,
-    account_id,
-    account_number,
-    crm_id,
-    subscription_id,
-    product_category,
-    delivery
-  FROM ANALYTICS.analytics.zuora_monthly_recurring_revenue
-
 ), zuora_subscription_product_category_saas_only AS (
 
   -- Filter to Self-Managed subscriptions Only 
@@ -61,18 +91,6 @@ WITH all_gitlab_user_information AS (
   SELECT *
   FROM zuora_subscription_product_category_saas_only
   WHERE mrr_month = DATE_TRUNC('month', CURRENT_DATE)
-
-), zuora_contacts_information AS (
-
-  -- Get the Zuora Contact information to check which columns are good 
-  SELECT DISTINCT
-    contact_id,
-    account_id,
-    first_name,
-    last_name,
-    work_email,
-    personal_email  
-  FROM ANALYTICS.analytics_staging.zuora_contact contact 
 
 ), zuora_subscription_product_category_saas_only_contacts AS (
 
@@ -91,23 +109,10 @@ WITH all_gitlab_user_information AS (
          FROM zuora_subscription_product_category_saas_only_current_month),
         'active', 'inactive')                                                  AS state, 
     'Zuora Only'                                                               AS source,
-  null as SFDC
-  FROM zuora_contacts_information contacts
-  INNER JOIN zuora_subscription_product_category_saas_only subscription 
+    NULL                                                                       AS SFDC
+  FROM zuora_contacts_information AS contacts
+  INNER JOIN zuora_subscription_product_category_saas_only AS subscription 
     ON contacts.account_id = subscription.account_id
-
-), salesforce_contacts_information AS (
-
-  -- Get the Salesforce Contact information to check which columns are good 
-  SELECT DISTINCT
-      account_id,
-      contact_id                                                                                                                                                AS user_id, 
-      contact_name                                                                                                                                              AS full_name,
-      SPLIT_PART(TRIM(contact_name), ' ', 1)                                                                                                                    AS first_name,
-      ARRAY_TO_STRING(ARRAY_SLICE(SPLIT(TRIM(contact_name), ' '), 1, 10), ' ')                                                                                  AS last_name,
-      contact_email                                                                                                                                             AS email, 
-      IFF(((inactive_contact = TRUE) OR (has_opted_out_email = TRUE) OR (invalid_email_address = TRUE) OR (email_is_bounced = TRUE)), 'Inactive', 'Active')     AS state 
-  FROM ANALYTICS.sfdc.sfdc_contact_source
 
 ), zuora_salesforce_subscription_product_category_saas_only_contacts AS (
 
