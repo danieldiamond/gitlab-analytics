351ca5fc7765e112adb036bd2ca170257ab52659
Parul Luthra <pluthra@gitlab.com>
2020-07-19 20:48:12 -0600

    updating assignee cte and has_no_answer field

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/recruiting_diversity_effort.sql
@@ -11,19 +11,17 @@ WITH issues AS (
 
 ), assignee AS (
 
-    SELECT 
-      assignee.*, 
-      user_name AS assignee
-    FROM {{ ref ('gitlab_dotcom_issue_assignees') }} assignee
-    LEFT JOIN users
-      ON assignee.user_id = users.user_id 
+    SELECT *
+    FROM {{ ref ('gitlab_dotcom_issue_assignees') }} 
 
 ), agg_assignee AS (
 
     SELECT
      issue_id,
-     ARRAY_AGG(LOWER(assignee)) WITHIN GROUP (ORDER BY assignee ASC) AS assignee
+     ARRAY_AGG(LOWER(user_name)) WITHIN GROUP (ORDER BY user_name ASC) AS assignee
     FROM assignee
+    LEFT JOIN users
+      ON assignee.user_id = users.user_id 
     GROUP BY issue_id
 
 ), intermediate AS (
@@ -39,11 +37,12 @@ WITH issues AS (
       issues.state                                              AS issue_state,
       agg_assignee.assignee,
       IFF(CONTAINS(issue_description, '[x] Yes, Diversity Sourcing methods were used'::VARCHAR) = True,
-        'Used Diversity Strings', NULL                          AS is_using_diversity_strings,
+        'Used Diversity Strings', NULL)                         AS is_using_diversity_strings,
       IFF(CONTAINS(issue_description, '[x] No, I did not use Diversity Sourcing methods'::VARCHAR) = True,
         'Did not Use', NULL)                                    AS is_not_using_diversity_srings,
-      IFF(used_diversity_booleanstrings IS NULL AND did_not_use IS NULL, 
-        'No Answer',NULL)                                       AS has_no_Answer
+      IFF(is_using_diversity_strings IS NULL 
+          AND is_not_using_diversity_srings IS NULL, 
+          'No Answer',NULL)                                     AS has_no_Answer
     FROM issues
     LEFT JOIN agg_assignee 
       ON agg_assignee.issue_id = issues.issue_id
