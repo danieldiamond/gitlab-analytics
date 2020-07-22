c4ced6e5eed59b3a46fc157b083a8807cbbc9d70
Jean Del Rosario <jpeguero@gitlab.com>
2020-07-22 16:25:22 -0400

    Change monthly quieries to views

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_namespace_lineage_monthly.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_namespace_lineage_monthly.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='view') }}
+
 WITH namespace_snapshots_monthly AS (
 
     SELECT *
--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_namespace_snapshots_monthly.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_namespace_snapshots_monthly.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='view') }}
+
 WITH date_details AS (
   
     SELECT *
