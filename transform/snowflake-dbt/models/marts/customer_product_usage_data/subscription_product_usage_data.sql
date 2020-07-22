72bed6f0c62afbf38a31c37da4fac377ce91d44b
derekatwood <datwood@gitlab.com>
2020-07-22 11:05:16 -0500

    format

--- transform/snowflake-dbt/models/marts/customer_product_usage_data/subscription_product_usage_data.sql
+++ transform/snowflake-dbt/models/marts/customer_product_usage_data/subscription_product_usage_data.sql
@@ -1,150 +1,183 @@
-WITH zuora_subscription AS (
+WITH customers_db_license_seat_links AS (
 
-  SELECT
-    original_id,
-    subscription_id,
-    subscription_name,
-    subscription_status,
-    subscription_start_date::DATE AS subscription_start_date,
-    subscription_end_date::DATE   AS subscription_end_date
-  FROM analytics_staging.zuora_subscription
-  WHERE original_id IS NOT NULL
-    AND subscription_status IN ('Active', 'Cancelled')
+    SELECT *
+    FROM {{ ref('customers_db_license_seat_links') }}
+
+), customers_db_orders AS (
+
+    SELECT *
+    FROM {{ ref('customers_db_orders') }}
+
+), gitlab_dotcom_gitlab_subscriptions AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_gitlab_subscriptions') }}  
+
+), gitlab_dotcom_memberships AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_gitlab_memberships') }}
 
 ), zuora_rate_plan AS (
 
-  SELECT
-    subscription_id,
-    ARRAY_AGG(DISTINCT delivery) AS delivery
-  FROM analytics_staging.zuora_rate_plan
-  WHERE amendement_type != 'RemoveProduct'
-  GROUP BY 1
+    SELECT *
+    FROM {{ ref('zuora_rate_plan') }}
+
+), zuora_subscription AS (
+
+    SELECT *
+    FROM {{ ref('zuora_subscription') }}
+
+), rate_plans AS (
+
+    SELECT
+      subscription_id,
+      ARRAY_AGG(DISTINCT delivery) AS delivery
+    FROM zuora_rate_plan
+    WHERE amendement_type != 'RemoveProduct'
+    GROUP BY 1  
+
+), subscriptions AS (
+
+    SELECT
+      original_id,
+      subscription_id,
+      subscription_name,
+      subscription_status,
+      subscription_start_date::DATE AS subscription_start_date,
+      subscription_end_date::DATE   AS subscription_end_date
+    FROM zuora_subscription
+    WHERE original_id IS NOT NULL
+      AND subscription_status IN ('Active', 'Cancelled')  
 
 ), zuora AS (
 
-  SELECT
-    zuora_subscription.*,
-    delivery,
-    CASE
-      WHEN ARRAY_CONTAINS('Self-Managed'::VARIANT, delivery) THEN 'Self-Managed'
-      WHEN ARRAY_CONTAINS('SaaS'::VARIANT, delivery) THEN 'SaaS'
+    SELECT
+      subscriptions.*,
+      rate_plans.delivery,
+      CASE
+        WHEN ARRAY_CONTAINS('Self-Managed'::VARIANT, delivery) THEN 'Self-Managed'
+        WHEN ARRAY_CONTAINS('SaaS'::VARIANT, delivery) THEN 'SaaS'
       ELSE 'Others' END AS delivery_group
-  FROM zuora_subscription
-  INNER JOIN zuora_rate_plan
-    ON zuora_subscription.subscription_id = zuora_rate_plan.subscription_id
+    FROM subscriptions
+    INNER JOIN rate_plans
+      ON subscriptions.subscription_id = rate_plans.subscription_id
 
 ), zuora_minus_exceptions AS (
   
-  SELECT *
-  FROM zuora
-  QUALIFY COUNT(*) OVER (PARTITION BY subscription_name) = 1
+    SELECT *
+    FROM zuora
+    QUALIFY COUNT(*) OVER (PARTITION BY subscription_name) = 1
   
 ), seat_link AS (
   
-  SELECT *
-  FROM analytics_staging.customers_db_license_seat_links
-  QUALIFY ROW_NUMBER() OVER (PARTITION BY zuora_subscription_name ORDER BY report_date DESC) = 1
+    SELECT *
+    FROM customers_db_license_seat_links
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY zuora_subscription_name ORDER BY report_date DESC) = 1
   
 ), self_managed AS (
   
-  SELECT
-    zuora_minus_exceptions.subscription_name,
-    zuora_minus_exceptions.original_id,
-    zuora_minus_exceptions.subscription_id,
-    zuora_minus_exceptions.subscription_status,
-    seat_link.report_date,
-    seat_link.active_user_count,
-    seat_link.max_historical_user_count,
-    seat_link.license_user_count
-  FROM zuora_minus_exceptions
-  LEFT JOIN seat_link
-    ON zuora_minus_exceptions.subscription_name = seat_link.zuora_subscription_name
-  WHERE zuora_minus_exceptions.delivery_group = 'Self-Managed'
+    SELECT
+      zuora_minus_exceptions.subscription_name,
+      zuora_minus_exceptions.original_id,
+      zuora_minus_exceptions.subscription_id,
+      zuora_minus_exceptions.subscription_status,
+      seat_link.report_date,
+      seat_link.active_user_count,
+      seat_link.max_historical_user_count,
+      seat_link.license_user_count
+    FROM zuora_minus_exceptions
+    LEFT JOIN seat_link
+      ON zuora_minus_exceptions.subscription_name = seat_link.zuora_subscription_name
+    WHERE zuora_minus_exceptions.delivery_group = 'Self-Managed'
   
 ), orders AS (
   
-  SELECT
-    subscription_id,
-    product_rate_plan_id,
-    gitlab_namespace_id,
-    order_start_date,
-    order_end_date
-  FROM analytics_staging.customers_db_orders
-  WHERE gitlab_namespace_id IS NOT NULL
-    AND order_is_trial = FALSE
-    AND order_end_date > CURRENT_DATE
+    SELECT
+      subscription_id,
+      product_rate_plan_id,
+      gitlab_namespace_id,
+      order_start_date,
+      order_end_date
+    FROM customers_db_orders
+    WHERE gitlab_namespace_id IS NOT NULL
+      AND order_is_trial = FALSE
+      AND order_end_date > CURRENT_DATE
   
 ), customers AS (
   
-  SELECT 
-    zuora_minus_exceptions.*,
-    orders.gitlab_namespace_id
-  FROM zuora_minus_exceptions
-  LEFT JOIN orders
-    ON zuora_minus_exceptions.subscription_id = orders.subscription_id
-  WHERE delivery_group = 'SaaS'
+    SELECT 
+      zuora_minus_exceptions.*,
+      orders.gitlab_namespace_id
+    FROM zuora_minus_exceptions
+    LEFT JOIN orders
+      ON zuora_minus_exceptions.subscription_id = orders.subscription_id
+    WHERE delivery_group = 'SaaS'
     
 ), customers_minus_exceptions AS (
   
-  SELECT *
-  FROM customers
-  QUALIFY COUNT(*) OVER (PARTITION BY subscription_id) = 1
+    SELECT *
+    FROM customers
+    QUALIFY COUNT(*) OVER (PARTITION BY subscription_id) = 1
   
 ), gitlab_subscriptions AS (
   
-  SELECT
-    namespace_id,
-    max_seats_used AS max_historical_user_count,
-    seats          AS license_user_count
-  FROM analytics_staging.gitlab_dotcom_gitlab_subscriptions
-  WHERE is_currently_valid = TRUE
+    SELECT
+      namespace_id,
+      max_seats_used AS max_historical_user_count,
+      seats          AS license_user_count
+    FROM gitlab_dotcom_gitlab_subscriptions
+    WHERE is_currently_valid = TRUE
   
 ), membership AS (
   
-  SELECT
-    ultimate_parent_id AS namespace_id,
-    COUNT(DISTINCT CASE WHEN is_billable = TRUE THEN user_id END) AS active_user_count
-  FROM analytics.gitlab_dotcom_memberships
-  GROUP BY 1
+    SELECT
+      ultimate_parent_id                                            AS namespace_id,
+      COUNT(DISTINCT CASE WHEN is_billable = TRUE THEN user_id END) AS active_user_count
+    FROM gitlab_dotcom_memberships
+    GROUP BY 1
   
 ), saas_seats AS (
   
-  SELECT
-    gitlab_subscriptions.namespace_id,
-    gitlab_subscriptions.max_historical_user_count,
-    gitlab_subscriptions.license_user_count,
-    membership.active_user_count
-  FROM gitlab_subscriptions
-  LEFT JOIN membership
-    ON gitlab_subscriptions.namespace_id = membership.namespace_id
+    SELECT
+      gitlab_subscriptions.namespace_id,
+      gitlab_subscriptions.max_historical_user_count,
+      gitlab_subscriptions.license_user_count,
+      membership.active_user_count
+    FROM gitlab_subscriptions
+    LEFT JOIN membership
+      ON gitlab_subscriptions.namespace_id = membership.namespace_id
   
 ), saas AS (
   
-  SELECT
-    customers_minus_exceptions.subscription_name,
-    customers_minus_exceptions.original_id,
-    customers_minus_exceptions.subscription_id,
-    customers_minus_exceptions.subscription_status,
-    CURRENT_DATE() AS report_date,
-    saas_seats.active_user_count,
-    saas_seats.max_historical_user_count,
-    saas_seats.license_user_count
-  FROM customers_minus_exceptions
-  LEFT JOIN saas_seats
-    ON customers_minus_exceptions.gitlab_namespace_id = saas_seats.namespace_id
+    SELECT
+      customers_minus_exceptions.subscription_name,
+      customers_minus_exceptions.original_id,
+      customers_minus_exceptions.subscription_id,
+      customers_minus_exceptions.subscription_status,
+      CURRENT_DATE() AS report_date,
+      saas_seats.active_user_count,
+      saas_seats.max_historical_user_count,
+      saas_seats.license_user_count
+    FROM customers_minus_exceptions
+    LEFT JOIN saas_seats
+      ON customers_minus_exceptions.gitlab_namespace_id = saas_seats.namespace_id
   
 ), final AS (
   
-  SELECT
-    'Self-Managed' AS delivery_group,
-    self_managed.*
-  FROM self_managed
+    SELECT
+      'Self-Managed' AS delivery_group,
+      self_managed.*
+    FROM self_managed
   
-  UNION
+    UNION
   
-  SELECT
-    'SaaS' AS delivery_group,
-    saas.*
-  FROM saas
+    SELECT
+      'SaaS' AS delivery_group,
+      saas.*
+    FROM saas
   
 )
+
+SELECT *
+FROM final
