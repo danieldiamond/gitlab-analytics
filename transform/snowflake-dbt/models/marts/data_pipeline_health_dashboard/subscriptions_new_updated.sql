181dd8cfba9358bf690a84847380fc8473b7c690
parmstrong <parmstrong@gitlab.com>
2020-07-27 15:57:00 +0200

    Added in models for creation of Zuora reports, and boneyard golden records. Very much an initial commit, still need to lint and add in another model.

---
+++ transform/snowflake-dbt/models/marts/data_pipeline_health_dashboard/subscriptions_new_updated.sql
@@ -0,0 +1,40 @@
+WITH zuora_subscription AS (
+  SELECT *
+  FROM {{ ref('zuora_subscription_source') }}
+),
+dim_dates AS (
+  SELECT *
+  FROM {{ ref('dim_dates') }}
+),
+created_date as (
+  select
+    cast(created_date as date) created_date,
+     count(*) as num_rows
+  from zuora_subscription
+  -- Exclude this date for scale, looks like a historic data load happened then so if it''s included
+  -- That date is all you can really see
+  -- where DATE_TRUNC("day", created_date) != ''2016-07-06''
+  group by cast(created_date as date)
+  order by 1 ),
+updated_date as
+(
+  select
+    cast(updated_date as date) updated_date,
+    count(*)as num_rows
+    from zuora_subscription
+  -- Exclude this date for scale, looks like a historic data load happened then so if it''s included
+  -- That date is all you can really see
+  -- where DATE_TRUNC("day", updated_date) != ''2018-01-19''
+  group by cast(updated_date as date)
+  order by 1
+)
+
+select
+        distinct
+          db.date_day,
+          cd.num_rows as created_rows,
+          ud.num_rows as updated_rows
+from dim_dates db
+left join created_date cd on cd.created_date = db.date_day
+left join updated_date ud on ud.updated_date = db.date_day
+where (cd.num_rows > 0 or ud.num_rows > 0)
