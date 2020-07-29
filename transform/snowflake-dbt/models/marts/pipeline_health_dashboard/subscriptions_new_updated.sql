76b99971dc97a79bc38bf2e946f27b6302438ab6
parmstrong <parmstrong@gitlab.com>
2020-07-29 11:57:22 +0200

    More cleanup.

--- transform/snowflake-dbt/models/marts/pipeline_health_dashboard/subscriptions_new_updated.sql
+++ transform/snowflake-dbt/models/marts/pipeline_health_dashboard/subscriptions_new_updated.sql
@@ -8,25 +8,25 @@ dim_dates AS (
 ),
 created_date AS (
   SELECT
-    CAST(created_date AS DATE) created_date,
-    COUNT(*) AS num_rows
+    CAST(created_date AS DATE)      AS created_date,
+    COUNT(*)                        AS num_rows
   FROM zuora_subscription
   GROUP BY CAST(created_date AS DATE)
 ),
 updated_date AS (
   SELECT
-    CAST(updated_date AS DATE) updated_date,
-    COUNT(*)AS num_rows
+    CAST(updated_date AS DATE)      AS updated_date,
+    COUNT(*)                        AS num_rows
   FROM zuora_subscription
   GROUP BY CAST(updated_date AS DATE)
 )
 
 SELECT
     DISTINCT
-      db.date_day            AS date_day,
-      db.day_name            AS day_name
-      ISNULL(cd.num_rows, 0) AS created_subscriptions,
-      ISNULL(ud.num_rows, 0) AS updated_subscriptions
+      db.date_day                   AS date_day,
+      db.day_name                   AS day_name
+      ISNULL(cd.num_rows, 0)        AS created_subscriptions,
+      ISNULL(ud.num_rows, 0)        AS updated_subscriptions
 FROM dim_dates db
 LEFT JOIN created_date cd ON cd.created_date = db.date_day
 LEFT JOIN updated_date ud ON ud.updated_date = db.date_day
