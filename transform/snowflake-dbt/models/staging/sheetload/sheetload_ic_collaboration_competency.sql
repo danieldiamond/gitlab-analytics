3891465c83f4b221ff759ac01c98a0711f0bebdd
Parul Luthra <pluthra@gitlab.com>
2020-07-29 17:13:25 +0000

    Update sheetload_ic_collaboration_competency.sql

--- transform/snowflake-dbt/models/staging/sheetload/sheetload_ic_collaboration_competency.sql
+++ transform/snowflake-dbt/models/staging/sheetload/sheetload_ic_collaboration_competency.sql
@@ -1,7 +1,7 @@
 WITH source AS (
 
 	SELECT *
-	FROM {{ source('sheetload', 'ic_collaboration_competency') }}
+	FROM {{ ref('sheetload_ic_collaboration_competency_source') }}
 
 {{cleanup_certificates("'ic_collaboration_competency'",
 	"Email_Address")}}
