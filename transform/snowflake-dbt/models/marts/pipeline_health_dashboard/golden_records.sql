b95f99d8bfba65a2ac42b0c43248ec9cafc8cb03
parmstrong <parmstrong@gitlab.com>
2020-07-29 17:28:08 +0200

    Indents.

--- transform/snowflake-dbt/models/marts/pipeline_health_dashboard/golden_records.sql
+++ transform/snowflake-dbt/models/marts/pipeline_health_dashboard/golden_records.sql
@@ -1,9 +1,9 @@
 WITH golden_records AS (
 
-      SELECT *
-      FROM {{ ref('sheetload_account_golden_records_source') }}
+  SELECT *
+  FROM {{ ref('sheetload_account_golden_records_source') }}
 
-  )
+)
 
 SELECT *
 FROM golden_records
