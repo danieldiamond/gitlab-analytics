d001df9b30add414649d9aa7c69e734bd5bc991f
msendal <msendal@gitlab.com>
2020-07-30 11:08:15 +0200

    update macro with _clones schema

--- transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
+++ transform/snowflake-dbt/macros/warehouse/grant_usage_to_schemas.sql
@@ -26,6 +26,10 @@
         grant select on all tables in schema {{ schema_name }}_staging to role {{ non_sensitive }};
         grant select on all views in schema {{ schema_name }}_staging to role {{ non_sensitive }};
 
+        grant usage on schema {{ schema_name }}_clones to role {{ non_sensitive }};
+        grant select on all tables in schema {{ schema_name }}_clones to role {{ non_sensitive }};
+        grant select on all views in schema {{ schema_name }}_clones to role {{ non_sensitive }};
+
         grant usage on schema covid19 to role {{ non_sensitive }};
         grant select on all tables in schema covid19 to role {{ non_sensitive }};
         grant select on all views in schema covid19 to role {{ non_sensitive }};
