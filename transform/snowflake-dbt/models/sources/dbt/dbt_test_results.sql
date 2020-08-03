c188426cb8dae56df63298d22a0a6e3628a74567
Taylor Murphy <tmurphy@gitlab.com>
2020-08-03 15:18:47 -0500

    Update existing dbt models

--- transform/snowflake-dbt/models/sources/dbt/dbt_test_results.sql
+++ transform/snowflake-dbt/models/sources/dbt/dbt_test_results.sql
@@ -14,11 +14,20 @@ WITH source AS (
 ), model_parsed_out AS (
 
     SELECT
-      data_by_row['execution_time']::FLOAT              AS test_execution_time_elapsed,
-      data_by_row['node']['name']::VARCHAR              AS test_name,
-      data_by_row['error']::VARCHAR                     AS test_error,
-      IFF(data_by_row['fail'] = 'true', True, False)    AS is_failed_test,
-      IFF(data_by_row['warn'] = 'true', True, False)    AS is_warned_test,
+      data_by_row['execution_time']::FLOAT                      AS test_execution_time_elapsed,
+      data_by_row['node']['unique_id']::VARCHAR                 AS test_id,
+      data_by_row['node']['name']::VARCHAR                      AS test_name,
+      data_by_row['error']::VARCHAR                             AS test_error,
+      data_by_row['node']['test_metadata']['name']::VARCHAR     AS test_type,
+      data_by_row['node']['config']['severity']::VARCHAR        AS test_severity,
+      data_by_row['node']['depends_on']['nodes']::VARCHAR       AS dependent_nodes,
+      IFF(data_by_row['fail'] = 'true', True, False)            AS is_failed_test,
+      IFF(data_by_row['warn'] = 'true', True, False)            AS is_warned_test,
+      CASE
+        WHEN is_failed_test = False AND is_warned_test = False
+          THEN True
+        ELSE False END                                          AS is_passed_test,
+
       test_result_generated_at     
     FROM flattened
   
--- transform/snowflake-dbt/models/staging/dbt/dbt_last_successful_test_runs.sql
+++ transform/snowflake-dbt/models/staging/dbt/dbt_last_successful_test_runs.sql
@@ -1,29 +1,20 @@
-WITH test_results AS (
+WITH passing_tests AS (
     
     SELECT *
     FROM {{ ref('dbt_test_results') }}
+    WHERE is_passed_test
+    QUALIFY row_number() OVER (PARTITION BY test_id ORDER BY test_result_generated_at DESC) = 1
 
 ), failing_tests AS (
 
-    SELECT *
+    SELECT test_id
     FROM {{ ref('dbt_failing_tests') }}
 
 ), last_successful_run AS (
 
-    SELECT 
-      test_name, 
-      MAX(test_result_generated_at) AS last_successful_run_at
-    FROM test_results
-    WHERE
-      NOT is_failed_test
-    AND test_name in (
-
-        SELECT
-          test_name
-        FROM failing_tests
-
-    )
-    GROUP BY 1
+    SELECT *
+    FROM passing_tests
+    WHERE test_id in (SELECT * FROM failing_tests)
 
 )
 
