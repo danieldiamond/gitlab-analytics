19128020fa5fcafd3fddbe55b0760defbe37fbe2
derekatwood <datwood@gitlab.com>
2020-08-03 13:44:20 -0500

    usage ping, license

--- transform/snowflake-dbt/models/staging/common/dim_licenses.sql
+++ transform/snowflake-dbt/models/staging/common/dim_licenses.sql
@@ -3,19 +3,28 @@ WITH licenses AS (
     SELECT *
     FROM {{ ref('license_db_licenses_source') }}
 
+), renamed AS (
+
+    SELECT
+      license_id,
+      license_md5,
+      zuora_subscription_id,
+      zuora_subscription_name,
+      users_count              AS license_user_count,
+      company,
+      plan_code,
+      is_trial,
+      IFF(
+          LOWER(email) LIKE '%@gitlab.com' AND LOWER(company) LIKE '%gitlab%',
+          TRUE, FALSE
+      )                        AS is_internal,
+      starts_at::DATE          AS license_start_date,
+      license_expires_at::DATE AS license_expire_date,
+      created_at,
+      updated_at
+    FROM licenses
+
 )
 
-SELECT
-  license_id,
-  license_md5,
-  zuora_subscription_id,
-  zuora_subscription_name,
-  users_count              AS license_user_count,
-  company,
-  plan_code,
-  is_trial,
-  starts_at::DATE          AS license_start_date,
-  license_expires_at::DATE AS license_expire_date,
-  created_at,
-  updated_at
-FROM licenses
+SELECT *
+FROM renamed
---
+++ transform/snowflake-dbt/models/staging/common/fct_usage_ping_payload.sql
@@ -0,0 +1,48 @@
+WITH usage_data AS (
+
+    SELECT *
+    FROM {{ ref('version_usage_data_source') }}
+    WHERE uuid IS NOT NULL
+
+), calculated AS (
+
+    SELECT
+      *,
+      REGEXP_REPLACE(NULLIF(version, ''), '\-.*')      AS cleaned_version,
+      IFF(version LIKE '%-pre%', TRUE, FALSE)::BOOLEAN AS is_pre_release,
+      IFF(version LIKE '%-rc%', TRUE, FALSE)::BOOLEAN  AS is_release_candidate,
+      IFF(edition = 'CE', 'CE', 'EE')                  AS main_edition,
+      CASE
+        WHEN edition IN ('CE', 'EE Free') THEN 'Core'
+        WHEN edition IN ('EE', 'EES') THEN 'Starter'
+        WHEN edition = 'EEP' THEN 'Premium'
+        WHEN edition = 'EEU' THEN 'Ultimate'
+      ELSE NULL END                                    AS tier
+    FROM usage_data
+
+), renamed AS (
+
+    SELECT
+      id              AS usage_ping_id,
+      uuid,
+      host_id,
+      license_md5,
+      hostname,
+      source_ip,
+      main_edition    AS edition,
+      tier,
+      cleaned_version AS version,
+      is_pre_release_version,
+      is_release_candidate,
+      instance_user_count,
+      license_plan,
+      license_trial   AS is_trial_license,
+      created_at,
+      recorded_at,
+      updated_at
+    FROM calculated
+
+)        
+
+SELECT *
+FROM renamed
