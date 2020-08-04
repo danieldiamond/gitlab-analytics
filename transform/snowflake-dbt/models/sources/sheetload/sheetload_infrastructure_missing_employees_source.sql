78c859668310c0777d2906ae63c7ef5c5ec0563e
Ken Aguilar <kaguilar@gitlab.com>
2020-08-04 06:34:28 +0000

    Correct spacing and add staging schema

---
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_infrastructure_missing_employees_source.sql
@@ -0,0 +1,18 @@
+WITH source AS (
+    
+    SELECT * 
+    FROM {{ source('sheetload','infrastructure_missing_employees') }}
+
+), final AS (
+    
+    SELECT 
+      NULLIF(employee_id, '')::INTEGER                                    AS email_domain,
+      NULLIF(gitlab_dotcom_user_id, '')::VARCHAR                          AS account_creation_date,
+      NULLIF(full_name, '')::VARCHAR                                      AS account_creation_time,
+      NULLIF(work_email, '')::VARCHAR                                     AS account_creation_timestamp,
+    FROM source
+
+) 
+
+SELECT * 
+FROM final
