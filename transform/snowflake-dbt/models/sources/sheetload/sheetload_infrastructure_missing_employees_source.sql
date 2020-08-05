0b157038dc851621dfd297cdb4ea8fc482411fa9
Michael Walker <mwalker@gitlab.com>
2020-08-05 14:48:26 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_infrastructure_missing_employees_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_infrastructure_missing_employees_source.sql
@@ -9,7 +9,7 @@ WITH source AS (
       NULLIF(employee_id, '')::INTEGER                                    AS email_domain,
       NULLIF(gitlab_dotcom_user_id, '')::VARCHAR                          AS account_creation_date,
       NULLIF(full_name, '')::VARCHAR                                      AS account_creation_time,
-      NULLIF(work_email, '')::VARCHAR                                     AS account_creation_timestamp,
+      NULLIF(work_email, '')::VARCHAR                                     AS account_creation_timestamp
     FROM source
 
 ) 
