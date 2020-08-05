b30fabdf841378d34ba57f699a5cbd6403d759c3
Taylor Murphy <tmurphy@gitlab.com>
2020-08-05 10:20:49 -0500

    STRING to VARCHAR

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_ally_certificate_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_ally_certificate_source.sql
@@ -15,7 +15,7 @@ WITH source AS (
       "Timestamp"::TIMESTAMP::DATE                                                                 AS completed_date,
       "Score"                                                                                      AS score,
       "First_&_Last_Name"                                                                          AS submitter_name,
-      "Email_Address_(GitLab_team_members,_please_use_your_GitLab_email_address)"::STRING          AS submitter_email,
+      "Email_Address_(GitLab_team_members,_please_use_your_GitLab_email_address)"::VARCHAR          AS submitter_email,
       "_UPDATED_AT"                                                                                AS last_updated_at
     FROM source
 
