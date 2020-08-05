a828fd573bdb2688f0071806e811bb5f1e0f18f2
parmstrong <parmstrong@gitlab.com>
2020-08-05 18:25:49 +0200

    Back to original golden records

--- transform/snowflake-dbt/models/marts/pipeline_health_dashboard/golden_records.sql
+++ transform/snowflake-dbt/models/marts/pipeline_health_dashboard/golden_records.sql
@@ -1,21 +1,9 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='account_id'
-    )
-}}
-WITH account_records AS (
+WITH golden_records AS (
 
   SELECT *
-  FROM {{ ref('zuora_account_source') }}
-  WHERE account_id in (
-        '2c92a0fe5654120701567a1e42f22ed3',
-        '2c92a0ff5e50a5f6015e76d832f80264',
-        '2c92a00c6f31ee5a016f47f78d236419'
-  )
+  FROM {{ ref('sheetload_account_golden_records_source') }}
+
 )
 
-
 SELECT *
-FROM account_records
-WHERE account_id NOT IN (SELECT DISTINCT ACCOUNT_ID FROM {{this}})
+FROM golden_records
