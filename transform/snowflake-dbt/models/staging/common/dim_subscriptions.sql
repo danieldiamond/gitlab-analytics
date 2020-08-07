d2bda4c5f01e46eef023c6e593feddb781956cb6
msendal <msendal@gitlab.com>
2020-08-07 13:10:26 +0200

    choose the latest version of subscription from snaphsots to reduce dupliocates

--- transform/snowflake-dbt/models/staging/common/dim_subscriptions.sql
+++ transform/snowflake-dbt/models/staging/common/dim_subscriptions.sql
@@ -2,6 +2,16 @@ WITH zuora_subscription AS (
   SELECT *
   FROM {{ ref('zuora_subscription_source') }}
 
+), zuora_subscription_snapshots AS (
+
+  SELECT
+  rank() over (partition by name order by DBT_VALID_FROM desc) as rank,
+  *
+  FROM {{ ref('zuora_subscription_snapshots_source') }}
+  WHERE --status in ('Cancelled', 'Active')
+  CURRENT_TIMESTAMP()::TIMESTAMP_TZ >= dbt_valid_from
+    AND {{ coalesce_to_infinity('dbt_valid_to') }} > current_timestamp()::TIMESTAMP_TZ
+
 ), zuora_account AS (
   SELECT
     account_id,
@@ -26,6 +36,9 @@ SELECT
   DATE_TRUNC('month', zuora_subscription.subscription_start_date)           AS subscription_start_month,
   DATE_TRUNC('month', zuora_subscription.subscription_end_date)             AS subscription_end_month
 FROM zuora_subscription
+INNER JOIN zuora_subscription_snapshots
+  ON zuora_subscription_snapshots.subscription_id = zuora_subscription.subscription_id
+  AND zuora_subscription_snapshots.rank = 1
 INNER JOIN zuora_account
   ON zuora_account.account_id = zuora_subscription.account_id
 WHERE is_deleted = FALSE
