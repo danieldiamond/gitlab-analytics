c35e4e686e4315022df54b01410501702712d829
iweeks <iweeks@gitlab.com>
2020-08-12 19:04:32 -0400

    fix caps

--- transform/snowflake-dbt/macros/sfdc/sales_segment_cleaning.sql
+++ transform/snowflake-dbt/macros/sfdc/sales_segment_cleaning.sql
@@ -1,11 +1,11 @@
 {%- macro sales_segment_cleaning(column_1) -%}
 
 CASE
-  WHEN lower({{ column_1 }}) = 'smb' THEN 'SMB'
+  WHEN lower({{ column_1 }}) = 'smb'                        THEN 'SMB'
   WHEN lower({{ column_1 }}) in ('mid-market','mid market') THEN 'Mid-Market'
-  WHEN lower({{ column_1 }}) = 'unknown' THEN 'SMB'
-  WHEN lower({{ column_1 }}) IS NULL THEN 'SMB'
-  WHEN {{ column_1 }} is not null THEN initcap({{ column_1 }})
+  WHEN lower({{ column_1 }}) = 'unknown'                    THEN 'SMB'
+  WHEN ({{ column_1 }}) IS NULL                             THEN 'SMB'
+  WHEN {{ column_1 }} IS NOT NULL                           THEN initcap({{ column_1 }})
   ELSE NULL
 END
 
