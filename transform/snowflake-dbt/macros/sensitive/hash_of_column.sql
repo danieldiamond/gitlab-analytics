bb89e7c5cf5e513af61f2df8ed2af8d3122af88e
Michael Walker <mwalker@gitlab.com>
2020-08-13 16:41:00 -0500

    fix salting so it's repeatable

--- transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
+++ transform/snowflake-dbt/macros/sensitive/hash_of_column.sql
@@ -3,7 +3,12 @@
     sha2(
         TRIM(
             LOWER(
-                {{column|lower}} || ENCRYPT('{{ get_salt(column|lower) }}', '{{ get_salt(column|lower) }}')::VARCHAR 
+                {{column|lower}} || 
+                ENCRYPT_RAW(
+                  to_binary('{{ get_salt(column|lower) }}', 'utf-8'), 
+                  to_binary('416C736F4E6F745365637265FFFFFFAB', 'HEX'), 
+                  to_binary('416C736F4E637265FFFFFFAB', 'HEX')
+                )['ciphertext']::VARCHAR 
             )
         )
     ) AS {{column|lower}}_hash,
--- transform/snowflake-dbt/macros/sensitive/hash_of_column_in_view.sql
+++ transform/snowflake-dbt/macros/sensitive/hash_of_column_in_view.sql
@@ -1,6 +1,13 @@
 {% macro hash_of_column_in_view(column) %}
 
-    {% set results = run_query("select ENCRYPT('{{ get_salt(column|lower) }}', '{{ get_salt(column|lower) }}')::VARCHAR") %}
+    {% set results = run_query("
+        select ENCRYPT_RAW(
+            to_binary('{{ get_salt(column|lower) }}', 'utf-8'), 
+            to_binary('416C736F4E6F745365637265FFFFFFAB', 'HEX'), 
+            to_binary('416C736F4E637265FFFFFFAB', 'HEX')
+        )['ciphertext']::VARCHAR "
+      ) 
+    %}
 
     sha2(
         TRIM(
