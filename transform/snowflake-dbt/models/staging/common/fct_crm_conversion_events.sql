7b420b3b72dee2aa3177838e8be223c05db88422
jjstark <jstark@gitlab.com>
2020-08-17 15:54:38 -0700

    style

--- transform/snowflake-dbt/models/staging/common/fct_crm_conversion_events.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_conversion_events.sql
@@ -7,14 +7,14 @@ WITH sfdc_leads AS(
 
   SELECT
     
-	lead_history.lead_history_id        AS event_id,
-	lead_history.field_modified_at      AS event_timestamp,
-  lead_history.lead_id                AS lead_id,
-  lead_history.created_by_id          AS crm_user_id,
-	sfdc_leads.converted_contact_id     AS contact_id,
-	sfdc_leads.converted_account_id     AS account_id,
-	sfdc_leads.converted_opportunity_id AS opportunity_id,
-	'lead conversion'                   AS event_name
+    lead_history.lead_history_id          AS event_id,
+    lead_history.field_modified_at        AS event_timestamp,
+    lead_history.lead_id                  AS lead_id,
+    lead_history.created_by_id            AS crm_user_id,
+    fdc_leads.converted_contact_id        AS contact_id,
+    fdc_leads.converted_account_id        AS account_id,
+    sfdc_leads.converted_opportunity_id   AS opportunity_id,
+    'lead conversion'                     AS event_name
 
   FROM {{ ref('sfdc_lead_history_source') }} lead_history
   INNER JOIN sfdc_leads
