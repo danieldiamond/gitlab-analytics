8ee7cd9233aaaca32330ee16f7f8277cf7e6c1da
Noel Figuera <nfiguera@gitlab.com>
2020-08-17 15:10:04 +0000

    add start_date to user

--- transform/snowflake-dbt/models/staging/sfdc/xf/sfdc_users_xf.sql
+++ transform/snowflake-dbt/models/staging/sfdc/xf/sfdc_users_xf.sql
@@ -9,16 +9,16 @@ with users as (
 )
 
 SELECT
-  users.name          AS name,
-  users.department    AS department,
-  users.title         AS title,
-  users.team__c       AS team,
-  users.email         AS email,
-  users.id            AS id,
-  manager.name        AS manager_name,
-  manager.id          AS manager_id,
-  user_role.name      AS role_name,
-  users.isactive      AS is_active
+  users.name            AS name,
+  users.department      AS department,
+  users.title           AS title,
+  users.team__c         AS team,
+  users.id              AS id,
+  manager.name          AS manager_name,
+  manager.id            AS manager_id,
+  user_role.name        AS role_name,
+  users.start_date__c   AS start_date,
+  users.isactive        AS is_active
 FROM users
 LEFT OUTER JOIN user_role 
 ON users.userroleid = user_role.id
