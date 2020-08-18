2d68b981bae2ddd2a5719b764cbd1b3ae9453622
jjstark <jstark@gitlab.com>
2020-08-18 11:34:00 -0700

    adding columns

--- transform/snowflake-dbt/models/staging/common/dim_crm_contacts.sql
+++ transform/snowflake-dbt/models/staging/common/dim_crm_contacts.sql
@@ -11,26 +11,52 @@ WITH sfdc_leads AS (
 )
 
 SELECT
-
+  --id
   contact_id       AS sfdc_record_id,
   'contact'        AS sfdc_record_type,
   email_domain,
+  
+  --keys
+  master_record_id,
+  owner_id,
+  record_type_id,
   account_id,
+  reports_to_id,
+
+  --info
   person_score,
-  contact_title    AS title
+  contact_title    AS title,
+  has_opted_out_email,
+  email_bounced_date,
+  email_bounced_reason,
+  lead_source,
+  lead_source_type
 
 FROM sfdc_contacts
 
 UNION
 
 SELECT
-
+  --id
   lead_id          AS sfdc_record_id,
   'lead'           AS sfdc_record_type,
   email_domain,
+  
+  --keys
+  master_record_id,
+  owner_id,
+  record_type_id,
   NULL             AS account_id,
+  NULL             AS reports_to_id,
+  
+  --info
   person_score,
-  title
+  title,
+  has_opted_out_email,
+  email_bounced_date,
+  email_bounced_reason,
+  lead_source,
+  lead_source_type
 
 FROM sfdc_leads
 WHERE is_converted = FALSE
--- transform/snowflake-dbt/models/staging/common/fct_crm_conversion_events.sql
+++ transform/snowflake-dbt/models/staging/common/fct_crm_conversion_events.sql
@@ -11,8 +11,8 @@ WITH sfdc_leads AS(
     lead_history.field_modified_at        AS event_timestamp,
     lead_history.lead_id                  AS lead_id,
     lead_history.created_by_id            AS crm_user_id,
-    fdc_leads.converted_contact_id        AS contact_id,
-    fdc_leads.converted_account_id        AS account_id,
+    sfdc_leads.converted_contact_id        AS contact_id,
+    sfdc_leads.converted_account_id        AS account_id,
     sfdc_leads.converted_opportunity_id   AS opportunity_id,
     'lead conversion'                     AS event_name
 
