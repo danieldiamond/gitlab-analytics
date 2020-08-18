2e75d28ad07bd3e020eb32382fde8f349dabbd42
jjstark <jstark@gitlab.com>
2020-08-18 11:35:15 -0700

    style

--- transform/snowflake-dbt/models/staging/common/dim_crm_contacts.sql
+++ transform/snowflake-dbt/models/staging/common/dim_crm_contacts.sql
@@ -25,7 +25,7 @@ SELECT
 
   --info
   person_score,
-  contact_title    AS title,
+  contact_title        AS title,
   has_opted_out_email,
   email_bounced_date,
   email_bounced_reason,
@@ -38,16 +38,16 @@ UNION
 
 SELECT
   --id
-  lead_id          AS sfdc_record_id,
-  'lead'           AS sfdc_record_type,
+  lead_id              AS sfdc_record_id,
+  'lead'               AS sfdc_record_type,
   email_domain,
   
   --keys
   master_record_id,
   owner_id,
   record_type_id,
-  NULL             AS account_id,
-  NULL             AS reports_to_id,
+  NULL                 AS account_id,
+  NULL                 AS reports_to_id,
   
   --info
   person_score,
