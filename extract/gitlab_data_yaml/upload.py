2790a0bda34c2ebb0b1c42250fdb2a6905e5fdb8
Taylor Murphy <tmurphy@gitlab.com>
2020-08-18 14:24:37 -0500

    Add performance indicators

--- extract/gitlab_data_yaml/upload.py
+++ extract/gitlab_data_yaml/upload.py
@@ -18,6 +18,25 @@ if __name__ == "__main__":
         team="team",
     )
 
+    pi_file_dict = dict(
+        chief_of_staff_team_pi="chief_of_staff_team",
+        corporate_finance_pi="corporate_finance",
+        dev_section_pi="dev_section",
+        enablement_section_pi="enablement_section",
+        engineering_function_pi="engineering_function",
+        finance_team_pi="finance_team",
+        marketing_pi="marketing",
+        ops_section_pi="ops_section",
+        people_success_pi="people_success",
+        product_pi="product",
+        quality_department_pi="quality_department",
+        recruiting_pi="recruiting",
+        sales_pi="sales",
+        security_department_pi="security_department",
+        support_department_pi="support_department",
+        ux_department_pi="ux_department",
+    )
+
     logging.basicConfig(stream=sys.stdout, level=20)
 
     config_dict = env.copy()
@@ -27,7 +46,7 @@ if __name__ == "__main__":
 
     job_failed = False
 
-    for key, value in file_dict.items():
+    def curl_and_upload(key, value):
         logging.info(f"Downloading {value}.yml to {value}.json file.")
         try:
             command = f"curl {base_url}{value}.yml | yaml2json -o {value}.json"
@@ -44,6 +63,13 @@ if __name__ == "__main__":
             f"raw.gitlab_data_yaml.{key}",
             snowflake_engine,
         )
+
+    for key, value in file_dict.items():
+        curl_and_upload(key, value)
+
+    for key, value in pi_file_dict.items():
+        directory_value = f"performance_indicators/{value}" 
+        curl_and_upload(key, directory_value)
 
     if job_failed:
         sys.exit(1)
