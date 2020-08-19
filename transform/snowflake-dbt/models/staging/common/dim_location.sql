69594d5e6003c77d7493dd2a9c23df02e58e4848
Michael Walker <mwalker@gitlab.com>
2020-08-19 09:39:12 -0500

    add dim models to schema.yml

--- transform/snowflake-dbt/models/staging/common/dim_location.sql
+++ transform/snowflake-dbt/models/staging/common/dim_location.sql
@@ -1,8 +1,8 @@
 WITH source AS (
 
     SELECT
-      geoname_id AS location_id,
-      country_name AS country_name,
+      geoname_id       AS location_id,
+      country_name     AS country_name,
       country_iso_code AS iso_2_country_code
     FROM {{ ref('maxmind_countries_source') }}
 ) 
