31f9e7614ea6df0c04202622fdbec99eb25e4778
davis_townsend <dtownsend@gitlab.com>
2020-08-19 17:34:38 -0500

    simplify base reference models

--- transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_project_storage_xf.sql
+++ transform/snowflake-dbt/models/staging/gitlab_dotcom/xf/gitlab_dotcom_project_storage_xf.sql
@@ -1,30 +1,14 @@
-WITH date_details AS (
-
-  SELECT
-    date_day,
-    first_day_of_month
-  FROM{{ ref("date_details") }}
-  WHERE last_day_of_month = date_day
-    AND date_day > '2020-03-01'
-
-), project_statistics_snapshot AS (
-
-  SELECT
-    *,
-    IFNULL(valid_to, CURRENT_TIMESTAMP) AS valid_to_
-  FROM {{ ref("gitlab_dotcom_project_statistics_snapshots_base") }}
-
-), namespaces_child AS (
+WITH project_statistics_snapshot_monthly AS (
 
   SELECT *
-  FROM {{ ref("gitlab_dotcom_namespaces_xf") }}
+  FROM {{ ref("gitlab_dotcom_project_statistic_snapshots_monthly") }}
 
-), namespaces_parent AS (
+), namespace_lineage_monthly AS (
 
   SELECT *
-  FROM {{ ref("gitlab_dotcom_namespaces_xf") }}
+  FROM {{ ref("gitlab_dotcom_namespace_lineage_monthly") }}
 
-), namespace_subscription_snapshot AS (
+),  namespace_subscription_snapshot AS (
 
   SELECT
   *,
@@ -34,10 +18,11 @@ WITH date_details AS (
 ), joined AS (
 
   SELECT
-    date_details.first_day_of_month                                                                             AS month,
-    project_statistics_snapshot.project_id                                                                      AS project_id,
-    project_statistics_snapshot.namespace_id                                                                    AS namespace_id,
-    COALESCE((namespace_subscription_snapshot.namespace_id IN {{ get_internal_parent_namespaces() }}), FALSE)   AS namespace_is_internal,
+    project_statistics_snapshot_monthly.snapshot_month                                                          AS month,
+    project_statistics_snapshot_monthly.project_id                                                              AS project_id,
+    project_statistics_snapshot_monthly.namespace_id                                                            AS namespace_id,
+    namespace_lineage_monthly.ultimate_parent_id                                                                AS ultimate_parent_namespace_id,
+    namespace_lineage_monthly.namespace_is_internal                                                             AS namespace_is_internal,
     namespace_subscription_snapshot.plan_id                                                                     AS plan_id,
     -- taking storage as of last updated time, so most of the time this will end up being the MAX value
     MAX(project_statistics_snapshot.storage_size/POW(1024,3))                                                   AS storage_size_GB,
@@ -46,17 +31,14 @@ WITH date_details AS (
     MAX(project_statistics_snapshot.build_artifacts_size/POW(1024,3))                                           AS build_artifacts_size_GB,
     MAX(project_statistics_snapshot.packages_size/POW(1024,3))                                                  AS packages_size_GB,
     MAX(project_statistics_snapshot.wiki_size/POW(1024,3))                                                      AS wiki_size_GB
-    FROM date_details
-    INNER JOIN project_statistics_snapshot
-      ON date_details.date_day BETWEEN project_statistics_snapshot.valid_from AND project_statistics_snapshot.valid_to_
-    INNER JOIN namespaces_child
-      ON project_statistics_snapshot.namespace_id = namespaces_child.namespace_id
-    INNER JOIN namespaces_parent
-      ON namespaces_child.namespace_ultimate_parent_id = namespaces_parent.namespace_id
+    FROM project_statistics_snapshot_monthly
+    INNER JOIN namespace_lineage_monthly
+      ON project_statistics_snapshot_monthly.namespace_id = namespace_lineage_monthly.namespace_id
+      AND project_statistics_snapshot_monthly.snapshot_month = namespace_lineage_monthly.snapshot_month
     INNER JOIN namespace_subscription_snapshot
-      ON namespaces_parent.namespace_id = namespace_subscription_snapshot.namespace_id
-        AND date_details.date_day BETWEEN namespace_subscription_snapshot.valid_from AND namespace_subscription_snapshot.valid_to_
-    GROUP BY 1,2,3,4,5
+      ON namespace_lineage_monthly.ultimate_parent_id = namespace_subscription_snapshot.namespace_id
+      AND namespace_lineage_monthly.snapshot_month BETWEEN namespace_subscription_snapshot.valid_from AND namespace_subscription_snapshot.valid_to_
+    GROUP BY 1,2,3,4,5,6
 )
 
 SELECT * FROM joined
