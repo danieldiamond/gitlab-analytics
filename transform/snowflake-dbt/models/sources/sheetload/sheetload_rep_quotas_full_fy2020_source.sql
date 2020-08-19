e6880e018dbda0c630f107c5c0f5678e331122a1
msendal <msendal@gitlab.com>
2020-08-19 14:22:15 +0200

    change logic to handle #N/A

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_rep_quotas_full_fy2020_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_rep_quotas_full_fy2020_source.sql
@@ -12,7 +12,11 @@ WITH source AS (
       fiscal_quarter::NUMBER                                           AS fiscal_quarter,
       fiscal_year::NUMBER                                              AS fiscal_year,
       adjusted_start_date::DATE                                        AS adjusted_start_date,
-      ZEROIFNULL(NULLIF(TRIM(full_quota),'NA')::NUMBER(16,5))          AS full_quota,
+      CASE
+        WHEN TRIM(full_quota) IN ('NA', '#N/A')
+          THEN 0
+        ELSE ZEROIFNULL(TRIM(full_quota)::NUMBER(16,5))
+      END                                                              AS full_quota,
       ZEROIFNULL(NULLIF(TRIM(ramping_quota),'')::NUMBER(16,5))         AS ramping_quota,
       ZEROIFNULL(NULLIF(TRIM(ramping_percent),'')::NUMBER(3,2))        AS ramping_percent,
       ZEROIFNULL(NULLIF(TRIM(seasonality_percent),'')::NUMBER(3,2))    AS seasonality_percent,
