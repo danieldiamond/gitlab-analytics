0d0d655368dd2d6ce7a68291af625034b53824e9
msendal <msendal@gitlab.com>
2020-08-20 17:37:27 +0200

    remove redundant code

--- transform/snowflake-dbt/models/marts/arr/charges_agg.sql
+++ transform/snowflake-dbt/models/marts/arr/charges_agg.sql
@@ -82,29 +82,7 @@ WITH fct_charges AS (
     LEFT JOIN dim_customers
       ON dim_accounts.crm_id = dim_customers.crm_id
 
-), latest_invoiced_charge_version_in_segment AS (
-
-    SELECT
-      base_charges.charge_id,
-      IFF(ROW_NUMBER() OVER (
-          PARTITION BY base_charges.rate_plan_charge_number, base_charges.rate_plan_charge_segment
-          ORDER BY base_charges.rate_plan_charge_version DESC, fct_invoice_items_agg.service_start_date DESC) = 1,
-          TRUE, FALSE
-      ) AS is_last_segment_version
-    FROM base_charges
-    INNER JOIN fct_invoice_items_agg
-      ON base_charges.charge_id = fct_invoice_items_agg.charge_id
-
-), final AS (
-
-    SELECT
-      base_charges.*,
-      latest_invoiced_charge_version_in_segment.is_last_segment_version
-    FROM base_charges
-    LEFT JOIN latest_invoiced_charge_version_in_segment
-      ON base_charges.charge_id = latest_invoiced_charge_version_in_segment.charge_id
-
 )
 
 SELECT *
-FROM final
+FROM base_charges
